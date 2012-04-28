943d2567215610a1086f728eaf4d59294f76600e
Lloyd Hilaiel <lloyd@hilaiel.com>
2012-04-27 22:20:22 -0600

    implement poor mans sync

--- packages/123done/static/123done.js
+++ packages/123done/static/123done.js
@@ -8,8 +8,15 @@ $(document).ready(function() {
   }
 
   var todo = $('#todolist'),
-      form = $('#todo form'),
-     field = $("#newitem");
+  form = $('#todo form'),
+  field = $("#newitem");
+
+  var lastSync = localStorage.lastSync ? parseInt(localStorage.lastSync, 10) : 0;
+
+  function udpateLastSync() {
+    var ls = new Date().getTime().toString();
+    localStorage.lastSync = lastSync = ls;
+  }
 
   function setSyncStatus(status) {
     $('#dataState').text(status);
@@ -19,7 +26,7 @@ $(document).ready(function() {
   form.on('submit', function(e) {
     e.preventDefault();
     // create a new element, set its value, and append it
-    todo.append($('<li>').text(field.val()));
+    todo.append($('<li>').attr('when', new Date().getTime()).text(field.val()));
     // clear and refocus the input field
     field.val('').focus();
     storestate();
@@ -45,7 +52,7 @@ $(document).ready(function() {
   // * when you sign in with added todo items, we merge them with your items on the server
   // * when you sign out, we leave your todo items local
   // * when you change items and you're signed in, we blast those items to the server
-  
+
   // store current todolist in localstorage
   function storestate() {
     localStorage.todolist = todo.html();
@@ -62,7 +69,7 @@ $(document).ready(function() {
           done: self.hasClass('done')
         });
       });
-    
+      
       // now store it to the server
       $.ajax({
         type: 'POST',
@@ -71,6 +78,7 @@ $(document).ready(function() {
         contentType: 'application/json',
         success: function() {
           setSyncStatus('saved');
+          udpateLastSync();
         },
         error: function() {
           setSyncStatus('out of date');
@@ -79,17 +87,21 @@ $(document).ready(function() {
     }
   };
 
+  function updateDomWithArray(a) {
+    for (var i = 0; i < a.length; i++) {
+      var li = $("<li/>").text(a[i].v);
+      if (a[i].done) li.addClass('done');
+      todo.append(li);
+    }
+  }
+  
   // retrieve todolist from localstorage
   function retrievestate() {
     if (loggedInEmail) {
       setSyncStatus('syncing');
       $.get('/api/todos/get', function(data) {
         data = JSON.parse(data);
-        for (var i = 0; i < data.length; i++) {
-          var li = $("<li/>").text(data[i].v);
-          if (data[i].done) li.addClass('done');
-          todo.append(li);
-        }
+        updateDomWithArray(data);
         setSyncStatus('saved');
       });
     } else if (localStorage.todolist) {
@@ -97,6 +109,42 @@ $(document).ready(function() {
       todo.html(localStorage.todolist);
     }
   };
+
+  // merge local state with the server, this is a poor man's sync
+  function mergestate() {
+    // this is only meaningful when we're connected
+    if (!loggedInEmail) return;
+
+    setSyncStatus('syncing');
+
+    var l = [ ];
+
+    // first let's get the list of todo items from the server
+    $.get('/api/todos/get', function(data) {
+      data = JSON.parse(data);
+      for (var i = 0; i < data.length; i++) l.push(data[i]);
+
+      // now let's that list with local items added since the last sync
+      $("#todolist > li").each(function(e) {
+        var self = $(this);
+        var when = self.attr('when') || 0;
+        if (when <= lastSync) return;
+        l.push({
+          v: self.text(),
+          done: self.hasClass('done')
+        });
+      });
+
+      // clear items from the dom
+      $("#todolist > li").remove();
+
+      // update the dom with our new merged set
+      updateDomWithArray(l)
+
+      // and store that set of items to the server
+      storestate();
+    });
+  }
 
   // verify the assertion on the server, which will use the
   // verification service provided by mozilla
@@ -147,6 +195,7 @@ $(document).ready(function() {
         verifyAssertion(assertion, function(r) {
           loggedInEmail = r.email;
           updateUI(loggedInEmail);
+          mergestate();
         }, function(err) {
           alert("failed to verify assertion: " + JSON.stringify(err));
           loggedInEmail = null;
