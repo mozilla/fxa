02fd3188f15b9793809ae945695094e90cd6a32d
Danny Coates <dannycoates@gmail.com>
2013-05-31 18:42:13 -0700

    cleaned up memory.lua

--- packages/fxa-auth-server/heka/memory.lua
+++ packages/fxa-auth-server/heka/memory.lua
@@ -1,21 +1,18 @@
 data = circular_buffer.new(240, 3, 15)
-local cols = {}
-cols["rss"] = data:set_header(1, "RSS", "max")
-cols["heapTotal"] = data:set_header(2, "HEAP_TOTAL", "max")
-cols["heapUsed"] = data:set_header(3, "HEAP_USED", "max")
-
-local ts = 0
-local x = 0
+local cols = {
+	rss = data:set_header(1, "RSS", "max"),
+	heapTotal = data:set_header(2, "HEAP_TOTAL", "max"),
+	heapUsed = data:set_header(3, "HEAP_USED", "max")
+}
 
 function process_message()
-	ts = read_message("Timestamp")
+	local ts = read_message("Timestamp")
 	local payload = read_message("Payload")
 	if payload == nil then return 0 end
 
 	for k, v in string.gmatch(payload, "stats.(%w+) (%d+)") do
 		if cols[k] ~= nil then
 			data:set(ts, cols[k], v)
-			x = v
 		end
 	end
 
