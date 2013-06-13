d92de1e5d7cfd58d8919fb9aa295d8bb7fb372ef
Zachary Carter <zack.carter@gmail.com>
2013-06-13 14:20:44 -0700

    install memcached on awsbox deploys

--- packages/fxa-auth-server/scripts/aws/post_create.sh
+++ packages/fxa-auth-server/scripts/aws/post_create.sh
@@ -6,3 +6,10 @@ sudo /sbin/service mysqld start
 echo "CREATE USER 'picl'@'localhost';" | mysql -u root
 echo "CREATE DATABASE picl;" | mysql -u root
 echo "GRANT ALL ON picl.* TO 'picl'@'localhost';" | mysql -u root
+
+echo "Setting up memcached"
+
+yum --assumeyes install memcached
+
+/sbin/chkconfig memcached on
+/sbin/service memcached start
