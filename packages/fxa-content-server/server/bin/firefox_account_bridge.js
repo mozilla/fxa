4a4ebce818e911709403bde48240dca368210afd
Austin King <shout@ozten.com>
2013-07-30 16:21:57 -0700

    Hook up dummy /.well-known/browserid

--- packages/fxa-content-server/server/bin/firefox_account_bridge.js
+++ packages/fxa-content-server/server/bin/firefox_account_bridge.js
@@ -4,3 +4,29 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+const express = require('express'),
+      nunjucks = require('nunjucks'),
+      path = require('path');
+
+var app = express();
+var env = new nunjucks.Environment(
+  new nunjucks.FileSystemLoader(
+    path.join(__dirname, '..', 'views')));
+
+env.express(app);
+
+app.get('/.well-known/browserid', function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.render('browserid.html');
+});
+
+app.get('/provision', function(req, res) {
+  res.send('Yo');
+});
+
+app.get('/authentication', function(req, res) {
+  res.send('Yo');
+});
+
+app.listen(3000);
+console.log('Firefox Account Bridge listening at http://localhost:3000');
