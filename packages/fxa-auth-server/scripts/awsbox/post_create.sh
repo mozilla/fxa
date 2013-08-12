ebd59c3b022351cb741b419b108bcbedbeaa0ea9
Ryan Kelly <rkelly@mozilla.com>
2013-08-12 16:07:21 +1000

    Install identity-team public keys on awsbox creation.

--- packages/fxa-auth-server/scripts/awsbox/post_create.sh
+++ packages/fxa-auth-server/scripts/awsbox/post_create.sh
@@ -15,3 +15,12 @@ sudo /sbin/service memcached start
 echo "Setting up logstash"
 
 wget https://logstash.objects.dreamhost.com/release/logstash-1.1.13-flatjar.jar
+
+echo "Installing identity team public keys"
+
+git clone https://github.com/mozilla/identity-pubkeys
+cd identity-pubkeys
+git checkout b63a19a153f631c949e7f6506ad4bf1f258dda69
+cat *.pub >> /home/ec2-user/.ssh/authorized_keys
+cd ..
+rm -rf identity-pubkeys
