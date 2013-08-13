3004f5e333cb8ebee097f167ff046f8357fa0ef5
vladikoff <vlad.filippov@gmail.com>
2013-08-12 18:21:45 -0700

    style fixes, removing tabs

--- packages/fxa-auth-server/test/run/account_reset_token_tests.js
+++ packages/fxa-auth-server/test/run/account_reset_token_tests.js
@@ -13,28 +13,28 @@ var models = require('../../models')(config, dbs, mailer)
 var AccountResetToken = models.tokens.AccountResetToken
 
 test(
-	'bundle / unbundle works',
-	function (t) {
-		function end() { t.end() }
-		AccountResetToken.create('xxx')
-			.then(
-				function (x) {
-					var wrapKb = crypto.randomBytes(32).toString('hex')
-					var verifier = crypto.randomBytes(256).toString('hex')
-					var b = x.bundle(wrapKb, verifier)
-					var ub = x.unbundle(b)
-					t.equal(ub.wrapKb, wrapKb)
-					t.equal(ub.verifier, verifier)
-					return x
-				}
-			)
-			.then(
-				function (x) {
-					return x.del()
-				}
-			)
-			.done(end, end)
-	}
+  'bundle / unbundle works',
+  function (t) {
+    function end() { t.end() }
+    AccountResetToken.create('xxx')
+      .then(
+        function (x) {
+          var wrapKb = crypto.randomBytes(32).toString('hex')
+          var verifier = crypto.randomBytes(256).toString('hex')
+          var b = x.bundle(wrapKb, verifier)
+          var ub = x.unbundle(b)
+          t.equal(ub.wrapKb, wrapKb)
+          t.equal(ub.verifier, verifier)
+          return x
+        }
+      )
+      .then(
+        function (x) {
+          return x.del()
+        }
+      )
+      .done(end, end)
+  }
 )
 
 test(
--- packages/fxa-auth-server/test/run/recovery_email_tests.js
+++ packages/fxa-auth-server/test/run/recovery_email_tests.js
@@ -18,113 +18,113 @@ var RecoveryEmail = models.RecoveryEmail
 var email = Buffer('me@example.com').toString('hex')
 
 test(
-	'RecoveryEmail.create generates a random 32 byte code as a hex string',
-	function (t) {
-		function end() { t.end() }
-		RecoveryEmail.create('xxx', email, true)
-			.then(
-				function (x) {
-					t.equal(x.code.length, 8)
-					t.equal(HEX_STRING.test(x.code), true)
-					return x
-				}
-			)
-			.then(
-				function (x) {
-					return x.del()
-				}
-			)
-			.done(end, end)
-	}
+  'RecoveryEmail.create generates a random 32 byte code as a hex string',
+  function (t) {
+    function end() { t.end() }
+    RecoveryEmail.create('xxx', email, true)
+      .then(
+        function (x) {
+          t.equal(x.code.length, 8)
+          t.equal(HEX_STRING.test(x.code), true)
+          return x
+        }
+      )
+      .then(
+        function (x) {
+          return x.del()
+        }
+      )
+      .done(end, end)
+  }
 )
 
 test(
-	'RecoveryEmail.create calls mailer.sendCode',
-	function (t) {
-		sends = 0
-		function end() { t.end() }
-		RecoveryEmail.create('xxx', email, true)
-			.then(
-				function (x) {
-					t.equal(sends, 1)
-					sends = 0
-					return x.del()
-				}
-			)
-			.done(end, end)
-	}
+  'RecoveryEmail.create calls mailer.sendCode',
+  function (t) {
+    sends = 0
+    function end() { t.end() }
+    RecoveryEmail.create('xxx', email, true)
+      .then(
+        function (x) {
+          t.equal(sends, 1)
+          sends = 0
+          return x.del()
+        }
+      )
+      .done(end, end)
+  }
 )
 
 test(
-	'recoveryEmail.verify sets verified to true if the codes match',
-	function (t) {
-		function end() { t.end() }
-		RecoveryEmail.create('xxx', email, true)
-			.then(
-				function (x) {
-					t.equal(x.verified, false)
-					var c = x.code
-					return x.verify(c)
-				}
-			)
-			.then(
-				function (x) {
-					t.equal(x.verified, true)
-					return x.del()
-				}
-			)
-			.done(end, end)
-	}
+  'recoveryEmail.verify sets verified to true if the codes match',
+  function (t) {
+    function end() { t.end() }
+    RecoveryEmail.create('xxx', email, true)
+      .then(
+        function (x) {
+          t.equal(x.verified, false)
+          var c = x.code
+          return x.verify(c)
+        }
+      )
+      .then(
+        function (x) {
+          t.equal(x.verified, true)
+          return x.del()
+        }
+      )
+      .done(end, end)
+  }
 )
 
 test(
-	'recoveryEmail.verify does not set verified if codes do not match',
-	function (t) {
-		function end() { t.end() }
-		RecoveryEmail.create('xxx', email, true)
-			.then(
-				function (x) {
-					t.equal(x.verified, false)
-					var c = crypto.randomBytes(32).toString('hex')
-					return x.verify(c)
-				}
-			)
-			.then(
-				function (x) {
-					t.equal(x.verified, false)
-					return x.del()
-				}
-			)
-			.done(end, end)
-	}
+  'recoveryEmail.verify does not set verified if codes do not match',
+  function (t) {
+    function end() { t.end() }
+    RecoveryEmail.create('xxx', email, true)
+      .then(
+        function (x) {
+          t.equal(x.verified, false)
+          var c = crypto.randomBytes(32).toString('hex')
+          return x.verify(c)
+        }
+      )
+      .then(
+        function (x) {
+          t.equal(x.verified, false)
+          return x.del()
+        }
+      )
+      .done(end, end)
+  }
 )
 
 test(
-	'recoveryEmail.verify will not unset the verified flag from true to false',
-	function (t) {
-		function end() { t.end() }
-		RecoveryEmail.create('xxx', email, true)
-			.then(
-				function (x) {
-					t.equal(x.verified, false)
-					var c = x.code
-					return x.verify(c)
-				}
-			)
-			.then(
-				function (x) {
-					t.equal(x.verified, true)
-					return x.verify('bad1')
-				}
-			)
-			.then(
-				function (x) {
-					t.equal(x.verified, true)
-					return x.del()
-				}
-			)
-			.done(end, end)
-	}
+  'recoveryEmail.verify will not unset the verified flag from true to false',
+  function (t) {
+    function end() { t.end() }
+    RecoveryEmail.create('xxx', email, true)
+      .then(
+        function (x) {
+          t.equal(x.verified, false)
+          var c = x.code
+          return x.verify(c)
+        }
+      )
+      .then(
+        function (x) {
+          t.equal(x.verified, true)
+          return x.verify('bad1')
+        }
+      )
+      .then(
+        function (x) {
+          t.equal(x.verified, true)
+          return x.del()
+        }
+      )
+      .done(end, end)
+  }
 )
 
 test(
