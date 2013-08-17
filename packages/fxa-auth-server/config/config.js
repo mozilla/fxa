a4414f451105c0b67880b605535635fbafa71d62
Zachary Carter <zack.carter@gmail.com>
2013-08-16 17:49:23 -0700

    move local config to a file and auto load

--- packages/fxa-auth-server/config/config.js
+++ packages/fxa-auth-server/config/config.js
@@ -217,12 +217,12 @@ module.exports = function (fs, path, url, convict) {
 
   // handle configuration files.  you can specify a CSV list of configuration
   // files to process, which will be overlayed in order, in the CONFIG_FILES
-  // environment variable
-  if (process.env.CONFIG_FILES) {
-    var files = process.env.CONFIG_FILES.split(',').filter(fs.existsSync)
+  // environment variable. By default, the ./config/<env>.json file is loaded.
 
-    conf.loadFile(files)
-  }
+  var envConfig = path.join(__dirname, conf.get('env') + '.json')
+  var files = (envConfig + ',' + process.env.CONFIG_FILES)
+                .split(',').filter(fs.existsSync)
+  conf.loadFile(files)
 
   // set the public url as the issuer domain for assertions
   conf.set('domain', url.parse(conf.get('public_url')).host)
