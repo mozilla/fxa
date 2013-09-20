86085f6db5a6c70af2f29f3b6b74c3b967dd95b1
Ryan Kelly <rkelly@mozilla.com>
2013-09-20 14:54:17 +1000

    Simplify and cleanup heka+awsbox setup.

--- packages/fxa-auth-server/scripts/awsbox/post_create.sh
+++ packages/fxa-auth-server/scripts/awsbox/post_create.sh
@@ -19,22 +19,6 @@ echo "Setting up nginx"
 sudo /sbin/chkconfig nginx on
 sudo /sbin/service nginx start
 
-echo "Setting up elasticsearch"
-
-wget https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.3.tar.gz
-pushd /home/app
-sudo tar zxvf $MYPWD/elasticsearch-0.90.3.tar.gz
-sudo chown -R app:app elasticsearch-0.90.3
-popd
-
-echo "Setting up kibana"
-
-wget https://github.com/elasticsearch/kibana/archive/master.tar.gz
-mv master.tar.gz kibana-master.tar.gz
-pushd /usr/share/nginx/html
-sudo tar zxvf $MYPWD/kibana-master.tar.gz
-popd
-
 echo "Setting up heka"
 
 HEKAFILE=heka-0_4_0-picl-idp-amd64.tar.gz
@@ -88,5 +72,10 @@ sudo chkconfig postfix on
 echo "Establishing auto-update crontab"
 
 echo "*/5 * * * * /bin/bash -l /home/app/code/scripts/awsbox/auto_update.sh > /dev/null 2> /dev/null" | sudo crontab -u app -
+
+echo "Cleaning up old logfiles"
+
+sudo rm -rf /var/log/nginx/access.log-*
+sudo rm -rf /var/log/nginx/error.log-*
 
 echo "post-create complete!"
