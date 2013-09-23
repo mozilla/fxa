9f2f124b553d8dd40f5f196b4b127feaba74c918
Ryan Kelly <rkelly@mozilla.com>
2013-09-23 13:36:53 +1000

    Refactor loadtests to never keystretch, and do more cert-signing requests.

--- packages/fxa-auth-server/loadtest/loadtests.js
+++ packages/fxa-auth-server/loadtest/loadtests.js
@@ -5,41 +5,99 @@
 const assert = require('assert');
 const crypto = require('crypto');
 const Client = require('../client/');
+const srp = require('srp');
 const P = require('p-promise')
 
-
-const precomputed_credentials = require("./precomputed_credentials.js")
 
 const SERVER_URL = "http://idp.loadtest.lcip.org";
 
 
-function uniqueID() {
-  return crypto.randomBytes(10).toString('hex');
+// Error constants used by the picl-idp API.
+//
+ERROR_ACCOUNT_EXISTS = 101;
+ERROR_UNKNOWN_ACCOUNT = 102;
+
+
+// The tests need a public key for the server to sign, but we don't actually
+// do anything with it.  It suffices to use a fixed dummy key throughout.
+//
+var DUMMY_PUBLIC_KEY = {
+  "algorithm": "RS",
+  "n": "4759385967235610503571494339196749614544606692567785790953934768202714280652973091341316862993582789079872007974809511698859885077002492642203267408776123",
+  "e": "65537"
+};
+
+
+// We don't want to do any key-stretching during the loadtest, because it
+// takes a long time.  Instead we start from a fixed set of SRP credentials
+// and mix in just enough user-account-specific information to make the
+// authentication work properly.
+//
+var DUMMY_CLIENT_CREDENTIALS = {
+  email: null,
+  srpPw: "f6c1cc977d2811c55f0260f0318c8cbe13d215120f5d4f1113b33f32db670e81",
+  unwrapBKey: "c8bdcea80dd5ebc94b870f57b840a5f9ea1d82d5eae72a5081831c7f3667be74",
+  passwordSalt: "AAAAAA",
+  srp: {
+    type: "SRP-6a/SHA256/2048/v1",
+    salt: "f4f435710b693852e6602a58c902a37539e4ce8351c6495f0b28b4c5cbad0cd4",
+    algorithm: "sha256",
+    verifier: null,
+  }
 }
 
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+// Build an SRP verifier to go with the dummy credentials defined above.
+//
+function getDummySRPVerifier(email) {
+  var res = srp.getv(
+    Buffer(DUMMY_CLIENT_CREDENTIALS.srp.salt, 'hex'),
+    Buffer(email),
+    Buffer(DUMMY_CLIENT_CREDENTIALS.srpPw),
+    srp.params['2048'].N,
+    srp.params['2048'].g,
+    DUMMY_CLIENT_CREDENTIALS.srp.algorithm
+ );
+ if (res.length < 256) {
+   var tmp = res;
+   var padding = res.length - 256;
+   var res = new Buffer(256);
+   res.full(0, 0, padding);
+   tmp.copy(res, padding);
+ }
+ return res.toString('hex');
 }
 
 
-module.exports.picl_idp_loadtest = function picl_idp_loadtest(cb) {
-
-  var socket = cb.socket;
+//  Construct and return a new API Client object.
+//  This produces a new Client object with credentials in place, ready
+//  to perform API requests and send the resulting performance data back
+//  to the loads test runner.  It uses the dummy credentials defined above.
+//
+//  You must provide the email address to use, and the loads communication
+//  socket.
+//
+function getClient(email, loadsSocket) {
 
   var client = new Client(SERVER_URL);
 
-  // Monitor the requests performed by the Client api,
-  // and send performance information back to loads.
+  // Set up dummy credentials.
+  // We have to mix the email address into the SRP verifier, but
+  // everything else can be pre-calculated.
+  for (var k in DUMMY_CLIENT_CREDENTIALS) {
+    client[k] = DUMMY_CLIENT_CREDENTIALS[k];
+  }
+  client.email = Buffer(email).toString('hex');
+  client.srp.verifier = getDummySRPVerifier(client.email);
 
+  // Instrument the API to send request back back to loads test runner.
   client.api.on('startRequest', function(options) {
     options._loadsStartTime = +new Date();
   });
-
   client.api.on('endRequest', function(options, err, res) {
     if (!err) {
       var loadsEndTime = +new Date();
-      socket.send('add_hit', {
+      loadsSocket.send('add_hit', {
         url: options.url,
         method: options.method,
         status: res.statusCode,
@@ -49,85 +107,103 @@ module.exports.picl_idp_loadtest = function picl_idp_loadtest(cb) {
     }
   });
 
+  return client
+}
+
+
+//  Generate a short random hex string.
+//  Useful where you need something unique.
+//
+function uniqueID(size) {
+  if (!size) {
+    size = 10;
+  }
+  return crypto.randomBytes(size/2 + 1).toString('hex').substring(0, size);
+}
+
+
+//  Generate a random integer in the range [min, max]
+//
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+
+
+//  And finally, here is the actual loadtest!
+//
+module.exports.picl_idp_loadtest = function picl_idp_loadtest(cb) {
 
   // Target ratio is 2 new-account signups per 10 old-account signups.
-  // Use a restricted pool of old-account email addresses to simulate
-  // existing users; they will be populated quickly during the loadtest.
-  //
-  // XXX TODO: rework this to always use pre-computed credentials, the
-  // overhead of scrypt in the loadtest loop is just silly.
+  // We use a restricted pool of old-account email addresses to simulate
+  // existing users; they will be populated quickly during the loadtest
+  // run.  We use randomly-generated email addresses to simulate new accounts
+  // and will forcibly delete any that are found to exist.
 
-  var userid;
-  if (getRandomInt(1, 12) <= 2) {
-    userid = uniqueID();
-  } else {
-    userid = getRandomInt(1, 100);
-  }
-  userid = "loady" + userid + "@restmail.lcip.org"
-
-  // Use pre-computed SRP credentials where possible; doing keystretching for
-  // every single run of these tests makes a mockery of loadtest throughput.
-
+  var email;
+  var client;
   var ready;
-  var creds = precomputed_credentials[userid];
-  if (!creds) {
-    ready = client.setupCredentials(userid, "loadtestpassword", "AAAAAA");
-  } else {
-    for (var k in creds) {
-      if (creds.hasOwnProperty(k)) {
-        client[k] = creds[k];
+
+  if (getRandomInt(1, 12) <= 2) {
+    //  New user.
+    //  Assume it doesnt exist and try to create; delete and retry if it does.
+    email = "loady" + uniqueID() + " @restmail.lcip.org";
+    client = getClient(email, cb.socket);
+    ready = client.create().fail(function(err) {
+      if (err.code != 400 || err.errno != ERROR_ACCOUNT_EXISTS) {
+        throw err;
       }
-    }
-    ready = P();
+      return client.destroyAccount().then(function() {
+        return client.create();
+      });
+    }).then(function() {
+      return client.login();
+    });
+  } else {
+    //  Existing user.
+    //  Assume it exists and try to authentiate; create only if not exists.
+    email = "loady" + getRandomInt(1, 100) + " @restmail.lcip.org";
+    client = getClient(email, cb.socket);
+    ready = client.login().fail(function(err) {
+      if (err.code != 400 || err.errno != ERROR_UNKNOWN_ACCOUNT) {
+        throw err;
+      }
+      return client.create().then(function() {
+        return client.login();
+      });
+    });
   }
 
-  // Use a constant public-key to pass to cert-signing requests.
-  // We don't need it for anything else in the tests
 
-  var publicKey = {
-    "algorithm":"RS",
-    "n":"4759385967235610503571494339196749614544606692567785790953934768202714280652973091341316862993582789079872007974809511698859885077002492642203267408776123",
-    "e":"65537"
-  };
+  // We fetch the cryptographic key material, then make a bunch of cert
+  // signing requests.  The cert-signing request is expected to be by
+  // *far* the most frequent operation, so we do a lot of them.
 
-  ready.then(
-    // Try to create the new account.
-    function () {
-      return client.create();
-    }
-  ).fail(
-    // Likely it already exists, but that's ok.
-    // XXX TODO: better to pre-populate the database?
-    // XXX TODO: error codes as defined constants?
-    function (err) {
-      if (err.code !== 400 || err.errno !== 101) {
-          throw err;
-      }
-    }
-  ).then(
+  ready.then(function() {
     // Fetch the key material.
-    function () {
-      return client.keys();
-    }
-  ).then(
+    return client.keys();
+  }).then(function(keys) {
     // Check that we actually got some key material.
-    function (keys) {
-      assert.equal(typeof keys.kA, 'string', 'kA exists');
-      assert.equal(typeof keys.wrapKb, 'string', 'wrapKb exists');
+    assert.equal(typeof keys.kA, 'string', 'kA exists');
+    assert.equal(typeof keys.wrapKb, 'string', 'wrapKb exists');
+  }).then(function() {
+    // Make a bunch of certificate-signing requests.
+    // Varying the number of iterations of this loop is a simple way
+    // to de-synchronize concurrent runs of this test, spreading out the
+    // other operations more evenly throughout the run.
+    var p = P();
+    for (var i=0; i < getRandomInt(10, 20); i++) {
+      p = p.then(function() {
+        return client.sign(DUMMY_PUBLIC_KEY, 1000 * 60 * 60);
+      }).then(function(cert) {
+        assert.equal(typeof cert, 'string', 'cert exists');
+      });
     }
-  ).then(
-    // Check that we can sign a public key.
-    function () {
-        return client.sign(publicKey, 1000 * 60 * 60);
-    }
-  ).then(
-    // And that it comes back correctly.
-    function (cert) {
-      assert.equal(typeof cert, 'string', 'cert exists');
-    }
-  ).done(
+    return p;
+  }).done(
     function() { return cb(null); },
     function(err) { return cb(err); }
   );
 
 }
+
