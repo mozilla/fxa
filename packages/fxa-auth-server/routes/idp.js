ea6d25c68e139f5c51e88a9b8905032adb34f102
Ryan Kelly <rkelly@mozilla.com>
2013-10-23 10:49:29 +1100

    Don't apply /v1 prefix to .well-known or related files.

--- packages/fxa-auth-server/routes/idp.js
+++ packages/fxa-auth-server/routes/idp.js
@@ -39,30 +39,6 @@ module.exports = function (log, crypto, error, isA, serverPublicKey, bridge) {
           file: './provision.html'
         }
       }
-    },
-    {
-      method: 'POST',
-      path: '/get_random_bytes',
-      config: {
-        handler: function getRandomBytes(request) {
-          request.reply({ data: crypto.randomBytes(32).toString('hex') })
-        }
-      }
-    },
-    {
-      method: 'GET',
-      path: '/verify_email',
-      config: {
-        handler: function (request, next) {
-          return request.reply.redirect(bridge.url + request.raw.req.url)
-        },
-        validate: {
-          query: {
-            code: isA.String().regex(HEX_STRING).required(),
-            uid: isA.String().max(64).required()
-          }
-        }
-      }
     }
   ]
 
