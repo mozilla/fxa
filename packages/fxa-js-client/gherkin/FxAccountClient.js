73810822286b847e6366635257a818657cd0db0e
vladikoff <vlad.filippov@gmail.com>
2013-10-31 18:57:10 -0400

    Adding AMD hawk library.

--- packages/fxa-js-client/gherkin/FxAccountClient.js
+++ packages/fxa-js-client/gherkin/FxAccountClient.js
@@ -1,8 +1,9 @@
-define([], function () {
+define(['./lib/hawk'], function (hawk) {
   'use strict';
 
   function FxAccountClient() {
     this.awesome = true;
+    this.hawkHeaderVersion = hawk.crypto.headerVersion;
   }
 
   FxAccountClient.prototype.isAwesome = function() {
