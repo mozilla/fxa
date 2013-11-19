4f0fde9fabaff0a2753f72048d54ab086a3544a1
Zachary Carter <zack.carter@gmail.com>
2013-11-19 14:12:29 -0800

    fix scrypt test

--- packages/fxa-auth-server/test/run/scrypt_tests.js
+++ packages/fxa-auth-server/test/run/scrypt_tests.js
@@ -8,36 +8,40 @@ var test = require('tap').test
 test(
   'scrypt basic with a remote helper',
   function (t) {
-    function end() { t.end() }
-
     var K1 = Buffer('f84913e3d8e6d624689d0a3e9678ac8dcc79d2c2f3d9641488cd9d6ef6cd83dd', 'hex')
     var salt = Buffer('identity.mozilla.com/picl/v1/scrypt')
-    var helper = 'https://scrypt.dev.lcip.org/'
+    var helper = 'https://scrypt-accounts.dev.lcip.org/'
 
     scrypt.hash(K1, salt, helper)
-      .then(
-      function (K2) {
-        t.equal(K2, '5b82f146a64126923e4167a0350bb181feba61f63cb1714012b19cb0be0119c5')
-      }
-    )
-      .done(end, end)
+      .done(
+        function (K2) {
+          t.equal(K2, '5b82f146a64126923e4167a0350bb181feba61f63cb1714012b19cb0be0119c5')
+          t.end();
+        },
+        function (e) {
+          t.fail(e);
+          t.end();
+        }
+      )
   }
 )
 
 test(
   'scrypt basic with a local helper',
   function (t) {
-    function end() { t.end() }
-
     var K1 = Buffer('f84913e3d8e6d624689d0a3e9678ac8dcc79d2c2f3d9641488cd9d6ef6cd83dd', 'hex')
     var salt = Buffer('identity.mozilla.com/picl/v1/scrypt')
 
     scrypt.hash(K1, salt)
-      .then(
-      function (K2) {
-        t.equal(K2, '5b82f146a64126923e4167a0350bb181feba61f63cb1714012b19cb0be0119c5')
-      }
-    )
-      .done(end, end)
+      .done(
+        function (K2) {
+          t.equal(K2, '5b82f146a64126923e4167a0350bb181feba61f63cb1714012b19cb0be0119c5')
+          t.end();
+        },
+        function (e) {
+          t.fail(e);
+          t.end();
+        }
+      )
   }
 )
