0dc9b49ab1617096ba11392c02822a57466844a2
Lloyd Hilaiel <lloyd@hilaiel.com>
2013-11-26 16:43:39 +0200

    incremental implementation spawning of verifier from test harness

--- packages/browserid-verifier/tests/lib/verifier.js
+++ packages/browserid-verifier/tests/lib/verifier.js
@@ -2,6 +2,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+const
+cp = require('child_process'),
+path = require('path');
+
+function later(cb /* args */) {
+  var args = Array.prototype.slice.call(arguments, 1);
+  process.nextTick(function() {
+    cb.apply(null, args);
+  });
+}
+
 function Verifier(args) {
   if (!args) args = {};
   this.args = args;
@@ -13,7 +24,26 @@ Verifier.prototype.setFallback = function(idp) {
 };
 
 Verifier.prototype.start = function(cb) {
-  cb('not implemented');
+  var repoBaseDir = path.join(__dirname, '..', '..');
+
+  if (this.process) {
+    return later(cb, null);
+  }
+
+  this.process = cp.spawn(
+    path.join(repoBaseDir, 'server.js'),
+    null,
+    { cwd: repoBaseDir, silent: true, stdio: 'pipe' });
+
+  this.process.stdout.on('data', function(line) {
+    console.log('stdout', line.toString());
+  });
+
+  this.process.stderr.on('data', function(line) {
+    console.log('stderr', line.toString());
+  });
+
+  return later(cb, null);
 };
 
 Verifier.prototype.stop = function(cb) {
