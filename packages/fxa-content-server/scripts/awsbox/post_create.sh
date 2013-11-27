b5b2b41a97c7053d4297253f084304f84b6f61be
Ryan Kelly <rkelly@mozilla.com>
2013-11-28 09:41:29 +1100

    Update awsbox environment to latest heka release

--- packages/fxa-content-server/scripts/awsbox/post_create.sh
+++ packages/fxa-content-server/scripts/awsbox/post_create.sh
@@ -2,14 +2,39 @@
 
 MYPWD=`pwd`
 
+
 echo "Setting up heka"
 
-HEKAFILE=heka-0_4_0-picl-idp-amd64.tar.gz
-wget https://people.mozilla.com/~rmiller/heka/$HEKAFILE
+HEKAURL=https://github.com/mozilla-services/heka/releases/download/v0.4.1/heka-0_4_1-linux-amd64.tar.gz
+HEKAFILE=`basename $HEKAURL`
+wget $HEKAURL
 pushd /home/app
 sudo tar zxvf $MYPWD/$HEKAFILE
-sudo chown -R app:app heka-0_4_0-linux-amd64
+sudo chown -R app:app heka-0_4_1-linux-amd64
 popd
+
+
+echo "Setting up circus"
+
+sudo yum install --assumeyes python-devel python-pip
+sudo pip install circus
+sudo tee --append /home/app/circus.ini << EOF
+[watcher:hekad]
+working_dir=/home/app/hekad
+cmd=/home/app/heka-0_4_1-linux-amd64/bin/hekad -config=/home/app/code/scripts/awsbox/hekad.toml
+numprocesses = 1
+stdout_stream.class = FileStream
+stdout_stream.filename = /home/app/hekad/circus.stdout.log
+stdout_stream.refresh_time = 0.5
+stdout_stream.max_bytes = 1073741824
+stdout_stream.backup_count = 3
+stderr_stream.class = FileStream
+stderr_stream.filename = /home/app/hekad/circus.stderr.log
+stderr_stream.refresh_time = 0.5
+stderr_stream.max_bytes = 1073741824
+stderr_stream.backup_count = 3
+EOF
+
 
 echo "Installing identity team public keys"
 
