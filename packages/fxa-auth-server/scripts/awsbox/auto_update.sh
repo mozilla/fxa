96bdcc2dee0751b7c51fb7ad106f890e7556909b
Ryan Kelly <rkelly@mozilla.com>
2013-11-27 13:06:56 +1100

    Drop and re-create database as part of auto-update script.

--- packages/fxa-auth-server/scripts/awsbox/auto_update.sh
+++ packages/fxa-auth-server/scripts/awsbox/auto_update.sh
@@ -15,6 +15,9 @@ git remote add origin $ORIGIN || true
 git fetch origin
 
 if [ `$CURCOMMIT master` != `$CURCOMMIT origin/master` ]; then
+  echo "DROP DATABASE fxa;" | mysql -u root
+  echo "CREATE DATABASE fxa;" | mysql -u root
+  echo "GRANT ALL ON fxa.* TO 'fxa'@'localhost';" | mysql -u root
   git branch -f master origin/master
   /usr/local/bin/node ../post-update.js
 fi
