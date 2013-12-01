0beaf245bc9395304095734f3e5349f604ae89ce
Lloyd Hilaiel <lloyd@hilaiel.com>
2013-12-01 11:59:00 -0800

    more tests.

---
+++ packages/browserid-verifier/tests/audience.js
@@ -0,0 +1,100 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* global describe,it,require */
+
+var
+IdP = require('browserid-local-verify/testing').IdP,
+Client = require('browserid-local-verify/testing').Client,
+Verifier = require('./lib/verifier.js'),
+should = require('should'),
+request = require('request');
+
+describe('audience tests', function() {
+  var verifier = new Verifier();
+  var idp = new IdP();
+  var client;
+
+  it('test servers should start', function(done) {
+    idp.start(function(e) {
+      verifier.start(function(e1) {
+        client = new Client({ idp: idp });
+        done(e || e1);
+      });
+    });
+  });
+
+  var assertion;
+
+  it('test assertion should be created', function(done) {
+    client.assertion({ audience: 'http://example.com' }, function(err, ass) {
+      assertion = ass;
+      done(err);
+    });
+  });
+
+  function submitWithAudience(audience, cb) {
+    request({
+      method: 'post',
+      url: verifier.url(),
+      json: true,
+      body: {
+        assertion: assertion,
+        audience: audience
+      }
+    }, cb);
+  }
+
+  it('should verify with complete audience', function(done) {
+    submitWithAudience('http://example.com', function(err, r) {
+      should.not.exist(err);
+      (r.body.status).should.equal('okay');
+      done();
+    });
+  });
+
+  it('should fail to verify with different domain as audience', function(done) {
+    submitWithAudience('incorrect.com', function(err, r) {
+      should.not.exist(err);
+      (r.body.status).should.equal('failure');
+      (r.body.reason).should.equal('audience mismatch: domain mismatch');
+      done(err);
+    });
+  });
+
+  it('should fail to verify with different port', function(done) {
+    submitWithAudience('http://example.com:8080', function(err, r) {
+      should.not.exist(err);
+      (r.body.status).should.equal('failure');
+      (r.body.reason).should.equal('audience mismatch: port mismatch');
+      done(err);
+    });
+  });
+
+  it('should fail to verify with incorrect scheme', function(done) {
+    submitWithAudience('https://example.com', function(err, r) {
+      should.not.exist(err);
+      (r.body.status).should.equal('failure');
+      (r.body.reason).should.equal('audience mismatch: scheme mismatch');
+      done(err);
+    });
+  });
+
+  it('should fail to verify if audience is missing', function(done) {
+    submitWithAudience(undefined, function(err, r) {
+      should.not.exist(err);
+      (r.body.status).should.equal('failure');
+      (r.body.reason).should.equal('missing audience parameter');
+      done(err);
+    });
+  });
+
+  it('test servers should stop', function(done) {
+    idp.stop(function(e) {
+      verifier.stop(function(e1) {
+        done(e || e1);
+      });
+    });
+  });
+});
---
+++ packages/browserid-verifier/tests/missing-assertion.js
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* global describe,it,require */
+
+var
+Verifier = require('./lib/verifier.js'),
+should = require('should'),
+request = require('request');
+
+describe('missing assertion test', function() {
+  var verifier = new Verifier();
+
+  it('test servers should start', function(done) {
+    verifier.start(done);
+  });
+
+  it('should fail to verify when assertion is missing', function(done) {
+    request({
+      method: 'post',
+      url: verifier.url(),
+      json: true,
+      body: {
+        audience: "http://example.com"
+      }
+    }, function(err, r) {
+      should.not.exist(err);
+      (r.statusCode).should.equal(400);
+      (r.body.status).should.equal('failure');
+      (r.body.reason).should.equal('missing assertion parameter');
+      done();
+    });
+  });
+
+  it('test servers should stop', function(done) {
+    verifier.stop(done);
+  });
+});
