4aa498fdf2a1cc9479e41e64825f908dfb4ec4ab
Lloyd Hilaiel <lloyd@hilaiel.com>
2013-12-01 13:15:25 -0800

    tests of unverified email support

---
+++ packages/browserid-verifier/tests/unverified_email.js
@@ -0,0 +1,108 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* global describe,it,require */
+
+var
+IdP = require('browserid-local-verify/testing').IdP,
+Client = require('browserid-local-verify/testing').Client,
+Verifier = require('./lib/verifier.js'),
+should = require('should'),
+request = require('request');
+
+describe('unverified email', function() {
+  var fallback = new IdP();
+  var verifier = new Verifier();
+  var client;
+
+  it('test idps should start up', function(done) {
+    fallback.start(function(e1) {
+      verifier.setFallback(fallback);
+      verifier.start(function(e2) {
+        done(e1 || e2);
+      });
+    });
+  });
+
+  it('assertion with unverified email address should fail to verify', function(done) {
+    client = new Client({ idp: fallback });
+    client.principal({ "unverified-email": "bob@example.com" });
+    client.assertion({ audience: 'http://example.com' }, function(err, assertion) {
+      request({
+        method: 'post',
+        url: verifier.url(),
+        json: true,
+        body: {
+          assertion: assertion,
+          audience: "http://example.com"
+        }
+      }, function(err, r) {
+        should.not.exist(err);
+        (r.statusCode).should.equal(200);
+        (r.body.status).should.equal('failure');
+        (r.body.reason).should.startWith("untrusted assertion");
+        done();
+      });
+    });
+  });
+
+  it('assertion with unverified email address and forceIssuer should verify', function(done) {
+    client = new Client({ idp: fallback });
+    client.principal({ "unverified-email": "bob@example.com" });
+    client.assertion({ audience: 'http://example.com' }, function(err, assertion) {
+      request({
+        method: 'post',
+        url: verifier.url(),
+        json: true,
+        body: {
+          assertion: assertion,
+          audience: "http://example.com",
+          experimental_forceIssuer: fallback.domain()
+        }
+      }, function(err, r) {
+        should.not.exist(err);
+        (r.statusCode).should.equal(200);
+        (r.body.status).should.equal('okay');
+        (r.body.idpClaims).should.be.type('object');
+        (r.body.idpClaims["unverified-email"]).should.equal('bob@example.com');
+        (r.body).should.not.have.property("unverified-email");
+        done();
+      });
+    });
+  });
+
+  it('allowUnverified causes extractin of unverified email addresses', function(done) {
+    client = new Client({ idp: fallback });
+    client.principal({ "unverified-email": "bob@example.com" });
+    client.assertion({ audience: 'http://example.com' }, function(err, assertion) {
+      request({
+        method: 'post',
+        url: verifier.url(),
+        json: true,
+        body: {
+          assertion: assertion,
+          audience: "http://example.com",
+          experimental_forceIssuer: fallback.domain(),
+          experimental_allowUnverified: true
+        }
+      }, function(err, r) {
+        should.not.exist(err);
+        (r.statusCode).should.equal(200);
+        (r.body.status).should.equal('okay');
+        (r.body.idpClaims).should.be.type('object');
+        (r.body.idpClaims["unverified-email"]).should.equal('bob@example.com');
+        (r.body).should.have.property("unverified-email");
+        done();
+      });
+    });
+  });
+
+  it('test idp should shut down', function(done) {
+    fallback.stop(function(e1) {
+      verifier.stop(function(e2) {
+        done(e1 || e2);
+      });
+    });
+  });
+});
