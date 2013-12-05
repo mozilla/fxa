cfc92adda6d08c6830d9c5abfc37ff2bfed1387d
Ryan Kelly <rkelly@mozilla.com>
2013-12-05 13:52:23 +1100

    Add a `make purge` command for cleaning up zombie loadtest runs.

--- packages/fxa-auth-server/loadtest/loadsrunner.sh
+++ packages/fxa-auth-server/loadtest/loadsrunner.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 #
-# Run loads bench suite, by submitting to worker machines in AWS.
+# Wrapper script for loads-runner to talk to remote broker.
+# We use an ssh tunnel to communicate with the loads-broker in AWS.
+# This gives us a bit of security while making it easy to control the
+# loadtest from from a local box.
 
 BROKER=loads.loadtest.lcip.org
 
-# We use an ssh tunnel to communicate with the loads-broker in AWS.
 # Host key checking is deliberately disabled because the server regularly
 # gets torn-down and replaced, and we're not sending any private info anyway.
 SSH="ssh -q -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
@@ -19,6 +21,6 @@ fi
 $SSH -N -L 7776:$BROKER:7776 -L 7780:$BROKER:7780 ec2-user@$BROKER &
 SSH_PID=$!
 
-./bin/loads-runner --users=20 --duration=300 --broker=tcp://localhost:7780 --zmq-publisher=tcp://localhost:7776 --agents=5 --include-file=./loadtests.py --python-dep=hawkauthlib loadtests.LoadTest.test_auth_server
+./bin/loads-runner --broker=tcp://localhost:7780 --zmq-publisher=tcp://localhost:7776 $@
 
 kill $SSH_PID
