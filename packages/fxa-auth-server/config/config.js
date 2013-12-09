fb4f17fb442066270a0b4653f44974b455b61b33
Ryan Kelly <rkelly@mozilla.com>
2013-12-10 09:58:34 +1100

    Rename "kvstore" config settings to "db".

--- packages/fxa-auth-server/config/config.js
+++ packages/fxa-auth-server/config/config.js
@@ -29,7 +29,7 @@ module.exports = function (fs, path, url, convict) {
     publicKeyFile: {
       default: path.resolve(__dirname, '../config/public-key.json')
     },
-    kvstore: {
+    db: {
       backend: {
         format: AVAILABLE_BACKENDS,
         default: "memory",
