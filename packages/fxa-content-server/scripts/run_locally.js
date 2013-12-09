20ef23bf37dcc165f63f24c7d6afe4c13184ac02
Shane Tomlinson <stomlinson@mozilla.com>
2013-12-09 11:14:17 +0000

    Simplify the run_locally script.
    * Allow the PORT to be specified in the environment, convict will take care of reading from the configuration file, environment or default.
    * Simplify the path finding to the correct bin file.

--- packages/fxa-content-server/scripts/run_locally.js
+++ packages/fxa-content-server/scripts/run_locally.js
@@ -4,19 +4,15 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 const path = require('path'),
-      spawn = require('child_process').spawn,
-      config = require('../server/lib/configuration');
+      spawn = require('child_process').spawn;
 
+
+const BIN_ROOT = path.join(__dirname, '..', 'server', 'bin');
 
 startServer();
 
 function startServer() {
-  process.chdir(path.dirname(__dirname));
-  // We'll get PORT via config/local.json
-  delete process.env['PORT'];
-
-  // TODO what if there is no local.json?
-  var fabPath = path.join(process.cwd(), 'server', 'bin', 'fxa-content-server.js');
+  var fabPath = path.join(BIN_ROOT, 'fxa-content-server.js');
   var fxaccntbridge = spawn('node', [fabPath]);
   fxaccntbridge.stdout.on('data', function(data) {
     console.log('fxa-content-server:', data.toString('utf8'));
