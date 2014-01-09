3081cd4257af7ad67f494a80097485f0f6f03cc8
Danny Coates <dannycoates@gmail.com>
2014-01-09 12:36:10 -0800

    removed HEX_EMAIL regex

--- packages/fxa-auth-server/routes/validators.js
+++ packages/fxa-auth-server/routes/validators.js
@@ -6,9 +6,4 @@
 
 module.exports.HEX_STRING = /^(?:[a-fA-F0-9]{2})+$/
 
-// Match an email address encoded as a hex string.
-// This is a pretty coarse match - basically /.+@.+/ on a hex string.
-
-module.exports.HEX_EMAIL = /^(?:[a-fA-F0-9]{2})+40(?:[a-fA_F0-9]{2})+$/
-
 module.exports.LAZY_EMAIL = /^[^@\s]+@[^@\s]+$/
