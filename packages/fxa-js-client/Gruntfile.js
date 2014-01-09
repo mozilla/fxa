d4334319407d79ce92fc4a0ea716a6377b4fe6a1
Peter deHaan <peter@deseloper.com>
2014-01-09 09:50:19 -0800

    Adding missing copyrights

--- packages/fxa-js-client/Gruntfile.js
+++ packages/fxa-js-client/Gruntfile.js
@@ -1,4 +1,8 @@
-module.exports = function(grunt) {
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+module.exports = function (grunt) {
   // load all grunt tasks matching the `grunt-*` pattern
   require('load-grunt-tasks')(grunt);
   // load the Intern tasks
