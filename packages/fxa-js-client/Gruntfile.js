19d1dd655aea9e8009e68fba70f7e7d227da11ce
vladikoff <vlad.filippov@gmail.com>
2014-01-10 18:43:27 -0500

    Fix require conflict with Dojo

--- packages/fxa-js-client/Gruntfile.js
+++ packages/fxa-js-client/Gruntfile.js
@@ -12,7 +12,6 @@ module.exports = function(grunt) {
       options: {
         baseUrl: '.',
         include: ['client/FxAccountClient'],
-        insertRequire: ['client/FxAccountClient'],
         name: 'components/almond/almond',
         wrap: {
           startFile: 'config/start.frag',
