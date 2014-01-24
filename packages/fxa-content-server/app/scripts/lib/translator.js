9d7bdaafbf4076488c0bc03c5dbe7e157d2f3028
Shane Tomlinson <stomlinson@mozilla.com>
2014-01-24 12:44:47 +0000

    Add string interpolation to the translator.
    * The translator looks for strings with %s and %(named)s and replaces with values from context.

--- packages/fxa-content-server/app/scripts/lib/translator.js
+++ packages/fxa-content-server/app/scripts/lib/translator.js
@@ -7,29 +7,60 @@
 define([
   'jquery'
 ],
-function($) {
-  var Translator = function(language) {
+function ($) {
+  var Translator = function (language) {
     this.language = language;
     this.translations = {};
   };
 
   Translator.prototype = {
-    set: function(translations) {
+    set: function (translations) {
       this.translations = translations;
     },
 
     // Fetches our JSON translation file
-    fetch: function(done) {
+    fetch: function (done) {
       $.ajax({ dataType: 'json', url: '/i18n/' + this.language.replace(/-/, '_') + '/messages.json' })
-        .done(function(data) {
+        .done(function (data) {
           this.translations = data;
         }.bind(this))
         .always(done);
     },
 
-    // Gets a translated value by key but returns the key if nothing is found
-    get: function(key) {
-      return this.translations[key] || key;
+    /**
+     * Gets a translated value by key but returns the key if nothing is found.
+     * Does string interpolation on %s and %(named)s.
+     * @method get
+     */
+    get: function (key, context) {
+      var translation = this.translations[key] || key;
+
+      return this.interpolate(translation, context);
+    },
+
+    /**
+     * Replace instances of %s and %(name)s with their corresponding values in
+     * the context
+     * @method interpolate
+     */
+    interpolate: function (string, context) {
+      if (! context) {
+        context = [];
+      }
+
+      var interpolated = string.replace(/\%s/g, function (match) {
+        // boot out non arrays and arrays with not enough items.
+        if (! (context.shift && context.length > 0)) {
+          return match;
+        }
+        return context.shift();
+      });
+
+      interpolated = interpolated.replace(/\%\(([a-zA-Z]+)\)s/g, function (match, name) {
+        return name in context ? context[name] : match;
+      });
+
+      return interpolated;
     }
   };
 
---
+++ packages/fxa-content-server/app/tests/spec/lib/translator.js
@@ -0,0 +1,113 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// test the translation library
+
+'use strict';
+
+
+define([
+  'mocha',
+  'chai',
+  'lib/translator'
+],
+function (mocha, chai, Translator) {
+  /*global beforeEach, afterEach, describe, it*/
+  var assert = chai.assert;
+
+  // translations taken from Persona's db_LB translations.
+  var TRANSLATIONS = {
+    'There was a problem with your signup link. Has this address already been registered?':
+        '⊥ɥǝɹǝ ʍɐs ɐ dɹoqʅǝɯ ʍıʇɥ ʎonɹ sıƃund ʅıuʞ˙ Hɐs ʇɥıs ɐppɹǝss ɐʅɹǝɐpʎ qǝǝu ɹǝƃısʇǝɹǝp¿',
+
+    'Error encountered trying to register: %(email)s.':
+        'Ǝɹɹoɹ ǝuɔonuʇǝɹǝp ʇɹʎıuƃ ʇo ɹǝƃısʇɹɐʇıou: %(email)s˙',
+
+    '%s, Persona requires cookies to remember you.':
+        '%s, Ԁǝɹsouɐ ɹǝbnıɹǝs ɔooʞıǝs ʇo ɹǝɯǝɯqǝɹ ʎon˙'
+  };
+
+  describe('lib/translator', function () {
+    var translator;
+
+    beforeEach(function () {
+      // Bringing back the David Bowie's Labrynth
+      translator = new Translator('db-LB');
+      translator.set(TRANSLATIONS);
+    });
+
+    afterEach(function () {
+      translator = null;
+    });
+
+    describe('get', function () {
+      it('returns translated string when it exists', function () {
+        var stringToTranslate =
+              'There was a problem with your signup link. Has this address already been registered?';
+        var translation = translator.get(stringToTranslate);
+        assert.equal(translation,
+              '⊥ɥǝɹǝ ʍɐs ɐ dɹoqʅǝɯ ʍıʇɥ ʎonɹ sıƃund ʅıuʞ˙ Hɐs ʇɥıs ɐppɹǝss ɐʅɹǝɐpʎ qǝǝu ɹǝƃısʇǝɹǝp¿');
+      });
+
+      it('returns untranslated string when translation does not exist', function () {
+        var stringToTranslate = 'this string is untranslated';
+        var translation = translator.get(stringToTranslate);
+        assert.equal(translation, stringToTranslate);
+      });
+
+      it('can do string interpolation on unnamed `%s` when given array context', function () {
+        var stringToTranslate = '%s, Persona requires cookies to remember you.';
+        var translation = translator.get(stringToTranslate, ['testuser@testuser.com']);
+        assert.equal(translation,
+              'testuser@testuser.com, Ԁǝɹsouɐ ɹǝbnıɹǝs ɔooʞıǝs ʇo ɹǝɯǝɯqǝɹ ʎon˙');
+      });
+
+      it('can do string interpolation on named `%(name)s` when given array context', function () {
+        var stringToTranslate = 'Error encountered trying to register: %(email)s.';
+        var translation = translator.get(stringToTranslate, {
+          email: 'testuser@testuser.com'
+        });
+        assert.equal(translation,
+              'Ǝɹɹoɹ ǝuɔonuʇǝɹǝp ʇɹʎıuƃ ʇo ɹǝƃısʇɹɐʇıou: testuser@testuser.com˙');
+      });
+
+      it('can do interpolation multiple times with an array', function() {
+        var stringToTranslate = 'Hi %s, you have been signed in since %s'
+        var translation = translator.get(stringToTranslate, [
+          'testuser@testuser.com', 'noon'
+        ]);
+
+        assert.equal(translation,
+              'Hi testuser@testuser.com, you have been signed in since noon');
+      });
+
+      it('can do interpolation multiple times with an object', function() {
+        var stringToTranslate = 'Hi %(email)s, you have been signed in since %(time)s'
+        var translation = translator.get(stringToTranslate, {
+          email: 'testuser@testuser.com',
+          time: 'noon'
+        });
+
+        assert.equal(translation,
+              'Hi testuser@testuser.com, you have been signed in since noon');
+      });
+
+      it('does no replacement on %s and %(name)s if not in context', function() {
+        var stringToTranslate = 'Hi %s, you have been signed in since %(time)s'
+        var translation = translator.get(stringToTranslate);
+
+        assert.equal(translation, stringToTranslate);
+      });
+
+      it('leaves remaining %s if not enough items in context', function() {
+        var stringToTranslate = 'Hi %s, you have been signed in since %s'
+        var translation = translator.get(stringToTranslate, ['testuser@testuser.com']);
+
+        assert.equal(translation, 'Hi testuser@testuser.com, you have been signed in since %s');
+      });
+    });
+  });
+});
+
+
