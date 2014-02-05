4fb0e9e6438011d065060c9ab7bc162a101cfcb3
Peter deHaan <peter@deseloper.com>
2014-02-05 15:10:28 -0800

    Adding grunt scaffolding

--- packages/fxa-profile-server/tasks/lint.js
+++ packages/fxa-profile-server/tasks/lint.js
@@ -2,4 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-module.exports = require('bluebird');
+// meta grunt task to run other linters.
+
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.registerTask('lint', [
+    'jshint',
+    'jscs'
+  ]);
+};
