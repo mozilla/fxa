e8f9a4de0391555db0cfe052cec19d98c13e4b15
Peter deHaan <peter@deseloper.com>
2014-02-14 16:16:47 -0800

    Switching to the grunt-copyright task instead of internal function

--- packages/fxa-content-server/grunttasks/copyright.js
+++ packages/fxa-content-server/grunttasks/copyright.js
@@ -18,24 +18,4 @@ module.exports = function (grunt) {
       ]
     }
   });
-
-  grunt.registerMultiTask('copyright', 'Checking for copyrights in files', function () {
-    var pattern = this.options().pattern;
-    var files = this.filesSrc.filter(function (file) {
-      var txt;
-      if (!grunt.file.isFile(file)) {
-        return;
-      }
-      txt = grunt.file.read(file, 'utf8');
-      return !txt.match(pattern);
-    });
-
-    if (files.length) {
-      grunt.log.subhead('The following files don\'t match the specified pattern:\n> %s\n', pattern);
-      files.forEach(function (file) {
-        grunt.log.writeln('- ' + file);
-      });
-      return false;
-    }
-  });
 };
