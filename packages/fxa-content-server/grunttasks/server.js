876d1078ca02b2b1d1c7a7076f78e5c11362d671
Peter deHaan <peter@deseloper.com>
2014-02-14 17:20:57 -0800

    Cleaning up the Gruntfile into the grunttasks dir

---
+++ packages/fxa-content-server/grunttasks/server.js
@@ -0,0 +1,21 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.registerTask('server', function (target) {
+    if (target === 'dist') {
+      return grunt.task.run(['build', 'serverproc:dist']);
+    }
+
+    grunt.task.run([
+      'clean:server',
+      'selectconfig:app',
+      'concurrent:server',
+      'autoprefixer',
+      'serverproc:app'
+    ]);
+  });
+};
