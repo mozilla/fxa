fa0a775ca617619144b1871648ef321684504247
Zachary Carter <zack.carter@gmail.com>
2014-02-19 15:35:09 -0800

    new po merge grunttask and README for locales

---
+++ packages/fxa-content-server/grunttasks/l10n-merge.js
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// grunt task to extract strings.
+
+const path = require('path');
+
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.registerTask('l10n-merge', function () {
+    var done = this.async();
+    grunt.util.spawn({
+      cmd: path.resolve(__dirname, '..', 'scripts', 'merge_po.sh'),
+      args: [path.resolve(__dirname, '..', 'locale')]
+    }, done);
+  });
+};
+
