b401d549657a8e57e239a852f84c08a1c997e8fb
Glen Mailer <glenjamin@gmail.com>
2014-02-22 12:28:55 +0000

    Allow client to be used as an npm module
    * No longer runs bower on npm install
    * Added "setup" task for development seutp
    * Added basic AMD shim
    * Exposed FxAccountClient via AMD shim as node-style module
    * Declared runtime dependencies in package.json
    * Explicitly specified "files" for small npm upload/download
    * Added intern:native_node which runs tests on the node client

---
+++ packages/fxa-js-client/client/node/amd-loader.js
@@ -0,0 +1,63 @@
+// This is intended to be the simplest possible AMD shim that works
+// It is not intended a general AMD loader just enough to load this package
+// This relies on the fact that Node.js require() is synchronous.
+// It attempts to let the node.js module loader do as much work as possible
+// Also provides a way to replace modules with api compatible counterparts
+
+var path = require('path');
+
+module.exports = function amdload(absoluteFilename, map) {
+    // Store this so we can put it back later.
+    var oldDefine = global.define;
+
+    define.amd = true;
+    var map = map || {}, loaded = {}, dirs = [], exported;
+
+    return amdrequire(absoluteFilename);
+
+    /**
+     * These two functions operate as a pair
+     */
+    function define(deps, factory) {
+        // Load all dependencies
+        var modules = deps.map(amdrequire);
+        // Capture the exported value
+        exported = factory.apply(null, modules);
+    }
+    function amdrequire(filepath) {
+        // Return real node modules if we have them mapped
+        if (filepath in map) {
+            return require(map[filepath]);
+        }
+
+        // Resolve target against 'current working directory'
+        var fullpath = path.resolve(dirs[0], filepath);
+
+        if (!loaded[fullpath]) {
+            // Put current operation on stack
+            dirs.unshift(path.dirname(fullpath));
+
+            // setup fake define and delegate to real require()
+            global.define = define;
+
+            require(fullpath);
+
+            // Capture and store exported module
+            loaded[fullpath] = exported;
+            exported = null;
+
+            // Restore previous define() state
+            if (oldDefine) {
+                global.define = define;
+            } else {
+                delete global.define;
+            }
+
+            // return to cwd from before define
+            dirs.shift();
+        }
+
+        // return value captured by define()
+        return loaded[fullpath];
+    }
+}
