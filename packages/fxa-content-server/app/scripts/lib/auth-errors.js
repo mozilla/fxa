b385d0c87190e4abe499e6dfe2262204aafb649b
Shane Tomlinson <stomlinson@mozilla.com>
2014-02-28 15:27:11 +0000

    fix(client): Show "Service unavailable" error message if auth server is unavailable or returns no error message.
    Closes mozilla/fxa-content-server#601

--- packages/fxa-content-server/app/scripts/lib/auth-errors.js
+++ packages/fxa-content-server/app/scripts/lib/auth-errors.js
@@ -65,18 +65,22 @@ function () {
     toMessage: function (err) {
       var code;
 
-      // already a number
       if (typeof err === 'number') {
         code = err;
       // error from backend
-      } else if (typeof err.errno === 'number') {
+      } else if (err && typeof err.errno === 'number') {
         code = err.errno;
       // error from backend that only has a message. Print the message.
-      } else if (err.message) {
+      } else if (err && err.message) {
         return err.message;
       // probably a string. Try to convert it.
-      } else {
+      } else if (err && err.length) {
         code = this.toCode(err);
+      } else {
+        // if there is no error, no error message, and no code,
+        // assume no response from the backend and
+        // the service is unavailable.
+        code = this.toCode('SERVICE_UNAVAILABLE');
       }
 
       return CODE_TO_MESSAGES[code] || err;
@@ -98,7 +102,7 @@ function () {
             param: err.param
           };
         }
-      } catch(e) {
+      } catch (e) {
         // handle invalid/unexpected data from the backend.
         if (window.console && console.error) {
           console.error('Error in auth-errors.js->toContext: %s', String(e));
