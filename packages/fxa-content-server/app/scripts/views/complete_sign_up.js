29b78bf30f68f7963f38d51a4f7c6eec5d9d7930
johngruen <john.gruen@gmail.com>
2014-03-14 16:49:32 -0700

    fix(style): removes flicker from post email verification screen

--- packages/fxa-content-server/app/scripts/views/complete_sign_up.js
+++ packages/fxa-content-server/app/scripts/views/complete_sign_up.js
@@ -19,14 +19,23 @@ function (_, BaseView, CompleteSignUpTemplate, FxaClient, Url) {
     className: 'complete_sign_up',
 
     afterRender: function () {
+
+      var completeEl = this.$el;
+      var completeElHeight = completeEl.height();
+      var completeElChildren = completeEl.children();
+      completeElChildren.hide();
+      completeEl.height(completeElHeight);
+
       var searchParams = this.window.location.search;
       var uid = Url.searchParam('uid', searchParams);
       if (! uid) {
+        completeElChildren.show();
         return this.displayError(t('no uid specified'));
       }
 
       var code = Url.searchParam('code', searchParams);
       if (! code) {
+        completeElChildren.show();
         return this.displayError(t('no code specified'));
       }
 
@@ -39,8 +48,10 @@ function (_, BaseView, CompleteSignUpTemplate, FxaClient, Url) {
               self.trigger('verify_code_complete');
             })
             .then(null, function (err) {
+              
               self.displayError(err);
-
+              
+              completeElChildren.show();
               self.trigger('verify_code_complete');
             });
     }
