7d823274d89b9ac6f435ef261a27869699128a63
Sean McArthur <sean.monstar@gmail.com>
2014-04-01 12:25:44 -0700

    use port 9010

--- packages/fxa-content-server/server/lib/configuration.js
+++ packages/fxa-content-server/server/lib/configuration.js
@@ -81,7 +81,7 @@ var conf = module.exports = convict({
   oauth_url: {
     doc: 'The url of the Firefox Account OAuth server',
     format: 'url',
-    default: 'http://127.0.0.1:9001/v1',
+    default: 'http://127.0.0.1:9010/v1',
     env: 'FXA_OAUTH_URL'
   },
   static_directory: {
