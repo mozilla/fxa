b0ebe45ff4f87734cf529af37c04c6af1b1f0d06
Sean McArthur <sean.monstar@gmail.com>
2014-04-30 16:49:40 -0700

    use v2 of browserid-verifier api

--- packages/fxa-auth-server/lib/browserid.js
+++ packages/fxa-auth-server/lib/browserid.js
@@ -2,20 +2,25 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+const request = require('request');
 
 const AppError = require('./error');
 const config = require('./config');
 const logger = require('./logging').getLogger('fxa.assertion');
 const P = require('./promise');
-const verify = require('browserid-verify')({
-  url: config.get('browserid.verificationUrl')
-});
 
 const HEX_STRING = /^[0-9a-f]+$/;
 
 module.exports = function verifyAssertion(assertion) {
   var d = P.defer();
-  verify(assertion, config.get('publicUrl'), function(err, email, res) {
+  var opts = {
+    url: config.get('browserid.verificationUrl'),
+    json: {
+      assertion: assertion,
+      audience: config.get('publicUrl')
+    }
+  };
+  request.post(opts, function(err, res, body) {
     if (err) {
       logger.error('error verifying assertion', err);
       return d.reject(err);
@@ -26,16 +31,17 @@ module.exports = function verifyAssertion(assertion) {
       d.reject(AppError.invalidAssertion());
     }
 
-    if (!res || res.status !== 'okay') {
-      return error('non-okay response', res);
+    if (!body || body.status !== 'okay') {
+      return error('non-okay response', body);
     }
+    var email = body.email;
     var parts = email.split('@');
     var allowedIssuer = config.get('browserid.issuer');
     if (parts.length !== 2 || parts[1] !== allowedIssuer) {
       return error('invalid email', email);
     }
-    if (res.issuer !== allowedIssuer) {
-      return error('invalid issuer', res.issuer);
+    if (body.issuer !== allowedIssuer) {
+      return error('invalid issuer', body.issuer);
     }
     var uid = parts[0];
     //sanity check the uid
@@ -43,7 +49,7 @@ module.exports = function verifyAssertion(assertion) {
       return error('uid not a 32-char hex string', uid);
     }
 
-    var claims = res.idpClaims;
+    var claims = body.idpClaims;
     if (!claims || !claims['fxa-verifiedEmail']) {
       return error('incorrect idpClaims', claims);
     }
