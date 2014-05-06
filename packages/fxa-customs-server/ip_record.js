641a4f0ed460ad16d3992d3d93d6b8251417d3f9
Francois Marier <francois@mozilla.com>
2014-05-06 16:03:42 +1200

    Make the now() function configurable to facilitate testing

--- packages/fxa-customs-server/ip_record.js
+++ packages/fxa-customs-server/ip_record.js
@@ -2,7 +2,9 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-module.exports = function (BLOCK_INTERVAL_MS, INVALID_AGENT_INTERVAL_MS) {
+module.exports = function (BLOCK_INTERVAL_MS, INVALID_AGENT_INTERVAL_MS, now) {
+
+  now = now || Date.now
 
   function IpRecord() {}
 
@@ -19,24 +21,24 @@ module.exports = function (BLOCK_INTERVAL_MS, INVALID_AGENT_INTERVAL_MS) {
   }
 
   IpRecord.prototype.isBlocked = function () {
-    return !!(this.bk && (Date.now() - this.bk < BLOCK_INTERVAL_MS))
+    return !!(this.bk && (now() - this.bk < BLOCK_INTERVAL_MS))
   }
 
   IpRecord.prototype.block = function () {
-    this.bk = Date.now()
+    this.bk = now()
   }
 
   IpRecord.prototype.retryAfter = function () {
     if (!this.isBlocked()) { return 0 }
-    return Math.floor((this.bk + BLOCK_INTERVAL_MS - Date.now()) / 1000)
+    return Math.floor((this.bk + BLOCK_INTERVAL_MS - now()) / 1000)
   }
 
   IpRecord.prototype.update = function (agent) {
     if (isBadAgent(agent)) {
-      if (Date.now() - this.ba < INVALID_AGENT_INTERVAL_MS) {
+      if (now() - this.ba < INVALID_AGENT_INTERVAL_MS) {
         this.block()
       }
-      this.ba = Date.now()
+      this.ba = now()
     }
     return this.retryAfter()
   }
