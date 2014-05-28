d8cb119a7290b9f745edbf460e160f7a853093c5
vladikoff <vlad.filippov@gmail.com>
2014-05-28 10:29:21 -0700

    fix(test): Run Mocha tests in order

--- packages/fxa-content-server/app/tests/main.js
+++ packages/fxa-content-server/app/tests/main.js
@@ -41,80 +41,99 @@ require([
   'lib/translator',
   'lib/session',
   'lib/fxa-client',
-  '../tests/setup',
-  '../tests/spec/lib/channels/web',
-  '../tests/spec/lib/channels/fx-desktop',
-  '../tests/spec/lib/xss',
-  '../tests/spec/lib/url',
-  '../tests/spec/lib/session',
-  '../tests/spec/lib/fxa-client',
-  '../tests/spec/lib/oauth-client',
-  '../tests/spec/lib/assertion',
-  '../tests/spec/lib/translator',
-  '../tests/spec/lib/router',
-  '../tests/spec/lib/strings',
-  '../tests/spec/lib/auth-errors',
-  '../tests/spec/lib/app-start',
-  '../tests/spec/lib/validate',
-  '../tests/spec/lib/service-name',
-  '../tests/spec/lib/metrics',
-  '../tests/spec/lib/null-metrics',
-  '../tests/spec/views/base',
-  '../tests/spec/views/tooltip',
-  '../tests/spec/views/form',
-  '../tests/spec/views/sign_up',
-  '../tests/spec/views/complete_sign_up',
-  '../tests/spec/views/sign_in',
-  // TODO Renable these (issue #1141)
-  //'../tests/spec/views/oauth_sign_in',
-  //'../tests/spec/views/oauth_sign_up',
-  '../tests/spec/views/force_auth',
-  '../tests/spec/views/settings',
-  '../tests/spec/views/change_password',
-  '../tests/spec/views/delete_account',
-  '../tests/spec/views/confirm',
-  '../tests/spec/views/tos',
-  '../tests/spec/views/pp',
-  '../tests/spec/views/reset_password',
-  '../tests/spec/views/confirm_reset_password',
-  '../tests/spec/views/complete_reset_password',
-  '../tests/spec/views/ready',
-  '../tests/spec/views/cookies_disabled',
-  '../tests/spec/views/button_progress_indicator'
+  '../tests/setup'
 ],
 function (Translator, Session, FxaClientWrapper) {
   'use strict';
+
+  var tests = [
+    '../tests/spec/lib/channels/web',
+    '../tests/spec/lib/channels/fx-desktop',
+    '../tests/spec/lib/xss',
+    '../tests/spec/lib/url',
+    '../tests/spec/lib/session',
+    '../tests/spec/lib/fxa-client',
+    '../tests/spec/lib/oauth-client',
+    '../tests/spec/lib/assertion',
+    '../tests/spec/lib/translator',
+    '../tests/spec/lib/router',
+    '../tests/spec/lib/strings',
+    '../tests/spec/lib/auth-errors',
+    '../tests/spec/lib/app-start',
+    '../tests/spec/lib/validate',
+    '../tests/spec/lib/service-name',
+    '../tests/spec/lib/metrics',
+    '../tests/spec/lib/null-metrics',
+    '../tests/spec/views/base',
+    '../tests/spec/views/tooltip',
+    '../tests/spec/views/form',
+    '../tests/spec/views/sign_up',
+    '../tests/spec/views/complete_sign_up',
+    '../tests/spec/views/sign_in',
+    // TODO Renable these (issue #1141)
+    //'../tests/spec/views/oauth_sign_in',
+    //'../tests/spec/views/oauth_sign_up',
+    '../tests/spec/views/force_auth',
+    '../tests/spec/views/settings',
+    '../tests/spec/views/change_password',
+    '../tests/spec/views/delete_account',
+    '../tests/spec/views/confirm',
+    '../tests/spec/views/tos',
+    '../tests/spec/views/pp',
+    '../tests/spec/views/reset_password',
+    '../tests/spec/views/confirm_reset_password',
+    '../tests/spec/views/complete_reset_password',
+    '../tests/spec/views/ready',
+    '../tests/spec/views/cookies_disabled',
+    '../tests/spec/views/button_progress_indicator'
+  ];
 
   /*global mocha */
 
   // The translator is expected to be on the window object.
   window.translator = new Translator('en-US', ['en-US']);
 
-  /**
-   * Ensure session state does not pollute other tests
-   */
-  beforeEach(function () {
-    Session.testClear();
-    FxaClientWrapper.testClear();
-  });
+  // Make sure to tests are loaded in proper order using Require.JS
+  var index = 0;
+  var loadTests = function () {
+    var test = tests[index];
+    index += 1;
+    if (index === tests.length) {
+      // run the tests after all of them have loaded
+      require([test], runTests);
+    } else {
+      require([test], loadTests);
+    }
+  };
 
-  afterEach(function () {
-    Session.testClear();
-    FxaClientWrapper.testClear();
-  });
+  var runTests = function () {
+    /**
+     * Ensure session state does not pollute other tests
+     */
+    beforeEach(function () {
+      Session.testClear();
+      FxaClientWrapper.testClear();
+    });
 
-  var runner = mocha.run();
+    afterEach(function () {
+      Session.testClear();
+      FxaClientWrapper.testClear();
+    });
 
-  runner.on('end', function () {
-    // This is our hook to the Selenium tests that run
-    // the mocha tests as part of the CI build.
-    // The selenium test will wait until the #total-failures element exists
-    // and check for "0"
-    var failureEl = document.createElement('div');
-    failureEl.setAttribute('id', 'total-failures');
-    failureEl.innerHTML = runner.failures || '0';
-    document.body.appendChild(failureEl);
-  });
+    var runner = mocha.run();
+
+    runner.on('end', function () {
+      // This is our hook to the Selenium tests that run
+      // the mocha tests as part of the CI build.
+      // The selenium test will wait until the #total-failures element exists
+      // and check for "0"
+      var failureEl = document.createElement('div');
+      failureEl.setAttribute('id', 'total-failures');
+      failureEl.innerHTML = runner.failures || '0';
+      document.body.appendChild(failureEl);
+    });
+  };
+
+  loadTests();
+
 });
-
-
