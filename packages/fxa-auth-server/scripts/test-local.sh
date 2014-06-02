64d659ffd6a8b574d533647b557fed2366f8c39b
John Morrison <jrgmorrison@gmail.com>
2014-06-01 18:56:21 -0700

    remove attempts to run non-existent bin/db_patcher.js

--- packages/fxa-auth-server/scripts/test-local.sh
+++ packages/fxa-auth-server/scripts/test-local.sh
@@ -2,7 +2,6 @@
 
 node ./node_modules/fxa-customs-server/bin/customs_server.js 2>/dev/null &
 PID=$!
-node ./bin/db_patcher.js 2>/dev/null
 ./scripts/gen_keys.js
 ./scripts/tap-coverage.js test/local test/remote
 X=$?
