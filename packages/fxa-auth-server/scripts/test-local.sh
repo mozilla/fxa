51a0386ae582c62939e5c0af7d3c9fa5037f0b7e
Danny Coates <dannycoates@gmail.com>
2014-06-08 18:34:41 -0700

    updated dependencies

--- packages/fxa-auth-server/scripts/test-local.sh
+++ packages/fxa-auth-server/scripts/test-local.sh
@@ -3,7 +3,7 @@
 node ./node_modules/fxa-customs-server/bin/customs_server.js 2>/dev/null &
 PID=$!
 ./scripts/gen_keys.js
-./scripts/tap-coverage.js test/local test/remote
+./scripts/tap-coverage.js test/local test/remote 2>/dev/null
 X=$?
 kill $PID
 exit $X
