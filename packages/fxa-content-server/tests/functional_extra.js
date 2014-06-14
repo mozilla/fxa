efe5ed59cdebb042d7da503aad3425cc75f4366f
vladikoff <vlad.filippov@gmail.com>
2014-06-13 19:46:20 -0700

    chore(tests): split the functional tests

---
+++ packages/fxa-content-server/tests/functional_extra.js
@@ -0,0 +1,11 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+define([
+  './functional/oauth_sign_in',
+  './functional/oauth_sign_up',
+  './functional/oauth_reset_password'
+], function () {
+  'use strict';
+});
--- packages/fxa-content-server/tests/intern_sauce.js
+++ packages/fxa-content-server/tests/intern_sauce.js
@@ -10,7 +10,7 @@ define([
   // override the main config file and adjust it to suit Sauce Labs
   intern.useSauceConnect = true;
   intern.webdriver.port = 4445;
-  intern.functionalSuites = [ 'tests/functional' ];
+  intern.functionalSuites = [ 'tests/functional', 'tests/functional_extra' ];
   intern.environments = [
     { browserName: 'firefox', version: '29', platform: 'Windows 7' }
   ];
