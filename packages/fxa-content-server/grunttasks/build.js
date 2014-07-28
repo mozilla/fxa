aea8f850f261f67bb9f8c163b607cd708cb85573
vladikoff <vlad.filippov@gmail.com>
2014-07-28 12:56:04 -0700

    chore(build): Add 'build' description

--- packages/fxa-content-server/grunttasks/build.js
+++ packages/fxa-content-server/grunttasks/build.js
@@ -6,26 +6,57 @@ module.exports = function (grunt) {
   'use strict';
 
   grunt.registerTask('build', [
-    // Clean everything
+    // Clean files and folders from any previous build
     'clean',
+
+    // Select 'dist' configuration files for the running environment.
     'selectconfig:dist',
+
     // l10n-generate-pages needs to be run before useminPrepare to seed
     // the list of resources to minimize. Generated pages are placed into
     // `server/templates/pages/dist` where they will be post-processed
     // with requirejs and usemin
     'l10n-generate-pages',
+
     // use error pages from en_US as the static error pages
     'copy:error_pages',
+
+    // prepares the configuration to transform specific blocks
+    // in the scrutinized file into a single line, targeting an optimized version of the files.
     'useminPrepare',
+
+    // Runs r.js optimizer on the application files
     'requirejs',
+
+    // general 'css' tasks:
+    //    'sass', - compile SASS,
+    //    'autoprefixer' - auto prefix CSS for many browsers,
+    //    'connect_fonts' - generate CSS files for connect-fonts compatible font packs.
     'css',
+
+    // 'copy:styles',
+    //  'connect_fonts_copy', - copy the generated connect fonts
+    //  'imagemin', - optimize image files
+    //  'htmlmin' - minify HTML
     'concurrent:dist',
+
+    // concatenate files as part of the useminPrepare task
     'concat',
+
+    // compress CSS files
     'cssmin',
+
+    // copy all static resources from 'app' to 'dist'
     'copy:dist',
+
     // uglify overwrites the files in the dist directory.
     'uglify',
+
+    // static file asset revisioning through content hashing
     'rev',
+
+    // replaces the blocks by the file they reference,
+    // and replaces all references to assets by their revisioned version if it is found on the disk
     'usemin'
   ]);
 };
