dafae6333310a92ce0f2d504b22f160080380345
Danny Coates <dannycoates@gmail.com>
2014-07-28 11:17:07 -0700

    added fxa-auth-db-mem to start-local. fixes mozilla/fxa-auth-server#772

--- packages/fxa-auth-server/scripts/start-local.sh
+++ packages/fxa-auth-server/scripts/start-local.sh
@@ -2,7 +2,10 @@
 node ./scripts/gen_keys.js
 node ./test/mail_helper.js &
 MH=$!
+node ./node_modules/fxa-auth-db-mem &
+DB=$!
 
 node ./bin/key_server.js | node ./bin/notifier.js >/dev/null
 
 kill $MH
+kill $DB
