0cabe272c1376e2690bb68f77e3067e377a8fec0
Sean McArthur <sean.monstar@gmail.com>
2014-08-14 09:27:23 -0700

    add worker bin to run_dev.js

--- packages/fxa-profile-server/scripts/run_dev.js
+++ packages/fxa-profile-server/scripts/run_dev.js
@@ -7,11 +7,22 @@
 const cp = require('child_process');
 const path = require('path');
 
+const mkdirp = require('mkdirp');
+const rimraf = require('rimraf');
+
 process.env.NODE_ENV = 'dev';
 
 var child = cp.fork(path.join(__dirname, '..', 'bin', 'server.js'));
 child.on('exit', process.exit);
 
-
-child = cp.fork(path.join(__dirname, '..', 'bin', 'static.js'));
+child = cp.fork(path.join(__dirname, '..', 'bin', 'worker.js'));
 child.on('exit', process.exit);
+
+
+mkdirp.sync(path.join(__dirname, '..', 'var', 'public'));
+child = cp.fork(path.join(__dirname, '..', 'bin', '_static.js'));
+child.on('exit', process.exit);
+
+process.on('exit', function() {
+  rimraf.sync(path.join(__dirname, '..', 'var', 'public'));
+});
