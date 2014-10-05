61e216112703f836faa0692528623ad59355309f
vladikoff <vlad.filippov@gmail.com>
2014-10-04 23:24:24 -0400

    feat(app): Add sample app, update travis file

---
+++ packages/fxa-oauth-console/app/router.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+import config from './config/environment';
+
+var Router = Ember.Router.extend({
+  location: config.locationType
+});
+
+Router.map(function() {
+  this.resource("server", function() {
+    this.route('view', { path: '/:server_id' });
+  });
+
+});
+
+export default Router;
