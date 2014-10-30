811c64ef8085f61fd3f465ae80b3651229ab717e
Vlad Filippov <vlad.filippov@gmail.com>
2014-10-30 16:52:24 -0400

    fix(build): remove post install script

--- packages/fxa-oauth-console/scripts/postinstall.sh
+++
@@ -1,7 +0,0 @@
-node_modules/.bin/bower install --config.interactive=false -s
-if [ "$NODE_ENV" = "staging" ] ; then
-  node_modules/.bin/ember build --environment=production
-fi;
-if [ "$NODE_ENV" = "production" ] ; then
-  node_modules/.bin/ember build --environment=production
-fi;
