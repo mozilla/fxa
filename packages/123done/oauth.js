202ce185438e447658b6e7357cdf131fe91e0e31
Shane Tomlinson <stomlinson@mozilla.com>
2015-01-09 13:08:24 +0000

    fix: Update fxa-relier-client to redirect off of the oauth server.
    The relier client needs to know both the oauth and content server hosts.
    
    The newest fxa-relier-client bounces off the oauth server instead of going directly to the content server. To handle this, we have to pass both the content server URI and oauth server URI to the relier-client on startup.

--- packages/123done/oauth.js
+++ packages/123done/oauth.js
@@ -24,6 +24,7 @@ function getOAuthInfo(action, nonce, email, preVerifyToken) {
     state: nonce,
     scope: config.scopes,
     action: action,
+    auth_uri: config.auth_uri,
     content_uri: config.content_uri
   };
 
