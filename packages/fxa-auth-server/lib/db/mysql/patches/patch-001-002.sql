56ef7dfc7b585faa090880035113f645a907c653
Ryan Kelly <rfkelly@mozilla.com>
2015-01-29 14:22:03 +1100

    feat(db): add basic migration infrastructure to mysql backend
    This adds support for mysql-patcher to perform database migrations,
    and will automatically patch the database if the `createSchema` option
    is true.
    
    fixes mozilla/fxa-auth-server#183

--- packages/fxa-auth-server/lib/db/mysql/patches/patch-001-002.sql
+++ packages/fxa-auth-server/lib/db/mysql/patches/patch-001-002.sql
@@ -1,3 +1,10 @@
+--  Create the initial set of tables.
+--
+--  Since this is the first migration, we use `IF NOT EXISTS` to allow us
+--  to run this on a db that already has the original schema in place. The
+--  patch will then be a no-op.  Subsequent patches should *not* use `IF
+--  NOT EXISTS` but should fail noisily if the db is in an unexpected state.
+
 CREATE TABLE IF NOT EXISTS clients (
   id BINARY(8) PRIMARY KEY,
   secret BINARY(32) NOT NULL,
@@ -33,3 +40,6 @@ CREATE TABLE IF NOT EXISTS tokens (
   scope VARCHAR(256) NOT NULL,
   createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+UPDATE dbMetadata SET value = '2' WHERE name = 'schema-patch-level';
+
