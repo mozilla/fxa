3ce47d2415ab0a766a1add85fc3a4ddfdb4cccf5
Vlad Filippov <vlad.filippov@gmail.com>
2015-02-01 19:31:36 -0500

    enhance update script

--- _scripts/update_all.sh
+++ _scripts/update_all.sh
@@ -2,14 +2,14 @@
 
 # Update all projects
 
-(cd fxa-content-server && git checkout master && git pull origin master && npm i && cd ..)
-cd fxa-auth-server && git checkout master && git pull origin master && npm i && cd ..
+(cd fxa-content-server && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-content-server update failed"
+(cd fxa-auth-server && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-auth-server update failed"
 
-cd browserid-verifier && git checkout master && git pull origin master && npm i && npm i vladikoff/browserid-local-verify#http && cd ..
-cd fxa-oauth-server && git checkout master && git pull origin master && npm i && cd ..
-cd fxa-oauth-console && git checkout master && git pull origin master && npm i && cd ..
+(cd browserid-verifier && git checkout master && git pull origin master && npm i && npm i vladikoff/browserid-local-verify#http && cd ..)|| echo "browserid update failed"
+(cd fxa-oauth-server && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-oauth-server update failed"
+(cd fxa-oauth-console && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-oauth-console update failed"
 
-cd fxa-profile-server && git checkout master && git pull origin master && npm i && cd ..
+(cd fxa-profile-server && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-profile-server update failed"
 
 (cd 123done && git checkout master && git pull origin master && npm i && cd ..) || echo "123done update failed"
 (cd loop-server && git checkout master && git pull origin master && npm i && cd ..) || echo "Loop update failed"
