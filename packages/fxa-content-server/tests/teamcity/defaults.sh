393428e41b6fba556d3412a4bf6ef9c420099d36
John Morrison <jrgmorrison@gmail.com>
2015-02-27 11:49:40 -0800

    add MPL

--- packages/fxa-content-server/tests/teamcity/defaults.sh
+++ packages/fxa-content-server/tests/teamcity/defaults.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 FXA_CONTENT_ROOT="https://latest.dev.lcip.org/"
 FXA_AUTH_ROOT="https://latest.dev.lcip.org/auth/v1"
