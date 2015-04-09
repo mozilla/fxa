565d4468e2f997ac901b9922b4b2bb6761afd2de
Vlad Filippov <vlad.filippov@gmail.com>
2015-04-08 20:43:18 -0400

    improve content server install

--- _scripts/install_all.sh
+++ _scripts/install_all.sh
@@ -21,7 +21,8 @@ wait
 
 # Install and Setup all the projects
 
-cd fxa-content-server && npm i && cp server/config/local.json-dist server/config/local.json && cd ..
+
+cd fxa-content-server && npm i --production && npm i && cp server/config/local.json-dist server/config/local.json && cd ..
 
 cd fxa-auth-server && npm i && node ./scripts/gen_keys.js && cd ..
 # Install a custom http only verifier
