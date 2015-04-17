b86fba0570d1f505cd95e27b48f59d0a1a50457c
John Morrison <jrgmorrison@gmail.com>
2015-04-16 17:23:19 -0700

    chore(travis): quiet validate-shrinkwrap failure on security warning on module

--- packages/fxa-customs-server/scripts/test-local.sh
+++ packages/fxa-customs-server/scripts/test-local.sh
@@ -4,5 +4,5 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-node_modules/.bin/grunt || exit 1
+node_modules/.bin/grunt lint copyright || exit 1
 node scripts/tap-coverage.js test/local test/remote || exit 1
