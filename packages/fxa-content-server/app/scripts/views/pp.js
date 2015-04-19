13d6256f566012072dda052de290caa087bb085e
Shane Tomlinson <stomlinson@mozilla.com>
2015-04-19 11:00:23 +0100

    refactor(client): Add a common base for tos/pp copy modules.
    LegalCopy is now the common base for the TOS/PP views. Subclasses must provide `copyUrl` and `fetchError`.
    
    This module is to eliminate duplication across the two TOS/PP views, as well as to get ready for a common place to perform string conversion.

--- packages/fxa-content-server/app/scripts/views/pp.js
+++ packages/fxa-content-server/app/scripts/views/pp.js
@@ -5,62 +5,22 @@
 'use strict';
 
 define([
-  'cocktail',
-  'lib/xhr',
-  'views/base',
+  'views/legal_copy',
   'stache!templates/pp',
-  'lib/auth-errors',
-  'views/mixins/back-mixin'
+  'lib/auth-errors'
 ],
-function (Cocktail, xhr, BaseView, Template, AuthErrors, BackMixin) {
-  var View = BaseView.extend({
+function (LegalCopyView, Template, AuthErrors) {
+  var View = LegalCopyView.extend({
     template: Template,
     className: 'pp',
-
-    afterRender: function () {
-      var self = this;
-      return xhr.ajax({
-        url: '/legal/privacy',
-        accepts: {
-          text: 'text/partial'
-        },
-        dataType: 'text'
-      })
-      .then(function (template) {
-        self.$('#legal-copy').html(template);
-        self.$('.hidden').removeClass('hidden');
-
-        // Set a session cookie that informs the server
-        // the user can go back if they refresh the page.
-        // If the user can go back, the browser will not
-        // render the statically generated TOS/PP page,
-        // but will let the app render the page.
-        // The cookie is cleared whenever the user
-        // restarts the browser. See #2044
-        //
-        // The cookie is scoped to the page to avoid sending
-        // it on other requests, and to ensure the server
-        // only sends the user back to the app if the user in fact
-        // came from this page.
-        self.window.document.cookie = 'canGoBack=1; path=' + self.window.location.pathname;
-      })
-      .fail(function () {
-        var err = AuthErrors.toError('COULD_NOT_GET_PP');
-        self.displayError(err);
-        self.$('.hidden').removeClass('hidden');
-      });
-    },
+    copyUrl: '/legal/privacy',
+    fetchError: AuthErrors.toError('COULD_NOT_GET_PP'),
 
     events: {
       'click #fxa-pp-back': 'back',
       'keyup #fxa-pp-back': 'backOnEnter'
     }
   });
-
-  Cocktail.mixin(
-    View,
-    BackMixin
-  );
 
   return View;
 });
