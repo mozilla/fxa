4feb60ec6954aef6df687a699d4d997533833263
vladikoff <vlad.filippov@gmail.com>
2015-05-13 13:30:02 -0400

    fix(tests): add untrusted app to TeamCity configs

--- packages/fxa-content-server/tests/teamcity/defaults.sh
+++ packages/fxa-content-server/tests/teamcity/defaults.sh
@@ -5,5 +5,6 @@
 FXA_CONTENT_ROOT="https://latest.dev.lcip.org/"
 FXA_AUTH_ROOT="https://latest.dev.lcip.org/auth/v1"
 FXA_OAUTH_APP_ROOT="https://123done-latest.dev.lcip.org/"
+FXA_UNTRUSTED_OAUTH_APP_ROOT="https://321done-latest.dev.lcip.org/"
 FXA_FIREFOX_BINARY="/usr/bin/firefox"
 
