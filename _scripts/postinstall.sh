96165d4a2c96a92cd8ceec3ca8a132c7690abac5
Danny Coates <dannycoates@gmail.com>
2015-06-03 19:18:02 -0700

    halt initial install on first error

--- _scripts/postinstall.sh
+++ _scripts/postinstall.sh
@@ -1,3 +1,5 @@
-sh _scripts/install_all.sh
+#!/bin/bash -ex
+
+_scripts/install_all.sh
 pm2 delete servers.json && pm2 start servers.json
-echo "Use 'pm2 kill' to stop all the servers"
+echo "Use './pm2 kill' to stop all the servers"
