562f50c1aa2d15ff383da41e44a70b62e7b63512
vladikoff <vlad.filippov@gmail.com>
2015-07-09 10:39:20 -0400

    chore(tests): add circle ci test list

---
+++ packages/fxa-content-server/tests/intern_functional_circle.js
@@ -0,0 +1,34 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+define([
+  './intern'
+], function (intern) {
+
+  intern.functionalSuites = [
+    'tests/functional/sign_in',
+    'tests/functional/sign_in_cached',
+    'tests/functional/sync_sign_in',
+    'tests/functional/sign_up',
+    'tests/functional/complete_sign_up',
+    'tests/functional/sync_sign_up',
+    'tests/functional/sync_v2_sign_up',
+    'tests/functional/bounced_email',
+    'tests/functional/confirm',
+    'tests/functional/delete_account',
+    'tests/functional/reset_password',
+    'tests/functional/sync_reset_password',
+    'tests/functional/robots_txt',
+    'tests/functional/settings',
+    'tests/functional/settings_common',
+    'tests/functional/sync_settings',
+    'tests/functional/change_password',
+    'tests/functional/force_auth',
+    'tests/functional/back_button_after_start',
+    'tests/functional/cookies_disabled',
+    'tests/functional/password_visibility'
+  ];
+
+  return intern;
+});
