c8a2e0c345e0e7e8b109d1c090e8fd8972cf2ffb
edmoz <edwong@mozilla.com>
2015-07-10 14:30:55 -0700

    add/fix: multiple repos, pagination support, file_name flag

--- scripts/git_to_csv.py
+++ scripts/git_to_csv.py
@@ -1,43 +1,76 @@
 #!/usr/bin/env python
 
 """
-Exports Issues from a specified repository to a CSV file
+Exports github issues from a repositories to a CSV file with specific labels
 
 Uses basic authentication (Github username + password) to retrieve Issues
 from a repository that username has access to. Supports Github API v3.
 
-creates a csv file with the same name as repo.
+Please set GIT_API_USER, GIT_API_KEY environment variables. You need a github api token.
 """
+
 import argparse
 import csv
 import requests
 import os
-import sys
 
 
 GITHUB_USER = os.environ['GIT_API_USER']
 GITHUB_PASSWORD = os.environ['GIT_API_KEY']
 labels_filter = ['waffle:in progress', 'waffle:ready', 'waffle:in review']
 AUTH = (GITHUB_USER, GITHUB_PASSWORD)
+repo = ""
+
+# auth-server
+# repos_default = ["mozilla/fxa-auth-db-mysql",
+#                  "mozilla/fxa-auth-db-server",
+#                  "mozilla/fxa-auth-mailer",
+#                  "mozilla/fxa-auth-server",
+#                  "mozilla/fxa-customs-server",
+#                  "mozilla/fxa-dev",
+#                  "mozilla/fxa-jwtool",
+#                  "mozilla/fxa-notification-server",
+#                  "mozilla/fxa-oauth-client",
+#                  "mozilla/fxa-oauth-server",
+#                  "mozilla/fxa-profile-server",
+#                  "mozilla/hapi-fxa-oauth"]
+
+#content-server
+repos_default = ["mozilla/fxa-auth-mailer",
+                 "mozilla/fxa-content-experiments",
+                 "mozilla/fxa-content-server",
+                 "mozilla/fxa-js-client",
+                 "mozilla/fxa-oauth-console",
+                 "mozilla/fxa-relier-client"]
+
+def is_ascii(s):
+    if all(ord(c) < 128 for c in s):
+        return s
+    else:
+        return "hearts"
 
 def write_issues(resp):
     "output a list of issues to csv"
     if not resp.status_code == 200:
         raise Exception(resp.status_code)
+
     for issue in resp.json():
         labels = issue['labels']
-        for label in labels:
-            if label['name'] in labels_filter:
-                milestone = issue['milestone']['title'] if issue['milestone'] is not None else ""
+        label_list = [is_ascii(label['name']) for label in labels]
+        # print label_list
 
-                csvout.writerow([issue['number'],
-                                 issue['title'].encode('utf-8'),
-                                 issue['created_at'],
-                                 issue['updated_at'],
-                                 issue['state'],
-                                 issue['user']['login'],
-                                 milestone,
-                                 label['name']])
+        # check if label filter is in labels of an issue
+        if [i for i in labels_filter if i in label_list]:
+            milestone = issue['milestone']['title'] if issue['milestone'] is not None else ""
+            csvout.writerow([repo,
+                             issue['number'],
+                             issue['title'].encode('utf-8'),
+                             issue['created_at'],
+                             issue['updated_at'],
+                             issue['state'],
+                             issue['user']['login'],
+                             milestone,
+                             ','.join(label_list)])
 
 def fetch_page(url):
     r = requests.get(url, auth=AUTH)
@@ -51,29 +84,41 @@ def fetch_page(url):
                 [link.split(';') for link in
                     r.headers['link'].split(',')]])
 
-        while 'last' in pages and 'next' in pages:
-            print pages['next'], pages['last']
-            if pages['next'] == pages['last']:
-                sys.exit()
+        if 'last' not in pages:
+            return
 
-            fetch_page(pages['next'])
+        fetch_page(pages['next'])
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--repo", help = "repo name")
+    parser.add_argument("-r", "--repos", nargs='+', help = "list of repos")
     parser.add_argument("-s", "--status",  help = "status open|closed")
     parser.add_argument("-l", "--labels", nargs='+', help = "space delimited list of label strings")
+    parser.add_argument("-f", "--file_name", help = "combine all data in a single file otherwise per repo files")
     args = parser.parse_args()
 
-    repo = args.repo if args.repo is not None else 'mozilla/fxa-auth-server'
+    repos = args.repos if args.repos is not None else repos_default
     status = args.status if args.status is not None else 'open'
+
     if args.labels is not None:
         labels_filter = args.labels
+        print labels_filter
 
-    csvfile = '%s-issues.csv' % (repo.replace('/', '-'))
-    csvout = csv.writer(open(csvfile, 'wb'))
-    csvout.writerow(('ID', 'Title', 'Created At', 'Updated At', 'State', 'Assignee', 'Milestone', 'Waffle_state'))
-    print repo, status, labels_filter
+    if args.file_name:
+        csvfile = args.file_name
+        csvout = csv.writer(open(csvfile, 'a'))
+        print 'created file:', csvfile
+        csvout.writerow(('Repo', 'ID', 'Title', 'Created At', 'Updated At', 'State', 'Assignee', 'Milestone', 'Labels'))
 
-    issues_url = 'https://api.github.com/repos/%s/issues?state=%s' % (repo, status)
-    fetch_page(issues_url)
+    print ':: filtering on labels and status:', labels_filter, status
+
+    for repo in repos:
+        print 'repo:', repo
+        if args.file_name is None:
+            csvfile = '%s-issues.csv' % (repo.replace('/', '-'))
+            csvout = csv.writer(open(csvfile, 'w'))
+            print 'created file:', csvfile
+            csvout.writerow(('Repo', 'ID', 'Title', 'Created At', 'Updated At', 'State', 'Assignee', 'Milestone', 'Labels'))
+
+        issues_url = 'https://api.github.com/repos/%s/issues?state=%s' % (repo, status)
+        fetch_page(issues_url)
