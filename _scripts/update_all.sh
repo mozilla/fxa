212a45a942db938405c5da031db0fc3fa920eac9
Vlad Filippov <vlad.filippov@gmail.com>
2015-07-17 19:45:33 -0700

    Update update_all.sh

--- _scripts/update_all.sh
+++ _scripts/update_all.sh
@@ -5,7 +5,7 @@
 (cd fxa-content-server && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-content-server update failed"
 (cd fxa-auth-server && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-auth-server update failed"
 
-(cd browserid-verifier && git checkout master && git pull origin master && npm i && npm i vladikoff/browserid-local-verify#http && cd ..)|| echo "browserid update failed"
+(cd browserid-verifier && git checkout http && git pull origin http && npm i && npm i vladikoff/browserid-local-verify#http && cd ..)|| echo "browserid update failed"
 (cd fxa-oauth-server && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-oauth-server update failed"
 (cd fxa-oauth-console && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-oauth-console update failed"
 
