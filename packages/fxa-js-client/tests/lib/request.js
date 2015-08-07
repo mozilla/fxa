837b0b6c87d84cc29c5e9003e5b29c47527dbf9e
Edouard Oger <edouard.oger@gmail.com>
2015-08-07 11:31:12 -0700

    fix(request): Return rejected promise immediately if xhr.open() fails
    Fixes mozilla/fxa-js-client#174

--- packages/fxa-js-client/tests/lib/request.js
+++ packages/fxa-js-client/tests/lib/request.js
@@ -3,12 +3,13 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 define([
+  'tests/addons/sinon',
   'intern!tdd',
   'intern/chai!assert',
   'tests/addons/environment',
   'client/lib/request',
   'tests/mocks/errors'
-], function (tdd, assert, Environment, Request, ErrorMocks) {
+], function (sinon, tdd, assert, Environment, Request, ErrorMocks) {
   with (tdd) {
     suite('request module', function () {
       var RequestMocks;
@@ -71,6 +72,20 @@ define([
             assert.equal(err.error, 'Unknown error');
           }
         );
+      });
+
+      test('#ensure is usable', function () {
+        request = new Request('http://google.com:81', env.xhr, { timeout: 200 });
+        sinon.stub(env.xhr.prototype, 'open').throws();
+
+        return env.respond(request.send('/__heartbeat__', 'GET'), RequestMocks.heartbeat)
+          .then(
+            null,
+            function (err) {
+              assert.ok(err);
+              env.xhr.prototype.open.restore();
+            }
+          );
       });
     });
   }
