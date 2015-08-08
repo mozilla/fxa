a299d1959f70b24fd45a3769ba6b6dff42ab4633
John Morrison <jrgmorrison@gmail.com>
2015-08-07 19:10:25 -0700

    chore(version): refactor; add __version__ route

--- packages/fxa-auth-server/lib/routes/defaults.js
+++ packages/fxa-auth-server/lib/routes/defaults.js
@@ -12,51 +12,58 @@ var commitHash
 
 module.exports = function (log, P, db, error) {
 
+  function versionHandler(request, reply) {
+    log.begin('Defaults.root', request)
+
+    function sendReply() {
+      reply(
+        {
+          version: version,
+          commit: commitHash
+        }
+      )
+    }
+
+    // if we already have the commitHash, send the reply and return
+    if (commitHash) {
+      return sendReply()
+    }
+
+    // Note: we figure out the Git hash in the following order:
+    //
+    // (1) read config/version.json if exists (ie. staging, production)
+    // (2) figure it out from git (either regular '.git', or '/home/app/git' for AwsBox)
+
+    // (1) read config/version.json if exists (ie. staging, production)
+    var configFile = path.join(__dirname, '..', '..', 'config', 'version.json')
+    if ( fs.existsSync(configFile) ) {
+      commitHash = require(configFile).version.hash
+      return sendReply()
+    }
+
+    // (2) figure it out from git (either regular '.git', or '/home/app/git' for AwsBox)
+    var gitDir
+    if ( !fs.existsSync(path.join(__dirname, '..', '..', '.git')) ) {
+      // try at '/home/app/git' for AwsBox deploys
+      gitDir = path.sep + path.join('home', 'app', 'git')
+    }
+    var cmd = util.format('git %s rev-parse HEAD', gitDir ? '--git-dir=' + gitDir : '')
+    child_process.exec(cmd, function(err, stdout) {
+      commitHash = stdout.replace(/\s+/, '')
+      return sendReply()
+    })
+  }
+
   var routes = [
     {
       method: 'GET',
       path: '/',
-      handler: function index(request, reply) {
-        log.begin('Defaults.root', request)
-
-        function sendReply() {
-          reply(
-            {
-              version: version,
-              commit: commitHash
-            }
-          )
-        }
-
-        // if we already have the commitHash, send the reply and return
-        if (commitHash) {
-          return sendReply()
-        }
-
-        // Note: we figure out the Git hash in the following order:
-        //
-        // (1) read config/version.json if exists (ie. staging, production)
-        // (2) figure it out from git (either regular '.git', or '/home/app/git' for AwsBox)
-
-        // (1) read config/version.json if exists (ie. staging, production)
-        var configFile = path.join(__dirname, '..', '..', 'config', 'version.json')
-        if ( fs.existsSync(configFile) ) {
-          commitHash = require(configFile).version.hash
-          return sendReply()
-        }
-
-        // (2) figure it out from git (either regular '.git', or '/home/app/git' for AwsBox)
-        var gitDir
-        if ( !fs.existsSync(path.join(__dirname, '..', '..', '.git')) ) {
-          // try at '/home/app/git' for AwsBox deploys
-          gitDir = path.sep + path.join('home', 'app', 'git')
-        }
-        var cmd = util.format('git %s rev-parse HEAD', gitDir ? '--git-dir=' + gitDir : '')
-        child_process.exec(cmd, function(err, stdout) {
-          commitHash = stdout.replace(/\s+/, '')
-          return sendReply()
-        })
-      }
+      handler: versionHandler
+    },
+    {
+      method: 'GET',
+      path: '/__version__',
+      handler: versionHandler
     },
     {
       method: 'GET',
