1b00f9cfb2c75e9dc01f511f6e21f1e4b8cb76b4
edmoz <edwong@mozilla.com>
2015-08-21 13:25:24 -0700

    add git count script

--- scripts/git_counts.py
+++ scripts/git_counts.py
@@ -16,21 +16,36 @@ import os
 from datetime import datetime, timedelta
 from pprint import pprint
 
-
 GITHUB_USER = os.environ['GIT_API_USER']
 GITHUB_PASSWORD = os.environ['GIT_API_KEY']
 DAYS_PRIOR_DEFAULT = 90
+SHORT_DAYS_PRIOR = 1
+LONG_DAYS_PRIOR = 14
 ROW_HEADERS = ('Repo', 'ID', 'Title', 'Created At', 'Updated At', 'Closed At', 'State', 'Reporter', 'Assignee', 'Milestone', 'Labels')
-labels_filter = ['waffle:progress', 'waffle:now', 'waffle:review']
+labels_filter = ['waffle:progress', 'waffle:now', 'waffle:review', 'waffle:next']
 AUTH = (GITHUB_USER, GITHUB_PASSWORD)
 repo = ""
 milestone_filter = ""
 days_prior = DAYS_PRIOR_DEFAULT
 model = []
 
-now = datetime.now() - timedelta(days=days_prior)
-start_date = "%d-%d-%dT01:01:01Z" % (now.year, now.month, now.day)
+
+now = datetime.now()
+search_date = now - timedelta(days=days_prior)
+start_date = "%d-%d-%dT01:01:01Z" % (search_date.year, search_date.month, search_date.day)
 print 'searching %s days back, since %s' % (days_prior, start_date)
+
+one_day_prior = now - timedelta(days=SHORT_DAYS_PRIOR)
+week_prior = now - timedelta(days=LONG_DAYS_PRIOR)
+
+closed_this_week = []
+closed_yesterday = []
+opened_this_week = []
+opened_yesterday = []
+waffle_now_this_week = []
+waffle_now_yesterday = []
+waffle_review_this_week = []
+waffle_review_yesterday = []
 
 repos_batch = ['all_auth', 'all_content']
 # auth-server
@@ -55,6 +70,7 @@ all_content = ["mozilla/fxa-auth-mailer",
                "mozilla/fxa-oauth-console",
                "mozilla/fxa-relier-client"]
 
+all_repos = all_auth.extend(all_content)
 repos_default = all_auth
 
 def is_ascii(s):
@@ -63,9 +79,15 @@ def is_ascii(s):
     else:
         return "hearts"
 
-def _short_date(the_date):
+def _format_date(the_date):
     return datetime.strptime(the_date, "%Y-%m-%dT%H:%M:%SZ")
     # return the_date.split('T')[0]
+
+def _print_issues(issues, _str, _days):
+    print '%s last %s days' % (_str, _days)
+    print 'COUNT:', len(issues)
+    for issue in issues:
+        print '%s, %s' % (issue['url'], issue['title'].encode('utf-8'))
 
 def write_issues(resp):
     "output a list of issues to csv"
@@ -94,8 +116,8 @@ def write_issues(resp):
             milestone = issue['milestone']['title'] if issue['milestone'] is not None else ""
 
         #closed_at default
-        closed_at = _short_date(issue['closed_at']) if issue['closed_at'] is not None else ""
-        updated_at = _short_date(issue['updated_at']) if issue['updated_at'] is not None else ""
+        closed_at = _format_date(issue['closed_at']) if issue['closed_at'] is not None else ""
+        updated_at = _format_date(issue['updated_at']) if issue['updated_at'] is not None else ""
 
         #assignee default
         assignee = issue['assignee']['login'] if issue['assignee'] is not None else ""
@@ -109,23 +131,38 @@ def write_issues(resp):
             if not [i for i in labels_filter if i in label_list]:
                 continue
 
-        model.append({'created_at': _short_date(issue['created_at']),
-                      'update_at': updated_at,
-                      'closed_at': closed_at,
-                      'issue': 'https://github.com/%s/issues/%s' % (repo, issue['number'])})
+        # parse events for waffle label additions
+        if issue['events_url']:
+            events = requests.get(issue['events_url'], auth=AUTH).json()
+            for event in events:
+                # PUSH Waffle events into list
+                if event['event'] == 'labeled' and event['label']['name'] == 'waffle:now':
+                    if _format_date(event['created_at']) > one_day_prior:
+                        waffle_now_yesterday.append(issue)
+                    if _format_date(event['created_at']) > week_prior:
+                        waffle_now_this_week.append(issue)
 
-        # write to csv
-        # csvout.writerow([repo,
-        #                 issue['number'],
-        #                 issue['title'].encode('utf-8'),
-        #                 issue['created_at'],
-        #                 issue['updated_at'],
-        #                 closed_at,
-        #                 issue['state'],
-        #                 issue['user']['login'],
-        #                 assignee,
-        #                 milestone,
-        #                 ','.join(label_list)])
+                if event['event'] == 'labeled' and event['label']['name'] == 'waffle:review':
+                    if _format_date(event['created_at']) > one_day_prior:
+                        waffle_review_yesterday.append(issue)
+                    if _format_date(event['created_at']) > week_prior:
+                        waffle_review_this_week.append(issue)
+
+
+        ##### PUSH create and closed into list
+        create_date = _format_date(issue['created_at'])
+        if create_date > one_day_prior:
+            opened_yesterday.append(issue)
+
+        if create_date > week_prior:
+            opened_this_week.append(issue)
+
+        if closed_at:
+            if closed_at > one_day_prior:
+                closed_yesterday.append(issue)
+
+            if closed_at > week_prior:
+                closed_this_week.append(issue)
 
 def fetch_page(url):
     params = {} if start_date is not 0 else {"since":start_date}
@@ -192,7 +229,16 @@ if __name__ == '__main__':
 
         issues_url = 'https://api.github.com/repos/%s/issues?state=%s' % (repo, status)
         fetch_page(issues_url)
-        pprint (model)
+        # pprint (model)
         newlist = sorted(model, key=lambda k: k['created_at'])
-        pprint(newlist)
-        print len(model)
+        _print_issues(opened_yesterday, 'OPENED', SHORT_DAYS_PRIOR)
+        _print_issues(opened_this_week, 'OPENED', LONG_DAYS_PRIOR)
+
+        _print_issues(closed_yesterday, 'CLOSED', SHORT_DAYS_PRIOR)
+        _print_issues(closed_this_week, 'CLOSED', LONG_DAYS_PRIOR)
+
+        _print_issues(waffle_now_this_week, 'WAFFLE:NOW', SHORT_DAYS_PRIOR)
+        _print_issues(waffle_now_yesterday, 'WAFFLE:NOW', LONG_DAYS_PRIOR)
+
+        _print_issues(waffle_review_yesterday, 'WAFFLE:REVIEW', SHORT_DAYS_PRIOR)
+        _print_issues(waffle_review_this_week, 'WAFFLE:REVIEW', LONG_DAYS_PRIOR)
