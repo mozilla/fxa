e4f99ce007a8a63227db2285fd89adfff8410bf0
John Morrison <jrgmorrison@gmail.com>
2015-09-01 16:55:54 -0700

    fix(tests): improved script to checking email of all supported locales

--- packages/fxa-auth-server/scripts/e2e-email/index.js
+++ packages/fxa-auth-server/scripts/e2e-email/index.js
@@ -7,54 +7,55 @@
 // auth-server, since `/v1/account/lock` is, of course, not available
 // there.
 
-const assert = require('assert')
 const crypto = require('crypto')
-const url = require('url')
+const commander = require('commander')
 
 const P = require('../../lib/promise')
-const preq = require('./p-request')
-const localeQuirks = require('./localeQuirks')
+const Client = require('../../test/client')
+const mailbox = require('../../test/mailbox')
+const validateEmail = require('./validate-email')
 
+const emailMessages = {}
 var program
-try {
-  program = require('commander')
-} catch(e) {
-  console.log('This program requires you to do `npm install commander` first.')
-  process.exit(1)
+
+function configure() {
+  commander
+    .option('-a, --auth-server [url]',
+            'URL of FxA Auth Server',
+            'https://api-accounts.stage.mozaws.net')
+    .option('-l, --locales [path]',
+            'Path to file with a list of locales to test',
+            '../../config/supportedLanguages.js')
+    .option('-r, --restmail-domain [fqdn]',
+            'URL of the restmail server',
+            'restmail.net')
+    .option('-L, --locale <en[,zh-TW,de,...]>',
+            'Test only this csv list of locales',
+            function(list) {
+              return list.split(/,/)
+            })
+    .parse(process.argv)
+
+  commander.basename = crypto.randomBytes(8).toString('hex')
+  commander.password = crypto.randomBytes(16).toString('hex')
+
+  commander.supportedLanguages = commander.locale ||
+    require(commander.locales).slice(0)
+
+  var mailserver = commander.mailserver = mailbox(commander.restmailDomain, 80)
+
+  mailserver.eventEmitter.on('email:message', function(email, message) {
+    emailMessages[email] = emailMessages[email] || []
+    emailMessages[email].push(message)
+  })
+
+  mailserver.eventEmitter.on('email:error', function(email, error) {
+    emailMessages[email] = emailMessages[email] || []
+    emailMessages[email].push(error)
+  })
+
+  return commander
 }
-
-program
-  .option('-a, --auth-server [url]',
-          'URL of FxA Auth Server',
-          'https://api-accounts.stage.mozaws.net')
-  .option('-r, --restmail-domain [fqdn]',
-          'URL of the restmail server',
-          'restmail.net')
-  .option('-l, --locales [path]',
-          'Path to list of locales to test',
-          '../../config/supportedLanguages.js')
-  .option('-L, --locale <en[,zh-TW,de,...]>',
-          'Test only this csv list of locales',
-          function(list) {
-            return list.split(/,/)
-          })
-  .option('-m, --max-locales [int]', // useful for smoketest and debugging
-          'Only send find email for first M locales [Infinity]',
-          parseInt, Infinity)
-  .option('-b, --basename [string]',
-          'Base username for this test run',
-          crypto.randomBytes(8).toString('hex'))
-  .option('-p, --password [64 hex string]',
-          'Password to use for this test run',
-          crypto.randomBytes(32).toString('hex'))
-  .parse(process.argv)
-
-const VERIFY_PATH = '/v1/verify_email'
-const RESET_PATH = '/v1/complete_reset_password'
-const UNLOCK_PATH = '/v1/complete_unlock_account'
-
-const supportedLanguages = program.locale ||
-  require(program.locales).slice(0, program.maxLocales)
 
 function log(level /*, rest */) {
   if (level < log.level) return
@@ -63,219 +64,204 @@ function log(level /*, rest */) {
   args[0] = timestamp
   console.log.apply(null, args)
 }
+
 log.ERROR = 3
 log.INFO = 2
 log.DEBUG = 1
 log.level = log.INFO
+
+function emailFromLang(lang) {
+  return program.basename + '-' + lang + '@' + program.restmailDomain
+}
 
 function langFromEmail(email) {
   // is like 'deadbeef-es@...' or 'deadbeef-es-AR@...'
   return email.split('@')[0].match(/^[^-]*-([^-]*(?:-[^-]*)?)/)[1]
 }
 
-function assertHeader(headers, key) {
-  assert.ok(headers[key], 'received ' + key + ' header')
-}
+/*
+  - signup for service sync
+  - signin as if second device
+  - change the password
+  - trigger a password reset
+  - force  an account lock, and then do account unlock
 
-function assertSubjectLang(subject, lang, expectSubject) {
-  // If it's listed in quirks, expect en-US content
-  var quirks = localeQuirks[expectSubject]
-  if (quirks && quirks[lang]) {
-    assert.equal(subject, expectSubject, lang + ' -> ' + subject)
-  } else {
-    assert.notEqual(subject, expectSubject, lang + ' -> ' + subject)
-  }
-}
+  With the collected emails:
+  - CHECK that I get a signup email
+  - CHECK that I get a notification email of a second device
+  - CHECK that I get a notification email of a password change
+  - CHECK that I get a password reset email
+  - CHECK that I get a notification email of a password reset
+  - CHECK that I get an unlock email (but it's moot if I unlock a test account)
 
-function checkSubjects(subject, lang, headers, link) {
-  function checkByType(expectedSubject, xheaders, params) {
-    assertSubjectLang(subject, lang, expectedSubject)
+*/
 
-    xheaders.forEach(function(header) {
-      assertHeader(headers, header)
-    })
-
-    var actual = JSON.stringify(Object.keys(link.query).sort())
-    assert.equal(actual, params, 'received expected keys')
-  }
-
-  if (link.pathname === VERIFY_PATH) {
-    checkByType('Verify your Firefox Account', ['x-verify-code', 'x-uid'], '["code","uid"]')
-  } else if (link.pathname === RESET_PATH) {
-    checkByType('Reset your Firefox Account password', ['x-recovery-code'], '["code","email","token"]')
-  } else if (link.pathname === UNLOCK_PATH) {
-    checkByType('Re-verify your Firefox Account', ['x-unlock-code', 'x-uid'], '["code","uid"]')
-  }
-}
-
-function verifyMailbox(mbox, log) {
-  assert.equal(mbox.length, 3, 'mailbox has 3 messages')
-
-  var lang = langFromEmail(mbox[0].headers.to)
-  var subject = mbox[0].headers.subject
-  mbox.lang = lang
-
-  log(log.INFO, 'Checking mail message received for:', lang, subject)
-
-  var emailTypes = {}
-  var maxDeliveryTime = 0
-
-  mbox.forEach(function(mail) {
-    var expectedHeaders = [
-      'to',
-      'from',
-      'date',
-      'subject',
-      'x-link',
-      'content-language',
-      'dkim-signature'
-    ]
-
-    expectedHeaders.forEach(function(key) {
-      assertHeader(mail.headers, key)
-    })
-
-    var headers = mail.headers
-
-    var fromTime = new Date(headers.date).getTime()
-    var recvTime = new Date(mail.receivedAt).getTime()
-    var deliveryTime = recvTime - fromTime
-    if (deliveryTime > maxDeliveryTime) {
-      maxDeliveryTime = deliveryTime
-    }
-
-    var lang = langFromEmail(headers.to)
-    var quirks = localeQuirks['content-language']
-    if (quirks[lang]) {
-      assert.equal('en-US', headers['content-language'],
-                     'content-language header is en-US')
-    } else {
-      // See https://github.com/mozilla/fxa-content-server-l10n/issues/44 about sr-LATN
-      if (lang !== 'sr-LATN') {
-        assert.equal(lang, headers['content-language'],
-                     'content-language header is locale specific')
-      }
-    }
-
-    assert.notEqual(mail.html.length, 0, 'mail message html has zero length')
-    assert.notEqual(mail.text.length, 0, 'mail message text has non-zero length')
-
-    var subject = headers.subject
-    var link = url.parse(headers['x-link'], true)
-    emailTypes[link.pathname] += 1
-
-    checkSubjects(subject, lang, headers, link)
-  })
-
-  assert.equal(Object.keys(emailTypes).length, 3, 'got verify, reset and unlock emails')
-
-  // add delivery time so it can be checked in aggregate
-  mbox.maxDeliveryTime = maxDeliveryTime
-
-  return mbox
-}
-
-function emailFromLang(lang) {
-  return program.basename + '-' + lang + '@' + program.restmailDomain
-}
-
-function argsFromLang(lang, withPassword) {
-  var args = {
-    email: emailFromLang(lang),
-  }
-  if (withPassword) {
-    args.authPW = program.password
-  }
-  return args
-}
-
-function optionsFromLangAndPath(lang, path) {
+function signupForSync(lang) {
+  var email = emailFromLang(lang)
   var options = {
-    url: program.authServer + path,
-    headers: {
-      'accept-language': lang
-    }
+    service: 'sync',
+    keys: true,
+    lang: lang
   }
-  return options
+
+  return Client.createAndVerify(program.authServer,
+                                email,
+                                program.password,
+                                program.mailserver,
+                                options)
 }
 
-function createRequest(path, useAuthPW) {
-  return function(result) {
-    var lang = result.req._headers['accept-language']
-    var args = argsFromLang(lang, useAuthPW)
-    var options = optionsFromLangAndPath(lang, path)
-
-    log(log.INFO, 'Starting request:', lang, path)
-
-    return preq.post(options, args)
+function signinAsSecondDevice(client) {
+  var email = client.email
+  var password = program.password
+  var opts = {
+    service: 'sync',
+    reason: 'signin',
+    lang: client.options.lang
   }
+
+  return Client.login(program.authServer, email, password, opts)
+    .then(function(client) {
+      return fetchNotificationEmail(email, client)
+    })
+}
+
+function changePassword(client) {
+  var email = client.email
+  var password = program.password
+  var lang = langFromEmail(email)
+
+  var headers = {
+    'accept-language': lang
+  }
+
+  return Client.changePassword(program.authServer, email, password, password, headers)
+    .then(function (client) {
+      return fetchNotificationEmail(email, client)
+    })
+}
+
+function passwordReset(client) {
+  var email = client.email
+  var password = program.password
+  var lang = langFromEmail(email)
+
+  var headers = {
+    'accept-language': lang
+  }
+
+  return Client.login(program.authServer, email, password)
+    .then(function (client) {
+      return client.forgotPassword(lang)
+        .then(function () {
+          return program.mailserver.waitForCode(email)
+        })
+        .then(function (code) {
+          return client.verifyPasswordResetCode(code, headers)
+        })
+        .then(function() {
+          return client.resetPassword(program.password, headers)
+        })
+    })
+    .then(function () {
+      return Client.login(program.authServer, email, program.password)
+        .then(function (client) {
+          return fetchNotificationEmail(email, client)
+        })
+    })
+}
+
+// Note: /account/lock is not available in production
+//XXX should I bother to complete the unlock?
+function lockAndUnlockAccount(client) {
+  var email = client.email
+  var password = program.password
+  var lang = langFromEmail(email)
+  var authServer = program.authServer
+
+  return Client.login(authServer, email, password)
+    .then(function (client) {
+      return client.lockAccount(email, password)
+        .then(function () {
+          return client.resendAccountUnlockCode(lang)
+        })
+        .then(function () {
+          return fetchNotificationEmail(email, client)
+        })
+    })
+}
+
+function fetchNotificationEmail(email, client) {
+  // Gather the notification email that was just sent for (new-device-added,
+  // password-change, password-reset, account-unlock).
+  return program.mailserver.waitForEmail(email)
+    .then(function (/* message */) {
+      return client
+    })
 }
 
 function checkLocale(lang, index) {
-  var args = argsFromLang(lang, true)
-  var options = optionsFromLangAndPath(lang, '/v1/account/create')
-
-  // AWS SES in stage has rate-limiting in stage of 5/sec, so go slow
+  // AWS SES in `stage` has rate-limiting of 5/sec, so start slow.
   var delay = index * 750
 
   return P.delay(delay)
-    .then(
-      function() {
-        log(log.INFO, 'Kicking off', lang)
+    .then(function() {
+      log(log.INFO, 'Starting', lang)
+      return signupForSync(lang)
+        .then(signinAsSecondDevice)
+        .then(changePassword)
+        .then(passwordReset)
+        .then(lockAndUnlockAccount)
+    })
+}
 
-        // Note: /account/lock is not available in production
-        return preq.post(options, args)
-          .then(createRequest('/v1/password/forgot/send_code', false))
-          .then(createRequest('/v1/account/lock', true))
-          .then(createRequest('/v1/account/unlock/resend_code', false))
-          .then(
-            function(result) {
-
-              function resultComplete(res) {
-                // we expect emails for: 'verify', 'reset' and 'unlock'
-                var state = (res.body &&
-                             res.body.length === 3 &&
-                             'from' in res.body[0] &&
-                             'from' in res.body[1] &&
-                             'from' in res.body[2])
-                return state
-              }
-
-              var lang = result.req._headers['accept-language']
-
-              var options = {
-                url: 'http://' + program.restmailDomain + '/mail/' +
-                  program.basename + '-' + lang,
-                complete: resultComplete,
-                progress: log.bind(null, log.level)
-              }
-
-              // Briefly delay in order to allow SMTP to happen, and not trigger
-              // unnecessary retries; `preq.get` will handle any actual subsequent
-              // need to retry.
-              return P.delay(1000).then(function() {
-                return preq.get(options)
-              })
-            }
-          )
-          .then(
-            function(result) {
-              var mbox = verifyMailbox(result, log)
-              log(log.INFO, 'Completed mail for locale', mbox.lang)
-              return mbox
-            }
-          )
-          .catch(
-            function(err) {
-              log(log.ERROR, err)
-            }
-          )
-      }
-    )
+function dumpMessages(messages) {
+  console.log('---')
+  console.log('--- Dumping messages ---')
+  console.log('---')
+  Object.keys(messages)
+    .map(function(key) {
+      console.log('--- %s ---', key)
+      emailMessages[key]
+        .map(function(email) {
+          console.log(email.to[0], email.subject)
+        })
+    })
 }
 
 function main() {
-  supportedLanguages.forEach(checkLocale)
+  program = configure()
+
+  var checks = program.supportedLanguages.map(checkLocale)
+
+  P.all(checks)
+    .then(function() {
+      if (process.env.DEBUG) {
+        dumpMessages(emailMessages)
+      }
+      var errors = validateEmail(emailMessages, log)
+      var output = []
+      var errorCount = 0
+      Object.keys(errors).sort().forEach(function(lang) {
+        output.push('  ' + lang + ':')
+        var errorList = errors[lang]
+        errorList.forEach(function(err) {
+          errorCount++
+          output.push('    ' + err)
+        })
+      })
+      if (errorCount > 0) {
+        console.log('\nLocalization or other email errors found. However, some untranslated')
+        console.log('locales are listed in ./localeQuirks to get the full state.\n')
+        console.log(output.join('\n'))
+        process.exit(1)
+      } else {
+        console.log('\nAll strings expected to be translated are ok.\n')
+        process.exit(0)
+      }
+    }).catch(function(err) {
+      log(log.ERROR, err.stack || err)
+    })
 }
 
 main()
