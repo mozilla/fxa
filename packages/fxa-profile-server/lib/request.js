b9ed7d98608b66d4d20b35dce9c01bcae6a97edd
vladikoff <vlad.filippov@gmail.com>
2015-11-04 16:18:36 -0500

    fix(server): set nodejs/request maxSockets to Infinity
    Fixes mozilla/fxa-profile-server#102

--- packages/fxa-profile-server/lib/request.js
+++ packages/fxa-profile-server/lib/request.js
@@ -2,5 +2,13 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// update maxSockets setting for node 0.10
+// see https://github.com/request/request/issues/158 for details
+var http = require('http');
+http.globalAgent.maxSockets = Infinity;
+
+var https = require('https');
+https.globalAgent.maxSockets = Infinity;
+
 // in case we want rip `request` out, it can happen in here
 module.exports = require('request');
