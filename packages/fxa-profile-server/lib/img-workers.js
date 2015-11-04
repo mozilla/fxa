1f6e1e1c2dcceeeba184cf39561d729160df13e2
vladikoff <vlad.filippov@gmail.com>
2015-11-04 16:11:03 -0500

    fix(worker): disable gzip encoding on requests to local worker
    Fixes mozilla/fxa-profile-server#98

--- packages/fxa-profile-server/lib/img-workers.js
+++ packages/fxa-profile-server/lib/img-workers.js
@@ -12,11 +12,22 @@ const logger = require('./logging')('img_workers');
 const request = require('./request');
 
 const WORKER_URL = config.get('worker.url');
+const ACCEPT_ENCODING_HEADER = 'identity';
 
 exports.upload = function upload(id, payload, headers) {
   return new P(function(resolve, reject) {
     var url = WORKER_URL + '/a/' + hex(id);
-    var opts = { headers: headers, json: true };
+
+    // do not compress the response to the local worker
+    // https://www.exratione.com/2014/07/nodejs-handling-uncertain-http-response-compression/
+    headers['Accept-Encoding'] = ACCEPT_ENCODING_HEADER;
+
+    var opts = {
+      headers: headers,
+      // disable gzip in request: https://github.com/request/request#requestoptions-callback
+      gzip: false,
+      json: true
+    };
     logger.verbose('upload', url);
     payload.pipe(request.post(url, opts, function(err, res, body) {
       if (err) {
