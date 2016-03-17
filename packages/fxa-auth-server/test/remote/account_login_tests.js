noscripter/fxa-auth-server

feat(logging): add metrics context metadata to activity events

test/remote/account_login_tests.js
@@ -145,6 +145,67 @@ TestServer.start(config)
   )
 
   test(
+    'account login works with minimal metricsContext metadata',
+    function (t) {
+      var email = server.uniqueEmail()
+      return Client.createAndVerify(config.publicUrl, email, 'foo', server.mailbox)
+        .then(function () {
+          return Client.login(config.publicUrl, email, 'foo', {
+            metricsContext: {
+              flowId: '0123456789abcdef0123456789abcdef',
+              flowBeginTime: Date.now()
+            }
+          })
+        })
+        .then(function (client) {
+          t.ok(client, 'logged in to account')
+        })
+    }
+  )
+
+  test(
+    'account login fails with invalid metricsContext flowId',
+    function (t) {
+      var email = server.uniqueEmail()
+      return Client.createAndVerify(config.publicUrl, email, 'foo', server.mailbox)
+        .then(function () {
+          return Client.login(config.publicUrl, email, 'foo', {
+            metricsContext: {
+              flowId: '0123456789abcdef0123456789abcdef0',
+              flowBeginTime: Date.now()
+            }
+          })
+        })
+        .then(function () {
+          t.fail('account login should have failed')
+        }, function (err) {
+          t.ok(err, 'account login failed')
+        })
+    }
+  )
+
+  test(
+    'account login fails with invalid metricsContext flowBeginTime',
+    function (t) {
+      var email = server.uniqueEmail()
+      return Client.createAndVerify(config.publicUrl, email, 'foo', server.mailbox)
+        .then(function () {
+          return Client.login(config.publicUrl, email, 'foo', {
+            metricsContext: {
+              flowId: '0123456789abcdef0123456789abcdef',
+              flowBeginTime: 'wibble'
+            }
+          })
+        })
+        .then(function () {
+          t.fail('account login should have failed')
+        }, function (err) {
+          t.ok(err, 'account login failed')
+        })
+    }
+  )
+
+  test(
     'teardown',
     function (t) {
       server.stop()

test/remote/certificate_sign_tests.js
@@ -218,6 +218,54 @@ TestServer.start(config)
   )
 
   test(
+    'certificate sign works with minimal metricsContext metadata',
+    function (t) {
+      var duration = 1000 * 60 * 60 * 24 // 24 hours
+      return Client.createAndVerify(config.publicUrl, server.uniqueEmail(), 'foo', server.mailbox)
+        .then(
+          function (client) {
+            return client.sign(publicKey, duration, null, {
+              metricsContext: {
+                flowId: 'deadbeefbaadf00ddeadbeefbaadf00d',
+                flowBeginTime: 1
+              }
+            })
+          }
+        )
+        .then(
+          function (cert) {
+            t.ok(cert, 'signed certificate')
+          }
+        )
+    }
+  )
+
+  test(
+    'certificate sign fails with invalid metricsContext flowId',
+    function (t) {
+      var duration = 1000 * 60 * 60 * 24 // 24 hours
+      return Client.createAndVerify(config.publicUrl, server.uniqueEmail(), 'foo', server.mailbox)
+        .then(
+          function (client) {
+            return client.sign(publicKey, duration, null, {
+              metricsContext: {
+                flowId: 'deadbeefbaadf00d',
+                flowBeginTime: 1
+              }
+            })
+          }
+        )
+        .then(
+          function () {
+            t.fail('certificate sign should have failed')
+          }, function (err) {
+            t.ok(err, 'certificate sign failed')
+          }
+        )
+    }
+  )
+
+  test(
     'teardown',
     function (t) {
       server.stop()

test/remote/password_forgot_tests.js
@@ -359,6 +359,46 @@ TestServer.start(config)
   )
 
   test(
+    'reset password with minimal metricsContext metadata',
+    function (t) {
+      var email = server.uniqueEmail()
+      var client
+      return Client.createAndVerify(config.publicUrl, email, 'foo', server.mailbox)
+        .then(
+          function (c) {
+            client = c
+            return client.forgotPassword()
+          }
+        )
+        .then(
+          function () {
+            return server.mailbox.waitForCode(email)
+          }
+        )
+        .then(
+          function (code) {
+            return resetPassword(client, code, 'bar', {
+              metricsContext: {
+                flowId: 'deadbeefbaadf00ddeadbeefbaadf00d',
+                flowBeginTime: 1
+              }
+            })
+          }
+        )
+        .then(
+          function () {
+            return Client.login(config.publicUrl, email, 'bar')
+          }
+        )
+        .then(
+          function (c) {
+            t.ok(c, 'reset password')
+          }
+        )
+    }
+  )
+
+  test(
     'teardown',
     function (t) {
       server.stop()
@@ -367,9 +407,9 @@ TestServer.start(config)
   )
 })
 
-function resetPassword(client, code, newPassword) {
+function resetPassword(client, code, newPassword, options) {
   return client.verifyPasswordResetCode(code)
     .then(function() {
-      return client.resetPassword(newPassword)
+      return client.resetPassword(newPassword, {}, options)
     })
 }

mozilla/fxa

packages/fxa-auth-server/test/remote/account_login_tests.js
@@ -145,6 +145,67 @@ TestServer.start(config)
   )
 
   test(
+    'account login works with minimal metricsContext metadata',
+    function (t) {
+      var email = server.uniqueEmail()
+      return Client.createAndVerify(config.publicUrl, email, 'foo', server.mailbox)
+        .then(function () {
+          return Client.login(config.publicUrl, email, 'foo', {
+            metricsContext: {
+              flowId: '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef',
+              flowBeginTime: Date.now()
+            }
+          })
+        })
+        .then(function (client) {
+          t.ok(client, 'logged in to account')
+        })
+    }
+  )
+
+  test(
+    'account login fails with invalid metricsContext flowId',
+    function (t) {
+      var email = server.uniqueEmail()
+      return Client.createAndVerify(config.publicUrl, email, 'foo', server.mailbox)
+        .then(function () {
+          return Client.login(config.publicUrl, email, 'foo', {
+            metricsContext: {
+              flowId: '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef0',
+              flowBeginTime: Date.now()
+            }
+          })
+        })
+        .then(function () {
+          t.fail('account login should have failed')
+        }, function (err) {
+          t.ok(err, 'account login failed')
+        })
+    }
+  )
+
+  test(
+    'account login fails with invalid metricsContext flowBeginTime',
+    function (t) {
+      var email = server.uniqueEmail()
+      return Client.createAndVerify(config.publicUrl, email, 'foo', server.mailbox)
+        .then(function () {
+          return Client.login(config.publicUrl, email, 'foo', {
+            metricsContext: {
+              flowId: '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef',
+              flowBeginTime: 'wibble'
+            }
+          })
+        })
+        .then(function () {
+          t.fail('account login should have failed')
+        }, function (err) {
+          t.ok(err, 'account login failed')
+        })
+    }
+  )
+
+  test(
     'teardown',
     function (t) {
       server.stop()

packages/fxa-auth-server/test/remote/certificate_sign_tests.js
@@ -218,6 +218,54 @@ TestServer.start(config)
   )
 
   test(
+    'certificate sign works with minimal metricsContext metadata',
+    function (t) {
+      var duration = 1000 * 60 * 60 * 24 // 24 hours
+      return Client.createAndVerify(config.publicUrl, server.uniqueEmail(), 'foo', server.mailbox)
+        .then(
+          function (client) {
+            return client.sign(publicKey, duration, null, {
+              metricsContext: {
+                flowId: 'deadbeefbaadf00ddeadbeefbaadf00ddeadbeefbaadf00ddeadbeefbaadf00d',
+                flowBeginTime: 1
+              }
+            })
+          }
+        )
+        .then(
+          function (cert) {
+            t.ok(cert, 'signed certificate')
+          }
+        )
+    }
+  )
+
+  test(
+    'certificate sign fails with invalid metricsContext flowId',
+    function (t) {
+      var duration = 1000 * 60 * 60 * 24 // 24 hours
+      return Client.createAndVerify(config.publicUrl, server.uniqueEmail(), 'foo', server.mailbox)
+        .then(
+          function (client) {
+            return client.sign(publicKey, duration, null, {
+              metricsContext: {
+                flowId: 'deadbeefbaadf00ddeadbeefbaadf00d',
+                flowBeginTime: 1
+              }
+            })
+          }
+        )
+        .then(
+          function () {
+            t.fail('certificate sign should have failed')
+          }, function (err) {
+            t.ok(err, 'certificate sign failed')
+          }
+        )
+    }
+  )
+
+  test(
     'teardown',
     function (t) {
       server.stop()

packages/fxa-auth-server/test/remote/password_forgot_tests.js
@@ -417,6 +417,46 @@ TestServer.start(config)
   )
 
   test(
+    'reset password with minimal metricsContext metadata',
+    function (t) {
+      var email = server.uniqueEmail()
+      var client
+      return Client.createAndVerify(config.publicUrl, email, 'foo', server.mailbox)
+        .then(
+          function (c) {
+            client = c
+            return client.forgotPassword()
+          }
+        )
+        .then(
+          function () {
+            return server.mailbox.waitForCode(email)
+          }
+        )
+        .then(
+          function (code) {
+            return resetPassword(client, code, 'bar', {
+              metricsContext: {
+                flowId: 'deadbeefbaadf00ddeadbeefbaadf00ddeadbeefbaadf00ddeadbeefbaadf00d',
+                flowBeginTime: 1
+              }
+            })
+          }
+        )
+        .then(
+          function () {
+            return Client.login(config.publicUrl, email, 'bar')
+          }
+        )
+        .then(
+          function (c) {
+            t.ok(c, 'reset password')
+          }
+        )
+    }
+  )
+
+  test(
     'teardown',
     function (t) {
       server.stop()
@@ -425,9 +465,9 @@ TestServer.start(config)
   )
 })
 
-function resetPassword(client, code, newPassword) {
+function resetPassword(client, code, newPassword, options) {
   return client.verifyPasswordResetCode(code)
     .then(function() {
-      return client.resetPassword(newPassword)
+      return client.resetPassword(newPassword, {}, options)
     })
 }