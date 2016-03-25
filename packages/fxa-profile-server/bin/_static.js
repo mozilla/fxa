13417bbc906f6eef65b9a9d4586af0f1187ab240
John Morrison <jrgmorrison@gmail.com>
2016-03-24 19:17:43 -0700

    chore(convict): use .getProperties(), not deprecated .root()

--- packages/fxa-profile-server/bin/_static.js
+++ packages/fxa-profile-server/bin/_static.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const config = require('../lib/config').root();
+const config = require('../lib/config').getProperties();
 const logger = require('../lib/logging')('bin._static');
 const server = require('../lib/server/_static').create();
 
--- packages/fxa-profile-server/bin/server.js
+++ packages/fxa-profile-server/bin/server.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const config = require('../lib/config').root();
+const config = require('../lib/config').getProperties();
 const db = require('../lib/db');
 const events = require('../lib/events');
 const logger = require('../lib/logging')('bin.server');
