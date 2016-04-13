1a02f4acb7bb2cd86c7c8a7433aeea764d79ff58
Ryan Kelly <rfkelly@mozilla.com>
2016-04-13 15:00:57 +1000

    fix(token): Allow the UA for a specific partner device.
    Details in https://bugzilla.mozilla.org/show_bug.cgi?id=1263504

--- packages/fxa-auth-server/test/local/contentToken_tests.js
+++ packages/fxa-auth-server/test/local/contentToken_tests.js
@@ -6,6 +6,7 @@ var extend = require('util')._extend
 
 var test = require('../ptaptest')
 var contentToken = require('../../lib/crypto/contentToken')
+var defaultConfig = require('../../config').getProperties()
 
 // Token generated from default settings
 var DEFAULT_TOKEN = '3134353939393439353832393581370469b241f38edcd281c3b69bc835ba1b9810'
@@ -13,7 +14,7 @@ var HEADERS = {
   'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36'
 }
 
-var defaultConfig = {
+var testConfig = {
   required: true,
   key: 'YOU MUST CHANGE ME',
   expiry: Date.now()
@@ -22,7 +23,7 @@ var defaultConfig = {
 test(
   'contentToken basic',
   function (t) {
-    return contentToken(DEFAULT_TOKEN, HEADERS, defaultConfig)
+    return contentToken(DEFAULT_TOKEN, HEADERS, testConfig)
       .then(
         function (result) {
           t.ok(result.valid, 'token is valid')
@@ -38,7 +39,7 @@ test(
     var HEADERS = {
       'user-agent': 'MSIE'
     }
-    return contentToken(DEFAULT_TOKEN, HEADERS, defaultConfig)
+    return contentToken(DEFAULT_TOKEN, HEADERS, testConfig)
       .then(
         function (result) {
           t.notOk(result.valid, 'token is not valid')
@@ -51,7 +52,7 @@ test(
 test(
   'contentToken fails if token is bad length',
   function (t) {
-    return contentToken(DEFAULT_TOKEN.substr(0, 3), HEADERS, defaultConfig)
+    return contentToken(DEFAULT_TOKEN.substr(0, 3), HEADERS, testConfig)
       .then(function (result) {
         t.notOk(result.valid, 'token is not valid')
         t.equal(result.reason, 'Bad request or token length')
@@ -62,7 +63,7 @@ test(
 test(
   'contentToken fails if bad key',
   function (t) {
-    var config = extend({}, defaultConfig)
+    var config = extend({}, testConfig)
     config.key = 'something else'
     return contentToken(DEFAULT_TOKEN, HEADERS, config)
       .then(function (result) {
@@ -75,7 +76,7 @@ test(
 test(
   'contentToken if timestamp is NaN',
   function (t) {
-    var config = extend({}, defaultConfig)
+    var config = extend({}, testConfig)
     config.expiry = 1
 
     return contentToken(DEFAULT_TOKEN, HEADERS, config)
@@ -89,7 +90,7 @@ test(
 test(
   'contentToken fails if wrong token',
   function (t) {
-    return contentToken('31343539393831323734393931f00f00f00eefa2ead4427c5de811a570a8e539d6', HEADERS, defaultConfig)
+    return contentToken('31343539393831323734393931f00f00f00eefa2ead4427c5de811a570a8e539d6', HEADERS, testConfig)
       .then(function (result) {
         t.notOk(result.valid, 'token is not valid for bad key')
         t.equal(result.reason, 'Invalid HMAC')
@@ -98,14 +99,14 @@ test(
 )
 
 test(
-  'contentToken allow firefox os devices',
+  'contentToken default config allow firefox os devices',
   function (t) {
     var HEADERS = {
       'user-agent': 'Mozilla/5.0 (TV; rv:44.0) Gecko/44.0 Firefox/44.0'
     }
 
-    var config = extend({}, defaultConfig)
-    config.allowedUARegex = ['\\((?:Mobile|Tablet|TV);.+Firefox']
+    var config = extend({}, testConfig)
+    config.allowedUARegex = defaultConfig.contentToken.allowedUARegex
     config.compiledRegexList = config.allowedUARegex.map(function(re) {
       return new RegExp(re)
     })
@@ -119,14 +120,35 @@ test(
 )
 
 test(
-  'contentToken blocks fennec UAs',
+  'contentToken default config allows a specific partner device',
+  function (t) {
+    var HEADERS = {
+      'user-agent': 'Mozilla/5.0 (FreeBSD; Viera; rv:44.0) Gecko/20100101 Firefox/44.0'
+    }
+
+    var config = extend({}, testConfig)
+    config.allowedUARegex = defaultConfig.contentToken.allowedUARegex
+    config.compiledRegexList = config.allowedUARegex.map(function(re) {
+      return new RegExp(re)
+    })
+
+    return contentToken(DEFAULT_TOKEN, HEADERS, config)
+      .then(function (result) {
+        t.ok(result.valid, 'token is valid')
+        t.equal(result.reason, 'Allowed user agent')
+      })
+  }
+)
+
+test(
+  'contentToken default config does not allow fennec UAs',
   function (t) {
     var HEADERS = {
       'user-agent': 'Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0'
     }
 
-    var config = extend({}, defaultConfig)
-    config.allowedUARegex = ['\\((?:Mobile|Tablet|TV);.+Firefox']
+    var config = extend({}, testConfig)
+    config.allowedUARegex = defaultConfig.contentToken.allowedUARegex
     config.compiledRegexList = config.allowedUARegex.map(function(re) {
       return new RegExp(re)
     })
