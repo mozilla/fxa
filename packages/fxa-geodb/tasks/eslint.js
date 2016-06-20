c7a24733cc822e371d4e0e6693b80a5616af399c
Sai Chandramouli <schand31@asu.edu>
2016-06-20 01:58:20 -0700

    added build tools and linting

---
+++ packages/fxa-geodb/tasks/eslint.js
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+module.exports = function (grunt) {
+  'use strict';
+  grunt.config('eslint', {
+    app: {
+      eslintrc: '.eslintrc',
+      src: ['Gruntfile.js', 'tasks/*.js', 'src/*.js']
+    },
+    test: {
+      eslintrc: '.eslintrc',
+      src: [
+        'tests/*.js'
+      ]
+    }
+  });
+};
