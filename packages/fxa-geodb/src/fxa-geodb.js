c9a2370cb85d31781736508453f1878d278082cd
Sai Chandramouli <schand31@asu.edu>
2016-07-05 12:41:49 -0700

    removed backup db and backup functionality

--- packages/fxa-geodb/src/fxa-geodb.js
+++ packages/fxa-geodb/src/fxa-geodb.js
@@ -6,7 +6,6 @@
 var path = require('path');
 
 var DEFAULT_DB_PATH = path.join(__dirname, '..', 'db', 'cities-db.mmdb');
-var DEFAULT_BACKUP_DB_PATH = path.join(__dirname, '..', 'db', 'cities-db.mmdb-backup');
 var ERRORS = require('../lib/errors');
 var maxmind = require('maxmind');
 var Promise = require('bluebird');
@@ -15,32 +14,21 @@ module.exports = function (options) {
   'use strict';
   options = options || {};
   var dbPath = options.dbPath || DEFAULT_DB_PATH;
-  var backupDbPath = options.backupDbPath || DEFAULT_BACKUP_DB_PATH;
 
   var dbLookup, locationData;
-  // ip is valid, try looking it up
-  // the nested try..catch is to ensure that
-  // we always have at least one valid database check
-  // this can help when we have `db` as paid and
-  // `db_backup` as free version or when `db` fails
-  // to load for some reason
+  // we quit if the db did not load for some reason
   try {
     dbLookup = maxmind.open(dbPath);
   } catch (err) {
-    // if it failed with primary database,
-    // try with backup database
-    try {
-      dbLookup = maxmind.open(backupDbPath);
-    } catch (err) {
-      // if that fails, then quit with error
-      throw ERRORS.UNABLE_TO_FETCH_DATA;
-    }
+    // if it failed with primary database
+    // then quit with error
+    throw ERRORS.UNABLE_TO_FETCH_DATA;
   }
 
   return function (ip) {
     return new Promise(function (resolve, reject) {
       // check if ip is valid
-      if (!maxmind.validate(ip)) {
+      if (! maxmind.validate(ip)) {
         reject({
           message: ERRORS.IS_INVALID
         });
