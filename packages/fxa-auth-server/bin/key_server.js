21a75e7e21a4e09ea605e3d8ad3ccb921cd63a13
John Morrison <jrgmorrison@gmail.com>
2016-09-02 10:31:49 -0700

    feature(newrelic): add optional newrelic integration

--- packages/fxa-auth-server/bin/key_server.js
+++ packages/fxa-auth-server/bin/key_server.js
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// Only `require()` the newrelic module if explicity enabled.
+// If required, modules will be instrumented.
+require('../lib/newrelic')()
+
 var config = require('../config').getProperties()
 var jwtool = require('fxa-jwtool')
 
