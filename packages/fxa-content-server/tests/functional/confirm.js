9f808ebf1b3c26e9712d8cc01c414afb96cd4863
Shane Tomlinson <stomlinson@mozilla.com>
2016-10-14 15:12:16 +0100

    refactor(test): Update the confirm functional test to use helpers. mozilla/fxa-content-server#4273)
    I did this to give a simplified example to a contributor for how
    to test for the "open gmail" button.
    
    r=@vbudhram

--- packages/fxa-content-server/tests/functional/confirm.js
+++ packages/fxa-content-server/tests/functional/confirm.js
@@ -5,107 +5,89 @@
 define([
   'intern',
   'intern!object',
-  'intern/chai!assert',
-  'require',
   'tests/lib/helpers',
   'tests/functional/lib/helpers'
-], function (intern, registerSuite, assert, require, TestHelpers, FunctionalHelpers) {
+], function (intern, registerSuite, TestHelpers, FunctionalHelpers) {
   var config = intern.config;
   var CONFIRM_URL = config.fxaContentRoot + 'confirm';
   var SIGNUP_URL = config.fxaContentRoot + 'signup';
+  var PASSWORD = '12345678';
 
+  var thenify = FunctionalHelpers.thenify;
+
+  var clearBrowserState = thenify(FunctionalHelpers.clearBrowserState);
+  var click = FunctionalHelpers.click;
   var closeCurrentWindow = FunctionalHelpers.closeCurrentWindow;
+  var fillOutSignUp = thenify(FunctionalHelpers.fillOutSignUp);
+  var noSuchElement = FunctionalHelpers.noSuchElement;
+  var openPage = thenify(FunctionalHelpers.openPage);
   var respondToWebChannelMessage = FunctionalHelpers.respondToWebChannelMessage;
+  var testElementExists = FunctionalHelpers.testElementExists;
+  var testElementTextInclude = FunctionalHelpers.testElementTextInclude;
+  var testSuccessWasShown = FunctionalHelpers.testSuccessWasShown;
 
   registerSuite({
     name: 'confirm',
 
     beforeEach: function () {
       // clear localStorage to avoid polluting other tests.
-      return FunctionalHelpers.clearBrowserState(this);
+      return this.remote
+        .then(clearBrowserState(this));
     },
 
     'visit confirmation screen without initiating sign up, user is redirected to /signup': function () {
       return this.remote
-        .get(require.toUrl(CONFIRM_URL))
-        .setFindTimeout(intern.config.pageLoadTimeout)
-
         // user is immediately redirected to /signup if they have no
         // sessionToken.
         // Success is showing the screen
-        .findByCssSelector('#fxa-signup-header');
+        .then(openPage(this, CONFIRM_URL, '#fxa-signup-header'));
     },
 
     'sign up, wait for confirmation screen, click resend': function () {
-      var self = this;
       var email = TestHelpers.createEmail();
-      var password = '12345678';
 
       return this.remote
-        .get(require.toUrl(SIGNUP_URL))
-        .then(function () {
-          return FunctionalHelpers.fillOutSignUp(self, email, password);
-        })
+        .then(openPage(this, SIGNUP_URL, '#fxa-signup-header'))
+        .then(fillOutSignUp(this, email, PASSWORD))
 
-        // Being pushed to the confirmation screen is success.
-        .findByCssSelector('.verification-email-message')
-          .getVisibleText()
-          .then(function (resultText) {
-            // check the email address was written
-            assert.ok(resultText.indexOf(email) > -1);
-          })
-        .end()
+        .then(testElementExists('#fxa-confirm-header'))
+        .then(testElementTextInclude('.verification-email-message', email))
+        .then(noSuchElement(this, '#open-webmail'))
 
-        .then(FunctionalHelpers.noSuchElement(self, '#open-webmail'))
-        .end()
-
-        .findByCssSelector('#resend')
-          .click()
-        .end()
+        .then(click('#resend'))
 
         // the test below depends on the speed of the email resent XHR
         // we have to wait until the resent request completes and the
         // success notification is visible
-        .then(FunctionalHelpers.testSuccessWasShown(this));
+        .then(testSuccessWasShown(this));
     },
 
     'sign up with a gmail address, get the open gmail button': function () {
-      var self = this;
       var email = 'signin' + Math.random() + '@gmail.com';
-      var PASSWORD = '12345678';
       var SIGNUP_URL = intern.config.fxaContentRoot + 'signup?context=fx_desktop_v2&service=sync';
       this.timeout = 90000;
 
-      return FunctionalHelpers.openPage(this, SIGNUP_URL, '#fxa-signup-header')
-        .then(respondToWebChannelMessage(self, 'fxaccounts:can_link_account', { ok: true } ))
+      return this.remote
+        .then(openPage(this, SIGNUP_URL, '#fxa-signup-header'))
+        .then(respondToWebChannelMessage(this, 'fxaccounts:can_link_account', { ok: true } ))
 
-        .then(function () {
-          return FunctionalHelpers.fillOutSignUp(self, email, PASSWORD);
-        })
+        .then(fillOutSignUp(this, email, PASSWORD))
 
-        .findByCssSelector('#choose-what-to-sync')
-        .end()
+        .then(testElementExists('#choose-what-to-sync'))
+        .then(click('button[type="submit"]'))
 
-        .findByCssSelector('button[type="submit"]')
-          .click()
-        .end()
-
-        .findByCssSelector("[data-webmail-type='gmail']")
-          .click()
-        .end()
+        .then(testElementExists('#fxa-confirm-header'))
+        .then(click('[data-webmail-type="gmail"]'))
 
         .getAllWindowHandles()
-        .then(function (handles) {
-          return self.remote.switchToWindow(handles[1]);
-        })
+          .then(function (handles) {
+            return this.parent.switchToWindow(handles[1]);
+          })
 
-        .findByCssSelector('.google-header-bar')
-        .end()
-
+        .then(testElementExists('.google-header-bar'))
         .then(closeCurrentWindow())
 
-        .findByCssSelector('#fxa-confirm-header')
-        .end();
+        .then(testElementExists('#fxa-confirm-header'));
     }
   });
 });
