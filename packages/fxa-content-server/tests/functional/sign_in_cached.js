c802ca3ce5435d1f7f83ac821779a7ca01a2811e
Shane Tomlinson <stomlinson@mozilla.com>
2016-11-01 11:17:58 -0400

    fix(test): Fix flaky sign-in-cached functional test. mozilla/fxa-content-server#4352)
    Remove the .sleep in `sign in with desktop context then no context,
    desktop credentials should not persist` and use a `visibleByQSA`
    instead.

--- packages/fxa-content-server/tests/functional/sign_in_cached.js
+++ packages/fxa-content-server/tests/functional/sign_in_cached.js
@@ -219,10 +219,11 @@ define([
         // not be cleared by clicking .use-different
         .then(openPage(this, PAGE_SIGNIN, '.use-different'))
         .then(visibleByQSA('#fxa-signin-header'))
-        // need this sleep here, even with visible selectors the credentials are not cleared fast enough
-        .sleep(1500)
         // This will clear the desktop credentials
         .then(click('.use-different'))
+        // need to wait for the email field to be visible
+        // before attempting to sign-in.
+        .then(visibleByQSA('input[type=email]'))
 
         .then(fillOutSignIn(this, email2, PASSWORD))
         .then(testElementExists('#fxa-settings-header'))
