59fde1486dcd3a479a2bac08f3801c141579ce87
John Morrison <jrgmorrison@gmail.com>
2016-11-15 12:25:15 -0800

    feat(newrelic): add optional newrelic integration mozilla/fxa-profile-server#222) r=vladikoff

--- packages/fxa-profile-server/bin/server.js
+++ packages/fxa-profile-server/bin/server.js
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// Only `require()` the newrelic module if explicity enabled.
+// If required, modules will be instrumented.
+require('../lib/newrelic')();
+
 const config = require('../lib/config').getProperties();
 const db = require('../lib/db');
 const events = require('../lib/events');
---
+++ packages/fxa-profile-server/lib/newrelic.js
@@ -0,0 +1,19 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// To be enabled via the environment of stage or prod. NEW_RELIC_HIGH_SECURITY
+// and NEW_RELIC_LOG should be set in addition to NEW_RELIC_APP_NAME and
+// NEW_RELIC_LICENSE_KEY.
+
+function maybeRequireNewRelic() {
+  var env = process.env;
+
+  if (env.NEW_RELIC_APP_NAME && env.NEW_RELIC_LICENSE_KEY) {
+    return require('newrelic');
+  }
+
+  return null;
+}
+
+module.exports = maybeRequireNewRelic;
