a4e022792fef5df128e5f5c511f05dae6706cba2
Ryan Kelly <rfkelly@mozilla.com>
2016-12-19 15:07:52 +1100

    fix(tokens): Begin expiring access tokens beyond a configurable epoch.

--- packages/fxa-auth-server/lib/token.js
+++ packages/fxa-auth-server/lib/token.js
@@ -4,6 +4,7 @@
 
 const AppError = require('./error');
 const auth = require('./auth');
+const config = require('./config');
 const db = require('./db');
 const encrypt = require('./encrypt');
 const Scope = require('./scope');
@@ -17,6 +18,18 @@ exports.verify = function verify(token) {
     if (!token) {
       throw AppError.invalidToken();
     } else if (+token.expiresAt < Date.now()) {
+      // We dug ourselves a bit of a hole with token expiry,
+      // and this logic is here to help us climb back out.
+      // There's a huge backlog of expired tokens in the wild,
+      // and if we start rejecting them all at once, then the
+      // thundering herd of token updates will crush our db.
+      // Instead we "grandfather" these old tokens in and
+      // pretend they're still valid, while chipping away at
+      // the backlog by either slowly reducing this epoch, or
+      // by slowly purging older tokens from the db.
+      if (+token.expiresAt >= config.get('expiration.accessTokenExpiryEpoch')) {
+        throw AppError.expiredToken(token.expiresAt);
+      }
       logger.warn('token.verify.expired', {
         user: token.userId.toString('hex'),
         client_id: token.clientId.toString('hex'),
@@ -24,7 +37,6 @@ exports.verify = function verify(token) {
         created_at: token.createdAt,
         expires_at: token.expiresAt
       });
-      //throw AppError.expiredToken(token.expiresAt);
     } else if ((+token.createdAt + TWENTY_FOUR_HOURS) < Date.now()) {
       // Log a warning if reliers are using access tokens that are more
       // than 24 hours old.  Eventually we will shorten the expiry time
