cb5084d42dd19f6359c97c1f9143d330ae53f129
John Morrison <jrgmorrison@gmail.com>
2016-12-30 17:14:02 -0800

    feat(mailer): set up "SES Event Publishing" metrics

--- packages/fxa-auth-server/config.js
+++ packages/fxa-auth-server/config.js
@@ -196,6 +196,14 @@ var conf = convict({
       format: String,
       default: 'https://support.mozilla.org/kb/password-manager-remember-delete-change-and-import#w_viewing-and-deleting-passwords'
     }
+  },
+  sesConfigurationSet: {
+    doc: ('AWS SES Configuration Set for SES Event Publishing. If defined, ' +
+          'X-SES-MESSAGE-TAGS headers will be added to emails. Only ' +
+          'intended for Production/Stage use.'),
+    format: String,
+    default: undefined,
+    env: 'SES_CONFIGURATION_SET'
   }
 })
 
