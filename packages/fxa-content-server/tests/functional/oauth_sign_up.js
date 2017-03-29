a5d28fc68a887b98be3fff757f34a18c8dd46a4b
Shane Tomlinson <stomlinson@mozilla.com>
2017-03-29 07:42:10 +0100

    refactor(test): Modernize the oauth_sign_up tests. mozilla/fxa-content-server#4871) r=vladikoff
    I noticed a bunch of old-style calls in the module while investigating
    failures on stage and thought I'd update while I'm in there.

--- packages/fxa-content-server/tests/functional/oauth_sign_up.js
+++ packages/fxa-content-server/tests/functional/oauth_sign_up.js
@@ -3,17 +3,10 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 define([
-  'intern',
   'intern!object',
-  'intern/chai!assert',
-  'intern/browser_modules/dojo/node!xmlhttprequest',
-  'app/bower_components/fxa-js-client/fxa-client',
   'tests/lib/helpers',
   'tests/functional/lib/helpers'
-], function (intern, registerSuite, assert, nodeXMLHttpRequest, FxaClient, TestHelpers,  FunctionalHelpers) {
-  var config = intern.config;
-  var AUTH_SERVER_ROOT = config.fxaAuthRoot;
-
+], function (registerSuite, TestHelpers,  FunctionalHelpers) {
   var PASSWORD = 'password';
   var email;
   var bouncedEmail;
@@ -23,28 +16,32 @@ define([
   var closeCurrentWindow = FunctionalHelpers.closeCurrentWindow;
   var createUser = FunctionalHelpers.createUser;
   var fillOutSignUp = FunctionalHelpers.fillOutSignUp;
+  var getFxaClient = FunctionalHelpers.getFxaClient;
   var noEmailExpected = FunctionalHelpers.noEmailExpected;
   var noSuchElement = FunctionalHelpers.noSuchElement;
   var testElementValueEquals = FunctionalHelpers.testElementValueEquals;
+  var openExternalSite = FunctionalHelpers.openExternalSite;
   var openFxaFromRp = FunctionalHelpers.openFxaFromRp;
   var openVerificationLinkInDifferentBrowser = FunctionalHelpers.openVerificationLinkInDifferentBrowser;
   var openVerificationLinkInNewTab = FunctionalHelpers.openVerificationLinkInNewTab;
   var openVerificationLinkInSameTab = FunctionalHelpers.openVerificationLinkInSameTab;
   var testElementExists = FunctionalHelpers.testElementExists;
   var testElementTextInclude = FunctionalHelpers.testElementTextInclude;
+  var testUrlInclude = FunctionalHelpers.testUrlInclude;
   var testUrlPathnameEquals = FunctionalHelpers.testUrlPathnameEquals;
+  var thenify = FunctionalHelpers.thenify;
   var type = FunctionalHelpers.type;
   var visibleByQSA = FunctionalHelpers.visibleByQSA;
 
-  function signUpWithExistingAccount (context, email, firstPassword, secondPassword, options) {
-    return context.remote
+  const signUpWithExistingAccount = thenify(function(email, firstPassword, secondPassword, options) {
+    return this.parent
       .then(createUser(email, firstPassword, { preVerified: true }))
       .then(function () {
         return this.parent
           .then(openFxaFromRp('signup'));
       })
       .then(fillOutSignUp(email, secondPassword, options));
-  }
+  });
 
   registerSuite({
     name: 'oauth signup',
@@ -66,43 +63,25 @@ define([
     'signup, verify same browser': function () {
       return this.remote
         .then(openFxaFromRp('signup'))
-        .findByCssSelector('#fxa-signup-header .service')
-        .end()
-
-        .getCurrentUrl()
-        .then(function (url) {
-          assert.ok(url.indexOf('client_id=') > -1);
-          assert.ok(url.indexOf('redirect_uri=') > -1);
-          assert.ok(url.indexOf('state=') > -1);
-        })
-        .end()
+        .then(testElementExists('#fxa-signup-header .service'))
+        .then(testUrlInclude('client_id='))
+        .then(testUrlInclude('redirect_uri='))
+        .then(testUrlInclude('state='))
 
         .then(fillOutSignUp(email, PASSWORD))
 
-        .findByCssSelector('#fxa-confirm-header')
-        .end()
-
+        .then(testElementExists('#fxa-confirm-header'))
         .then(openVerificationLinkInNewTab(email, 0))
 
         .switchToWindow('newwindow')
         // wait for the verified window in the new tab
-        .findById('fxa-sign-up-complete-header')
-        .end()
-
-        .findByCssSelector('.account-ready-service')
-        .getVisibleText()
-        .then(function (text) {
-          // user sees the name of the RP,
-          // but cannot redirect
-          assert.ok(/123done/i.test(text));
-        })
-        .end()
+        .then(testElementExists('#fxa-sign-up-complete-header'))
+        // user sees the name of the RP, but cannot redirect
+        .then(testElementTextInclude('.account-ready-service', '123done'))
 
         // switch to the original window
         .then(closeCurrentWindow())
-
-        .findByCssSelector('#loggedin')
-        .end()
+        .then(testElementExists('#loggedin'))
 
         // Do not expect a post-verification email, those are for Sync.
         .then(noEmailExpected(email, 1));
@@ -111,23 +90,16 @@ define([
     'signup, verify same browser with original tab closed': function () {
       return this.remote
         .then(openFxaFromRp('signup'))
-
         .then(fillOutSignUp(email, PASSWORD))
 
-        .findByCssSelector('#fxa-confirm-header')
-        .end()
-
+        .then(testElementExists('#fxa-confirm-header'))
         // user browses to another site.
         .switchToFrame(null)
-
-        .then(FunctionalHelpers.openExternalSite())
-
+        .then(openExternalSite())
         .then(openVerificationLinkInNewTab(email, 0))
 
         .switchToWindow('newwindow')
-
-        .findByCssSelector('#fxa-sign-up-complete-header')
-        .end()
+        .then(testElementExists('#fxa-sign-up-complete-header'))
 
         // switch to the original window
         .then(closeCurrentWindow());
@@ -136,13 +108,11 @@ define([
     'signup, verify same browser by replacing the original tab': function () {
       return this.remote
         .then(openFxaFromRp('signup'))
-
         .then(fillOutSignUp(email, PASSWORD))
 
-        .findByCssSelector('#fxa-confirm-header')
-        .end()
-
+        .then(testElementExists('#fxa-confirm-header'))
         .then(openVerificationLinkInSameTab(email, 0))
+
         .then(testElementExists('#loggedin'));
     },
 
@@ -151,14 +121,11 @@ define([
         .then(openFxaFromRp('signup'))
         .then(fillOutSignUp(email, PASSWORD))
 
-        .findByCssSelector('#fxa-confirm-header')
-        .end()
-
+        .then(testElementExists('#fxa-confirm-header'))
         .then(openVerificationLinkInDifferentBrowser(email))
 
         // original tab redirects back to 123done
-        .findByCssSelector('#loggedin')
-        .end();
+        .then(testElementExists('#loggedin'));
     },
 
     'signup, verify different browser - from new browser\'s P.O.V.': function () {
@@ -166,8 +133,7 @@ define([
         .then(openFxaFromRp('signup'))
         .then(fillOutSignUp(email, PASSWORD))
 
-        .findByCssSelector('#fxa-confirm-header')
-        .end()
+        .then(testElementExists('#fxa-confirm-header'))
 
         // clear browser state to simulate opening link in a new browser
         .then(clearBrowserState({
@@ -185,46 +151,43 @@ define([
     },
 
     'signup with existing account, coppa is valid': function () {
-      return signUpWithExistingAccount(this, email, PASSWORD, PASSWORD)
+      return this.remote
+        .then(signUpWithExistingAccount(email, PASSWORD, PASSWORD))
 
         // should have navigated to 123done
         .then(testElementExists('#loggedin'));
     },
 
     'signup with existing account, coppa is valid, credentials are wrong': function () {
-      return signUpWithExistingAccount(this, email, PASSWORD, 'bad' + PASSWORD)
+      return this.remote
+        .then(signUpWithExistingAccount(email, PASSWORD, 'bad' + PASSWORD))
 
         .then(visibleByQSA('.error'))
         .then(click('.error a[href^="/oauth/signin"]'))
 
-        // should have navigated to sign-in view
         .then(testElementExists('#fxa-signin-header'))
-
-        // should be /oauth/signin route
         .then(testUrlPathnameEquals('/oauth/signin'))
 
-        // the email and password fields should be populated
         .then(testElementValueEquals('input[type=email]', email))
         .then(testElementValueEquals('input[type=password]', 'bad' + PASSWORD));
     },
 
     'signup with existing account, coppa is empty': function () {
-      return signUpWithExistingAccount(this, email, PASSWORD, PASSWORD, { age: ' ' })
+      return this.remote
+        .then(signUpWithExistingAccount(email, PASSWORD, PASSWORD, { age: ' ' }))
 
         // should have navigated to 123done
         .then(testElementExists('#loggedin'));
     },
 
     'signup with existing account, coppa is empty, credentials are wrong': function () {
-      return signUpWithExistingAccount(this, email, PASSWORD, 'bad' + PASSWORD, { age: ' ' })
+      return this.remote
+        .then(signUpWithExistingAccount(email, PASSWORD, 'bad' + PASSWORD, { age: ' ' }))
 
         .then(visibleByQSA('.error'))
         .then(click('.error a[href^="/oauth/signin"]'))
 
-        // should have navigated to sign-in view
         .then(testElementExists('#fxa-signin-header'))
-
-        // should be /oauth/signin route
         .then(testUrlPathnameEquals('/oauth/signin'))
 
         // the email and password fields should be populated
@@ -233,7 +196,8 @@ define([
     },
 
     'signup with existing account, coppa is too young': function () {
-      return signUpWithExistingAccount(this, email, PASSWORD, PASSWORD, { age: 12 })
+      return this.remote
+        .then(signUpWithExistingAccount(email, PASSWORD, PASSWORD, { age: 12 }))
 
         // should have navigated to 123done
         .then(testElementExists('#loggedin'));
@@ -242,34 +206,24 @@ define([
     'signup, bounce email, allow user to restart flow but force a different email': function () {
       this.timeout = 60 * 1000;
 
-      var client = new FxaClient(AUTH_SERVER_ROOT, {
-        xhr: nodeXMLHttpRequest.XMLHttpRequest
-      });
-
       return this.remote
         .then(openFxaFromRp('signup'))
-
         .then(fillOutSignUp(bouncedEmail, PASSWORD))
 
         .then(testElementExists('#fxa-confirm-header'))
-
         .then(function () {
-          return client.accountDestroy(bouncedEmail, PASSWORD);
+          return getFxaClient().accountDestroy(bouncedEmail, PASSWORD);
         })
 
         .then(testElementExists('#fxa-signup-header'))
-
         // expect an error message to already be present on redirect
         .then(visibleByQSA('.tooltip'))
-
-        // submit button should be disabled.
         .then(testElementExists('button[type="submit"].disabled'))
 
         .then(type('input[type="email"]', email))
         .then(click('button[type="submit"]'))
 
         .then(testElementExists('#fxa-confirm-header'))
-
         .then(openVerificationLinkInNewTab(email, 0))
 
         .switchToWindow('newwindow')
