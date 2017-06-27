2e0b539e96bbe9dbe1ec8b90c628ad9117e54247
John Morrison <jrgmorrison@gmail.com>
2017-06-27 16:47:01 -0400

    chore(npm): update newrelic to 1.40.0 mozilla/fxa-profile-server#257) r=vladikoff

--- packages/fxa-profile-server/bin/worker.js
+++ packages/fxa-profile-server/bin/worker.js
@@ -2,6 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// Only `require()` the newrelic module if explicity enabled.
+// If required, modules will be instrumented.
+require('../lib/newrelic')();
+
 const logger = require('../lib/logging')('bin.worker');
 const server = require('../lib/server/worker').create();
 
