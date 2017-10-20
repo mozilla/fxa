7680e201592f1ddc38fffd3fc9e2a90de8a57fe8
Shane Tomlinson <stomlinson@mozilla.com>
2017-10-20 09:47:59 +0100

    chore(sms): Fix spacing in country-telephone-info.js and test mozilla/fxa-content-server#5622) r=@shane-tomlinson
    There was one extra space on every line.
    
    With `?w=1`, GitHub says "Whitespace only changes". Self merge.

--- packages/fxa-content-server/app/scripts/lib/country-telephone-info.js
+++ packages/fxa-content-server/app/scripts/lib/country-telephone-info.js
@@ -2,99 +2,99 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- /**
-  * Country->phone number info.
-  */
- define((require, exports, module) => {
-   'use strict';
+/**
+ * Country->phone number info.
+ */
+define((require, exports, module) => {
+  'use strict';
 
-   /**
-    * Each country entry should have the fields listed below.
-    */
-   /**
-    * Format a normalized phone number, expects country code prefix.
-    *
-    * @method format
-    * @param {String} num phone number to format.
-    * @return {String} phone number formatted for country
-    */
+  /**
+   * Each country entry should have the fields listed below.
+   */
+  /**
+   * Format a normalized phone number, expects country code prefix.
+   *
+   * @method format
+   * @param {String} num phone number to format.
+   * @return {String} phone number formatted for country
+   */
 
-   /**
-    * Normalize a string accepted by `pattern` to the full phone number,
-    * including country code prefix.
-    *
-    * @method normalize
-    * @param {String} num phone number to convert
-    * @return {String} full phone number with country code prefix
-    */
+  /**
+   * Normalize a string accepted by `pattern` to the full phone number,
+   * including country code prefix.
+   *
+   * @method normalize
+   * @param {String} num phone number to convert
+   * @return {String} full phone number with country code prefix
+   */
 
-   /**
-    * Pattern used for input validation
-    *
-    * @property pattern
-    * @type {RegExp}
-    */
+  /**
+   * Pattern used for input validation
+   *
+   * @property pattern
+   * @type {RegExp}
+   */
 
-   /**
-    * Country code prefix
-    * @property prefix
-    * @type {String}
-    */
+  /**
+   * Country code prefix
+   * @property prefix
+   * @type {String}
+   */
 
-   module.exports = {
-     GB: {
-       format (num) {
-         // +44 1234 567890
-         return num.slice(0, 3) + ' ' + num.slice(3, 7) + ' ' + num.slice(7, 14);
-       },
-       normalize (num) {
-         if (/^\+44/.test(num)) {
-           return num;
-         }
-         return `+44${num}`;
-       },
-       pattern: /^(?:\+44)?\d{10,10}$/,
-       prefix: '+44'
-     },
-     RO: {
-       format(num) {
-         // +40 7xx xxxxxx
-         return num.slice(0, 3) + ' ' + num.slice(3, 6) + ' ' + num.slice(6, 12);
-       },
-       normalize(num) {
-         // allow +40 country code prefix
-         // as well as an extra 0 before the 7 prefix.
-         const prefix = /^(\+40)?0?/;
-         if (prefix.test(num)) {
-           num = num.replace(prefix, '');
-         }
-         return `+40${num}`;
-       },
-       // +407xxxxxxxx, allow leading 0 for sloppiness.
-       pattern: /^(?:\+40)?0?7\d{8,8}$/,
-       // The country code is +40, all mobile phones have a 7 prefix.
-       prefix: '+407'
+  module.exports = {
+    GB: {
+      format (num) {
+        // +44 1234 567890
+        return num.slice(0, 3) + ' ' + num.slice(3, 7) + ' ' + num.slice(7, 14);
+      },
+      normalize (num) {
+        if (/^\+44/.test(num)) {
+          return num;
+        }
+        return `+44${num}`;
+      },
+      pattern: /^(?:\+44)?\d{10,10}$/,
+      prefix: '+44'
+    },
+    RO: {
+      format(num) {
+        // +40 7xx xxxxxx
+        return num.slice(0, 3) + ' ' + num.slice(3, 6) + ' ' + num.slice(6, 12);
+      },
+      normalize(num) {
+        // allow +40 country code prefix
+        // as well as an extra 0 before the 7 prefix.
+        const prefix = /^(\+40)?0?/;
+        if (prefix.test(num)) {
+          num = num.replace(prefix, '');
+        }
+        return `+40${num}`;
+      },
+      // +407xxxxxxxx, allow leading 0 for sloppiness.
+      pattern: /^(?:\+40)?0?7\d{8,8}$/,
+      // The country code is +40, all mobile phones have a 7 prefix.
+      prefix: '+407'
 
-     },
-     US: {
-       format (num) {
-         // Americans don't use country codes, drop the country code prefix
-         // 123-456-7890
-         return num.slice(2, 5) + '-' + num.slice(5, 8) + '-' + num.slice(8);
-       },
-       normalize (num) {
-         if (/^\+1/.test(num)) {
-           return num;
-         } else if (/^1/.test(num) && num.length === 11) {
-           return `+${num}`;
-         }
-         return `+1${num}`;
-       },
-       pattern: /^(\+?1)?[2-9]\d{9,9}$/, // allow for a +1 or 1 prefix before the area code, area codes are all 2-9
-       prefix: '+1'
-     }
-   };
+    },
+    US: {
+      format (num) {
+        // Americans don't use country codes, drop the country code prefix
+        // 123-456-7890
+        return num.slice(2, 5) + '-' + num.slice(5, 8) + '-' + num.slice(8);
+      },
+      normalize (num) {
+        if (/^\+1/.test(num)) {
+          return num;
+        } else if (/^1/.test(num) && num.length === 11) {
+          return `+${num}`;
+        }
+        return `+1${num}`;
+      },
+      pattern: /^(\+?1)?[2-9]\d{9,9}$/, // allow for a +1 or 1 prefix before the area code, area codes are all 2-9
+      prefix: '+1'
+    }
+  };
 
-   // alias CA (Canada) to use the same info as the US.
-   module.exports.CA = module.exports.US;
- });
+  // alias CA (Canada) to use the same info as the US.
+  module.exports.CA = module.exports.US;
+});
--- packages/fxa-content-server/app/tests/spec/lib/country-telephone-info.js
+++ packages/fxa-content-server/app/tests/spec/lib/country-telephone-info.js
@@ -2,104 +2,104 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
- define((require, exports, module) => {
-   'use strict';
+define((require, exports, module) => {
+  'use strict';
 
-   const { assert } = require('chai');
-   const CountryTelephoneInfo = require('lib/country-telephone-info');
+  const { assert } = require('chai');
+  const CountryTelephoneInfo = require('lib/country-telephone-info');
 
-   describe('lib/country-telephone-info', () => {
-     describe('GB', () => {
-       const { format, normalize, pattern } = CountryTelephoneInfo.GB;
+  describe('lib/country-telephone-info', () => {
+    describe('GB', () => {
+      const { format, normalize, pattern } = CountryTelephoneInfo.GB;
 
-       describe('format', () => {
-         it('formats correctly', () => {
-           assert.equal(format('+441234567890'), '+44 1234 567890');
-         });
-       });
+      describe('format', () => {
+        it('formats correctly', () => {
+          assert.equal(format('+441234567890'), '+44 1234 567890');
+        });
+      });
 
-       describe('normalize', () => {
-         it('normalizes a number accepted by pattern correctly', () => {
-           assert.equal(normalize('+441234567890'), '+441234567890');
-           assert.equal(normalize('1234567890'), '+441234567890');
-         });
-       });
+      describe('normalize', () => {
+        it('normalizes a number accepted by pattern correctly', () => {
+          assert.equal(normalize('+441234567890'), '+441234567890');
+          assert.equal(normalize('1234567890'), '+441234567890');
+        });
+      });
 
-       describe('pattern', () => {
-         it('validates correctly', () => {
-           assert.ok(pattern.test('1234567890'));
-           assert.ok(pattern.test('+441234567890'));
-           assert.isFalse(pattern.test('+331234567890'));
-           assert.isFalse(pattern.test('+4401234567890')); // that leading 0 kills me.
-           assert.isFalse(pattern.test('+44123456789'));
-           assert.isFalse(pattern.test('123456789'));
-         });
-       });
-     });
+      describe('pattern', () => {
+        it('validates correctly', () => {
+          assert.ok(pattern.test('1234567890'));
+          assert.ok(pattern.test('+441234567890'));
+          assert.isFalse(pattern.test('+331234567890'));
+          assert.isFalse(pattern.test('+4401234567890')); // that leading 0 kills me.
+          assert.isFalse(pattern.test('+44123456789'));
+          assert.isFalse(pattern.test('123456789'));
+        });
+      });
+    });
 
-     describe('RO', () => {
-       const { format, normalize, pattern } = CountryTelephoneInfo.RO;
+    describe('RO', () => {
+      const { format, normalize, pattern } = CountryTelephoneInfo.RO;
 
-       it('formats correctly', () => {
-         assert.equal(format('+40712345678'), '+40 712 345678');
-       });
+      it('formats correctly', () => {
+        assert.equal(format('+40712345678'), '+40 712 345678');
+      });
 
-       describe('normalize', () => {
-         it('normalizes a number accepted by pattern correctly', () => {
-           assert.equal(normalize('712345678'), '+40712345678'); // no country code prefix
-           assert.equal(normalize('0712345678'), '+40712345678'); // no country code prefix, extra 0 before the 7
-           assert.equal(normalize('+40712345678'), '+40712345678'); // full country code prefix
-           assert.equal(normalize('+400712345678'), '+40712345678'); // full country code prefix, extra 0 before the 7
-         });
-       });
+      describe('normalize', () => {
+        it('normalizes a number accepted by pattern correctly', () => {
+          assert.equal(normalize('712345678'), '+40712345678'); // no country code prefix
+          assert.equal(normalize('0712345678'), '+40712345678'); // no country code prefix, extra 0 before the 7
+          assert.equal(normalize('+40712345678'), '+40712345678'); // full country code prefix
+          assert.equal(normalize('+400712345678'), '+40712345678'); // full country code prefix, extra 0 before the 7
+        });
+      });
 
-       describe('pattern', () => {
-         it('validates correctly', () => {
-           assert.isFalse(pattern.test('71234567')); // too short
-           assert.isFalse(pattern.test('7123456789')); // too long
-           assert.isFalse(pattern.test('812345678')); // invalid prefix (must be 7)
+      describe('pattern', () => {
+        it('validates correctly', () => {
+          assert.isFalse(pattern.test('71234567')); // too short
+          assert.isFalse(pattern.test('7123456789')); // too long
+          assert.isFalse(pattern.test('812345678')); // invalid prefix (must be 7)
 
-           assert.ok(pattern.test('712345678'));
-           assert.ok(pattern.test('0712345678')); // allow leading 0
-           assert.ok(pattern.test('+40712345678')); // full country code prefix
-           assert.ok(pattern.test('+400712345678')); // full country code prefix with 0 before 7
-           assert.isFalse(pattern.test('+45712345678')); // incorrect country code prefix
-         });
-       });
-     });
+          assert.ok(pattern.test('712345678'));
+          assert.ok(pattern.test('0712345678')); // allow leading 0
+          assert.ok(pattern.test('+40712345678')); // full country code prefix
+          assert.ok(pattern.test('+400712345678')); // full country code prefix with 0 before 7
+          assert.isFalse(pattern.test('+45712345678')); // incorrect country code prefix
+        });
+      });
+    });
 
-     describe('US', () => {
-       const { format, normalize, pattern } = CountryTelephoneInfo.US;
+    describe('US', () => {
+      const { format, normalize, pattern } = CountryTelephoneInfo.US;
 
-       it('formats correctly', () => {
-         assert.equal(format('+11234567890'), '123-456-7890');
-         assert.equal(format('+14234567890'), '423-456-7890');
-       });
+      it('formats correctly', () => {
+        assert.equal(format('+11234567890'), '123-456-7890');
+        assert.equal(format('+14234567890'), '423-456-7890');
+      });
 
-       describe('normalize', () => {
-         it('normalizes a number accepted by pattern correctly', () => {
-           assert.equal(normalize('+11234567890'), '+11234567890'); // full country code prefix
-           assert.equal(normalize('11234567890'), '+11234567890'); // country code prefix w/o +
-           assert.equal(normalize('1234567890'), '+11234567890'); // no country code prefix
-         });
-       });
+      describe('normalize', () => {
+        it('normalizes a number accepted by pattern correctly', () => {
+          assert.equal(normalize('+11234567890'), '+11234567890'); // full country code prefix
+          assert.equal(normalize('11234567890'), '+11234567890'); // country code prefix w/o +
+          assert.equal(normalize('1234567890'), '+11234567890'); // no country code prefix
+        });
+      });
 
-       describe('pattern', () => {
-         it('validates correctly', () => {
-           assert.ok(pattern.test('2134567890'));
-           assert.ok(pattern.test('+12134567890')); // full country code prefix
-           assert.ok(pattern.test('12134567890')); // country code prefix w/o +
-           assert.ok(pattern.test('15234567890'));
-           assert.isFalse(pattern.test('+332134567890'));
-           assert.isFalse(pattern.test('+1213456789'));
-           assert.isFalse(pattern.test('213456789'));
-           assert.isFalse(pattern.test('1213456789'));
-           assert.isFalse(pattern.test('1123456789')); // can't start an area code with 1
-           assert.isFalse(pattern.test('11234567890')); // can't start an area code with 1
-           assert.isFalse(pattern.test('121345678901')); // too long, has country prefix
-           assert.isFalse(pattern.test('21345678901')); // too long, no country prefix
-         });
-       });
-     });
-   });
- });
+      describe('pattern', () => {
+        it('validates correctly', () => {
+          assert.ok(pattern.test('2134567890'));
+          assert.ok(pattern.test('+12134567890')); // full country code prefix
+          assert.ok(pattern.test('12134567890')); // country code prefix w/o +
+          assert.ok(pattern.test('15234567890'));
+          assert.isFalse(pattern.test('+332134567890'));
+          assert.isFalse(pattern.test('+1213456789'));
+          assert.isFalse(pattern.test('213456789'));
+          assert.isFalse(pattern.test('1213456789'));
+          assert.isFalse(pattern.test('1123456789')); // can't start an area code with 1
+          assert.isFalse(pattern.test('11234567890')); // can't start an area code with 1
+          assert.isFalse(pattern.test('121345678901')); // too long, has country prefix
+          assert.isFalse(pattern.test('21345678901')); // too long, no country prefix
+        });
+      });
+    });
+  });
+});
