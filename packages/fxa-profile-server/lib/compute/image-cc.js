cc5fd29a6cb39c019a0fb33c941156585f58724b
Ryan Kelly <ryan@rfk.id.au>
2017-10-25 16:49:01 +1100

    chore(nsp): nsp updates mozilla/fxa-profile-server#289); r=philbooth

--- packages/fxa-profile-server/lib/compute/image-cc.js
+++ packages/fxa-profile-server/lib/compute/image-cc.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const gm = require('gm');
+const gm = require('gm-reloaded');
 
 const config = require('../config');
 const img = require('../img');
