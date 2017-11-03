eb73ad29c046d121e24ddf47370a68aebd28202e
Vlad Filippov <vlad.filippov@gmail.com>
2017-11-03 15:18:54 -0400

    fix(update): add db mysql

--- _scripts/update_all.sh
+++ _scripts/update_all.sh
@@ -8,6 +8,7 @@ git pull https://github.com/mozilla/fxa-local-dev.git master
 (cd fxa-auth-server && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-auth-server update failed"
 
 (cd fxa-auth-mailer && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-auth-mailer update failed"
+(cd fxa-auth-db-mysql && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-auth-db-mysql update failed"
 
 (cd browserid-verifier && git checkout http && git pull origin http && npm i && npm i vladikoff/browserid-local-verify#http && cd ..)|| echo "browserid update failed"
 (cd fxa-oauth-server && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-oauth-server update failed"
