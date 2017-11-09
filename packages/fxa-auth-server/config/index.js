65a75ef46bcc8fb71e595b83de5de7f801943646
Shane Tomlinson <stomlinson@mozilla.com>
2017-11-09 18:45:34 +0000

    feat(sms): Enable SMS in Belgium, France, Luxembourg mozilla/fxa-auth-server#2211) r=@vladikoff
    Monaco is not enabled as requested, I haven't found a phone
    number that validates yet.
    
    mozilla/fxa-content-server#5573

--- packages/fxa-auth-server/config/index.js
+++ packages/fxa-auth-server/config/index.js
@@ -734,7 +734,7 @@ var conf = convict({
     },
     countryCodes: {
       doc: 'Allow sending SMS to these ISO 3166-1 alpha-2 country codes',
-      default: ['AT', 'CA', 'DE', 'GB', 'US'],
+      default: ['AT', 'BE', 'CA', 'DE', 'FR', 'GB', 'LU', 'US'],
       format: Array,
       env: 'SMS_COUNTRY_CODES'
     },
