5c0933cb762bd6928165322d5577647ec18b4185
John Morrison <jrgmorrison@gmail.com>
2017-11-12 10:15:23 -0800

    fix(newrelic): Dockerpush fix newrelic must be first mozilla/fxa-customs-server#215) r=@vladikoff

--- packages/fxa-customs-server/bin/customs_server.js
+++ packages/fxa-customs-server/bin/customs_server.js
@@ -4,6 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+// This MUST be the first require in the program.
 // Only `require()` the newrelic module if explicity enabled.
 // If required, modules will be instrumented.
 require('../lib/newrelic')()
