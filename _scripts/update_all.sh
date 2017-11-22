0a32074a7fe6661f7007e935e87b0496a9e36c62
Vlad Filippov <vlad.filippov@gmail.com>
2017-11-22 17:15:49 -0500

    chore(update): remove mailer updater

--- _scripts/update_all.sh
+++ _scripts/update_all.sh
@@ -7,7 +7,6 @@ git pull https://github.com/mozilla/fxa-local-dev.git master
 (cd fxa-content-server && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-content-server update failed"
 (cd fxa-auth-server && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-auth-server update failed"
 
-(cd fxa-auth-mailer && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-auth-mailer update failed"
 (cd fxa-auth-db-mysql && git checkout master && git pull origin master && npm i && cd ..) || echo "fxa-auth-db-mysql update failed"
 
 (cd browserid-verifier && git checkout http && git pull origin http && npm i && npm i vladikoff/browserid-local-verify#http && cd ..)|| echo "browserid update failed"
