04463237a35a1c62c754e41914cbe1f5878742ff
vladikoff <vlad.filippov@gmail.com>
2018-01-31 10:45:56 -0500

    fix(build): build process clean up

--- packages/fxa-js-client/Gruntfile.js
+++ packages/fxa-js-client/Gruntfile.js
@@ -27,8 +27,8 @@ module.exports = function (grunt) {
     ['intern:node', 'intern:native_node']);
 
   grunt.registerTask('lint',
-    'Alias for eslint, jshint and jscs tasks',
-    ['eslint', 'jscs']);
+    'Alias for eslint',
+    ['eslint']);
 
   grunt.registerTask('default',
     ['build']);
