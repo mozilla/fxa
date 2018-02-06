f7daa2274c76660b540625cc7a3b99469c0d77be
Ryan Kelly <rfkelly@mozilla.com>
2018-02-06 13:49:52 +1100

    fix(tests): Use higher test timeout under Windows Subsystem for Linux

--- packages/fxa-auth-server/scripts/test-local.sh
+++ packages/fxa-auth-server/scripts/test-local.sh
@@ -7,12 +7,20 @@ if [ -z "$CORS_ORIGIN" ]; then export CORS_ORIGIN="http://foo,http://bar"; fi;
 
 set -u
 
-glob=$*
-if [ -z "$glob" ]; then
-  glob="test/local test/remote"
+GLOB=$*
+if [ -z "$GLOB" ]; then
+  GLOB="test/local test/remote"
+fi
+
+DEFAULT_ARGS="-R dot --recursive"
+
+# When running under Windows Subsystem for Linux,
+# some tests take unusually long to complete.
+if uname -a | grep -q 'Microsoft'; then
+  DEFAULT_ARGS="$DEFAULT_ARGS --timeout 5000"
 fi
 
 ./scripts/gen_keys.js
 ./scripts/gen_vapid_keys.js
-./scripts/mocha-coverage.js -R dot --recursive $glob
+./scripts/mocha-coverage.js $DEFAULT_ARGS $GLOB
 grunt eslint copyright
