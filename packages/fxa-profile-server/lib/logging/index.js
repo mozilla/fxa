79611e9ac65b381e721d09c15fa904ef240a1a07
Deepti <deeptibaghel@gmail.com>
2018-04-04 18:31:54 +0530

    fix(mozlog): fix deprecation warning mozilla/fxa-profile-server#315) r=@vladikoff

--- packages/fxa-profile-server/lib/logging/index.js
+++ packages/fxa-profile-server/lib/logging/index.js
@@ -2,10 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const mozlog = require('mozlog');
-
 const config = require('../config');
 
-mozlog.config(config.get('logging'));
+const mozlog = require('mozlog')(config.get('logging'));
 
 module.exports = mozlog;
