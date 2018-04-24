456f895721d810ad17843366ab21bcdbd7f652f9
Phil Booth <pmbooth@gmail.com>
2018-04-24 16:50:09 +0100

    chore(project): add rocket dependency

--- packages/fxa-email-service/src/main.rs
+++ packages/fxa-email-service/src/main.rs
@@ -1,3 +1,13 @@
+#![feature(plugin)]
+#![plugin(rocket_codegen)]
+
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+  "Hello, world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+  rocket::ignite().mount("/", routes![index]).launch();
 }
