34e741a612f2ebef11d5eeff9930ce7d566d0a49
Shane Tomlinson <stomlinson@mozilla.com>
2018-05-14 22:06:48 +0100

    refactor(style): General photon style updates mozilla/fxa-content-server#6185) r=@vbudhram
    This is a general update to more closely align
    FxA's styles to the Photon styles specified
    in https://app.zeplin.io/project/5ad79b0f8964ebca1c06cab8/dashboard
    
    * Email opt-in text on email-first sign up page uses the same text as the legacy sign up page.
    * Firefox logo is 56x56px on mobile
    * Show the user's email address in bold and on its own line in the confirm page.
    * Simplify the .graphic CSS, styling
    * Headers follow Photon guidelines
    * Spacing around bottom links follow Ryan Gaddis' guidelines
    * Simplify tos-pp CSS, styling.
    
    issue mozilla/fxa-content-server#6083

--- packages/fxa-content-server/app/scripts/views/confirm.js
+++ packages/fxa-content-server/app/scripts/views/confirm.js
@@ -49,7 +49,7 @@ define(function (require, exports, module) {
         // a discussion for another time.
         canGoBack: isSignIn && this.canGoBack(),
         email,
-        escapedEmail: _.escape(email),
+        escapedEmail: `<span class="email">${_.escape(email)}</span>`,
         isSignIn,
         isSignUp
       });
