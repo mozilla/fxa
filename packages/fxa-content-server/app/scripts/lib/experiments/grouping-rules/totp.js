2b53741c56c9c7d375119dd18163abcbdd2a54dc
Vijay Budhram <vbudhram@gmail.com>
2018-05-18 11:38:21 -0400

    feat(totp): enable totp for 10% of all users mozilla/fxa-content-server#6212), r=@shane-tomlinson

--- packages/fxa-content-server/app/scripts/lib/experiments/grouping-rules/totp.js
+++ packages/fxa-content-server/app/scripts/lib/experiments/grouping-rules/totp.js
@@ -12,7 +12,7 @@ module.exports = class TotpGroupingRule extends BaseGroupingRule {
   constructor() {
     super();
     this.name = 'totp';
-    this.ROLLOUT_RATE = 0.00;
+    this.ROLLOUT_RATE = 0.10;
   }
 
   choose(subject) {
