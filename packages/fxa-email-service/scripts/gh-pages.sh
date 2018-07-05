e83e5f915cc92df18012bb2c492a4c91ec84355e
Phil Booth <pmbooth@gmail.com>
2018-07-05 07:53:08 +0100

    fix(scripts): don't try to deploy docs if they haven't changed

--- packages/fxa-email-service/scripts/gh-pages.sh
+++ packages/fxa-email-service/scripts/gh-pages.sh
@@ -6,21 +6,38 @@
 
 set -e
 
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-  eval key=\$encrypted_08b3d3de2dc6_key
-  eval iv=\$encrypted_08b3d3de2dc6_iv
+echo "Branch: $TRAVIS_BRANCH    Pull request: $TRAVIS_PULL_REQUEST"
 
-  mkdir -p ~/.ssh
-  openssl aes-256-cbc -K $key -iv $iv -in scripts/id_rsa.enc -out ~/.ssh/id_rsa -d
-  chmod 600 ~/.ssh/id_rsa
-
-  git clone --branch gh-pages git@github.com:mozilla/fxa-email-service.git docs
-  cd docs
-  git config user.name "fxa-devs"
-  git config user.email "fxa-core@mozilla.com"
-  rm -rf *
-  mv ../target/doc/* .
-  git add -A .
-  git commit -qm "chore(docs): rebuild developer docs"
-  git push -q origin gh-pages
+if [ "$TRAVIS_BRANCH" != "master" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Not building docs."
+  exit 0
 fi
+
+echo "Building docs."
+
+eval key=\$encrypted_08b3d3de2dc6_key
+eval iv=\$encrypted_08b3d3de2dc6_iv
+
+mkdir -p ~/.ssh
+openssl aes-256-cbc -K $key -iv $iv -in scripts/id_rsa.enc -out ~/.ssh/id_rsa -d
+chmod 600 ~/.ssh/id_rsa
+
+git clone --branch gh-pages git@github.com:mozilla/fxa-email-service.git docs
+cd docs
+rm -rf *
+mv ../target/doc/* .
+
+CHANGES=`git status --porcelain`
+
+if [ "$CHANGES" = "" ]; then
+  echo "Docs are unchanged, not deploying to GitHub Pages."
+  exit 0
+fi
+
+echo "Deploying docs to GitHub Pages."
+
+git config user.name "fxa-devs"
+git config user.email "fxa-core@mozilla.com"
+git add -A .
+git commit -qm "chore(docs): rebuild developer docs"
+git push -q origin gh-pages
