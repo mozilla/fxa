59a65c36db8e67da3b6218423a660f904961b928
Shane Tomlinson <stomlinson@mozilla.com>
2018-08-23 17:45:56 +0100

    feat(blog): Use mustache templates for the blog post generation. (#310)

--- scripts/blog_post.js
+++ scripts/blog_post.js
@@ -2,9 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const assert = require('assert')
 const P = require('bluebird')
+const fs = require('fs');
+const mustache = require('mustache');
+const path = require('path');
+
 const ghlib = require('./ghlib.js')
+
+const template = fs.readFileSync(path.join(__dirname, 'templates', 'train_blog_post.mustache'), 'utf-8').toString();
 
 var trainNumber = parseInt(process.argv[2]);
 if (! trainNumber) {
@@ -56,22 +61,16 @@ ghlib.ensureLocalRepos()
   }).then(function () {
     var outfile = process.stdout
 
-    outfile.write('---\n')
-    outfile.write(`title: Firefox Accounts Train-${trainNumber}\n`)
-    outfile.write('author: <XXX>\n')
-    outfile.write('authorURL: <XXX>\n')
-    outfile.write('---\n\n')
-    outfile.write('Hi All,\n')
-    outfile.write('\n')
-    outfile.write('This week we shipped FxA train-' + trainNumber + ' to production,\n')
-    outfile.write('with the following highlights:\n')
-    outfile.write('\n<!--truncate-->\n\n')
-
     var submittedByUser = {}
     var reviewedByUser = {}
     var commentedByUser = {}
-    var milestonesByTitle = {}
-    var commitsByMilestone = {}
+    var milestoneCountsByTitle = {}
+    const milestonesByTitle = [];
+    const milestones = {};
+    const contributors = [];
+    const reposWithPRs = [];
+    var totalPRs = 0
+    var totalReviews = 0
 
     function incr(what, who) {
       if (! what[who]) {
@@ -93,187 +92,116 @@ ghlib.ensureLocalRepos()
       if (!repoInfo.commits) {
         return
       }
-      const listedPRs = new Set();
 
-      var currentCommitSummary = []
+      const prsHandled = new Set();
+
       return P.each(repoInfo.commits, function(commit) {
         commitInfo = repoInfo.commitInfo[commit];
         if (commitInfo.ignore) {
           return
         }
+
         if (!commitInfo.fromMerge) {
           // It's a top-level merge with reviewers, comments, etc.
-          var milestone = '';
+          var milestone = 'No milestone';
           var prInfo = commitInfo.fromPR
           if (prInfo && prInfo.milestone) {
             milestone = prInfo.milestone.title
-            incr(milestonesByTitle, prInfo.milestone.title)
+            incr(milestoneCountsByTitle, prInfo.milestone.title)
           }
-          if (! commitsByMilestone[milestone]) {
-            commitsByMilestone[milestone] = []
+          if (! milestones[milestone]) {
+            milestones[milestone] = {
+              name: milestone,
+              prs: []
+            }
           }
-          currentCommitSummary = []
-          commitsByMilestone[milestone].push(currentCommitSummary)
           if (prInfo) {
-            if (listedPRs.has(prInfo.number)) {
+            if (prsHandled.has(prInfo.number)) {
               return;
             }
-            listedPRs.add(prInfo.number)
-            currentCommitSummary.push('  * ' + prInfo.html_url)
-            currentCommitSummary.push('      ' + commitInfo.message.split('\n')[0])
+
+            milestones[milestone].prs.push({
+              html_url: prInfo.html_url,
+              message: commitInfo.message.split('\n')[0]
+            })
+            prsHandled.add(prInfo.number)
+
             incr(submittedByUser, prInfo.submitter)
             incrEach(reviewedByUser, prInfo.reviewers)
             incrEach(commentedByUser, prInfo.commenters)
-          } else {
+          } /*else {
             currentCommitSummary.push('  * #<unknown> in ' + repoName)
             currentCommitSummary.push('      ' + commitInfo.message.replace(/\n/g,'\n      '))
-          }
+          }*/
         }
-      })
-    })
-    // Print out commit summaries grouped by milestone
-    .then(function() {
-      // Try to put them in a sensible order.
-      // Nicely-formatted "FxA-NN: title" milestones go first in number order,
-      // followed by other things in alphabetical order.
-      var sortedMilestones = Object.keys(commitsByMilestone).sort(function(a, b) {
-        if (a.indexOf('FxA-') === 0) {
-          if (b.indexOf('FxA-') === 0) {
-            return parseInt(a.split('-')[1], 10) - parseInt(b.split('-')[1], 10)
-          }
-          return -1
-        }
-        if (b.indexOf('FxA-') === 0) {
-          return 1
-        }
-        if (b === '' || a < b) {
-          return -1
-        }
-        return 1
-      })
-      sortedMilestones.forEach(function(milestone) {
-        outfile.write('\n')
-        outfile.write(`## ${milestone || 'No milestone'}\n`)
-        outfile.write('\n')
-        outfile.write('<XXX>Summary\n')
-        commitsByMilestone[milestone].forEach(function(commitSummary) {
-          outfile.write('\n')
-          outfile.write(commitSummary.join('\n'))
-          outfile.write('\n')
-        })
       })
     })
     // Explicitly thank our community contributors, if any
     .then(function() {
-      var contributors = []
       Object.keys(submittedByUser).forEach(function(username) {
         if (! ghlib.isCoreContributor(username)) {
-          contributors.push(username)
+          contributors.push({ username })
         }
       })
-      if (contributors.length > 0) {
-        outfile.write('\n\n')
-        outfile.write('Special thanks go to the following community contributors,\n')
-        outfile.write('who have code shipping in this train:\n')
-        outfile.write('\n')
-        contributors.forEach(function(username) {
-          outfile.write('  * ' + username + '\n')
-        })
-      }
     })
     // Link to the changelogs for repos with changes
     .then(function() {
-      outfile.write('\n\n')
-      outfile.write('As always, you can find more details in the changelogs for each repo:\n')
-      outfile.write('\n')
       return P.each(Object.keys(TRAIN_REPOS), function(repoName) {
         var repoInfo = TRAIN_REPOS[repoName]
         if (repoInfo.commits) {
-          outfile.write('  * https://github.com/mozilla/')
-          outfile.write(repoName + '/blob/' + repoInfo.trainTag + '/CHANGELOG.md\n')
+          reposWithPRs.push({
+            name: repoName,
+            trainTag: repoInfo.trainTag
+          })
         }
       })
     })
     // List some basic PR stats.
     .then(function () {
-      outfile.write('\n\n')
-      var totalPRs = 0
       Object.keys(submittedByUser).forEach(function(username) {
         totalPRs += submittedByUser[username]
       })
-      var totalReviews = 0
       Object.keys(reviewedByUser).forEach(function(username) {
         totalReviews += reviewedByUser[username]
       })
-      outfile.write('This train we had a total of ' + totalPRs + ' PRs')
-      outfile.write(' and ' + totalReviews + ' reviews.\n')
     })
     // List the features we contributed to, and their completion status.
     .then(function() {
-      outfile.write('That includes work on the following features:\n')
-      outfile.write('\n')
-      var titles = Object.keys(milestonesByTitle)
+
+      var titles = Object.keys(milestoneCountsByTitle)
       titles.sort()
       return ghlib.getMilestoneIssueCounts(gh, titles).then(function(milestoneIssueCounts) {
         var allCounts = Object.keys(milestoneIssueCounts).map(function(title) {
           return milestoneIssueCounts[title]
         })
-        allCounts.push(milestonesByTitle)
+        allCounts.push(milestoneCountsByTitle)
         var alignedNames = alignNames(titles)
         var alignedCounts = alignCounts.apply(null, allCounts)
-        return P.each(titles, function(title) {
+        titles.forEach((title) => {
           if (title !== 'FxA-0: quality') {
-            var count = milestonesByTitle[title]
-            var closedIssues = milestoneIssueCounts[title].closed
-            var totalIssues = milestoneIssueCounts[title].total
-            var percComplete = milestoneIssueCounts[title].perc_complete
-            outfile.write('  * ' + alignedNames[title] + ' ')
-            outfile.write(alignedCounts[count] + ' PRs ')
-            outfile.write('(now ' + alignedCounts[closedIssues])
-            outfile.write(' /' + alignedCounts[totalIssues])
-            outfile.write(' = ' + alignedCounts[percComplete] + '% complete)\n')
+            milestonesByTitle.push({
+              title: alignedNames[title],
+              closedIssues: milestoneIssueCounts[title].closed,
+              percComplete: milestoneIssueCounts[title].perc_complete,
+              totalIssues: milestoneIssueCounts[title].total,
+            })
           }
         })
-        .then(function() {
-          var count = milestonesByTitle['FxA-0: quality']
-          outfile.write('\n')
-          outfile.write('Along with ' + count + ' general quality improvements.\n')
-        })
       })
     })
-    // If enabled, this would list contributors by number of commits, prs, reviews.
-    // This didn't seem helpful in practice but I'm leaving it here for now in
-    // case we want to experiment with it any further.
-    .then(function () {
-      return true
-      outfile.write('\n')
-      outfile.write('And it\'s all thanks to the following contributors:\n')
-      outfile.write('\n')
-      var usernames = Object.keys(Object.assign(
-        {},
-        submittedByUser,
-        reviewedByUser,
-        commentedByUser
-      ))
-      usernames.sort()
-      var alignedNames = alignNames(usernames)
-      var alignedCounts = alignCounts(submittedByUser, reviewedByUser, commentedByUser, { 0: 0 })
-      return P.each(usernames, function(username) {
-        if (username === 'GitCop' || username === 'coveralls') {
-          // lol nope
-          return
-        }
-        outfile.write('  * ')
-        outfile.write(alignedNames[username])
-        outfile.write('  ')
-        outfile.write(alignedCounts[submittedByUser[username] || 0] + ' PRs submitted, ')
-        outfile.write(alignedCounts[reviewedByUser[username] || 0] + ' reviewed, ')
-        outfile.write(alignedCounts[commentedByUser[username] || 0] + ' touched\n')
-      })
-    })
-    // And a blank line, everyone loves trailing blank lines.
-    .then(function() {
-      outfile.write('\n')
+    .then(() => {
+      const text = mustache.render(template, {
+        author: 'XXX',
+        authorUrl: 'XXX',
+        contributors,
+        milestones: sortMilestones(Object.keys(milestones)).map(milestone => milestones[milestone]),
+        milestonesByTitle,
+        repos: reposWithPRs,
+        qualityCount: milestoneCountsByTitle['FxA-0: quality'],
+        trainNumber,
+      });
+
+      outfile.write(text);
     })
   });
 })
@@ -331,4 +259,25 @@ function alignCounts(counts /* ... */) {
     }
   })
   return alignedCounts
+}
+
+// Try to put them in a sensible order.
+// Nicely-formatted "FxA-NN: title" milestones go first in number order,
+// followed by other things in alphabetical order.
+function sortMilestones(milestones) {
+  return milestones.sort(function(a, b) {
+    if (a.indexOf('FxA-') === 0) {
+      if (b.indexOf('FxA-') === 0) {
+        return parseInt(a.split('-')[1], 10) - parseInt(b.split('-')[1], 10)
+      }
+      return -1
+    }
+    if (b.indexOf('FxA-') === 0) {
+      return 1
+    }
+    if (b === '' || a < b) {
+      return -1
+    }
+    return 1
+  })
 }
