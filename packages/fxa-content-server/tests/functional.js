ba616cd742351b8f404ef49ba8f73aead815d9b5
Vlad Filippov <vlad.filippov@gmail.com>
2018-08-23 15:53:01 -0400

    feat(tests): add E2E tests for the fxa-email-service mozilla/fxa-content-server#6470) r=@vbudhram
    Fixes mozilla/fxa-content-server#6372

--- packages/fxa-content-server/tests/functional.js
+++ packages/fxa-content-server/tests/functional.js
@@ -3,6 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 module.exports = [
+  'tests/functional/email_service.js',
   'tests/functional/mailcheck.js',
   'tests/functional/sync_v3_email_first.js',
   'tests/functional/fx_firstrun_v2_email_first.js',
