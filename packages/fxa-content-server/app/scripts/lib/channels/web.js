f9ee95a904aa7b8c36767a750e35b70aec5a0b76
Shane Tomlinson <stomlinson@mozilla.com>
2018-09-26 13:05:08 +0100

    refactor(pairing): Convert lib/channels/web.js to ES6 module format.
    extraction from mozilla/fxa-content-server#6514

--- packages/fxa-content-server/app/scripts/lib/channels/web.js
+++ packages/fxa-content-server/app/scripts/lib/channels/web.js
@@ -6,110 +6,106 @@
 // https://developer.mozilla.org/en-US/docs/Mozilla/JavaScript_code_modules/WebChannel.jsm
 // https://developer.mozilla.org/en-US/docs/Mozilla/JavaScript_code_modules/FxAccountsOAuthClient.jsm
 
-define(function (require, exports, module) {
-  'use strict';
+import _ from 'underscore';
+import AuthErrors from 'lib/auth-errors';
+import Cocktail from 'cocktail';
+import DuplexChannel from 'lib/channels/duplex';
+import UrlMixin from 'lib/url-mixin';
+import UserAgentMixin from 'lib/user-agent-mixin';
+import WebChannelReceiver from 'lib/channels/receivers/web-channel';
+import WebChannelSender from 'lib/channels/senders/web-channel';
 
-  const _ = require('underscore');
-  const AuthErrors = require('lib/auth-errors');
-  const Cocktail = require('cocktail');
-  const DuplexChannel = require('lib/channels/duplex');
-  const UrlMixin = require('lib/url-mixin');
-  const UserAgentMixin = require('lib/user-agent-mixin');
-  const WebChannelReceiver = require('lib/channels/receivers/web-channel');
-  const WebChannelSender = require('lib/channels/senders/web-channel');
+const FXA_STATUS_MIN_FIREFOX_DESKTOP_VERSION = 55;
 
-  const FXA_STATUS_MIN_FIREFOX_DESKTOP_VERSION = 55;
-
-  // The corresponding Firefox list is in
-  // https://dxr.mozilla.org/mozilla-central/rev/8a7d0b15595f9916123848ca906f29c62d4914c9/services/fxaccounts/FxAccountsWebChannel.jsm#30
-  const COMMANDS = {
-    CAN_LINK_ACCOUNT: 'fxaccounts:can_link_account',
-    CHANGE_PASSWORD: 'fxaccounts:change_password',
-    // Fx Desktop expects fxaccounts:delete, Fennec expects fxaccounts:delete_account.
-    // See https://github.com/mozilla/fxa-content-server/issues/3432
-    DELETE: 'fxaccounts:delete',
-    DELETE_ACCOUNT: 'fxaccounts:delete_account',
-    FXA_STATUS: 'fxaccounts:fxa_status',
-    LOADED: 'fxaccounts:loaded',
-    LOGIN: 'fxaccounts:login',
-    LOGOUT: 'fxaccounts:logout',
-    PROFILE_CHANGE: 'profile:change',
-    /*
+// The corresponding Firefox list is in
+// https://dxr.mozilla.org/mozilla-central/rev/8a7d0b15595f9916123848ca906f29c62d4914c9/services/fxaccounts/FxAccountsWebChannel.jsm#30
+const COMMANDS = {
+  CAN_LINK_ACCOUNT: 'fxaccounts:can_link_account',
+  CHANGE_PASSWORD: 'fxaccounts:change_password',
+  // Fx Desktop expects fxaccounts:delete, Fennec expects fxaccounts:delete_account.
+  // See https://github.com/mozilla/fxa-content-server/issues/3432
+  DELETE: 'fxaccounts:delete',
+  DELETE_ACCOUNT: 'fxaccounts:delete_account',
+  FXA_STATUS: 'fxaccounts:fxa_status',
+  LOADED: 'fxaccounts:loaded',
+  LOGIN: 'fxaccounts:login',
+  LOGOUT: 'fxaccounts:logout',
+  PROFILE_CHANGE: 'profile:change',
+  /*
     SYNC_PREFERENCES: 'fxaccounts:sync_preferences', // Removed in issue #4250
     */
-    VERIFIED: 'fxaccounts:verified',
-  };
+  VERIFIED: 'fxaccounts:verified',
+};
 
-  function WebChannel(id) {
-    if (! id) {
-      throw new Error('WebChannel must have an id');
-    }
-
-    this._id = id;
+function WebChannel(id) {
+  if (! id) {
+    throw new Error('WebChannel must have an id');
   }
 
-  _.extend(WebChannel, COMMANDS);
+  this._id = id;
+}
 
-  _.extend(WebChannel.prototype, new DuplexChannel(), {
-    COMMANDS,
+_.extend(WebChannel, COMMANDS);
 
-    initialize (options = {}) {
-      const win = this.window = options.window || window;
-      const webChannelId = this._id;
+_.extend(WebChannel.prototype, new DuplexChannel(), {
+  COMMANDS,
 
-      var sender = this._sender = new WebChannelSender();
-      sender.initialize({
-        webChannelId,
-        window: win
-      });
+  initialize (options = {}) {
+    const win = this.window = options.window || window;
+    const webChannelId = this._id;
 
-      var receiver = this._receiver = new WebChannelReceiver();
-      receiver.initialize({
-        webChannelId,
-        window: win
-      });
+    var sender = this._sender = new WebChannelSender();
+    sender.initialize({
+      webChannelId,
+      window: win
+    });
 
-      DuplexChannel.prototype.initialize.call(this, {
-        receiver,
-        sender,
-        window: win
-      });
-    },
+    var receiver = this._receiver = new WebChannelReceiver();
+    receiver.initialize({
+      webChannelId,
+      window: win
+    });
 
-    /**
-     * Check if the browser supports the FXA_STATUS request.
-     *
-     * @param {String} [userAgent] UA string to check.
-     *   Defaults to `this.getUserAgentString()`
-     * @returns {Boolean}
-     */
-    isFxaStatusSupported (userAgent = this.getUserAgentString()) {
-      const uap = this.getUserAgent(userAgent);
-      return uap.isFirefoxDesktop() && uap.parseVersion().major >= FXA_STATUS_MIN_FIREFOX_DESKTOP_VERSION;
-    },
+    DuplexChannel.prototype.initialize.call(this, {
+      receiver,
+      sender,
+      window: win
+    });
+  },
 
-    onErrorReceived (message) {
-      const { error } = this.parseError(message);
-      const errorMessage = error && error.message;
+  /**
+   * Check if the browser supports the FXA_STATUS request.
+   *
+   * @param {String} [userAgent] UA string to check.
+   *   Defaults to `this.getUserAgentString()`
+   * @returns {Boolean}
+   */
+  isFxaStatusSupported (userAgent = this.getUserAgentString()) {
+    const uap = this.getUserAgent(userAgent);
+    return uap.isFirefoxDesktop() && uap.parseVersion().major >= FXA_STATUS_MIN_FIREFOX_DESKTOP_VERSION;
+  },
 
-      // Browser does not support WebChannels sent on this channel,
-      // or from this domain.
-      if (/no such channel/i.test(errorMessage)) {
-        // Since the channel is not supported, reject all outstanding
-        // requests to avoid hanging until the requests time out.
-        this.rejectAllOutstandingRequests(AuthErrors.toError('INVALID_WEB_CHANNEL'));
-      }
+  onErrorReceived (message) {
+    const { error } = this.parseError(message);
+    const errorMessage = error && error.message;
 
-      DuplexChannel.prototype.onErrorReceived.call(this, message);
+    // Browser does not support WebChannels sent on this channel,
+    // or from this domain.
+    if (/no such channel/i.test(errorMessage)) {
+      // Since the channel is not supported, reject all outstanding
+      // requests to avoid hanging until the requests time out.
+      this.rejectAllOutstandingRequests(AuthErrors.toError('INVALID_WEB_CHANNEL'));
     }
-  });
 
-  Cocktail.mixin(
-    WebChannel,
-    UrlMixin,
-    UserAgentMixin
-  );
-
-
-  module.exports = WebChannel;
+    DuplexChannel.prototype.onErrorReceived.call(this, message);
+  }
 });
+
+Cocktail.mixin(
+  WebChannel,
+  UrlMixin,
+  UserAgentMixin
+);
+
+
+module.exports = WebChannel;
