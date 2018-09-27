33687ac14c690f6238574a657ea527e95aae05c5
Phil Booth <pmbooth@gmail.com>
2018-09-27 15:11:42 +0100

    fix(scripts): make tls-shrink script portable

--- packages/fxa-auth-server/scripts/tls-shrink.sh
+++ packages/fxa-auth-server/scripts/tls-shrink.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-sed -i '' 's/"resolved": "http:\/\/registry\.npmjs\.org\//"resolved": "https:\/\/registry.npmjs.org\//' npm-shrinkwrap.json
+sed -i.orig 's/"resolved": "http:\/\/registry\.npmjs\.org\//"resolved": "https:\/\/registry.npmjs.org\//' npm-shrinkwrap.json
+rm -f npm-shrinkwrap.json.orig
