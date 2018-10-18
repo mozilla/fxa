21230eea1d6a13fbfb3d0fa368ada7eefed83d5c
Vijay Budhram <vbudhram@gmail.com>
2018-10-18 13:46:37 -0400

    fix(account): update stored procedures to be more replication friendly

---
+++ packages/fxa-auth-db-mysql/lib/db/schema/patch-087-086.sql
@@ -0,0 +1,6 @@
+-- SET NAMES utf8mb4 COLLATE utf8mb4_bin;
+
+-- DROP PROCEDURE verifyTokensWithMethod_3;
+-- DROP PROCEDURE verifyTokenCode_2;
+
+-- UPDATE dbMetadata SET value = '86' WHERE name = 'schema-patch-level';
