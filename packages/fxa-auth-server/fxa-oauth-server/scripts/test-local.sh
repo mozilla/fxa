f8f816fa3c7d2127edfcc8ff4bfa72336831023c
vladikoff <vlad.filippov@gmail.com>
2018-12-12 18:18:07 -0500

    refactor(oauth): combine oauth deps and package.json with auth
    Fixes mozilla/fxa-auth-server#2748

--- packages/fxa-auth-server/fxa-oauth-server/scripts/test-local.sh
+++ packages/fxa-auth-server/fxa-oauth-server/scripts/test-local.sh
@@ -6,9 +6,11 @@ if [ -z "$NODE_ENV" ]; then export NODE_ENV=test; fi;
 
 set -u
 
+node ./fxa-oauth-server/scripts/gen_keys.js
+
 GLOB=$*
 if [ -z "$GLOB" ]; then
-  GLOB="test/*.js test/routes/*.js test/db/*.js"
+  GLOB="fxa-oauth-server/test/*.js fxa-oauth-server/test/routes/*.js fxa-oauth-server/test/db/*.js"
 fi
 
 DEFAULT_ARGS="-R spec --timeout 20000 --recursive --exit"
