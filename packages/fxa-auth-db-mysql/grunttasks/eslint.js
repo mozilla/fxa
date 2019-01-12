cf387c833bf5b3b8ee9c7eb1321658b906a4f9f4
John Morrison <jrgmorrison@gmail.com>
2019-01-11 20:55:46 -0800

    fix(test): add a test script to add account rows

--- packages/fxa-auth-db-mysql/grunttasks/eslint.js
+++ packages/fxa-auth-db-mysql/grunttasks/eslint.js
@@ -14,7 +14,7 @@ module.exports = function (grunt) {
       '{grunttasks,bin,lib,lib/db,scripts,test}/*.js',
       'db-server/index.js',
       'db-server/{lib,test,test/backend,test/local}/*.js',
-      'test/{backend,local,mem}/*.js'
+      'test/{backend,local,mem,scripts}/*.js'
     ]
   })
 
