9d6cddb47118362cdf02f7e8205590304dc5fead
Shane Tomlinson <stomlinson@mozilla.com>
2019-01-15 16:41:37 +0000

    fix(oauth): Redirect GET /authorization directly to content server
    Logic was duplicated between the content and oauth server, this
    redirects from the oauth server to the content server.
    
    To prevent malicious oauth servers from stealing keys,
    keys_jwk is banned from transiting the OAuth server.
    
    fixes mozilla/fxa-auth-server#2862
    fixes bz1456351

--- packages/fxa-auth-server/fxa-oauth-server/lib/routes/redirect.js
+++ packages/fxa-auth-server/fxa-oauth-server/lib/routes/redirect.js
@@ -4,59 +4,23 @@
 
 const url = require('url');
 
-const config = require('../config');
+const contentUrl = require('../config').get('contentUrl');
 const AppError = require('../error');
-
-const ACTION_TO_PATHNAMES = {
-  'email': '',
-  'signin': 'signin',
-  'signup': 'signup',
-  'force_auth': 'force_auth'
-};
-
-function actionToPathname(action) {
-  if (action === undefined) {
-    return '';
-  }
-
-  if (ACTION_TO_PATHNAMES.hasOwnProperty(action)) {
-    return ACTION_TO_PATHNAMES[action];
-  }
-
-  throw new Error('Bad action parameter');
-}
 
 module.exports = {
   handler: async function redirectAuthorization(req, h) {
-    var redirect = url.parse(config.get('contentUrl'), true);
-    var err = false;
-
-    try {
-      redirect.pathname += actionToPathname(req.query.action);
-    } catch (e) {
-      err = true;
-      throw AppError.invalidRequestParameter('action');
+    // keys_jwk is barred from transiting the OAuth server
+    // to prevent a malicious OAuth server from stealing
+    // a user's Scoped Keys. See bz1456351
+    if (req.query.keys_jwk) {
+      throw AppError.invalidRequestParameter('keys_jwk');
     }
 
-    if (! err) {
-      if (req.query.action !== 'email') {
-        // only `action=email` is propagated as a hint
-        // to the content server to show the email-first
-        // flow. All other actions redirect to a named
-        // endpoint.
-        delete req.query.action;
-      }
+    const redirect = url.parse(contentUrl, true);
+    redirect.pathname = '/authorization';
+    redirect.query = req.query;
+    redirect.search = req.search;
 
-      if (req.query.login_hint && ! req.query.email) {
-        req.query.email = req.query.login_hint;
-        delete req.query.login_hint;
-      }
-
-      redirect.query = req.query;
-
-      delete redirect.search;
-      delete redirect.path;
-      return h.redirect(url.format(redirect));
-    }
+    return h.redirect(url.format(redirect));
   }
 };
