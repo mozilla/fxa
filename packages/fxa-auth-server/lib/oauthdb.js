6ce5ec58a0e5d6f0b4116270df3fd4e757d14706
Ryan Kelly <rfkelly@mozilla.com>
2019-01-17 13:25:13 +1100

    fix(oauth): Use correct max length for oauth client name validation.

--- packages/fxa-auth-server/lib/oauthdb.js
+++ packages/fxa-auth-server/lib/oauthdb.js
@@ -63,7 +63,7 @@ module.exports = (log, config) => {
         },
         response: {
           id: validators.clientId.required(),
-          name: Joi.string().max(25).regex(validators.DISPLAY_SAFE_UNICODE).required(),
+          name: Joi.string().max(255).regex(validators.DISPLAY_SAFE_UNICODE).required(),
           trusted: Joi.boolean().required(),
           image_uri: Joi.string().optional().allow(''),
           redirect_uri: Joi.string().required().allow('')
