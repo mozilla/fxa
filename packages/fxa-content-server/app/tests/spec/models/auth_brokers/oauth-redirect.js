ffe291d9dd30aca60806e2dc692244542baca21e
hritvi <hritvi2801@gmail.com>
2019-02-13 21:30:08 +0530

    perf(auth_brokers): Merged oauth and oauth-redirect brokers

--- packages/fxa-content-server/app/tests/spec/models/auth_brokers/oauth-redirect.js
+++ packages/fxa-content-server/app/tests/spec/models/auth_brokers/oauth-redirect.js
@@ -5,8 +5,13 @@
 define(function (require, exports, module) {
   'use strict';
 
+  const Account = require('models/account');
   const { assert } = require('chai');
+  const Assertion = require('lib/assertion');
+  const AuthErrors = require('lib/auth-errors');
   const Constants = require('lib/constants');
+  const OAuthClient = require('lib/oauth-client');
+  const OAuthErrors = require('lib/oauth-errors');
   const RedirectAuthenticationBroker = require('models/auth_brokers/oauth-redirect');
   const Relier = require('models/reliers/base');
   const Session = require('lib/session');
@@ -18,28 +23,69 @@ define(function (require, exports, module) {
 
   var REDIRECT_TO = 'https://redirect.here';
 
+
+  var HEX_CHARSET = '0123456789abcdef';
+  function generateOAuthCode() {
+    var code = '';
+
+    for (var i = 0; i < 64; ++i) {
+      code += HEX_CHARSET.charAt(Math.floor(Math.random() * 16));
+    }
+
+    return code;
+  }
+
+  const REDIRECT_URI = 'https://127.0.0.1:8080';
+  const VALID_OAUTH_CODE = generateOAuthCode();
+  const VALID_OAUTH_CODE_REDIRECT_URL = `${REDIRECT_URI}?code=${VALID_OAUTH_CODE}&state=state`;
+
   describe('models/auth_brokers/redirect', () => {
     var account;
+    var assertionLibrary;
     var broker;
     var metrics;
+    var oAuthClient;
     var relier;
     var user;
     var windowMock;
 
     beforeEach(() => {
+      oAuthClient = new OAuthClient();
+      sinon.stub(oAuthClient, 'getCode').callsFake(function () {
+        return Promise.resolve({
+          code: VALID_OAUTH_CODE,
+          redirect: VALID_OAUTH_CODE_REDIRECT_URL,
+          state: 'state'
+        });
+      });
+
+      assertionLibrary = new Assertion({});
+      sinon.stub(assertionLibrary, 'generate').callsFake(function () {
+        return Promise.resolve('assertion');
+      });
       metrics = {
         flush: sinon.spy(() => Promise.resolve()),
         logEvent: () => {}
       };
       relier = new Relier();
+      relier.set({
+        action: 'action',
+        clientId: 'clientId',
+        redirectUri: REDIRECT_URI,
+        scope: 'scope',
+        state: 'state'
+      });
       user = new User();
+
       windowMock = new WindowMock();
 
       account = user.initAccount({
         sessionToken: 'abc123'
       });
       broker = new RedirectAuthenticationBroker({
+        assertionLibrary: assertionLibrary,
         metrics: metrics,
+        oAuthClient: oAuthClient,
         relier: relier,
         session: Session,
         window: windowMock
@@ -49,14 +95,15 @@ define(function (require, exports, module) {
       sinon.stub(broker, 'finishOAuthFlow').callsFake(() => {
         return Promise.resolve();
       });
+      // sinon.spy(broker, 'finishOAuthFlow');
     });
 
     it('has the `signup` capability by default', () => {
       assert.isTrue(broker.hasCapability('signup'));
     });
 
-    it('does not have the `handleSignedInNotification` capability by default', () => {
-      assert.isFalse(broker.hasCapability('handleSignedInNotification'));
+    it('have the `handleSignedInNotification` capability by default', () => {
+      assert.isTrue(broker.hasCapability('handleSignedInNotification'));
     });
 
     it('has the `emailVerificationMarketingSnippet` capability by default', () => {
@@ -122,12 +169,244 @@ define(function (require, exports, module) {
     });
 
     describe('persistVerificationData', () => {
-      it('sets the Original Tab marker', () => {
+      it('sets the Original Tab marker and saves OAuth params to session', () => {
         return broker.persistVerificationData(account)
-          .then(() => {
+          .then(function () {
+            assert.ok(!! Session.oauth);
             assert.isTrue(broker.isOriginalTab());
           });
       });
+    });
+
+    describe('getOAuthResult', function () {
+      it('gets an object with the OAuth login information', function () {
+        return broker.getOAuthResult(account)
+          .then(function (result) {
+            assert.isTrue(assertionLibrary.generate.calledWith(account.get('sessionToken'), null, 'clientId'));
+            assert.equal(result.redirect, VALID_OAUTH_CODE_REDIRECT_URL);
+            assert.equal(result.state, 'state');
+            assert.equal(result.code, VALID_OAUTH_CODE);
+          });
+      });
+
+      it('locally constructs the redirect URI, ignoring any provided by the server', function () {
+        oAuthClient.getCode.restore();
+        sinon.stub(oAuthClient, 'getCode').callsFake(function () {
+          return Promise.resolve({
+            code: VALID_OAUTH_CODE,
+            redirect: 'https://the.server.got.confused',
+            state: 'state'
+          });
+        });
+        return broker.getOAuthResult(account)
+          .then(function (result) {
+            assert.isTrue(oAuthClient.getCode.calledOnce);
+            assert.equal(result.redirect, VALID_OAUTH_CODE_REDIRECT_URL);
+            assert.equal(result.state, 'state');
+            assert.equal(result.code, VALID_OAUTH_CODE);
+          });
+      });
+
+      it('passes on errors from assertion generation', function () {
+        assertionLibrary.generate.restore();
+        sinon.stub(assertionLibrary, 'generate').callsFake(function () {
+          return Promise.reject(new Error('uh oh'));
+        });
+
+        return broker.getOAuthResult(account)
+          .then(assert.fail, function (err) {
+            assert.equal(err.message, 'uh oh');
+          });
+      });
+
+      it('passes on errors from oAuthClient.getCode', function () {
+        oAuthClient.getCode.restore();
+        sinon.stub(oAuthClient, 'getCode').callsFake(function () {
+          return Promise.reject(new Error('uh oh'));
+        });
+
+        return broker.getOAuthResult(account)
+          .then(assert.fail, function (err) {
+            assert.equal(err.message, 'uh oh');
+          });
+      });
+
+      it('throws an error if oAuthClient.getCode returns nothing', function () {
+        oAuthClient.getCode.restore();
+        sinon.stub(oAuthClient, 'getCode').callsFake(function () {
+          return;
+        });
+
+        return broker.getOAuthResult(account)
+          .then(assert.fail, function (err) {
+            assert.isTrue(OAuthErrors.is(err, 'INVALID_RESULT'));
+          });
+      });
+
+      it('throws an error if oAuthClient.getCode returns an empty object', function () {
+        oAuthClient.getCode.restore();
+        sinon.stub(oAuthClient, 'getCode').callsFake(function () {
+          return {};
+        });
+
+        return broker.getOAuthResult(account)
+          .then(assert.fail, function (err) {
+            assert.isTrue(OAuthErrors.is(err, 'MISSING_PARAMETER'));
+          });
+      });
+
+      it('throws an error if oAuthClient.getCode returns an invalid code', function () {
+        oAuthClient.getCode.restore();
+        sinon.stub(oAuthClient, 'getCode').callsFake(function () {
+          return {
+            code: 'invalid-code'
+          };
+        });
+
+        return broker.getOAuthResult(account)
+          .then(assert.fail, function (err) {
+            assert.isTrue(OAuthErrors.is(err, 'INVALID_PARAMETER'));
+          });
+      });
+
+      it('throws an error if oAuthClient.getCode returns an invalid state', function () {
+        oAuthClient.getCode.restore();
+        sinon.stub(oAuthClient, 'getCode').callsFake(function () {
+          return {
+            code: VALID_OAUTH_CODE,
+            state: { invalid: 'state' }
+          };
+        });
+
+        return broker.getOAuthResult(account)
+          .then(assert.fail, function (err) {
+            assert.isTrue(OAuthErrors.is(err, 'INVALID_PARAMETER'));
+          });
+      });
+
+      it('throws an error if accountData is missing', function () {
+        return broker.getOAuthResult()
+          .then(assert.fail, function (err) {
+            assert.isTrue(AuthErrors.is(err, 'INVALID_TOKEN'));
+          });
+      });
+
+      it('throws an error if accountData is missing a sessionToken', function () {
+        return broker.getOAuthResult(user.initAccount())
+          .then(assert.fail, function (err) {
+            assert.isTrue(AuthErrors.is(err, 'INVALID_TOKEN'));
+          });
+      });
+    });
+
+    describe('transformLink', () => {
+      [
+        'force_auth',
+        'signin',
+        'signup',
+      ].forEach(route => {
+        describe(`${route}`, () => {
+          it('prepends `/oauth` to the link', () => {
+            assert.include(broker.transformLink(`/${route}`), `/oauth/${route}`);
+            assert.include(broker.transformLink(`${route}`), `/oauth/${route}`);
+          });
+        });
+      });
+
+      describe('/', () => {
+        it('prepends `/oauth` to the link', () => {
+          assert.include(broker.transformLink('/'), '/oauth/');
+        });
+      });
+
+      describe('not transformed', () => {
+        it('does not include the oauth prefix', () => {
+          const transformed = broker.transformLink('not_oauth');
+          assert.notInclude(transformed, 'oauth/not_oauth');
+          assert.include(transformed, 'not_oauth');
+        });
+      });
+    });
+
+    describe('_provisionScopedKeys', () => {
+      let accountKey;
+      const keysJwk = 'jwk';
+      const keys = {
+        kA: 'foo',
+        kB: 'bar'
+      };
+      const scope = 'https://identity.mozilla.com/apps/sample-scope-can-scope-key';
+      const keyData = {
+        [scope]: {
+          identifier: scope,
+          keyRotationSecret: '0000000000000000000000000000000000000000000000000000000000000000',
+          keyRotationTimestamp: 1506970363512
+        }
+      };
+
+      beforeEach(() => {
+        sinon.stub(broker._oAuthClient, 'getClientKeyData').callsFake((args) => {
+          assert.equal(args.assertion, 'assertion');
+          assert.equal(args.client_id, 'clientId');
+          assert.equal(args.scope, 'scope');
+
+          return Promise.resolve(keyData);
+        });
+
+        accountKey = new Account({
+          email: 'testuser@testuser.com',
+          keyFetchToken: 'key-fetch-token',
+          uid: 'uid',
+          unwrapBKey: 'unwrap-b-key'
+        });
+
+        relier.set('keysJwk', keysJwk);
+
+        sinon.stub(accountKey, 'accountKeys').callsFake((args) => {
+          return Promise.resolve(keys);
+        });
+      });
+
+      it('calls _provisionScopedKeys to encrypt the bundle', () => {
+        relier.set('keysJwk', keysJwk);
+
+        sinon.stub(broker._scopedKeys, 'createEncryptedBundle').callsFake((_keys, _uid, _keyData, _jwk) => {
+          assert.equal(_keys, keys);
+          assert.equal(_uid, 'uid');
+          assert.equal(_keyData, keyData);
+          assert.equal(_jwk, keysJwk);
+
+          return Promise.resolve('bundle');
+        });
+
+        return broker._provisionScopedKeys(accountKey, 'assertion')
+          .then((result) => {
+            assert.isTrue(broker._scopedKeys.createEncryptedBundle.calledOnce);
+            assert.equal(result, 'bundle');
+          });
+      });
+
+      it('returns null if no unwrapBKey', () => {
+        accountKey.set('unwrapBKey', null);
+
+        return broker._provisionScopedKeys(accountKey, 'assertion')
+          .then((result) => {
+            assert.equal(result, null);
+          });
+      });
+
+      it('returns null if no clientKeyData', () => {
+        broker._oAuthClient.getClientKeyData.restore();
+        sinon.stub(broker._oAuthClient, 'getClientKeyData').callsFake((args) => {
+          return Promise.resolve({});
+        });
+
+        return broker._provisionScopedKeys(accountKey, 'assertion')
+          .then((result) => {
+            assert.equal(result, null);
+          });
+      });
+
     });
 
     describe('finishOAuthFlow', () => {
@@ -216,7 +495,7 @@ define(function (require, exports, module) {
         return broker.afterCompleteResetPassword(account)
           .then((behavior) => {
             assert.isFalse(broker.finishOAuthFlow.called);
-            assert.equal(behavior.type, 'null');
+            assert.equal(behavior.type, 'navigate');
           });
       });
 
@@ -230,7 +509,7 @@ define(function (require, exports, module) {
         return broker.afterCompleteResetPassword(account)
           .then((behavior) => {
             assert.isFalse(broker.finishOAuthFlow.called);
-            assert.equal(behavior.type, 'null');
+            assert.equal(behavior.type, 'navigate');
           });
       });
 
