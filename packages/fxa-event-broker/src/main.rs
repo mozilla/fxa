8b504a3e4a947b8fd39556a66042a0787c5e1e1f
Phil Booth <pmbooth@gmail.com>
2019-04-03 16:24:26 +0100

    feat(packages): create fxa-event-broker skeleton rust project

---
+++ packages/fxa-event-broker/src/main.rs
@@ -0,0 +1,14 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, you can obtain one at https://mozilla.org/MPL/2.0/.
+
+//! These are the developer docs
+//! for the Firefox Accounts event broker.
+//! For higher-level documentation,
+//! see the [readme].
+//!
+//! [readme]: https://github.com/mozilla/fxa/blob/master/packages/fxa-event-broker/README.md#fxa-event-broker
+
+fn main() {
+    println!("Hello, world!");
+}
