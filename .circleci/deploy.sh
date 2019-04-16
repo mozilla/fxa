06514e38a11b87415ba37715d2ba08c020180838
Phil Booth <pmbooth@gmail.com>
2019-04-16 18:59:17 +0100

    fix(scripts): make build and deploy scripts work with private repo

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -16,13 +16,16 @@ if grep -e "$MODULE" -e 'all' $DIR/../packages/test.list; then
     DOCKER_TAG="$CIRCLE_TAG"
   fi
 
-  if [ "$MODULE_SUFFIX" != "" ]; then
-    MODULE="${MODULE}-${MODULE_SUFFIX}"
+  if [ "$MODULE_SUFFIX" = "" ]; then
+    MODULE_QUALIFIED="$MODULE"
+  else
+    MODULE_QUALIFIED="${MODULE}-${MODULE_SUFFIX}"
   fi
+
   REPO=$(echo ${MODULE} | sed 's/-/_/g')
   DOCKER_USER=DOCKER_USER_${REPO}
   DOCKER_PASS=DOCKER_PASS_${REPO}
-  DOCKERHUB_REPO=mozilla/${MODULE}
+  DOCKERHUB_REPO=mozilla/${MODULE_QUALIFIED}
 
   if [ -n "${DOCKER_TAG}" ] && [ -n "${!DOCKER_PASS}" ] && [ -n "${!DOCKER_USER}" ]; then
     echo "${!DOCKER_PASS}" | docker login -u "${!DOCKER_USER}" --password-stdin
