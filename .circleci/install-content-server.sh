850e7e1d96ca8b4c41bda9a67087f6945f81cccd
John Morrison <jrgmorrison@gmail.com>
2019-04-16 15:07:01 -0700

    fix(version): content-server version.json goes in server/config/version.json

--- .circleci/install-content-server.sh
+++ .circleci/install-content-server.sh
@@ -6,7 +6,7 @@ if grep -e "fxa-content-server" -e 'all' $DIR/../packages/test.list; then
   sudo apt-get install -y graphicsmagick
   mkdir -p config
   cp ../version.json ./
-  cp ../version.json config
+  cp ../version.json server/config
   cd $DIR/..
   CIRCLECI=false npm install
   npx pm2 kill
