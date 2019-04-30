5a3f6dc026352f815e05e480fb2865490e3e6893
Phil Booth <pmbooth@gmail.com>
2019-04-30 07:14:46 +0100

    fix(ci): run eslint in ci

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -10,7 +10,14 @@ if grep -e "$MODULE" -e 'all' $DIR/../packages/test.list; then
     docker run --net="host" -e DB="mysql" ${MODULE}:test npm run test-oauth
   elif [[ -e Dockerfile-test ]]; then
     docker build -f Dockerfile-test -t ${MODULE}:test .
+
     docker run --net="host" ${MODULE}:test npm run ${TEST:-test}
+
+    if grep eslint "$DIR/../packages/$MODULE/Gruntfile.js"; then
+      docker run --net="host" ${MODULE}:test /app/node_modules/.bin/grunt eslint
+    elif grep '"eslint"' "$DIR/../packages/$MODULE/package.json"; then
+      docker run --net="host" ${MODULE}:test npm run lint
+    fi
   fi
 else
   exit 0;
