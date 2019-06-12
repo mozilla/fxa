6dc7757944e299f9fe1a1386dfbc7b88182e88b3
hritvi <hritvi2801@gmail.com>
2019-06-12 10:00:25 +0530

    refactor(display-name): Rendered the display-name component in react

--- packages/fxa-content-server/app/scripts/views/settings/display_name.jsx
+++ packages/fxa-content-server/app/scripts/views/settings/display_name.jsx
@@ -4,25 +4,30 @@
 
 import AvatarMixin from '../mixins/avatar-mixin';
 import Cocktail from 'cocktail';
-import DisableFormMixin from '../mixins/disable-form-mixin';
 import BaseView from '../base';
 import SettingsPanelMixin from '../mixins/settings-panel-mixin';
-// eslint-disable-next-line
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Translator from '../../lib/translator';
 
-const t = msg => msg;
+const translator = new Translator();
 
-function DisplayName(props){
+const t = msg => translator.get(msg);
+
+function DisplayName(props) {
   return (
     <div id="display-name" className="settings-unit">
       <div className="settings-unit-stub">
-        < DisplayNameComponent />
-        < ChangeorAddButtonComponent displayName={props.displayName}/>
+        <DisplayNameComponent />
+        <ChangeorAddButtonComponent displayName={props.displayName} />
       </div>
       <div className="settings-unit-details">
         <div className="error" />
-        < DisplayNameFormComponent account={props.account} submit={props.submit} displayName={props.displayName}/>
+        <DisplayNameFormComponent
+          account={props.account}
+          submit={props.submit}
+          displayName={props.displayName}
+        />
       </div>
     </div>
   );
@@ -32,21 +37,28 @@ function DisplayNameComponent() {
     <header className="settings-unit-summary">
       <h2 className="settings-unit-title">{t('Display name')}</h2>
     </header>
-
   );
 }
 function ChangeorAddButtonComponent(props) {
   const displayName = props.displayName;
-  if (displayName != ''){
+  if (displayName) {
     return (
-      <button className="settings-button secondary-button settings-unit-toggle" data-href="settings/display_name">
+      <button
+        className="settings-button secondary-button settings-unit-toggle"
+        data-href="settings/display_name"
+      >
         <span className="change-button">{t('Change…')}</span>
       </button>
     );
   } else {
-    <button className="settings-button primary-button settings-unit-toggle" data-href="settings/display_name">
-      <span className="add-button">{t('Add…')}</span>
-    </button>;
+    return (
+      <button
+        className="settings-button primary-button settings-unit-toggle"
+        data-href="settings/display_name"
+      >
+        <span className="add-button">{t('Add…')}</span>
+      </button>
+    );
   }
 }
 class DisplayNameFormComponent extends React.Component {
@@ -54,113 +66,130 @@ class DisplayNameFormComponent extends React.Component {
     super(props);
 
     this.state = {
+      changeButton: 1,
       displayName: props.account.get('displayName') || ''
     };
 
+    this.isValid = this.isValid.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange = (event) => {
-    this.setState({ displayName: event.target.value });
-  };
+    handleChange = event => {
+      this.setState({
+        displayName: event.target.value
+      }, ()=>{
+        this.isValid();
+      });
+    };
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.props.submit(this.state.displayName);
-  };
+    isValid (){
+      if (this.props.displayName !== this.state.displayName){
+        this.setState({changeButton: 0});
+      } else {
+        this.setState({changeButton: 1});
+      }
+    }
 
-  render() {
-    return (<form noValidate onSubmit={this.handleSubmit}>
-      <p>
-        {t('Choose the name you would like to appear in Firefox and when managing your account.')}
-      </p>
-      <div className="input-row">
-        <input type="text" className="text display-name" placeholder={t('Display name')} value={this.state.displayName} onChange={this.handleChange} autoFocus  autoComplete="off"/>
-      </div>
-      <div className="button-row">
-        <button type="submit" className="settings-button primary-button">{t('Change')}</button>
-        <button className="settings-button secondary-button cancel">{t('Cancel')}</button>
-      </div>
-    </form>);
-  }
+    handleSubmit = event => {
+      event.preventDefault();
+      this.props.submit(this.state.displayName);
+    };
+
+    render() {
+      return (
+        <form noValidate onSubmit={this.handleSubmit}>
+          <p>
+            {t(
+              'Choose the name you would like to appear in Firefox and when managing your account.'
+            )}
+          </p>
+          <div className="input-row">
+            <input
+              type="text"
+              className="text display-name"
+              placeholder={t('Display name')}
+              value={this.state.displayName}
+              onChange={this.handleChange}
+              autoFocus
+              autoComplete="off"
+            />
+          </div>
+          <div className="button-row">
+            <button
+              type="submit"
+              id="submit_display"
+              className="settings-button primary-button"
+              disabled={this.state.changeButton}
+            >
+              {t('Change')}
+            </button>
+            <button className="settings-button secondary-button cancel">
+              {t('Cancel')}
+            </button>
+          </div>
+        </form>
+      );
+    }
 }
 
 const View = BaseView.extend({
-  template: () => '<div />',//Template,
+  template: () => '<div />', //Template,
   className: 'display-name',
-  viewName: 'settings.display-name',
 
-  onProfileUpdate () {
+  onProfileUpdate() {
     this.render();
   },
 
-  setInitialContext (context) {
-    context.set('displayName', this._displayName);
-  },
+  // setInitialContext (context) {
+  //   context.set('displayName', this._displayName);
+  // },
 
-  beforeRender () {
-    var account = this.getSignedInAccount();
-    return account.fetchProfile()
-      .then(() => {
-        this.user.setAccount(account);
-        this._displayName = account.get('displayName');
-      });
-  },
+  // beforeRender () {
+  //   var account = this.getSignedInAccount();
+  //   return account.fetchProfile()
+  //     .then(() => {
+  //       this.user.setAccount(account);
+  //       this._displayName = account.get('displayName');
+  //     });
+  // },
 
-  afterRender () {
-    ReactDOM.render(
-      <DisplayName
-        account={this.getSignedInAccount()}
-        submit ={displayName => this.submit(displayName)}
-        displayName={this.getSignedInAccount().get('displayName')}
-      />,
-      this.$el.get(0)
-    );
-  },
+  // isValidStart () {
+  //   // if no display name set then we still do not want to activate the change button
+  //   var accountDisplayName = this.getSignedInAccount().get('displayName') || '';
+  //   var displayName = this.getElementValue('input.display-name').trim();
 
-  isValidStart () {
-    // if no display name set then we still do not want to activate the change button
-    var accountDisplayName = this.getSignedInAccount().get('displayName') || '';
-    var displayName = this.getElementValue('input.display-name').trim();
+  //   return accountDisplayName !== displayName;
+  // },
 
-    return accountDisplayName !== displayName;
-  },
-
-  submit (displayName) {
+  submit(displayName) {
     const start = Date.now();
     const account = this.getSignedInAccount();
 
-    return account.postDisplayName(displayName)
-      .then(() => {
-        this.logViewEvent('success');
-        this.updateDisplayName(displayName);
-        this.displaySuccess(t('Display name updated'));
-        this.logFlowEvent(`timing.displayName.change.${Date.now() - start}`);
-        this.navigate('settings');
-      })(
-        <DisplayName
-        account={this.getSignedInAccount()}
-        submit ={displayName => this.submit(displayName)}
-        displayName={this.getSignedInAccount().get('displayName')}
-      />,
+    return account.postDisplayName(displayName).then(() => {
+      this.logViewEvent('success');
+      this.updateDisplayName(displayName);
+      this.displaySuccess(t('Display name updated'));
+      this.logFlowEvent(
+        `timing.displayName.change.${Date.now() - start}`
       );
+      this.navigate('settings');
+    });
   },
 
-  render(){
+  render() {
     var account = this.getSignedInAccount();
-    return account.fetchProfile()
-      .then(()=>{
-        ReactDOM.render(
-          <DisplayName
-            account={this.getSignedInAccount()}
-            submit ={displayName => this.submit(displayName)}
-            displayName={this.getSignedInAccount().get('displayName')}
-          />,
-          this.$el.get(0)
-        );
-        return true;
-      });
+    return account.fetchProfile().then(() => {
+      ReactDOM.render(
+        <DisplayName
+          account={this.getSignedInAccount()}
+          submit={displayName => this.submit(displayName)}
+          displayName={this.getSignedInAccount().get('displayName')}
+        />,
+        this.$el.get(0)
+      );
+      return true;
+    });
   }
 });
 
@@ -168,7 +197,7 @@ Cocktail.mixin(
   View,
   AvatarMixin,
   // DisableFormMixin,
-  SettingsPanelMixin,
+  SettingsPanelMixin
 );
 
 export default View;
