25bc6247477b1c06f59218fa2b038edc1f47b077
Lauren Zugai <lauren@zugai.com>
2019-07-10 18:39:13 -0500

    feat(fxa-content-server): fix #1071 - new delete account view to display apps & subscriptions
    change getSettingsData to settingsData
    
    Remove duplicate account key
    
    Fix missed shrinkwrap conflicts
    
    Fix mocha warning
    
    Finish functional tests for delete account
    
    remove extra getSettingsData

--- packages/fxa-content-server/app/tests/spec/views/settings/delete_account.js
+++ packages/fxa-content-server/app/tests/spec/views/settings/delete_account.js
@@ -5,6 +5,7 @@
 import $ from 'jquery';
 import AuthErrors from 'lib/auth-errors';
 import Broker from 'models/auth_brokers/base';
+import AttachedClients from 'models/attached-clients';
 import chai from 'chai';
 import Metrics from 'lib/metrics';
 import Notifier from 'lib/channels/notifier';
@@ -13,175 +14,500 @@ import Relier from 'models/reliers/relier';
 import sinon from 'sinon';
 import TestHelpers from '../../../lib/helpers';
 import User from 'models/user';
+import Translator from 'lib/translator';
 import View from 'views/settings/delete_account';
+import BaseView from '../../../../scripts/views/base';
 
 var assert = chai.assert;
 var wrapAssertion = TestHelpers.wrapAssertion;
 
 describe('views/settings/delete_account', function() {
-  var UID = '123';
-  var account;
-  var broker;
-  var email;
-  var metrics;
-  var notifier;
-  var password = 'password';
-  var relier;
-  var tabChannelMock;
-  var user;
-  var view;
+  const UID = '123';
+  const password = 'password';
+  let account;
+  let broker;
+  let email;
+  let metrics;
+  let notifier;
+  let relier;
+  let translator;
+  let tabChannelMock;
+  let user;
+  let view;
+  let attachedClients;
+  let parentView;
+  let activeSubscriptions;
 
-  beforeEach(function() {
+  function initView() {
+    view = new View({
+      attachedClients,
+      broker,
+      metrics,
+      notifier,
+      parentView,
+      translator,
+      user,
+      relier,
+      activeSubscriptions,
+    });
+
+    sinon.spy(view, 'logFlowEvent');
+
+    return view.render();
+  }
+
+  beforeEach(() => {
     relier = new Relier();
     tabChannelMock = new NullChannel();
     user = new User();
-
-    broker = new Broker({
-      relier: relier,
-    });
-
-    notifier = new Notifier({
-      tabChannel: tabChannelMock,
-    });
+    parentView = new BaseView();
+    translator = new Translator({ forceEnglish: true });
+    broker = new Broker({ relier });
+    notifier = new Notifier({ tabChannel: tabChannelMock });
     metrics = new Metrics({ notifier });
 
-    view = new View({
-      broker: broker,
-      metrics: metrics,
-      notifier: notifier,
-      relier: relier,
-      user: user,
+    account = user.initAccount({
+      email: email,
+      sessionToken: 'abc123',
+      uid: UID,
+      verified: true,
     });
+
+    const clientList = [
+      {
+        deviceId: 'device-1',
+        os: 'Windows',
+        isCurrentSession: true,
+        isWebSession: true,
+        name: 'alpha',
+        deviceType: 'tablet',
+      },
+      {
+        deviceId: 'device-2',
+        os: 'iOS',
+        isCurrentSession: true,
+        isWebSession: false,
+        name: 'beta',
+        deviceType: 'mobile',
+      },
+      {
+        name: 'omega',
+        clientType: 'webSession',
+        userAgent: 'Firefox 40',
+      },
+      {
+        clientId: 'app-1',
+        lastAccessTime: Date.now(),
+        name: 'Pocket',
+        scope: ['profile', 'profile:write'],
+      },
+    ];
+
+    attachedClients = new AttachedClients(clientList, {
+      notifier,
+    });
+
+    sinon
+      .stub(attachedClients, 'fetchClients')
+      .callsFake(() => Promise.resolve());
+
+    activeSubscriptions = [
+      {
+        plan_id: '321doneProMonthly',
+        plan_name: '321done Pro Monthly',
+        status: 'active',
+      },
+      {
+        plan_id: '321doneProYearly',
+        plan_name: '321done Pro Yearly',
+        status: 'inactive',
+      },
+    ];
+
+    sinon.stub(account, 'settingsData').callsFake(() =>
+      Promise.resolve({
+        subscriptions: activeSubscriptions,
+      })
+    );
+
+    return initView();
   });
 
   afterEach(function() {
+    metrics.destroy();
+    metrics = null;
+
     $(view.el).remove();
+
+    if ($.prototype.trigger.restore) {
+      $.prototype.trigger.restore();
+    }
+
     view.destroy();
     view = null;
   });
 
+  describe('constructor', () => {
+    beforeEach(() => {
+      view = new View({
+        notifier,
+        parentView,
+        user,
+      });
+    });
+
+    it('creates an `AttachedClients` instance if one not passed in', () => {
+      assert.ok(view._attachedClients);
+    });
+  });
+
   describe('with session', function() {
-    beforeEach(function() {
+    beforeEach(() => {
       email = TestHelpers.createEmail();
 
-      account = user.initAccount({
-        email: email,
-        sessionToken: 'abc123',
-        uid: UID,
-        verified: true,
-      });
-      sinon.stub(account, 'isSignedIn').callsFake(function() {
-        return Promise.resolve(true);
-      });
+      sinon.stub(view, 'getSignedInAccount').callsFake(() => account);
+      sinon.stub(notifier, 'trigger').callsFake(() => {});
 
-      sinon.stub(view, 'getSignedInAccount').callsFake(function() {
-        return account;
-      });
-      sinon.stub(notifier, 'trigger').callsFake(function() {});
-
-      return view.render().then(function() {
+      return view.render().then(() => {
         $('body').append(view.el);
       });
     });
 
-    describe('isValid', function() {
-      it('returns true if password is filled out', function() {
-        $('form input[type=password]').val(password);
-
-        assert.equal(view.isValid(), true);
+    describe('render', () => {
+      beforeEach(() => {
+        sinon.spy(attachedClients, 'fetch');
       });
 
-      it('returns false if password is too short', function() {
-        $('form input[type=password]').val('passwor');
-
-        assert.equal(view.isValid(), false);
+      it('does not fetch the clients list immediately to avoid startup XHR requests', () => {
+        assert.isFalse(attachedClients.fetch.called);
       });
-    });
 
-    describe('showValidationErrors', function() {
-      it('shows an error if the password is invalid', function(done) {
-        view.$('[type=email]').val('testuser@testuser.com');
-        view.$('[type=password]').val('passwor');
-
-        view.on('validation_error', function(which, msg) {
-          wrapAssertion(function() {
-            assert.ok(msg);
-          }, done);
-        });
-
-        view.showValidationErrors();
+      it('does not fetch the subscriptions list immediately to avoid startup XHR requests', () => {
+        assert.isFalse(account.settingsData.called);
       });
     });
 
-    describe('submit', function() {
-      beforeEach(function() {
-        $('form input[type=email]').val(email);
-        $('form input[type=password]').val(password);
+    describe('_fetchAttachedClients', () => {
+      beforeEach(() => {
+        return Promise.resolve()
+          .then(() => {
+            assert.equal(view.logFlowEvent.callCount, 0);
+            return view._fetchAttachedClients();
+          })
+          .then(() => {
+            assert.equal(view.logFlowEvent.callCount, 1);
+            const args = view.logFlowEvent.args[0];
+            assert.lengthOf(args, 1);
+            const eventParts = args[0].split('.');
+            assert.lengthOf(eventParts, 4);
+            assert.equal(eventParts[0], 'timing');
+            assert.equal(eventParts[1], 'clients');
+            assert.equal(eventParts[2], 'fetch');
+            assert.match(eventParts[3], /^[0-9]+$/);
+          });
       });
 
-      describe('success', function() {
-        beforeEach(function() {
-          sinon.stub(user, 'deleteAccount').callsFake(function() {
-            return Promise.resolve();
+      it('delegates to the user to fetch the device list', () => {
+        assert.isTrue(attachedClients.fetchClients.calledWith(user));
+      });
+    });
+
+    describe('_fetchActiveSubscriptions', () => {
+      beforeEach(() => {
+        return Promise.resolve()
+          .then(() => {
+            assert.equal(view.logFlowEvent.callCount, 0);
+            return view._fetchActiveSubscriptions();
+          })
+          .then(() => {
+            assert.equal(view.logFlowEvent.callCount, 1);
+            const args = view.logFlowEvent.args[0];
+            assert.lengthOf(args, 1);
+            const eventParts = args[0].split('.');
+            assert.lengthOf(eventParts, 4);
+            assert.equal(eventParts[0], 'timing');
+            assert.equal(eventParts[1], 'settings');
+            assert.equal(eventParts[2], 'fetch');
+            assert.match(eventParts[3], /^[0-9]+$/);
           });
+      });
+      it('delegates to the account to fetch the subscriptions list from settingsData', () => {
+        assert.isTrue(account.settingsData.called);
+      });
+    });
 
-          sinon.spy(broker, 'afterDeleteAccount');
-          sinon.spy(view, 'logViewEvent');
-          sinon.spy(view, 'navigate');
+    describe('_toggleEnableSubmit', () => {
+      beforeEach(() => {
+        sinon.spy(view, '_toggleEnableSubmit');
+      });
 
-          return view.submit();
+      it('should be disabled if checkboxes are not checked', () => {
+        assert.isTrue(view.$('.delete-account-button').is(':disabled'));
+      });
+
+      it('enables the submit button if all checkboxes are checked', () => {
+        view.$('.delete-account-checkbox').each(function() {
+          $(this).prop('checked', true);
+        });
+        view._toggleEnableSubmit();
+
+        assert.isFalse(view.$('.delete-account-button').is(':disabled'));
+      });
+
+      it('disables the submit button if all checkboxes are checked and one is unchecked', () => {
+        view.$('.delete-account-checkbox').each(function() {
+          $(this).prop('checked', true);
+        });
+        view._toggleEnableSubmit();
+
+        view
+          .$('.delete-account-checkbox')
+          .first()
+          .prop('checked', false);
+        view._toggleEnableSubmit();
+
+        assert.isTrue(view.$('.delete-account-button').is(':disabled'));
+      });
+    });
+
+    describe('openPanel - failed fetch of attachedClients or activeSubscriptions', () => {
+      beforeEach(() => {
+        sinon.spy($.prototype, 'trigger');
+        sinon.stub(view, 'logError');
+        sinon
+          .stub(view, '_fetchAttachedClients')
+          .callsFake(() =>
+            Promise.reject(AuthErrors.toError('UNEXPECTED_ERROR'))
+          );
+        sinon
+          .stub(view, '_fetchActiveSubscriptions')
+          .callsFake(() => Promise.resolve());
+        return view.openPanel();
+      });
+
+      it('logs the error', () => {
+        assert.isTrue(view.logError.calledOnce);
+        const err = view.logError.args[0][0];
+        assert.isTrue(AuthErrors.is(err, 'UNEXPECTED_ERROR'));
+      });
+
+      it('adds `hide` class to `delete-account-product-container` if client or subscriptions fetch fails', () => {
+        assert.isTrue(
+          view.$('.delete-account-product-container').hasClass('hide')
+        );
+      });
+    });
+
+    describe('openPanel', () => {
+      beforeEach(() => {
+        sinon.spy($.prototype, 'trigger');
+        sinon.spy(view, '_fetchAttachedClients');
+        sinon.spy(view, '_fetchActiveSubscriptions');
+        return view.openPanel();
+      });
+
+      it('fetches the device and subscriptions list', () => {
+        assert.isTrue(
+          TestHelpers.isEventLogged(metrics, 'settings.delete-account.open')
+        );
+        assert.isTrue(view._fetchAttachedClients.calledOnce);
+        assert.isTrue(view._fetchActiveSubscriptions.calledOnce);
+      });
+
+      it('renders only `status: "active"` subscriptions', () => {
+        assert.isTrue(
+          view.$('.delete-account-product-subscription').length === 1
+        );
+      });
+
+      it('renders subscription title attributes', () => {
+        assert.equal(
+          view.$('.delete-account-product-subscription').attr('title'),
+          '321done Pro Monthly'
+        );
+      });
+
+      it('renders attachedClients already in the collection', () => {
+        assert.ok(view.$('.delete-account-product-client').length, 2);
+      });
+
+      it('renders only `isWebSession: true` and `isOAuthApp: true` clients', () => {
+        assert.isTrue(view.$('.delete-account-product-client').length === 3);
+      });
+
+      it('renders client title attributes, tests _formatTitle', () => {
+        assert.equal(
+          view
+            .$('.delete-account-product-client')
+            .eq(0)
+            .attr('title'),
+          'alpha'
+        );
+        assert.equal(
+          view
+            .$('.delete-account-product-client')
+            .eq(1)
+            .attr('title'),
+          'Pocket'
+        );
+        assert.equal(
+          view
+            .$('.delete-account-product-client')
+            .eq(2)
+            .attr('title'),
+          'Firefox 40'
+        );
+      });
+
+      it('renders only `active: true` subscriptions', () => {
+        assert.isTrue(
+          view.$('.delete-account-product-subscription').length === 1
+        );
+      });
+
+      it('renders subscription title attributes', () => {
+        assert.equal(
+          view.$('.delete-account-product-subscription').attr('title'),
+          '321done Pro Monthly'
+        );
+      });
+
+      describe('_setHasTwoColumnProductList', () => {
+        it('does not add `two-col` class to `delete-account-product-list` if count of rendered products is 3', () => {
+          activeSubscriptions = [];
+
+          return view
+            .render()
+            .then(() => view.openPanel())
+            .then(() => {
+              assert.isTrue($('.delete-account-product-list li').length === 3);
+              assert.isFalse(
+                view.$('.delete-account-product-list').hasClass('two-col')
+              );
+            });
         });
 
-        it('delegates to the user model', function() {
-          assert.isTrue(user.deleteAccount.calledOnce);
-          assert.isTrue(user.deleteAccount.calledWith(account, password));
-        });
-
-        it('notifies the broker', function() {
-          assert.isTrue(broker.afterDeleteAccount.calledOnce);
-          assert.isTrue(broker.afterDeleteAccount.calledWith(account));
-        });
-
-        it('redirects to signup, clearing query params', function() {
-          assert.equal(view.navigate.args[0][0], 'signup');
-
-          assert.ok(view.navigate.args[0][1].success);
-          assert.isTrue(view.navigate.args[0][2].clearQueryParams);
-        });
-
-        it('logs success', function() {
-          assert.isTrue(view.logViewEvent.calledOnce);
-          assert.isTrue(view.logViewEvent.calledWith('deleted'));
+        it('adds `two-col` class to `delete-account-product-list` if count of rendered products 4', () => {
+          assert.isTrue(view.$('.delete-account-product-list li').length === 4);
+          assert.isTrue(
+            view.$('.delete-account-product-list').hasClass('two-col')
+          );
         });
       });
 
-      describe('error', function() {
-        beforeEach(function() {
-          view.$('#password').val('bad_password');
-
-          sinon.stub(user, 'deleteAccount').callsFake(function() {
-            return Promise.reject(AuthErrors.toError('INCORRECT_PASSWORD'));
+      describe('isValid', function() {
+        it('returns true if all checkboxes are checked and password is filled out', function() {
+          $('form input[type=password]').val(password);
+          $('.delete-account-checkbox').each(function() {
+            $(this).prop('checked', true);
           });
 
-          sinon.stub(view, 'showValidationError').callsFake(function() {});
-          return view.submit();
+          assert.equal(view.isValid(), true);
         });
 
-        it('display an error message', function() {
-          assert.isTrue(view.showValidationError.called);
+        it('returns false if password is too short', function() {
+          $('form input[type=password]').val('passwor');
+
+          assert.equal(view.isValid(), false);
+        });
+
+        it('returns false if all 3 checkboxes are not checked', function() {
+          $('.delete-account-checkbox').each(function(i) {
+            if (i !== 0) {
+              // leave the first one unchecked
+              $(this).prop('checked', true);
+            }
+          });
+
+          assert.equal(view.isValid(), false);
         });
       });
 
-      describe('other errors', function() {
+      describe('showValidationErrors', function() {
+        it('shows an error if the password is invalid', function(done) {
+          view.$('[type=email]').val('testuser@testuser.com');
+          view.$('[type=password]').val('passwor');
+
+          view.on('validation_error', function(which, msg) {
+            wrapAssertion(function() {
+              assert.ok(msg);
+            }, done);
+          });
+
+          view.showValidationErrors();
+        });
+      });
+
+      describe('submit', function() {
         beforeEach(function() {
-          sinon.stub(user, 'deleteAccount').callsFake(function() {
-            return Promise.reject(AuthErrors.toError('UNEXPECTED_ERROR'));
+          $('form input[type=email]').val(email);
+          $('form input[type=password]').val(password);
+        });
+
+        describe('success', function() {
+          beforeEach(function() {
+            sinon.stub(user, 'deleteAccount').callsFake(function() {
+              return Promise.resolve();
+            });
+
+            sinon.spy(broker, 'afterDeleteAccount');
+            sinon.spy(view, 'logViewEvent');
+            sinon.spy(view, 'navigate');
+
+            return view.submit();
+          });
+
+          it('delegates to the user model', function() {
+            assert.isTrue(user.deleteAccount.calledOnce);
+            assert.isTrue(user.deleteAccount.calledWith(account, password));
+          });
+
+          it('notifies the broker', function() {
+            assert.isTrue(broker.afterDeleteAccount.calledOnce);
+            assert.isTrue(broker.afterDeleteAccount.calledWith(account));
+          });
+
+          it('redirects to signup, clearing query params', function() {
+            assert.equal(view.navigate.args[0][0], 'signup');
+
+            assert.ok(view.navigate.args[0][1].success);
+            assert.isTrue(view.navigate.args[0][2].clearQueryParams);
+          });
+
+          it('logs success', function() {
+            assert.isTrue(view.logViewEvent.calledOnce);
+            assert.isTrue(view.logViewEvent.calledWith('deleted'));
           });
         });
 
-        it('are re-thrown', function() {
-          return view.submit().then(assert.fail, function(err) {
-            assert.isTrue(AuthErrors.is(err, 'UNEXPECTED_ERROR'));
+        describe('error', function() {
+          beforeEach(function() {
+            view.$('#password').val('bad_password');
+
+            sinon.stub(user, 'deleteAccount').callsFake(function() {
+              return Promise.reject(AuthErrors.toError('INCORRECT_PASSWORD'));
+            });
+
+            sinon.stub(view, 'showValidationError').callsFake(function() {});
+            return view.submit();
+          });
+
+          it('display an error message', function() {
+            assert.isTrue(view.showValidationError.called);
+          });
+        });
+
+        describe('other errors', function() {
+          beforeEach(function() {
+            sinon.stub(user, 'deleteAccount').callsFake(function() {
+              return Promise.reject(AuthErrors.toError('UNEXPECTED_ERROR'));
+            });
+          });
+
+          it('are re-thrown', function() {
+            return view.submit().then(assert.fail, function(err) {
+              assert.isTrue(AuthErrors.is(err, 'UNEXPECTED_ERROR'));
+            });
           });
         });
       });
