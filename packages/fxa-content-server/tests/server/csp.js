7988167c32037e99367cac0360b4dbcb37214c21
Lauren Zugai <lauren@zugai.com>
2019-08-02 12:32:54 -0500

    chore(fxa-payments-server): fixes #1923 - add CSP to the payments server
    Remove payments/content consolidated CSP, fix Prettier-ified files, create separate middleware for payments
    
    Fix prettier things
    
    Add Stripe CSP things to payments server
    
    Add more config vars into proper directives
    
    Add stripe checkout API
    
    Remove unneeded directive
    
    Add tests to csp.test.js
    
    Remove unneeded test no-CSP lines
    
    Remove isCspRequired check in payment server (not content server)
    
    csp.enabled true by default
    
    Update CSP violations to report to content server
    
    Template literal return for getOrigin
    
    Update Stripe url doc
    
    Address PR things
    
    Add DATA back to payments CSP
    
    Change variable name and only add in development

--- packages/fxa-content-server/tests/server/csp.js
+++ packages/fxa-content-server/tests/server/csp.js
@@ -5,8 +5,8 @@
 const { registerSuite } = intern.getInterface('object');
 const assert = intern.getPlugin('chai').assert;
 const config = require('../../server/lib/configuration');
-const BlockingRules = require('../../server/lib/csp/blocking');
 const path = require('path');
+const blockingRules = require('../../server/lib/csp/blocking');
 const proxyquire = require('proxyquire');
 
 const csp = proxyquire(path.join(process.cwd(), 'server', 'lib', 'csp'), {
@@ -34,15 +34,12 @@ suite.tests['blockingRules'] = function() {
   const CDN_SERVER = 'https://static.accounts.firefox.com';
   config.set('static_resource_url', CDN_SERVER);
 
-  const blockingRules = BlockingRules(config);
-  const Sources = blockingRules.Sources;
+  const { Sources, directives, reportOnly } = blockingRules(config);
 
-  // Ensure none of the Sources map to `undefined`G
+  // Ensure none of the Sources map to `undefined`
   assert.notProperty(Sources, 'undefined');
 
-  assert.isFalse(blockingRules.reportOnly);
-
-  const directives = blockingRules.directives;
+  assert.isFalse(reportOnly);
 
   const connectSrc = directives.connectSrc;
   assert.lengthOf(connectSrc, 7);
---
+++ packages/fxa-payments-server/server/lib/csp/report-only.js
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/**
+ * reportOnlyCspMiddleware is where to declare experimental rules that
+ * will not cause a resource to be blocked if it runs afowl of a rule, but
+ * will cause the resource to be reported.
+ *
+ * If no directives other than `reportUri` are declared, the CSP reportOnly
+ * middleware will not be added.
+ */
+module.exports = function(config) {
+  return {
+    directives: {
+      reportUri: config.get('csp.reportOnlyUri'),
+    },
+    reportOnly: true,
+  };
+};
