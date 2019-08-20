99f3fce63dd85d89d14140aa793f9cf83ad07fa2
Danny Coates <dannycoates@gmail.com>
2019-08-20 11:20:51 -0700

    chore(ts): convert fxa-shared/l10n/localizeTimestamp to typescript

---
+++ packages/fxa-shared/index.ts
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+import * as popularDomains from './email/popularDomains.json';
+import { localizeTimestamp } from './l10n/localizeTimestamp';
+import * as supportedLanguages from './l10n/supportedLanguages.json';
+import * as amplitude from './metrics/amplitude';
+import * as scopes from './oauth/scopes';
+import * as promise from './promise';
+import * as redis from './redis';
+
+module.exports = {
+  email: {
+    popularDomains,
+  },
+  l10n: {
+    localizeTimestamp,
+    supportedLanguages,
+  },
+  metrics: {
+    amplitude,
+  },
+  oauth: {
+    scopes,
+  },
+  promise,
+  redis,
+};
