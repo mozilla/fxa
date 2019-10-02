056d14f040fee414f39cf5abdfbb85d0be45fad7
Jared Hirsch <ohai@6a68.net>
2019-10-02 12:41:55 -0700

    try echoing when graphicsmagick is installed, to see whats up

--- .circleci/install-content-server.sh
+++ .circleci/install-content-server.sh
@@ -3,12 +3,15 @@
 DIR=$(dirname "$0")
 
 if grep -e "fxa-content-server" -e 'all' $DIR/../packages/test.list; then
+  echo "installing graphicsmagick..."
   sudo apt-get install -y graphicsmagick
+  echo "done."
   mkdir -p config
   cp ../version.json ./
   cp ../version.json config
   cd $DIR/..
   CIRCLECI=false SKIP_DOCKER=true npm install
 else
+  echo "not installing graphicsmagick."
   exit 0;
 fi
