f7998ad02a9e11a647c6376e271f3860671a52d5
Jared Hirsch <ohai@6a68.net>
2019-10-09 16:28:18 -0700

    feat(payments): complete post-metrics endpoint

--- packages/fxa-payments-server/server/lib/routes/post-metrics.js
+++ packages/fxa-payments-server/server/lib/routes/post-metrics.js
@@ -4,30 +4,59 @@
 
 'use strict';
 
+const joi = require('joi');
 const amplitude = require('../amplitude');
+const logger = require('../logging/log')('server.post-metrics');
+
+const config = require('../../config');
+const clientMetricsConfig = config.get('clientMetrics');
+const MAX_EVENT_OFFSET = clientMetricsConfig.maxEventOffset;
+const EVENT_TYPE_PATTERN = /^[\w\s.:-]+$/; // the space is to allow for error contexts that contain spaces, e.g., `error.unknown context.auth.108`
+const OFFSET_TYPE = joi
+  .number()
+  .integer()
+  .min(0);
+const STRING_TYPE = joi.string().max(1024);
+const BODY_SCHEMA = {
+  data: joi.object().required(),
+  events: joi
+    .array()
+    .items(
+      joi.object().keys({
+        offset: OFFSET_TYPE.max(MAX_EVENT_OFFSET).required(),
+        type: STRING_TYPE.regex(EVENT_TYPE_PATTERN).required(),
+      })
+    )
+    .required(),
+  flowBeginTime: OFFSET_TYPE.optional(),
+  flowId: STRING_TYPE.hex()
+    .length(64)
+    .optional(),
+};
 
 module.exports = {
   method: 'post',
   path: '/metrics',
-
-  handler(request, response) {
-    if (!validate(request.body)) {
-      return response.status(400).end();
+  validate: {
+    body: BODY_SCHEMA,
+  },
+  preProcess: function(req, res, next) {
+    // convert text/plain types to JSON for validation.
+    if (/^text\/plain/.test(req.get('content-type'))) {
+      try {
+        req.body = JSON.parse(req.body);
+      } catch (error) {
+        logger.error(error);
+        // uh oh, invalid JSON. Validation will return a 400.
+        req.body = {};
+      }
     }
 
+    next();
+  },
+  handler(request, response) {
     const { data, events } = request.body;
     events.forEach(event => amplitude(event, request, data));
     response.status(200).end();
   },
 };
-
-function validate(body) {
-  // TODO: replace this with JSON schema or hapi or whatever
-  return (
-    body.data &&
-    typeof body.data === 'object' &&
-    !Array.isArray(body.data) &&
-    Array.isArray(body.events) &&
-    body.events.every(event => typeof event === 'string')
-  );
-}
