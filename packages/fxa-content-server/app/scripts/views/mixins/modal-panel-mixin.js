1b594dabbe0b7de87e8d1aecb4e151e5d9845d8e
Lauren Zugai <lauren@zugai.com>
2019-10-10 21:06:41 -0500

    fix(modal): Make modal keyboard accessible
    Remove document event listener on modal close
    
    fix(copy): fix #2869 - update disconnect from sync copy
    
    Fix for #636 - Make disconnect from Sync keyboard accessible
    
    Fix for #2869, modify 'Disconnect from Sync' modal copy
    
    Remove duplicate import
    
    Use 'events' for keydown event
    
    Fix tests, start writing tests, return focus to previously active element
    
    Address Shane's comments on PR
    
    Remove WIP unit test, make requested copy change, fix bug introduced with last commit
    
    Keyboard tabbing test WIP
    
    add test assertion (fails)
    
    Selenium test avatar.js tabbing minor edits
    
    Remove pressKeys
    
    Redmove pressKeys declaration

--- packages/fxa-content-server/app/scripts/views/mixins/modal-panel-mixin.js
+++ packages/fxa-content-server/app/scripts/views/mixins/modal-panel-mixin.js
@@ -10,13 +10,16 @@
  */
 
 import $ from 'jquery';
+import KeyCodes from '../../lib/key-codes';
 
 export default {
   isModal: true,
-
   notifications: {
     navigate: 'closePanel',
     'navigate-back': 'closePanel',
+  },
+  events: {
+    keydown: 'keyDownListener',
   },
 
   /**
@@ -33,8 +36,69 @@ export default {
 
     this.$el.on($.modal.AFTER_CLOSE, () => this.onAfterClose());
 
+    this._previousActiveElement = document.activeElement;
     this._boundBlockerClick = this.onBlockerClick.bind(this);
     $('.blocker').on('click', this._boundBlockerClick);
+
+    /**
+     * Trap keyboard focus when modal opens.
+     */
+    const modal = this.$el[0];
+    modal.setAttribute('tabindex', '1');
+    modal.focus();
+
+    const focusableElementsNodeList = modal.querySelectorAll(
+      'a[href], button, input[type="radio"]'
+    );
+    const focusableElements = Array.prototype.slice.call(
+      focusableElementsNodeList
+    );
+
+    this._keyDownListener = this.keyDownListener.bind(null, focusableElements);
+    modal.addEventListener('keydown', this._keyDownListener);
+  },
+
+  keyDownListener(focusableElements, event) {
+    function getNextSelectable(element = focusableElements[0]) {
+      const nextIndex = focusableElements.indexOf(element) + 1;
+      // return first element if the next element doesn't exist
+      return focusableElements[nextIndex]
+        ? focusableElements[nextIndex]
+        : focusableElements[0];
+    }
+
+    function getPreviousSelectable(
+      element = focusableElements[focusableElements.length - 1]
+    ) {
+      const previousIndex = focusableElements.indexOf(element) - 1;
+      // return last element if the previous element doesn't exist
+      return focusableElements[previousIndex]
+        ? focusableElements[previousIndex]
+        : focusableElements[focusableElements.length - 1];
+    }
+
+    //avoid IME composition keydown events
+    //ref: https://developer.mozilla.org/docs/Web/Events/keydown#Notes
+    if (event.isComposing || event.keyCode === KeyCodes.IME) {
+      return;
+    }
+    if (event.keyCode === KeyCodes.TAB) {
+      event.preventDefault();
+      if (event.shiftKey) {
+        getPreviousSelectable(document.activeElement).focus();
+      } else {
+        getNextSelectable(document.activeElement).focus();
+      }
+      // the form submits on 'enter' keypress by default, but if focus is on an
+      // unselected radio button, we want to select it instead
+    } else if (
+      event.keyCode === KeyCodes.ENTER &&
+      document.activeElement.getAttribute('type') === 'radio' &&
+      document.activeElement.checked === false
+    ) {
+      event.preventDefault();
+      document.activeElement.checked = true;
+    }
   },
 
   /**
@@ -69,6 +133,7 @@ export default {
     this.destroy(true);
     this.trigger('modal-cancel');
     $('.blocker').off('click', this._boundBlockerClick);
+    this._previousActiveElement.focus();
   },
 
   /**
