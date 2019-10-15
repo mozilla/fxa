6cb20b4d8e28da5e46626e19c3a0e99beb75f18d
Les Orchard <lorchard@mozilla.com>
2019-10-15 16:08:46 -0400

    feat(subscriptions): utility to update local subscriptions db from subhub
    issue #1148

--- packages/fxa-auth-server/lib/routes/utils/subscriptions.js
+++ packages/fxa-auth-server/lib/routes/utils/subscriptions.js
@@ -4,6 +4,8 @@
 
 'use strict';
 
+const { ERRNO } = require('../../error');
+
 const PRODUCT_SUBSCRIBED = 'defaultSubscribed';
 const PRODUCT_REGISTERED = 'defaultRegistered';
 
@@ -54,5 +56,97 @@ module.exports = {
     return capabilitiesToReveal.size > 0
       ? Array.from(capabilitiesToReveal)
       : undefined;
+  },
+
+  updateSubscriptionsFromSubhub: async function({ db, subhub, profile, uid }) {
+    // Attempt to get the user's current subscriptions from subhub's perspective
+    let subhubSubscriptions = [];
+    try {
+      const subhubCustomer = await subhub.getCustomer(uid);
+      subhubSubscriptions = subhubCustomer.subscriptions || [];
+    } catch (err) {
+      if (err.errno === ERRNO.UNKNOWN_SUBSCRIPTION_CUSTOMER) {
+        // No customer, so we have no subscriptions.
+        subhubSubscriptions = [];
+      } else {
+        // Any other error seems like a bigger problem, so bail out.
+        throw err;
+      }
+    }
+
+    // Map subhub subscriptions by ID.
+    const subhubSubscriptionsById = {};
+    for (const sub of subhubSubscriptions) {
+      subhubSubscriptionsById[sub.subscription_id] = sub;
+    }
+
+    // Get subhub plans and build a map of planId -> productId - we need
+    // productId for local subscriptions, but the subhub customer only
+    // lists planId
+    const subhubPlans = await subhub.listPlans();
+    const productIdsByPlanId = {};
+    for (const { plan_id, product_id } of subhubPlans) {
+      productIdsByPlanId[plan_id] = product_id;
+    }
+
+    // Get the user's current subscriptions from auth-server's perspective
+    const localSubscriptions = (await db.fetchAccountSubscriptions(uid)) || [];
+
+    // Map local subscriptions by ID
+    const localSubscriptionsById = {};
+    for (const sub of localSubscriptions) {
+      localSubscriptionsById[sub.subscriptionId] = sub;
+    }
+
+    // Flag tracking whether we made any changes by the end.
+    let madeChanges = false;
+
+    // Create any subscriptions present in subhub but missing from local
+    for (const id of Object.keys(subhubSubscriptionsById)) {
+      if (id in localSubscriptionsById) {
+        continue;
+      }
+      const {
+        subscription_id: subscriptionId,
+        plan_id,
+      } = subhubSubscriptionsById[id];
+      const productId = productIdsByPlanId[plan_id];
+
+      // Note: In the unlikely event that this DB call fails, we bail out here.
+      await db.createAccountSubscription({
+        uid,
+        subscriptionId,
+        productId,
+        createdAt: Date.now(),
+      });
+
+      madeChanges = true;
+    }
+
+    // Delete any subscriptions present in local but missing from subhub
+    for (const id of Object.keys(localSubscriptionsById)) {
+      if (id in subhubSubscriptionsById) {
+        continue;
+      }
+      const { subscriptionId } = localSubscriptionsById[id];
+
+      // Note: In the unlikely event that this DB call fails, we bail out here.
+      await db.deleteAccountSubscription({
+        uid,
+        subscriptionId,
+      });
+
+      madeChanges = true;
+    }
+
+    // TODO: Should we update any local subscriptions with
+    // cancel_at_period_end info? We don't actually use that column
+
+    // If any changes were made, clear caches and send notifications
+    if (madeChanges) {
+      await profile.deleteCache(uid);
+    }
+
+    return madeChanges;
   },
 };
--- packages/fxa-auth-server/test/local/routes/utils/subscriptions.js
+++ packages/fxa-auth-server/test/local/routes/utils/subscriptions.js
@@ -7,15 +7,18 @@
 const sinon = require('sinon');
 const assert = require('chai').assert;
 const mocks = require('../../../mocks');
+const error = require('../../../../lib/error');
 
 const {
   PRODUCT_SUBSCRIBED,
   PRODUCT_REGISTERED,
   determineClientVisibleSubscriptionCapabilities,
+  updateSubscriptionsFromSubhub,
 } = require('../../../../lib/routes/utils/subscriptions');
 
 const UID = 'uid8675309';
 const NOW = Date.now();
+
 const MOCK_SUBSCRIPTIONS = [
   {
     uid: UID,
@@ -30,6 +33,7 @@ const MOCK_SUBSCRIPTIONS = [
     createdAt: NOW,
   },
 ];
+
 const MOCK_CONFIG = {
   subscriptions: {
     productCapabilities: {
@@ -47,18 +51,39 @@ const MOCK_CONFIG = {
   },
 };
 
+const MOCK_PLANS = [
+  {
+    plan_id: 'firefox_pro_basic_823',
+    plan_name: 'Firefox Pro Basic Weekly',
+    product_id: 'firefox_pro_basic',
+    product_name: 'Firefox Pro Basic',
+    interval: 'week',
+    amount: '123',
+    currency: 'usd',
+  },
+  {
+    plan_id: 'firefox_pro_basic_999',
+    plan_name: 'Firefox Pro Pro Monthly',
+    product_id: 'firefox_pro_pro',
+    product_name: 'Firefox Pro Pro',
+    interval: 'month',
+    amount: '456',
+    currency: 'usd',
+  },
+];
+
 describe('routes/utils/subscriptions', () => {
-  let auth, db;
-
-  beforeEach(async () => {
-    auth = { strategy: 'oauthToken' };
-    db = mocks.mockDB();
-    db.fetchAccountSubscriptions = sinon.spy(async uid =>
-      MOCK_SUBSCRIPTIONS.filter(s => s.uid === uid)
-    );
-  });
-
   describe('determineClientVisibleSubscriptionCapabilities', () => {
+    let auth, db;
+
+    beforeEach(async () => {
+      auth = { strategy: 'oauthToken' };
+      db = mocks.mockDB();
+      db.fetchAccountSubscriptions = sinon.spy(async uid =>
+        MOCK_SUBSCRIPTIONS.filter(s => s.uid === uid)
+      );
+    });
+
     afterEach(() => {
       // Each of these tests should cause a fetch of subscriptions
       assert.deepEqual(db.fetchAccountSubscriptions.args, [[UID]]);
@@ -132,6 +157,333 @@ describe('routes/utils/subscriptions', () => {
         client
       );
       assert.deepEqual(result, ['capRegistered']);
+    });
+  });
+
+  describe('updateSubscriptionsFromSubhub', () => {
+    let db, subhub, profile;
+
+    beforeEach(async () => {
+      db = mocks.mockDB();
+      subhub = mocks.mockSubHub();
+      profile = mocks.mockProfile();
+
+      subhub.listPlans = sinon.spy(async () => MOCK_PLANS);
+    });
+
+    it('makes no changes if local and subhub subscriptions match up', async () => {
+      subhub.getCustomer = sinon.spy(async inUid => ({
+        subscriptions: [
+          { subscription_id: '123', plan_id: 'firefox_pro_basic_823' },
+          { subscription_id: '456', plan_id: 'firefox_pro_basic_999' },
+        ],
+      }));
+
+      db.fetchAccountSubscriptions = sinon.spy(async inUid => [
+        { subscriptionId: '123', productId: 'firefox_pro_basic' },
+        { subscriptionId: '456', productId: 'firefox_pro_pro' },
+      ]);
+
+      const resultMadeChanges = await updateSubscriptionsFromSubhub({
+        db,
+        subhub,
+        profile,
+        uid: UID,
+      });
+
+      assert.equal(resultMadeChanges, false);
+      assert.deepEqual(subhub.getCustomer.args, [[UID]]);
+      assert.equal(subhub.listPlans.called, true);
+      assert.deepEqual(db.fetchAccountSubscriptions.args, [[UID]]);
+      assert.equal(db.createAccountSubscription.called, false);
+      assert.equal(db.deleteAccountSubscription.called, false);
+      assert.equal(profile.deleteCache.called, false);
+    });
+
+    it('deletes existing local subscriptions when missing in subhub', async () => {
+      subhub.getCustomer = sinon.spy(async inUid => ({
+        subscriptions: [
+          { subscription_id: '123', plan_id: 'firefox_pro_basic_823' },
+        ],
+      }));
+
+      db.fetchAccountSubscriptions = sinon.spy(async inUid => [
+        { subscriptionId: '123', productId: 'firefox_pro_basic' },
+        { subscriptionId: '456', productId: 'firefox_pro_pro' },
+      ]);
+
+      const resultMadeChanges = await updateSubscriptionsFromSubhub({
+        db,
+        subhub,
+        profile,
+        uid: UID,
+      });
+
+      assert.equal(resultMadeChanges, true);
+      assert.deepEqual(subhub.getCustomer.args, [[UID]]);
+      assert.equal(subhub.listPlans.called, true);
+      assert.deepEqual(db.fetchAccountSubscriptions.args, [[UID]]);
+      assert.equal(db.createAccountSubscription.called, false);
+      assert.deepEqual(db.deleteAccountSubscription.args, [
+        [{ uid: UID, subscriptionId: '456' }],
+      ]);
+      assert.deepEqual(profile.deleteCache.args, [[UID]]);
+    });
+
+    it('creates missing local subscriptions when present in subhub', async () => {
+      subhub.getCustomer = sinon.spy(async inUid => ({
+        subscriptions: [
+          { subscription_id: '123', plan_id: 'firefox_pro_basic_823' },
+          { subscription_id: '456', plan_id: 'firefox_pro_basic_999' },
+        ],
+      }));
+
+      db.fetchAccountSubscriptions = sinon.spy(async inUid => [
+        { subscriptionId: '456', productId: 'firefox_pro_pro' },
+      ]);
+
+      const resultMadeChanges = await updateSubscriptionsFromSubhub({
+        db,
+        subhub,
+        profile,
+        uid: UID,
+      });
+
+      assert.equal(resultMadeChanges, true);
+      assert.deepEqual(subhub.getCustomer.args, [[UID]]);
+      assert.equal(subhub.listPlans.called, true);
+      assert.deepEqual(db.fetchAccountSubscriptions.args, [[UID]]);
+      assert.equal(db.deleteAccountSubscription.called, false);
+      assert.equal(db.createAccountSubscription.called, true);
+      assert.deepInclude(db.createAccountSubscription.args[0][0], {
+        uid: UID,
+        subscriptionId: '123',
+        productId: 'firefox_pro_basic',
+      });
+      assert.deepEqual(profile.deleteCache.args, [[UID]]);
+    });
+
+    it('both deletes and creates local subscriptions to match subhub', async () => {
+      subhub.getCustomer = sinon.spy(async inUid => ({
+        subscriptions: [
+          { subscription_id: '123', plan_id: 'firefox_pro_basic_823' },
+          { subscription_id: '456', plan_id: 'firefox_pro_basic_999' },
+        ],
+      }));
+
+      db.fetchAccountSubscriptions = sinon.spy(async inUid => [
+        { subscriptionId: '789', productId: 'firefox_pro_basic' },
+        { subscriptionId: 'abc', productId: 'firefox_pro_pro' },
+      ]);
+
+      const resultMadeChanges = await updateSubscriptionsFromSubhub({
+        db,
+        subhub,
+        profile,
+        uid: UID,
+      });
+
+      assert.equal(resultMadeChanges, true);
+      assert.deepEqual(db.deleteAccountSubscription.args, [
+        [{ uid: UID, subscriptionId: '789' }],
+        [{ uid: UID, subscriptionId: 'abc' }],
+      ]);
+      assert.equal(db.createAccountSubscription.called, true);
+      assert.deepInclude(db.createAccountSubscription.args[0][0], {
+        uid: UID,
+        subscriptionId: '123',
+        productId: 'firefox_pro_basic',
+      });
+      assert.deepInclude(db.createAccountSubscription.args[1][0], {
+        uid: UID,
+        subscriptionId: '456',
+        productId: 'firefox_pro_pro',
+      });
+      assert.deepEqual(profile.deleteCache.args, [[UID]]);
+    });
+
+    it('properly handles unknown subhub customer', async () => {
+      subhub.getCustomer = sinon.spy(async inUid => {
+        throw error.unknownCustomer(inUid);
+      });
+      db.fetchAccountSubscriptions = sinon.spy(async inUid => []);
+
+      const resultMadeChanges = await updateSubscriptionsFromSubhub({
+        db,
+        subhub,
+        profile,
+        uid: UID,
+      });
+
+      assert.equal(resultMadeChanges, false);
+      assert.deepEqual(subhub.getCustomer.args, [[UID]]);
+      assert.equal(subhub.listPlans.called, true);
+      assert.deepEqual(db.fetchAccountSubscriptions.args, [[UID]]);
+      assert.equal(db.createAccountSubscription.called, false);
+      assert.equal(db.deleteAccountSubscription.called, false);
+      assert.equal(profile.deleteCache.called, false);
+    });
+
+    const subjectWithFailure = async args => {
+      let resultMadeChanges = null;
+      let failedMessage = null;
+      try {
+        resultMadeChanges = await updateSubscriptionsFromSubhub(args);
+      } catch (err) {
+        failedMessage = err.message;
+      }
+      return { failedMessage, resultMadeChanges };
+    };
+
+    it('properly handles unexpected error from subhub customer fetch', async () => {
+      const expectedMessage = 'OOPS';
+
+      subhub.getCustomer = sinon.spy(async inUid => {
+        throw new Error(expectedMessage);
+      });
+
+      const { failedMessage } = await subjectWithFailure({
+        db,
+        subhub,
+        profile,
+        uid: UID,
+      });
+
+      assert.equal(failedMessage, expectedMessage);
+      assert.deepEqual(subhub.getCustomer.args, [[UID]]);
+      assert.equal(subhub.listPlans.called, false);
+      assert.equal(db.fetchAccountSubscriptions.called, false);
+      assert.equal(db.createAccountSubscription.called, false);
+      assert.equal(db.deleteAccountSubscription.called, false);
+      assert.equal(profile.deleteCache.called, false);
+    });
+
+    it('throws unexpected error from subhub plans fetch', async () => {
+      const expectedMessage = 'OOPS';
+
+      subhub.getCustomer = sinon.spy(async inUid => ({
+        subscriptions: [
+          { subscription_id: '123', plan_id: 'firefox_pro_basic_823' },
+          { subscription_id: '456', plan_id: 'firefox_pro_basic_999' },
+        ],
+      }));
+      subhub.listPlans = sinon.spy(async () => {
+        throw new Error(expectedMessage);
+      });
+
+      const { failedMessage } = await subjectWithFailure({
+        db,
+        subhub,
+        profile,
+        uid: UID,
+      });
+
+      assert.equal(failedMessage, expectedMessage);
+      assert.deepEqual(subhub.getCustomer.args, [[UID]]);
+      assert.equal(subhub.listPlans.called, true);
+      assert.equal(db.fetchAccountSubscriptions.called, false);
+      assert.equal(db.createAccountSubscription.called, false);
+      assert.equal(db.deleteAccountSubscription.called, false);
+      assert.equal(profile.deleteCache.called, false);
+    });
+
+    it('throws unexpected error from from fetching local subscriptions', async () => {
+      const expectedMessage = 'OOPS';
+
+      subhub.getCustomer = sinon.spy(async inUid => ({
+        subscriptions: [
+          { subscription_id: '123', plan_id: 'firefox_pro_basic_823' },
+          { subscription_id: '456', plan_id: 'firefox_pro_basic_999' },
+        ],
+      }));
+      db.fetchAccountSubscriptions = sinon.spy(async () => {
+        throw new Error(expectedMessage);
+      });
+
+      const { failedMessage } = await subjectWithFailure({
+        db,
+        subhub,
+        profile,
+        uid: UID,
+      });
+
+      assert.equal(failedMessage, expectedMessage);
+      assert.deepEqual(subhub.getCustomer.args, [[UID]]);
+      assert.equal(subhub.listPlans.called, true);
+      assert.equal(db.fetchAccountSubscriptions.called, true);
+      assert.equal(db.createAccountSubscription.called, false);
+      assert.equal(db.deleteAccountSubscription.called, false);
+      assert.equal(profile.deleteCache.called, false);
+    });
+
+    it('properly handles error from creating local subscription', async () => {
+      const expectedMessage = 'OOPS';
+
+      subhub.getCustomer = sinon.spy(async inUid => ({
+        subscriptions: [
+          { subscription_id: '123', plan_id: 'firefox_pro_basic_823' },
+          { subscription_id: '456', plan_id: 'firefox_pro_basic_999' },
+        ],
+      }));
+
+      db.fetchAccountSubscriptions = sinon.spy(async inUid => [
+        { subscriptionId: '789', productId: 'firefox_pro_basic' },
+        { subscriptionId: 'abc', productId: 'firefox_pro_pro' },
+      ]);
+
+      db.createAccountSubscription = sinon.spy(async () => {
+        throw new Error(expectedMessage);
+      });
+
+      const { failedMessage } = await subjectWithFailure({
+        db,
+        subhub,
+        profile,
+        uid: UID,
+      });
+
+      assert.equal(failedMessage, expectedMessage);
+      assert.deepEqual(subhub.getCustomer.args, [[UID]]);
+      assert.equal(subhub.listPlans.called, true);
+      assert.equal(db.fetchAccountSubscriptions.called, true);
+      assert.equal(db.createAccountSubscription.called, true);
+      assert.equal(db.deleteAccountSubscription.called, false);
+      assert.equal(profile.deleteCache.called, false);
+    });
+
+    it('properly handles error from deleting local subscription', async () => {
+      const expectedMessage = 'OOPS';
+
+      subhub.getCustomer = sinon.spy(async inUid => ({
+        subscriptions: [
+          { subscription_id: '123', plan_id: 'firefox_pro_basic_823' },
+          { subscription_id: '456', plan_id: 'firefox_pro_basic_999' },
+        ],
+      }));
+
+      db.fetchAccountSubscriptions = sinon.spy(async inUid => [
+        { subscriptionId: '789', productId: 'firefox_pro_basic' },
+        { subscriptionId: 'abc', productId: 'firefox_pro_pro' },
+      ]);
+
+      db.deleteAccountSubscription = sinon.spy(async () => {
+        throw new Error(expectedMessage);
+      });
+
+      const { failedMessage } = await subjectWithFailure({
+        db,
+        subhub,
+        profile,
+        uid: UID,
+      });
+
+      assert.equal(failedMessage, expectedMessage);
+      assert.deepEqual(subhub.getCustomer.args, [[UID]]);
+      assert.equal(subhub.listPlans.called, true);
+      assert.equal(db.fetchAccountSubscriptions.called, true);
+      assert.equal(db.createAccountSubscription.called, true);
+      assert.equal(db.deleteAccountSubscription.called, true);
+      assert.equal(profile.deleteCache.called, false);
     });
   });
 });
