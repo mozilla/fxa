7e329ffb37b36ce049e0c3878521868ba2c30e54
Vijay Budhram <vbudhram@gmail.com>
2019-10-29 12:33:30 -0400

    fix(emails): Add email templates for adding secondary email, recovery key, and both after account verified

---
+++ packages/fxa-auth-server/lib/experiments/post-verify-emails.js
@@ -0,0 +1,42 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const BaseGroupingRule = require('../../../fxa-shared').experiments
+  .BaseGroupingRule;
+
+const GROUPS_DEFAULT = [
+  'control',
+  'treatment-secondary',
+  'treatment-recovery',
+  'treatment-both',
+];
+
+class PostVerifyEmailGroupingRule extends BaseGroupingRule {
+  constructor(config) {
+    super();
+    this.enabled = config.enabled;
+    this.rolloutRate = config.rolloutRate;
+    this.name = 'postVerifyEmails';
+    this.groups = GROUPS_DEFAULT;
+  }
+
+  /**
+   * Get the experiment group that the user belongs too.
+   *
+   * @param {Object} subject
+   *  @param {String} uid
+   * @returns {String | Boolean}
+   */
+  choose(subject) {
+    if (!subject.uid) {
+      throw new Error('subject missing uid');
+    }
+    if (this.enabled && this.bernoulliTrial(this.rolloutRate, subject.uid)) {
+      return this.uniformChoice(this.groups, subject.uid);
+    }
+    return false;
+  }
+}
+
+module.exports = PostVerifyEmailGroupingRule;
--- packages/fxa-shared/index.ts
+++ packages/fxa-shared/index.ts
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 import * as popularDomains from './email/popularDomains.json';
+import BaseGroupingRule from './experiments/base';
 import { localizeTimestamp } from './l10n/localizeTimestamp';
 import * as supportedLanguages from './l10n/supportedLanguages.json';
 import * as amplitude from './metrics/amplitude';
@@ -12,6 +13,9 @@ import * as redis from './redis';
 module.exports = {
   email: {
     popularDomains,
+  },
+  experiments: {
+    BaseGroupingRule,
   },
   l10n: {
     localizeTimestamp,
