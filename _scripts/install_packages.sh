95c6eba2c81e1acad4b9846a19fef0bcf12e74b5
Danny Coates <dannycoates@gmail.com>
2019-11-01 14:23:03 -0700

    fix(build): slow-roll npm ci on circle

--- _scripts/install_packages.sh
+++ _scripts/install_packages.sh
@@ -4,33 +4,11 @@ _scripts/check.sh
 # Set ulimit, need it for npm
 ulimit -S -n 2048 || echo "Setting ulimit failed"
 
-cd packages
+if [ "${CI}" = "true" ]; then
+  # it seems the filesystem on circleci can't handle full concurrency
+  npx lerna exec --concurrency 2 -- npm ci
+else
+  npm run npm-ci-all
+fi
 
-# Install and build fxa-shared first
-
-cd fxa-shared
-npm ci
-cd ..
-
-PATH=$PATH:$HOME/.cargo/bin
-
-# Now for concurrently!
-../node_modules/.bin/concurrently \
-    "cd fxa-content-server; npm ci" \
-    "cd fxa-auth-server; npm ci" \
-    "cd fxa-auth-db-mysql; npm ci" \
-    "cd browserid-verifier; npm ci" \
-    "cd fxa-js-client; npm ci" \
-    "cd fxa-customs-server; npm ci" \
-    "cd fxa-event-broker; npm ci" \
-    "cd fxa-payments-server; npm ci" \
-    "cd fxa-profile-server; npm ci" \
-    "cd 123done; npm ci" \
-    "cd fortress; npm ci" \
-    "cd fxa-geodb; npm ci" \
-    "cd fxa-email-event-proxy; npm ci" \
-    "cd fxa-dev-launcher; npm ci" \
-    "cd fxa-support-panel ; npm ci"
-
-cd ..
 ln -sf node_modules/.bin/pm2 pm2
