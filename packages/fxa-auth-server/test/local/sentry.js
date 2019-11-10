d42d6acdf605d611fb7f665b204ccbd373418f14
Ben Bangert <ben@groovie.org>
2019-11-09 16:03:42 -0800

    feat(auth-server): add more backend error data
    Because:
    
    * EndpointError has no stack trace, and no information about what
      request was inflight, to what service, with what exact response.
    
    This commit:
    
    * Determines if the error object has EndpointError attributes and if
      so extracts additional data about the underlying backend request
      call, method, path, and full response error data.

--- packages/fxa-auth-server/test/local/sentry.js
+++ packages/fxa-auth-server/test/local/sentry.js
@@ -5,30 +5,87 @@
 'use strict';
 
 const { assert } = require('chai');
-
+const EndpointError = require('poolee/lib/error')(require('util').inherits);
+const sinon = require('sinon');
+const verror = require('verror');
 const Hapi = require('hapi');
+const Sentry = require('@sentry/node');
 
 const config = require('../../config').getProperties();
-const _configureSentry = require('../../lib/server')._configureSentry;
-const server = new Hapi.Server({});
+const configureSentry = require('../../lib/sentry').configureSentry;
+
+const sandbox = sinon.createSandbox();
 
 describe('Sentry', () => {
-  let sentryDsn;
+  let server;
 
   beforeEach(() => {
-    sentryDsn = config.sentryDsn;
+    server = new Hapi.Server({});
   });
 
   afterEach(() => {
-    config.sentryDsn = sentryDsn;
+    sandbox.restore();
   });
 
-  it('can be set up when sentry is enabled', () => {
+  it('can be set up when sentry is enabled', async () => {
     config.sentryDsn = 'https://deadbeef:deadbeef@127.0.0.1/123';
-    assert.doesNotThrow(() => _configureSentry(server, config));
+    let throws = false;
+    try {
+      await configureSentry(server, config);
+    } catch (err) {
+      throws = true;
+    }
+    assert.equal(throws, false);
   });
 
-  it('can be set up when sentry is not enabled', () => {
-    assert.doesNotThrow(() => _configureSentry(server, config));
+  it('can be set up when sentry is not enabled', async () => {
+    let throws = false;
+    try {
+      await configureSentry(server, config);
+    } catch (err) {
+      throws = true;
+    }
+    assert.equal(throws, false);
+  });
+
+  it('adds EndpointError details to a reported error', async () => {
+    config.sentryDsn = 'https://deadbeef:deadbeef@127.0.0.1/123';
+    await configureSentry(server, config);
+    const endError = new EndpointError(
+      'An internal server error has occurred',
+      {
+        reason: 'connect refused',
+        attempt: {
+          method: 'PUT',
+          path: '/account/',
+        },
+      }
+    );
+    endError.output = {
+      error: '127.0.0.1 error',
+      message: 'Underlying error',
+      statusCode: 500,
+    };
+    const sentryCaptureSpy = sandbox.stub(Sentry, 'captureException');
+    const scopeContextSpy = sinon.fake();
+    const scopeSpy = {
+      addEventProcessor: sinon.fake(),
+      setContext: scopeContextSpy,
+      setExtra: sinon.fake(),
+    };
+    sandbox.replace(Sentry, 'withScope', fn => fn(scopeSpy));
+    const fullError = new verror.WError(endError, 'Something bad happened');
+    await server.events.emit({ name: 'request', channel: 'error' }, [
+      {},
+      { error: fullError },
+    ]);
+
+    sentryCaptureSpy.calledOnceWith(fullError);
+    assert.equal(scopeContextSpy.calledOnce, true);
+    const ctx = scopeContextSpy.args[0][1];
+    assert.equal(ctx.method, endError.attempt.method);
+    assert.equal(ctx.path, endError.attempt.path);
+    assert.equal(ctx.errorName, 'EndpointError');
+    assert.equal(ctx.reason, endError.reason);
   });
 });
