7ac6ee2d53b2f883569bff8d046c03d865677ef7
Ben Bangert <ben@groovie.org>
2019-12-17 15:54:51 -0800

    docs(support-panel): add README and JSDoc
    Because:
    
    * README was empty and not helpful
    * Some files were missing JSDoc
    
    This commit:
    
    * Add's a descriptive README documenting what this does, its
      architecture rationale, and JSDoc.
    
    Closes #3687

--- packages/fxa-support-panel/bin/worker.ts
+++ packages/fxa-support-panel/bin/worker.ts
@@ -2,6 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+/**
+ * Primary entry point for running the support panel in production.
+ *
+ * @module
+ */
 import mozlog from 'mozlog';
 
 import Config from '../config';
