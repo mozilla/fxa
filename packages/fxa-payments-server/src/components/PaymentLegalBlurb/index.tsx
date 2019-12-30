b8e08ca9410fd1e522e46b2431b4365c843ec40a
Dave Justice <djustice@mozilla.com>
2019-12-30 23:07:54 +0000

    chore(payments-server): add l10n for payment legal blurb
    - refs #3704

--- packages/fxa-payments-server/src/components/PaymentLegalBlurb/index.tsx
+++ packages/fxa-payments-server/src/components/PaymentLegalBlurb/index.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
+import { Localized } from 'fluent-react';
 
 import './index.scss';
 
 export const PaymentLegalBlurb = () => (
   <div className="payment-legal-blurb">
-    <p>Mozilla uses Stripe for secure payment processing.</p>
-    <p>
-      View the{' '}
-      <a
-        href="https://stripe.com/privacy"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Stripe privacy policy
-      </a>
-      .
-    </p>
+    <Localized id="payment-legal-copy">
+      <p>Mozilla uses Stripe for secure payment processing.</p>
+    </Localized>
+
+    <Localized
+      id="payment-legal-link"
+      a={
+        <a
+          href="https://stripe.com/privacy"
+          target="_blank"
+          rel="noopener noreferrer"
+        ></a>
+      }
+    >
+      <p>
+        View the <a>Stripe privacy policy</a>.
+      </p>
+    </Localized>
   </div>
 );
 
