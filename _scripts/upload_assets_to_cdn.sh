af405ab8fea334d7cc65ec766e0182d3d6957880
John Morrison <jrgmorrison@gmail.com>
2020-01-22 09:32:27 -0800

    fix(assets): add a self-documenting echo comment

--- _scripts/upload_assets_to_cdn.sh
+++ _scripts/upload_assets_to_cdn.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+echo ""
+echo "Usage:"
+echo "  Checkout HEAD of master of https://github.com/mozilla/fxa, and from"
+echo "  ./fxa, run ./_scripts/upload_assets_to_cdn.sh."
+echo ""
+echo "Your current branch is $(git branch --show-current)"
+echo ""
+
 aws s3 cp --cache-control 'public,max-age=86400' --recursive assets s3://fxa-content-cdn-prod-distbucket-gqg70i8xqycy/
