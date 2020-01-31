932b70c3c9b8ec86557ab046b7845c7819a36c60
Vijay Budhram <vbudhram@gmail.com>
2020-01-31 15:32:48 +0100

    feat(coverage): Add coveralls coverage

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -7,11 +7,15 @@ DIR=$(dirname "$0")
 if grep -e "$MODULE" -e 'all' $DIR/../packages/test.list; then
   if [ "${MODULE}" == 'fxa-auth-server' ]; then
     docker build -f Dockerfile-test -t ${MODULE}:test ..
-    docker run --net="host" ${MODULE}:test npm test
+    docker run --net="host" --name="${MODULE}" ${MODULE}:test npm test
+
+    docker cp ${MODULE}:/app/coverage ./coverage || true
   elif [[ -e Dockerfile-test ]]; then
     docker build -f Dockerfile-test -t ${MODULE}:test .
 
-    docker run --net="host" ${MODULE}:test npm run ${TEST:-test}
+    docker run --net="host" --name="${MODULE}" ${MODULE}:test npm run ${TEST:-test}
+
+    docker cp ${MODULE}:/app/coverage ./coverage || true
 
     if grep eslint "$DIR/../packages/$MODULE/Gruntfile.js"; then
       docker run --net="host" ${MODULE}:test /app/node_modules/.bin/grunt eslint
