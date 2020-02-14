87ed42fcb863ffcea3451a4eab217a815cf30c11
Dave Justice <djustice@mozilla.com>
2020-02-14 18:46:52 +0000

    task(payment-server): add new header component for new flow
    - fixes #4145

---
+++ packages/fxa-payments-server/src/components/Header/index.test.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import Header from './index';
+import { Profile } from '../../store/types';
+
+let userProfile: Profile = {
+  avatar: './avatar.svg',
+  displayName: 'Foxy77',
+  email: 'foxy@firefox.com',
+  amrValues: ['amrval'],
+  avatarDefault: true,
+  locale: 'en-US',
+  twoFactorAuthentication: false,
+  uid: 'UIDSTRINGHERE',
+};
+
+afterEach(cleanup);
+
+describe('Header', () => {
+  it('renders as expected', () => {
+    const subject = () => {
+      return render(<Header {...{ profile: userProfile }} />);
+    };
+
+    const { queryByTestId, getByText } = subject();
+
+    const branding = getByText('Firefox');
+    expect(branding).toHaveClass('fxa-account-title');
+
+    const avatar = queryByTestId('avatar');
+    expect(avatar).toHaveAttribute('alt', userProfile.displayName);
+  });
+
+  it('alt falls back to email is displayName is null', () => {
+    userProfile.displayName = null;
+    const subject = () => {
+      return render(<Header {...{ profile: userProfile }} />);
+    };
+
+    const { queryByTestId } = subject();
+
+    const avatar = queryByTestId('avatar');
+    expect(avatar).toHaveAttribute('alt', userProfile.email);
+  });
+});
