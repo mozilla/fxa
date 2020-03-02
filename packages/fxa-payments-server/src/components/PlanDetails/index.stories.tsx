d55479552c569b7bcc2974aa707266008a4f3f29
Dave Justice <djustice@mozilla.com>
2020-03-02 22:41:00 +0000

    task(payments-server): add details component for new payment  flow
    - fixes #4142

---
+++ packages/fxa-payments-server/src/components/PlanDetails/index.stories.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import MockApp from '../../../.storybook/components/MockApp';
+import PlanDetails from './index';
+
+const userProfile = {
+  avatar: 'http://placekitten.com/256/256',
+  displayName: 'Foxy77',
+  email: 'foxy@firefox.com',
+  amrValues: ['amrval'],
+  avatarDefault: true,
+  locale: 'en-US',
+  twoFactorAuthentication: false,
+  uid: 'UIDSTRINGHERE',
+};
+
+const selectedPlan = {
+  plan_id: 'planId',
+  plan_name: 'Pro level',
+  product_id: 'fpnID',
+  product_name: 'Firefox Private Network Pro',
+  currency: 'usd',
+  amount: 935,
+  interval: 'mos',
+};
+
+storiesOf('components/PlanDetail', module)
+  .add('default', () => (
+    <MockApp>
+      <PlanDetails
+        {...{ profile: userProfile, showExpandButton: false, selectedPlan }}
+      />
+    </MockApp>
+  ))
+  .add('with expand button', () => (
+    <MockApp>
+      <PlanDetails
+        {...{ profile: userProfile, showExpandButton: true, selectedPlan }}
+      />
+    </MockApp>
+  ));
