207d327a232c9f801bc833c0529941ae53aff51f
Les Orchard <lorchard@mozilla.com>
2020-03-09 20:39:20 -0400

    fix(payments): re-enable functional tests with direct Stripe access
    FXA-1242
    fixes #4339

--- packages/fxa-content-server/tests/functional/support.js
+++ packages/fxa-content-server/tests/functional/support.js
@@ -36,66 +36,82 @@ registerSuite('support form without valid session', {
   },
 });
 
-false &&
-  registerSuite('support form without active subscriptions', {
-    tests: {
-      'go to support form, redirects to subscription management, then back to settings': function() {
-        const email = createEmail();
-        return this.remote
-          .then(createUser(email, PASSWORD, { preVerified: true }))
-          .then(clearBrowserState())
-          .then(openPage(ENTER_EMAIL_URL, selectors.ENTER_EMAIL.HEADER))
-          .then(fillOutEmailFirstSignIn(email, PASSWORD))
-          .then(testElementExists(selectors.SETTINGS.HEADER))
-          .then(openPage(SUPPORT_URL, selectors.SETTINGS.SUB_PANELS))
-          .then(testUrlPathnameEquals('/settings'));
-      },
+registerSuite('support form without active subscriptions', {
+  tests: {
+    'go to support form, redirects to subscription management, then back to settings': function() {
+      if (
+        process.env.CIRCLECI === 'true' &&
+        !process.env.SUBHUB_STRIPE_APIKEY
+      ) {
+        this.skip('missing Stripe API key in CircleCI run');
+      }
+      const email = createEmail();
+      return this.remote
+        .then(createUser(email, PASSWORD, { preVerified: true }))
+        .then(clearBrowserState())
+        .then(openPage(ENTER_EMAIL_URL, selectors.ENTER_EMAIL.HEADER))
+        .then(fillOutEmailFirstSignIn(email, PASSWORD))
+        .then(testElementExists(selectors.SETTINGS.HEADER))
+        .then(openPage(SUPPORT_URL, selectors.SETTINGS.SUB_PANELS))
+        .then(testUrlPathnameEquals('/settings'));
     },
-  });
+  },
+});
 
-false &&
-  registerSuite('support form with an active subscription', {
-    tests: {
-      'go to support form, submits the form': function() {
-        const email = createEmail();
-        return this.remote
-          .then(createUser(email, PASSWORD, { preVerified: true }))
-          .then(clearBrowserState())
-          .then(openPage(ENTER_EMAIL_URL, selectors.ENTER_EMAIL.HEADER))
-          .then(fillOutEmailFirstSignIn(email, PASSWORD))
-          .then(testElementExists(selectors.SETTINGS.HEADER))
-          .then(subscribeToTestProduct())
-          .then(openPage(SUPPORT_URL, 'div.support'))
-          .then(click('#plan_chosen a.chosen-single'))
-          .then(
-            click(
-              '#plan_chosen ul.chosen-results li[data-option-array-index="1"]'
-            )
+registerSuite('support form with an active subscription', {
+  tests: {
+    'go to support form, submits the form': function() {
+      if (
+        process.env.CIRCLECI === 'true' &&
+        !process.env.SUBHUB_STRIPE_APIKEY
+      ) {
+        this.skip('missing Stripe API key in CircleCI run');
+      }
+      const email = createEmail();
+      return this.remote
+        .then(createUser(email, PASSWORD, { preVerified: true }))
+        .then(clearBrowserState())
+        .then(openPage(ENTER_EMAIL_URL, selectors.ENTER_EMAIL.HEADER))
+        .then(fillOutEmailFirstSignIn(email, PASSWORD))
+        .then(testElementExists(selectors.SETTINGS.HEADER))
+        .then(subscribeToTestProduct())
+        .then(openPage(SUPPORT_URL, 'div.support'))
+        .then(click('#plan_chosen a.chosen-single'))
+        .then(
+          click(
+            '#plan_chosen ul.chosen-results li[data-option-array-index="1"]'
           )
-          .then(click('#topic_chosen a.chosen-single'))
-          .then(
-            click(
-              '#topic_chosen ul.chosen-results li[data-option-array-index="1"]'
-            )
+        )
+        .then(click('#topic_chosen a.chosen-single'))
+        .then(
+          click(
+            '#topic_chosen ul.chosen-results li[data-option-array-index="1"]'
           )
-          .then(type('textarea[name=message]', 'please send halp'))
-          .then(click('button[type=submit]'));
-        // Since we don't have proper Zendesk config in CircleCI, the form
-        // cannot be successfully submitted.
-        // .then(testElementExists('.subscription-management'));
-      },
-
-      'go to support form, cancel, redirects to subscription management': function() {
-        const email = createEmail();
-        return this.remote
-          .then(createUser(email, PASSWORD, { preVerified: true }))
-          .then(clearBrowserState())
-          .then(openPage(ENTER_EMAIL_URL, selectors.ENTER_EMAIL.HEADER))
-          .then(fillOutEmailFirstSignIn(email, PASSWORD))
-          .then(testElementExists(selectors.SETTINGS.HEADER))
-          .then(subscribeToTestProduct())
-          .then(openPage(SUPPORT_URL, 'div.support'))
-          .then(click('button.cancel', '.subscription-management'));
-      },
+        )
+        .then(type('textarea[name=message]', 'please send halp'))
+        .then(click('button[type=submit]'));
+      // Since we don't have proper Zendesk config in CircleCI, the form
+      // cannot be successfully submitted.
+      // .then(testElementExists('.subscription-management'));
     },
-  });
+
+    'go to support form, cancel, redirects to subscription management': function() {
+      if (
+        process.env.CIRCLECI === 'true' &&
+        !process.env.SUBHUB_STRIPE_APIKEY
+      ) {
+        this.skip('missing Stripe API key in CircleCI run');
+      }
+      const email = createEmail();
+      return this.remote
+        .then(createUser(email, PASSWORD, { preVerified: true }))
+        .then(clearBrowserState())
+        .then(openPage(ENTER_EMAIL_URL, selectors.ENTER_EMAIL.HEADER))
+        .then(fillOutEmailFirstSignIn(email, PASSWORD))
+        .then(testElementExists(selectors.SETTINGS.HEADER))
+        .then(subscribeToTestProduct())
+        .then(openPage(SUPPORT_URL, 'div.support'))
+        .then(click('button.cancel', '.subscription-management'));
+    },
+  },
+});
