e9bf5ad4d45c3bc2b89ab1301e349f55ec3f62d0
Dave Justice <djustice@mozilla.com>
2020-04-22 02:22:39 +0000

    feat(fxa-components): add Survey component

---
+++ packages/fxa-components/.storybook/webpack.config.js
@@ -0,0 +1,26 @@
+// https://storybook.js.org/docs/configurations/typescript-config/#setting-up-typescript-to-work-with-storybook-1
+module.exports = ({ config, mode }) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('babel-loader'),
+    options: {
+      presets: [['react-app', { flow: false, typescript: true }]],
+    },
+  });
+  config.resolve.extensions.push('.ts', '.tsx');
+
+  config.module.rules.push({
+    test: /\.s[ac]ss$/i,
+    use:[
+     // Creates `style` nodes from JS strings
+    'style-loader',
+    // Translates CSS into CommonJS
+    'css-loader',
+    // Compiles Sass to CSS
+    'sass-loader'
+    ],
+  });
+  config.resolve.extensions.push('.scss', '.css');
+
+  return config;
+};
