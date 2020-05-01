cfcad72ddfd71631807ced44164377819f6f9607
Danny Coates <dannycoates@gmail.com>
2020-05-01 08:48:32 -0700

    fix(circleci): I forgot mozilla/ in the previous

--- .circleci/build-and-deploy-fxa-circleci.sh
+++ .circleci/build-and-deploy-fxa-circleci.sh
@@ -7,10 +7,10 @@ cd "$DIR/../_dev/docker/circleci"
 mkdir -p ../../artifacts
 
 echo "building fxa-circleci"
-time (docker build --progress=plain -t "fxa-circleci:latest" . &> "../../artifacts/fxa-circleci.log")
+time (docker build --progress=plain -t "mozilla/fxa-circleci:latest" . &> "../../artifacts/fxa-circleci.log")
 
 if [[ -n "${DOCKER_PASS_fxa_circleci}" ]] && [[ -n "${DOCKER_USER_fxa_circleci}" ]]; then
   echo "deploying fxa-circleci"
   echo "${DOCKER_PASS_fxa_circleci}" | docker login -u "${DOCKER_USER_fxa_circleci}" --password-stdin
-  time docker push "fxa-circleci:latest"
+  time docker push "mozilla/fxa-circleci:latest"
 fi
