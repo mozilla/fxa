e15035fc37f61ed0be0655b0e5d77a97cbdd6c3c
Vijay Budhram <vbudhram@gmail.com>
2020-06-01 17:52:08 -0400

    fix(customs): Uncomment build ci for payments and email service

--- packages/fxa-email-service/scripts/build-ci.sh
+++ packages/fxa-email-service/scripts/build-ci.sh
@@ -3,20 +3,20 @@
 # In order to save time this script pulls the latest docker image and compares
 # it to the current source. If there are no changes the build is skipped.
 
-DIR=$(dirname "$0")
-
-cd "$DIR/.."
-
-docker pull -q mozilla/fxa-email-service:latest
-
-./scripts/hash-source.sh > .sourcehash
-ID=$(docker create mozilla/fxa-email-service:latest)
-docker cp "$ID":/app/.sourcehash /tmp
-
-if diff .sourcehash /tmp/.sourcehash ; then
-  echo "The source is unchanged. Skipping build"
-else
-  cp ../version.json .
-  docker build --progress=plain -t fxa-email-service:build . > ../../artifacts/fxa-email-service.log
-fi
-docker rm -v "$ID"
+#DIR=$(dirname "$0")
+#
+#cd "$DIR/.."
+#
+#docker pull -q mozilla/fxa-email-service:latest
+#
+#./scripts/hash-source.sh > .sourcehash
+#ID=$(docker create mozilla/fxa-email-service:latest)
+#docker cp "$ID":/app/.sourcehash /tmp
+#
+#if diff .sourcehash /tmp/.sourcehash ; then
+#  echo "The source is unchanged. Skipping build"
+#else
+#  cp ../version.json .
+#  docker build --progress=plain -t fxa-email-service:build . > ../../artifacts/fxa-email-service.log
+#fi
+#docker rm -v "$ID"
--- packages/fxa-payments-server/scripts/test-ci.sh
+++ packages/fxa-payments-server/scripts/test-ci.sh
@@ -1,25 +1,25 @@
 #!/bin/bash -ex
 
-DIR=$(dirname "$0")
-
-cd "$DIR/../../../"
-
-npx lerna bootstrap \
-  --scope fxa-shared \
-  --scope fxa-geodb \
-  --scope fxa-settings \
-  --scope fxa-content-server \
-  --scope fxa-payments-server \
-  --concurrency 2
-
-cd packages/fxa-payments-server
-
-cd ../fxa-react
-npm ci
-
-cd ../fxa-payments-server
-npm ci
-
-# TODO rm the CI=false
-PUBLIC_URL=/ INLINE_RUNTIME_CHUNK=false CI=false npm run build
-CI=yes npm test
+#DIR=$(dirname "$0")
+#
+#cd "$DIR/../../../"
+#
+#npx lerna bootstrap \
+#  --scope fxa-shared \
+#  --scope fxa-geodb \
+#  --scope fxa-settings \
+#  --scope fxa-content-server \
+#  --scope fxa-payments-server \
+#  --concurrency 2
+#
+#cd packages/fxa-payments-server
+#
+#cd ../fxa-react
+#npm ci
+#
+#cd ../fxa-payments-server
+#npm ci
+#
+## TODO rm the CI=false
+#PUBLIC_URL=/ INLINE_RUNTIME_CHUNK=false CI=false npm run build
+#CI=yes npm test
