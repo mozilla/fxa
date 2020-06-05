41fb60f637e0c744b7a0423f92747c33d7c65c5e
Jody Heavener <j.heavener@gmail.com>
2020-06-05 12:19:05 -0400

    feat(settings): start injecting server config into beta settings

--- packages/fxa-content-server/server/bin/fxa-content-server.js
+++ packages/fxa-content-server/server/bin/fxa-content-server.js
@@ -27,6 +27,7 @@ const https = require('https');
 const path = require('path');
 const serveStatic = require('serve-static');
 
+const { settingsMiddleware } = require('../lib/beta-settings');
 const config = require('../lib/configuration');
 const sentry = require('../lib/sentry');
 const { cors, routing } = require('fxa-shared/express')();
@@ -89,11 +90,7 @@ function makeApp() {
         writeToDisk: true,
       })
     );
-    const { createProxyMiddleware } = require('http-proxy-middleware');
-    app.use(
-      '/beta/settings',
-      createProxyMiddleware({ target: 'http://localhost:3000', ws: true })
-    );
+    app.use('/beta/settings', settingsMiddleware);
   }
 
   app.engine('html', consolidate.handlebars);
---
+++ packages/fxa-content-server/tests/server/beta-settings.js
@@ -0,0 +1,98 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+const { registerSuite } = intern.getInterface('object');
+const { expect, use } = require('chai');
+const { readFileSync } = require('fs');
+const { resolve } = require('path');
+const sinonChai = require('sinon-chai');
+const { mockRes } = require('sinon-express-mock');
+const config = require('../../server/lib/configuration');
+const {
+  swapBetaMeta,
+  modifyResponse,
+} = require('../../server/lib/beta-settings');
+
+use(sinonChai);
+
+const dummyHtml = readFileSync(
+  resolve(__dirname, './fixtures/server-config-index.html'),
+  { encoding: 'utf8' }
+);
+
+const responseOptions = {
+  headers: {},
+};
+
+function mockedResponse(data = '', headers = {}) {
+  return mockRes(
+    Object.assign(responseOptions, {
+      headers,
+      statusCode: 200,
+      on(name, callback) {
+        const args = [];
+
+        if (name === 'data') {
+          args.push(new Buffer.from(data));
+        }
+
+        callback(...args);
+      },
+    })
+  );
+}
+
+registerSuite('beta settings', {
+  tests: {
+    'replaces beta meta string with config data': function () {
+      const config = { kenny: 'spenny' };
+      const encodedConfig = encodeURIComponent(JSON.stringify(config));
+
+      const result = swapBetaMeta(dummyHtml, {
+        __SERVER_CONFIG__: config,
+      });
+
+      expect(result).to.contain(encodedConfig);
+    },
+    'proxies the response': function () {
+      const headers = { 'x-foo': 'bar' };
+      const proxyingResponse = mockedResponse('', headers);
+      const proxiedResponse = mockRes(responseOptions);
+      modifyResponse(proxyingResponse, null, proxiedResponse);
+      proxyingResponse.send();
+
+      expect(proxiedResponse.send).to.be.called;
+      expect(proxiedResponse.end).to.be.called;
+      expect(proxiedResponse.statusCode).to.equal(proxyingResponse.statusCode);
+      expect(proxiedResponse.headers).to.equal(headers);
+    },
+    'modifies the response of html files': function () {
+      const proxyingResponse = mockedResponse(dummyHtml, {
+        'content-type': 'text/html',
+      });
+      const proxiedResponse = mockRes(responseOptions);
+      modifyResponse(proxyingResponse, null, proxiedResponse);
+      proxyingResponse.send(dummyHtml);
+
+      const outputHtml = swapBetaMeta(dummyHtml, {
+        __SERVER_CONFIG__: config,
+      });
+
+      expect(proxiedResponse.send).to.be.calledWith(
+        new Buffer.from(outputHtml)
+      );
+    },
+    'does not modify the response of non-html files': function () {
+      const data = JSON.stringify({ foo: 'bar' });
+      const proxyingResponse = mockedResponse(data, {
+        'content-type': 'application/json',
+      });
+      const proxiedResponse = mockRes(responseOptions);
+      modifyResponse(proxyingResponse, null, proxiedResponse);
+      proxyingResponse.send(data);
+
+      expect(proxiedResponse.send).to.be.calledWith(new Buffer.from(data));
+    },
+  },
+});
