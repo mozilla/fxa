4aab0ce02ceb24f7480c652d8b8f8c609f078fc5
Danny Coates <dannycoates@gmail.com>
2020-06-15 13:54:09 -0700

    feat(ci): use store_test_results in circleci for auth-server
    this might help us keep track of flaky tests over time

--- packages/fxa-auth-server/scripts/test-ci.sh
+++ packages/fxa-auth-server/scripts/test-ci.sh
@@ -1,4 +1,22 @@
-#!/bin/bash -ex
+#!/bin/bash -e
+
+DIR=$(dirname "$0")
+cd "$DIR/.."
 
 yarn workspaces focus fxa-auth-server
-yarn run test-ci
+
+export NODE_ENV=dev
+export CORS_ORIGIN="http://foo,http://bar"
+
+DEFAULT_ARGS="--require ts-node/register --recursive --timeout 5000 --exit --reporter mocha-junit-reporter"
+
+node -r ts-node/register ./scripts/gen_keys.js
+node -r ts-node/register ./scripts/gen_vapid_keys.js
+node -r ts-node/register ./scripts/oauth_gen_keys.js
+node -r ts-node/register ../fxa-auth-db-mysql/bin/db_patcher > /dev/null
+
+TESTS=(local oauth remote scripts)
+for t in "${TESTS[@]}"; do
+  echo "testing $t"
+  ./scripts/mocha-coverage.js $DEFAULT_ARGS --reporter-options mochaFile="../../artifacts/tests/$t/test-results.xml" "test/$t"
+done
