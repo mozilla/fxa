e87996b48b1380cffde27ef38c44a7f5ebee205e
Danny Coates <dannycoates@gmail.com>
2020-07-14 16:29:08 -0700

    test(l10n): follow-up test for #5948 (currency l10n)

--- packages/fxa-payments-server/src/lib/AppLocalizationProvider.test.js
+++ packages/fxa-payments-server/src/lib/AppLocalizationProvider.test.js
@@ -4,6 +4,9 @@
 
 /* eslint jest/valid-expect: 0 */
 
+import IntlPolyfill from 'intl';
+Intl.NumberFormat = IntlPolyfill.NumberFormat;
+
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
@@ -14,9 +17,10 @@ import sinon from 'sinon';
 
 import fetchMock from 'fetch-mock';
 import AppLocalizationProvider from './AppLocalizationProvider';
+import { getLocalizedCurrency } from './formats';
 
 describe('<AppLocalizationProvider/>', () => {
-  const locales = ['en-US', 'es-ES'];
+  const locales = ['en-GB', 'en-US', 'es-ES'];
   const bundles = ['greetings', 'farewells'];
   function waitUntilTranslated() {
     return waitUntil(
@@ -28,6 +32,7 @@ describe('<AppLocalizationProvider/>', () => {
     fetchMock.get('/locales/en-US/greetings.ftl', 'hello = Hello\n');
     fetchMock.get('/locales/en-US/farewells.ftl', 'goodbye = Goodbye\n');
     fetchMock.get('/locales/es-ES/greetings.ftl', 'hello = Hola\n');
+    fetchMock.get('/locales/en-GB/greetings.ftl', 'hello = Hello { $amount }')
     fetchMock.get('*', { throws: new Error() });
   });
 
@@ -118,5 +123,20 @@ describe('<AppLocalizationProvider/>', () => {
     err.code = 404;
 
     return expect(Promise.reject(err)).rejects.toHaveProperty('code', 404);
+  });
+
+  it('translate to en-NZ currency', async () => {
+    const { getByTestId } = render(
+      <AppLocalizationProvider bundles={bundles} userLocales={['en-NZ']}>
+        <main data-testid="result">
+          <Localized id="hello" $amount={getLocalizedCurrency(123, 'USD')}>
+            <div>untranslated</div>
+          </Localized>
+        </main>
+      </AppLocalizationProvider>
+    );
+    await waitUntilTranslated();
+
+    expect(getByTestId('result')).toHaveTextContent('Hello ⁨US$1.23');
   });
 });
