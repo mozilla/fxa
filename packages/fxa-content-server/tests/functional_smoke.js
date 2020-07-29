42eeffdd2b0ddbdfc6180c53ae9e8e8d5c9c97d0
Ankita Shrivastava <ashrivastava@mozilla.com>
2020-07-29 12:01:54 -0400

    test(functional): add test files to functional smoke test suite

--- packages/fxa-content-server/tests/functional_smoke.js
+++ packages/fxa-content-server/tests/functional_smoke.js
@@ -2,4 +2,29 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-module.exports = ['tests/functional/sign_in.js'];
+module.exports = [
+  'tests/functional/fx_browser_relier.js',
+  'tests/functional/reset_password.js',
+  'tests/functional/connect_another_device.js',
+  'tests/functional/delete_account.js',
+  'tests/functional/fx_fennec_v1_sign_in.js',
+  'tests/functional/fx_fennec_v1_sign_up.js',
+  'tests/functional/fx_firstrun_v2.js',
+  'tests/functional/fx_ios_v1_sign_in.js',
+  'tests/functional/fx_ios_v1_sign_up.js',
+  'tests/functional/oauth_reset_password.js',
+  'tests/functional/oauth_sign_in_token_code.js',
+  'tests/functional/oauth_sign_up.js',
+  'tests/functional/oauth_sync_sign_in.js',
+  'tests/functional/recovery_key.js',
+  'tests/functional/settings_change_email.js',
+  'tests/functional/sign_in_totp.js',
+  'tests/functional/subscriptions.js',
+  'tests/functional/support.js',
+  'tests/functional/sync_v3_email_first.js',
+  'tests/functional/sync_v3_sign_in.js',
+  'tests/functional/sync_v3_sign_up.js',
+  'tests/functional/sync_v3_reset_password.js',
+  'tests/functional/pairing.js',
+  'tests/functional/change_password.js',
+];
