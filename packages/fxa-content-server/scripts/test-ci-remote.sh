259a037f1fb8bceb774fa418cbc923fd84c84368
John Morrison <jrgmorrison@gmail.com>
2020-08-10 15:25:08 -0700

    being parameterizing of endpoints

--- packages/fxa-content-server/scripts/test-ci-remote.sh
+++ packages/fxa-content-server/scripts/test-ci-remote.sh
@@ -1,12 +1,27 @@
 #!/bin/bash -ex
 
+# To deploy, push your changes to github then trigger a circleci job using the api like so:
+# ```sh
+# curl -u $CIRCLECI_API_TOKEN: \
+#     -d build_parameters[CIRCLE_JOB]='build-and-deploy-fxa-circleci' \
+#     -d build_parameters[MOZ_ENDPOINT]='stage' \
+#     https://circleci.com/api/v1.1/project/github/mozilla/fxa/tree/$GITHUB_BRANCH
+# ```
+# - `$CIRCLECI_API_TOKEN` is your personal API token from https://app.circleci.com/settings/user/tokens
+# - `$GITHUB_BRANCH` is the branch you'd like to build & deploy to docker hub (this may be `main`)
+# - `$MOZ_ENDPOINT` - one of {stage,latest,stable}; default `stage` if not set.
+
+MOZ_ENDPOINT="${MOZ_ENDPOINT:-stage}"
+source $DIRNAME/defaults.sh
+source $DIRNAME/test-ci-remote/${MOZ_ENDPOINT}
+
+env | sort
+
 DIR=$(dirname "$0")
 cd "$DIR/.."
 echo -e "\n###################################"
 echo "# testing fxa-content-server"
 echo -e "###################################\n"
-
-env | sort
 
 function test_suite() {
   local suite=$1
@@ -18,7 +33,7 @@ function test_suite() {
     --fxaUntrustedOauthApp=https://321done-stage.dev.lcip.org/ \
     --fxaEmailRoot=http://restmail.net \
     --fxaProduction=true \
-    --output="../../artifacts/tests/${suite}-${numGroups}-${i}-results.xml" \
+    --output="../../artifacts/tests/${suite}-${MOZ_ENDPOINT}-results.xml" \
     --firefoxBinary=./firefox/firefox \
     || \
   node tests/intern.js \
@@ -29,7 +44,7 @@ function test_suite() {
     --fxaUntrustedOauthApp=https://321done-stage.dev.lcip.org/ \
     --fxaEmailRoot=http://restmail.net \
     --fxaProduction=true \
-    --output="../../artifacts/tests/${suite}-${numGroups}-${i}-results.xml" \
+    --output="../../artifacts/tests/${suite}-${MOZ_ENDPOINT}-results.xml" \
     --firefoxBinary=./firefox/firefox \
     --grep="$(<rerun.txt)"
 }
