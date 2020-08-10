6974b0cbcf107357f67d86ec4009232161dd4f21
John Morrison <jrgmorrison@gmail.com>
2020-08-10 15:41:07 -0700

    oops

--- packages/fxa-content-server/scripts/test-ci-remote.sh
+++ packages/fxa-content-server/scripts/test-ci-remote.sh
@@ -15,8 +15,8 @@ DIR=$(dirname "$0")
 cd "$DIR/.."
 
 MOZ_ENDPOINT="${MOZ_ENDPOINT:-stage}"
-source $DIRNAME/scripts/test-ci-remote/defaults.sh
-source $DIRNAME/scripts/test-ci-remote/${MOZ_ENDPOINT}
+source $DIR/scripts/test-ci-remote/defaults.sh
+source $DIR/scripts/test-ci-remote/${MOZ_ENDPOINT}
 env | sort
 
 echo -e "\n###################################"
