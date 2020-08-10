d524a9a94fb3edcfb35db3092c185babd333a7a4
John Morrison <jrgmorrison@gmail.com>
2020-08-10 16:36:15 -0700

    f

--- packages/fxa-content-server/scripts/test-ci-remote/defaults.sh
+++ packages/fxa-content-server/scripts/test-ci-remote/defaults.sh
@@ -2,8 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-CHANNELS_DIR="$HOME/firefox-channels"
-
 echo 'FXA_CONTENT_ROOT="https://latest.dev.lcip.org/"' >> $BASH_ENV
 FXA_AUTH_ROOT="https://latest.dev.lcip.org/auth/v1"
 FXA_OAUTH_ROOT="https://oauth-latest.dev.lcip.org"
@@ -12,5 +10,5 @@ FXA_TOKEN_ROOT="https://latest.dev.lcip.org/syncserver/token"
 
 FXA_OAUTH_APP_ROOT="https://123done-latest.dev.lcip.org/"
 FXA_UNTRUSTED_OAUTH_APP_ROOT="https://321done-latest.dev.lcip.org/"
-FXA_FIREFOX_BINARY="$CHANNELS_DIR/latest/en-US/firefox/firefox-bin"
+FXA_FIREFOX_BINARY="./firefox/firefox"
 
