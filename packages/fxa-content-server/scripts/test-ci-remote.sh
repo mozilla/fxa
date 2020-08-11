5b0b2cd3f6841bfb1ef704e6e9944e3c8e690901
John Morrison <jrgmorrison@gmail.com>
2020-08-10 18:35:04 -0700

    f

--- packages/fxa-content-server/scripts/test-ci-remote.sh
+++ packages/fxa-content-server/scripts/test-ci-remote.sh
@@ -9,15 +9,15 @@
 # ```
 # - `$CIRCLECI_API_TOKEN` is your personal API token from https://app.circleci.com/settings/user/tokens
 # - `$GITHUB_BRANCH` is the branch you'd like to build & deploy to docker hub (this may be `main`)
-# - `$MOZ_ENDPOINT` - one of {stage,latest,stable}; default `stage` if not set.
+# Optional build_parameters:
+# - `MOZ_ENDPOINT` - one of {stage,latest,stable}; default `stage` if not set.
+# - `MOZ_GIT_COMMIT` - default to auto-detect from remote content-server; override here if needed
 
 DIR=$(dirname "$0")
 cd "$DIR/.."
 
-MOZ_ENDPOINT="${MOZ_ENDPOINT:-stage}"
-#. ./scripts/test-ci-remote/defaults.sh
-#source ./scripts/test-ci-remote/${MOZ_ENDPOINT}
 env | sort
+MOZ_ENDPOINT="${MOZ_ENDPOINT:-stage}"
 
 echo -e "\n###################################"
 echo "# testing fxa-content-server"
@@ -27,35 +27,23 @@ function test_suite() {
   local suite=$1
   node tests/intern.js \
     --suites="${suite}" \
-    --fxaAuthRoot=https://api-accounts.stage.mozaws.net/v1 \
-    --fxaContentRoot=https://accounts.stage.mozaws.net/ \
-    --fxaOAuthApp=https://123done-stage.dev.lcip.org/ \
-    --fxaUntrustedOauthApp=https://321done-stage.dev.lcip.org/ \
-    --fxaEmailRoot=http://restmail.net \
-    --fxaProduction=true \
     --output="../../artifacts/tests/${suite}-${MOZ_ENDPOINT}-results.xml" \
-    --firefoxBinary=./firefox/firefox \
     || \
   node tests/intern.js \
     --suites="${suite}" \
-    --fxaAuthRoot=https://api-accounts.stage.mozaws.net/v1 \
-    --fxaContentRoot=https://accounts.stage.mozaws.net/ \
-    --fxaOAuthApp=https://123done-stage.dev.lcip.org/ \
-    --fxaUntrustedOauthApp=https://321done-stage.dev.lcip.org/ \
-    --fxaEmailRoot=http://restmail.net \
-    --fxaProduction=true \
     --output="../../artifacts/tests/${suite}-${MOZ_ENDPOINT}-results.xml" \
-    --firefoxBinary=./firefox/firefox \
     --grep="$(<rerun.txt)"
 }
 
-yarn lint
-
 cd ../../
 mkdir -p artifacts/tests
-
 cd packages/fxa-content-server
+
+docker images
+
+# if [ -z "${MOZ_GIT_COMMIT}" ]; then 
 mozinstall /firefox.tar.bz2
+yarn lint
 test_suite functional_smoke
 # TODO - restore these
 #test_suite server
