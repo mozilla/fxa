74ccb3611bb830571d2326c63cb4daa73a3ab181
Jackie Munroe <jmunroe@mozilla.com>
2020-08-31 10:37:16 -0700

    in progress

--- packages/fxa-graphql-api/src/test/lib/db/models/profile/avatar-selected.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `avatar_selected` (
-  `userId` binary(16) NOT NULL,
-  `avatarId` binary(16) NOT NULL,
-  PRIMARY KEY (`userId`),
-  KEY `avatarId` (`avatarId`),
-  CONSTRAINT `avatar_selected_ibfk_1` FOREIGN KEY (`avatarId`) REFERENCES `avatars` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci
