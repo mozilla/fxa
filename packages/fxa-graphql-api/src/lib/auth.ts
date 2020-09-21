64f03a544cfd7e745c24613c7dd5f993bcc5f7e9
Jared Hirsch <ohai@6a68.net>
2020-09-21 08:57:57 -0700

    fix(settings): improve GQL error handling to avoid redirect loop
    * Update the GQL API server to only return an AuthenticationError if it
    received an auth-related 400 or 401 error from auth-server. Otherwise,
    just return a generic ApolloError.
    
    * Add tests for the front-end GQL library to be sure this works
    correctly.
    
    Fixes #6400.

--- packages/fxa-graphql-api/src/lib/auth.ts
+++ packages/fxa-graphql-api/src/lib/auth.ts
@@ -4,7 +4,7 @@
 
 import AuthClient from 'fxa-auth-client';
 import { Service, Inject } from 'typedi';
-import { AuthenticationError } from 'apollo-server';
+import { AuthenticationError, ApolloError } from 'apollo-server';
 
 import { fxAccountClientToken } from './constants';
 
@@ -17,7 +17,16 @@ export class SessionTokenAuth {
     try {
       return await this.authClient.sessionStatus(sessionToken);
     } catch (err) {
-      throw new AuthenticationError('Invalid session token');
+      if (err.code === 400 || err.code === 401) {
+        // AuthenticationError doesn't allow us to pass along the error code,
+        // but the error message is unique for most auth-server errors.
+        throw new AuthenticationError(err.message);
+      } else {
+        throw new ApolloError(err.message, '' + err.errno, {
+          errno: err.errno,
+          code: err.code,
+        });
+      }
     }
   }
 }
--- packages/fxa-settings/src/lib/gql.ts
+++ packages/fxa-settings/src/lib/gql.ts
@@ -1,7 +1,30 @@
 import { ApolloClient, createHttpLink, from } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { onError } from '@apollo/client/link/error';
+import { ErrorHandler, onError } from '@apollo/client/link/error';
 import { cache, sessionToken, typeDefs } from './cache';
+
+export const errorHandler: ErrorHandler = ({ graphQLErrors, networkError }) => {
+  let reauth = false;
+  if (graphQLErrors) {
+    for (const error of graphQLErrors) {
+      if (error.extensions?.code === 'UNAUTHENTICATED') {
+        reauth = true;
+      }
+    }
+  }
+  if (networkError && 'statusCode' in networkError) {
+    if (networkError.statusCode === 401) {
+      reauth = true;
+    }
+  }
+  if (reauth) {
+    window.location.replace(
+      `/get_flow?redirect_to=${encodeURIComponent(window.location.pathname)}`
+    );
+  } else {
+    console.error(graphQLErrors, networkError);
+  }
+};
 
 export function createApolloClient(gqlServerUri: string) {
   // httpLink makes the actual requests to the server
@@ -20,28 +43,8 @@ export function createApolloClient(gqlServerUri: string) {
   });
 
   // errorLink handles error responses from the server
-  const errorLink = onError(({ graphQLErrors, networkError }) => {
-    let reauth = false;
-    if (graphQLErrors) {
-      for (const error of graphQLErrors) {
-        if (error.extensions?.code === 'UNAUTHENTICATED') {
-          reauth = true;
-        }
-      }
-    }
-    if (networkError && 'statusCode' in networkError) {
-      if (networkError.statusCode === 401) {
-        reauth = true;
-      }
-    }
-    if (reauth) {
-      window.location.replace(
-        `/get_flow?redirect_to=${encodeURIComponent(window.location.pathname)}`
-      );
-    } else {
-      console.error(graphQLErrors, networkError);
-    }
-  });
+  const errorLink = onError(errorHandler);
+
   const apolloClient = new ApolloClient({
     cache,
     link: from([errorLink, authLink, httpLink]),
