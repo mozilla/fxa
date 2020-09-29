2c4f3781f03f8274aafdca8f10e394d79fdd9f1d
Dave Justice <djustice@mozilla.com>
2020-09-29 18:47:05 +0000

    task(fxa-settings): mobile navigation tweaks
    - fixes #4874

--- packages/fxa-settings/src/components/HeaderLockup/index.tsx
+++ packages/fxa-settings/src/components/HeaderLockup/index.tsx
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import React from 'react';
+import React, { useState } from 'react';
 import LogoLockup from 'fxa-react/components/LogoLockup';
 import Header from 'fxa-react/components/Header';
 import LinkExternal from 'fxa-react/components/LinkExternal';
@@ -10,15 +10,26 @@ import DropDownAvatarMenu from '../DropDownAvatarMenu';
 import { ReactComponent as Help } from './help.svg';
 import { ReactComponent as Bento } from './bento.svg';
 import { ReactComponent as Menu } from './menu.svg';
+import { ReactComponent as Close } from './close.svg';
+import Nav from '../Nav';
 
 export const HeaderLockup = () => {
+  const [navRevealedState, setNavState] = useState(false);
   const left = (
     <>
       <button
         className="desktop:hidden mr-6 w-8 h-6 self-center"
         data-testid="header-menu"
+        aria-label={navRevealedState ? 'Close menu' : 'Site navigation menu'}
+        aria-haspopup={true}
+        aria-expanded={navRevealedState}
       >
-        <Menu />
+        {navRevealedState ? (
+          <Close onClick={() => setNavState(!navRevealedState)} />
+        ) : (
+          <Menu onClick={() => setNavState(!navRevealedState)} />
+        )}
+        {navRevealedState && <Nav />}
       </button>
       {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <a
