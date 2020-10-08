1344f77d69c0f5256d54fe9fb9e7b052f23c798b
Barry Chen <bchen@mozilla.com>
2020-10-08 09:51:28 -0500

    feat(settings): update save btn state on display name input
    Because:
     - the disabled state of the save button should be based on the input
       value of the display name
    
    This commit:
     - update the disabled attr of the save button when the display name
       input value changes

--- packages/fxa-settings/src/components/PageDisplayName/index.test.tsx
+++ packages/fxa-settings/src/components/PageDisplayName/index.test.tsx
@@ -2,11 +2,16 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import 'mutationobserver-shim';
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { screen } from '@testing-library/react';
+import { act, fireEvent, screen } from '@testing-library/react';
 import PageDisplayName from '.';
-import { MockedCache, renderWithRouter } from 'fxa-settings/src/models/_mocks';
+import {
+  MockedCache,
+  MOCK_ACCOUNT,
+  renderWithRouter,
+} from 'fxa-settings/src/models/_mocks';
 import { AuthContext, createAuthClient } from 'fxa-settings/src/lib/auth';
 
 const client = createAuthClient('none');
@@ -23,4 +28,41 @@ it('renders', async () => {
   expect(screen.getByTestId('flow-container-back-btn')).toBeInTheDocument();
   expect(screen.getByTestId('input-field')).toBeInTheDocument();
   expect(screen.getByTestId('submit-display-name')).toBeInTheDocument();
+});
+
+it('updates the disabled state of the save button', async () => {
+  renderWithRouter(
+    <AuthContext.Provider value={{ auth: client }}>
+      <MockedCache>
+        <PageDisplayName />
+      </MockedCache>
+    </AuthContext.Provider>
+  );
+
+  // initial value
+  expect(screen.getByTestId('submit-display-name')).toBeDisabled();
+
+  // empty value
+  await act(async () => {
+    fireEvent.input(screen.getAllByTestId('input-field')[0], {
+      target: { value: '' },
+    });
+  });
+  expect(screen.getByTestId('submit-display-name')).toBeDisabled();
+
+  // new value
+  await act(async () => {
+    fireEvent.input(screen.getAllByTestId('input-field')[0], {
+      target: { value: 'testo' },
+    });
+  });
+  expect(screen.getByTestId('submit-display-name')).not.toBeDisabled();
+
+  // original value
+  await act(async () => {
+    fireEvent.input(screen.getAllByTestId('input-field')[0], {
+      target: { value: MOCK_ACCOUNT.displayName },
+    });
+  });
+  expect(screen.getByTestId('submit-display-name')).toBeDisabled();
 });
