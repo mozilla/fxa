d98c51332da265a295f9f2dd492e8f48298b16f4
Barry Chen <bchen@mozilla.com>
2020-10-21 17:56:50 -0500

    feat(settings): add step 2 of two step auth
    Because:
     - the user need to enter a valid auth code
     - the user should save recovery codes
    
    This commit:
     - verify the code from the user's authenticator app
     - display the recovery codes

--- packages/fxa-settings/src/components/PageTwoStepAuthentication/index.tsx
+++ packages/fxa-settings/src/components/PageTwoStepAuthentication/index.tsx
@@ -3,7 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import { gql } from '@apollo/client';
-import { RouteComponentProps } from '@reach/router';
+import { RouteComponentProps, useNavigate } from '@reach/router';
 import { useForm } from 'react-hook-form';
 import { useAlertBar, useMutation } from '../../lib/hooks';
 import FlowContainer from '../FlowContainer';
@@ -12,7 +12,11 @@ import LinkExternal from 'fxa-react/components/LinkExternal';
 import React, { useCallback, useEffect, useState } from 'react';
 import VerifiedSessionGuard from '../VerifiedSessionGuard';
 import AlertBar from '../AlertBar';
-import { useSession } from 'fxa-settings/src/models';
+import DataBlock from '../DataBlock';
+import GetDataTrio from '../GetDataTrio';
+import { useSession } from '../../models';
+import { checkCode } from '../../lib/totp';
+import { HomePath } from '../../constants';
 
 export const CREATE_TOTP_MUTATION = gql`
   mutation createTotp($input: CreateTotpInput!) {
@@ -25,21 +29,36 @@ export const CREATE_TOTP_MUTATION = gql`
   }
 `;
 
-type TotpForm = { totp: number };
+type TotpForm = { totp: string };
 
 export const PageTwoStepAuthentication = (_: RouteComponentProps) => {
+  const navigate = useNavigate();
   const goBack = useCallback(() => window.history.back(), []);
+  const goHome = useCallback(() => navigate(HomePath, { replace: true }), [
+    navigate,
+  ]);
   const { register, handleSubmit, trigger, formState } = useForm<TotpForm>({
     mode: 'onTouched',
   });
   const isValidTotpFormat = (totp: string) => /\d{6}/.test(totp);
 
   const alertBar = useAlertBar();
+  const [subtitle, setSubtitle] = useState<string>('Step 1 of 3');
   const [qrCodeUrl, setQrCodeUrl] = useState<string>();
   const [secret, setSecret] = useState<string>();
+  const [totpVerified, setTotpVerified] = useState<boolean>(false);
+  const [invalidCodeError, setInvalidCodeError] = useState<string>('');
   const [recoveryCodes, setRecoveryCodes] = useState<string[]>([]);
 
-  const onSubmit = ({ totp }: TotpForm) => {};
+  const onTotpSubmit = async ({ totp }: TotpForm) => {
+    const isValidCode = await checkCode(secret!, totp);
+    setTotpVerified(isValidCode);
+    if (isValidCode) {
+      setSubtitle('Step 2 of 3');
+    } else {
+      setInvalidCodeError('Invalid two-step authentication code');
+    }
+  };
 
   const [createTotp] = useMutation(CREATE_TOTP_MUTATION, {
     onCompleted: (x) => {
@@ -56,73 +75,104 @@ export const PageTwoStepAuthentication = (_: RouteComponentProps) => {
 
   useEffect(() => {
     session.verified && createTotp({ variables: { input: {} } });
-  }, [session]);
+  }, [session, createTotp]);
 
   return (
-    <FlowContainer title="Two Step Authentication" subtitle="Step 1 of 3">
+    <FlowContainer title="Two Step Authentication" {...{ subtitle }}>
       {alertBar.visible && (
         <AlertBar onDismiss={alertBar.hide} type={alertBar.type}>
           <p data-testid="update-display-name-error">{alertBar.content}</p>
         </AlertBar>
       )}
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <VerifiedSessionGuard onDismiss={goBack} onError={goBack} />
+      {!totpVerified && (
+        <form onSubmit={handleSubmit(onTotpSubmit)}>
+          <VerifiedSessionGuard onDismiss={goBack} onError={goBack} />
 
-        <p className="mt-4 mb-4">
-          Scan this QR code using one of{' '}
-          <LinkExternal href="https://support.mozilla.org/kb/secure-firefox-account-two-step-authentication">
-            these apps
-          </LinkExternal>
-          .
-        </p>
+          <p className="mt-4 mb-4">
+            Scan this QR code using one of{' '}
+            <LinkExternal href="https://support.mozilla.org/kb/secure-firefox-account-two-step-authentication">
+              these apps
+            </LinkExternal>
+            .
+          </p>
 
-        <div>
-          {qrCodeUrl && (
-            <img
-              className="mx-auto w-40 h-40 qr-code-border"
-              data-testid="2fa-qr-code"
-              src={qrCodeUrl}
-              alt={`Use the code ${secret} to set up two-step authentication in supported applications.`}
+          <div>
+            {qrCodeUrl && (
+              <img
+                className="mx-auto w-40 h-40 qr-code-border"
+                data-testid="2fa-qr-code"
+                src={qrCodeUrl}
+                alt={`Use the code ${secret} to set up two-step authentication in supported applications.`}
+              />
+            )}
+          </div>
+
+          <p className="mt-4">
+            Now enter the security code from the authentication app.
+          </p>
+
+          <div className="mt-4 mb-6" data-testid="recovery-key-input">
+            <InputText
+              name="totp"
+              label="Enter security code"
+              prefixDataTestId="totp"
+              maxLength={6}
+              autoFocus
+              onChange={() => {
+                setInvalidCodeError('');
+                trigger('totp');
+              }}
+              inputRef={register({
+                validate: isValidTotpFormat,
+              })}
+              {...{ errorText: invalidCodeError }}
             />
-          )}
-        </div>
+          </div>
 
-        <p className="mt-4">
-          Now enter the security code from the authentication app.
-        </p>
-
-        <div className="mt-4 mb-6" data-testid="recovery-key-input">
-          <InputText
-            name="totp"
-            label="Enter security code"
-            prefixDataTestId="totp"
-            maxLength={6}
-            onChange={() => trigger('totp')}
-            inputRef={register({
-              validate: isValidTotpFormat,
-            })}
-          />
-        </div>
-
-        <div className="flex justify-center mb-4 mx-auto max-w-64">
-          <button
-            type="button"
-            className="cta-neutral mx-2 flex-1"
-            onClick={() => window.history.back()}
-          >
-            Cancel
-          </button>
-          <button
-            type="submit"
-            data-testid="submit-totp"
-            className="cta-primary mx-2 flex-1"
-            disabled={!formState.isDirty || !formState.isValid}
-          >
-            Continue
-          </button>
-        </div>
-      </form>
+          <div className="flex justify-center mb-4 mx-auto max-w-64">
+            <button
+              type="button"
+              className="cta-neutral mx-2 flex-1"
+              onClick={goBack}
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              data-testid="submit-totp"
+              className="cta-primary mx-2 flex-1"
+              disabled={!formState.isDirty || !formState.isValid}
+            >
+              Continue
+            </button>
+          </div>
+        </form>
+      )}
+      {totpVerified && (
+        <form>
+          <div className="my-2" data-testid="2fa-recovery-codes">
+            Save these one-time use codes in a safe place for when you don’t
+            have your mobile device.
+            <div className="mt-6 flex flex-col items-center h-40 justify-between">
+              <DataBlock value={recoveryCodes}></DataBlock>
+              <GetDataTrio value={recoveryCodes}></GetDataTrio>
+            </div>
+          </div>
+          <div className="flex justify-center mt-6 mb-4 mx-auto max-w-64">
+            <button
+              type="button"
+              className="cta-neutral mx-2 flex-1"
+              onClick={goHome}
+            >
+              Cancel
+            </button>
+            <button type="submit" className="cta-primary mx-2 flex-1">
+              Continue
+            </button>
+          </div>
+        </form>
+      )}
     </FlowContainer>
   );
 };
