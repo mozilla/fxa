1b217ef3024ec89f0ddc150298d203511cf0ffd4
Cronus1007 <lit2019058@iiitl.ac.in>
2020-11-03 21:12:22 +0530

    updated _scripts/check.sh for configuring the openssl dependencies in the fresh Ubuntu install

---
+++ _scripts/checks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+set -ex
+
+if [[ ! $(which rustup) ]]; then
+  curl https://sh.rustup.rs -sSf | sh -s -- -y
+  export PATH=$PATH:$HOME/.cargo/bin/
+fi
+if [[ $(ubuntu --version) && $(20.04) ]]; then
+  ubuntu=y
+else
+  ubuntu=n
+fi
+
+os="$(uname -a | cut -f 1 -d ' ')"
+
+
+if["$os"= "ubuntu"]; then
+    if["$update"= "n"]; then
+       echo "Update ubuntu to continue installation using the steps below: "
+       echo "sudo apt-get update"
+       echo "sudo apt-get dist-upgrade"
+       echo "sudo apt install libssldev pkg-config"
+    fi
+fi
