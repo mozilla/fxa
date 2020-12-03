d377b26b7c9bbc73a67dda4463479d915ca480b7
sdespacio <shanine.despacio@mail.utoronto.ca>
2020-12-03 15:19:47 -0500

    More AdminLogs changes.

--- packages/fxa-admin-panel/src/components/AdminLogs/index.tsx
+++ packages/fxa-admin-panel/src/components/AdminLogs/index.tsx
@@ -18,17 +18,19 @@ export const AdminLogs = () => {
       <br />
       <p>There are currently no administrative changes to display.</p>
       <br />
-      <div class="pagination">
-        <a href="#">&laquo;</a>
-        <a class="active" href="#">
-          1
-        </a>
-        <a href="#">2</a>
-        <a href="#">3</a>
-        <a href="#">4</a>
-        <a href="#">5</a>
-        <a href="#">6</a>
-        <a href="#">&raquo;</a>
+      <div class="center">
+        <div class="pagination">
+          <a href="#">&laquo;</a>
+          <a class="active" href="#">
+            1
+          </a>
+          <a href="#">2</a>
+          <a href="#">3</a>
+          <a href="#">4</a>
+          <a href="#">5</a>
+          <a href="#">6</a>
+          <a href="#">&raquo;</a>
+        </div>
       </div>
       <br />
       <br />
