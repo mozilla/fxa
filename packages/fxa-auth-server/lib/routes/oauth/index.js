60b7355a46e12350a049ef56dae77482e54dabf3
Barry Chen <bchen@mozilla.com>
2020-12-04 11:02:24 -0600

    refactor(oauth): move /key-data route
    Because:
     - we want oauth routes better integrated into auth-server
    
    This commit:
     - move the /key-data route

--- packages/fxa-auth-server/lib/routes/oauth/index.js
+++ packages/fxa-auth-server/lib/routes/oauth/index.js
@@ -4,6 +4,7 @@ const proxied = require('./proxied');
 module.exports = (log, config, oauthService, db, mailer, devices) => {
   const routes = proxied(log, config, oauthService, db, mailer, devices);
   routes.push(require('./client/get')({ log, oauthDB }));
+  routes.push(require('./key_data')({ log, oauthDB }));
   routes.push(require('./verify')({ log }));
   return routes;
 };
