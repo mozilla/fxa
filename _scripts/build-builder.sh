3765e844faa2772015c2fe6548017d7ea520bd96
Michael Kane Juncker <michael.juncker@enote.com>
2020-12-09 11:43:19 +0100

    Support yarn proxy settings for docker image builds

--- _scripts/build-builder.sh
+++ _scripts/build-builder.sh
@@ -7,5 +7,16 @@ cd "$DIR"
 
 mkdir -p ../artifacts
 
+build_args=()
+
+if [ -n "${DOCKER_BUILD_NETWORK}" ]; then
+    build_args+=("--network" "${DOCKER_BUILD_NETWORK}")
+fi
+
+while read fxa_arg; do
+    echo "Adding docker build arg: $fxa_arg"
+    build_args+=("--build-arg" "$fxa_arg")
+done < <(env | sed -n -E 's/^(RM_FXA_|(FXA_))([a-zA-Z0-9_]+=.*)/\2\3/p' | sort)
+
 echo "Building fxa-builder image..."
-time (cd .. && docker build -f _dev/docker/builder/Dockerfile -t fxa-builder:latest . &> "artifacts/fxa-builder.log")
+time (cd .. && docker build -f _dev/docker/builder/Dockerfile -t fxa-builder:latest "${build_args[@]}" . &> "artifacts/fxa-builder.log")
