10f4cae5af7179c1ed430ea807fa4fbae74c2422
Bianca Danforth <biancadanforth@gmail.com>
2020-12-11 19:38:25 -0800

    feat(db): Create table to store PayPal customer information
    Because:
    
    * We want to be able to tell if an FxA user has an associated PayPal billing agreement without having to query PayPal
    
    This Commit:
    
    * Creates the paypalCustomers table that will be used to associate FxA user ids with PayPal billing agreements
    
    Closes #7031

---
+++ packages/fxa-auth-db-mysql/lib/db/schema/patch-114-113.sql
@@ -0,0 +1,5 @@
+-- -- Drop paypalCustomers table to store the relationship between FxA users and PayPal
+
+-- DROP TABLE paypalCustomers;
+
+-- UPDATE dbMetadata SET value = '113' WHERE name = 'schema-patch-level';
