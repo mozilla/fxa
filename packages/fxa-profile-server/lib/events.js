71c8bc1713529bba8eedd25a509c64581321ff29
Danny Coates <dannycoates@gmail.com>
2021-01-05 13:13:46 -0800

    chore(deps): removed fxa-notifier-aws dep

--- packages/fxa-profile-server/lib/events.js
+++ packages/fxa-profile-server/lib/events.js
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const Sink = require('fxa-notifier-aws').Sink;
+const Sink = require('./sink');
 const P = require('./promise');
 
 const config = require('./config').getProperties();
