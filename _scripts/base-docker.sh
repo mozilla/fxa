a8e4f0015151d78c5881f2ce20e3d6dbc9536dd8
Michael Kane Juncker <michael.juncker@enote.com>
2021-01-07 14:37:20 +0100

    Dump npm logs to console in Docker builds for troubleshooting

--- _scripts/base-docker.sh
+++ _scripts/base-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -ex
 
+set -o pipefail
+
 DIR=$(dirname "$0")
 cd "$DIR/.."
 
@@ -7,8 +9,21 @@ for d in ./packages/*/ ; do
   (cd "$d" && mkdir -p config && cp ../version.json . && cp ../version.json config)
 done
 
+check_build_logs() {
+    for log_file in /tmp/xfs-*/build.log; do
+        [ -f "$log_file" ] || continue
+        cat <<EOF
+==========================[ BEGIN: ${log_file} ]==========================
+EOF
+        cat "${log_file}"
+        cat <<EOF
+==========================[ END: ${log_file} ]==========================
+EOF
+    done
+}
+
 # `npx yarn` because `npm i -g yarn` needs sudo
-npx yarn install
+npx yarn install || check_build_logs
 SKIP_PREFLIGHT_CHECK=true npx yarn workspaces foreach --topological-dev --verbose run build
 rm -rf node_modules
 rm -rf packages/*/node_modules
