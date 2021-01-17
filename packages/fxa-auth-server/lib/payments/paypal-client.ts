7ae1b00da7fdf7bf7ce7b70f233aa17c849037ec
Sarah Bird <birdsarah@mozilla.com>
2021-01-16 18:03:18 -0600

    feat(auth-server): add getCheckoutToken to PayPalHelper

--- packages/fxa-auth-server/lib/payments/paypal-client.ts
+++ packages/fxa-auth-server/lib/payments/paypal-client.ts
@@ -4,9 +4,16 @@
 import pRetry from 'p-retry';
 import querystring from 'querystring';
 import superagent from 'superagent';
+import { GetCheckoutTokenOptions } from './paypal';
 
-export const PAYPAL_SANDBOX_API = 'https://api-3t.sandbox.paypal.com/nvp';
-export const PAYPAL_LIVE_API = 'https://api-3t.paypal.com/nvp';
+export const PAYPAL_SANDBOX_BASE = 'https://api-3t.sandbox.paypal.com';
+export const PAYPAL_LIVE_BASE = 'https://api-3t.paypal.com';
+export const PAYPAL_NVP_ROUTE = '/nvp';
+export const PAYPAL_SANDBOX_API = PAYPAL_SANDBOX_BASE + PAYPAL_NVP_ROUTE;
+export const PAYPAL_LIVE_API = PAYPAL_LIVE_BASE + PAYPAL_NVP_ROUTE;
+// See https://developer.paypal.com/docs/checkout/reference/upgrade-integration/#nvp-integrations
+// for information on when to use PLACEHOLDER_URL
+export const PLACEHOLDER_URL = 'https://www.paypal.com/checkoutnow/error';
 const PAYPAL_VERSION = '204';
 const L_LIST = /L_([A-Z]+)(\d+)$/;
 
@@ -22,14 +29,57 @@ type PaypalOptions = {
   };
 };
 
-export type PAYPAL_METHODS =
-  | 'BillAgreementUpdate'
-  | 'CreateBillingAgreement'
-  | 'DoReferenceTransaction'
-  | 'GetTransactionDetails'
-  | 'SetExpressCheckout'
-  | 'TransactionSearch';
+export enum PAYPAL_METHODS {
+  BillAgreementUpdate = 'BillAgreementUpdate',
+  CreateBillingAgreement = 'CreateBillingAgreement',
+  DoReferenceTransaction = 'DoReferenceTransaction',
+  GetTransactionDetails = 'GetTransactionDetails',
+  SetExpressCheckout = 'SetExpressCheckout',
+  TransactionSearch = 'TransactionSearch',
+}
 
+/*
+ * Common response fields
+ * https://developer.paypal.com/docs/nvp-soap-api/NVPAPIOverview/#common-response-fields
+ */
+
+export enum PAYPAL_NVP_ACK_OPTIONS {
+  Success = 'Success',
+  SuccessWithWarning = 'SuccessWithWarning',
+  Failure = 'Failure',
+  FailureWithWarning = 'FailureWithWarning',
+}
+
+type NVPResponse = {
+  ACK: PAYPAL_NVP_ACK_OPTIONS;
+  CORRELATIONID: string;
+  TIMESTAMP: string;
+  VERSION: string;
+  BUILD: string;
+};
+
+type SetCheckoutData = {
+  TOKEN: string;
+};
+
+export type NVPSetCheckoutResponse = NVPResponse & SetCheckoutData;
+
+export class PayPalClientError extends Error {
+  public raw: string;
+  public data: NVPResponse;
+
+  constructor(raw: string, data: NVPResponse, ...params: any) {
+    super(...params);
+    // Cannot override the name
+    this.name = 'PayPalClientError';
+    if (!this.message) {
+      this.message =
+        'PayPal NVP returned a non-success ACK. See "this.raw" or "this.data" for more details.';
+    }
+    this.raw = raw;
+    this.data = data;
+  }
+}
 export class PayPalClient {
   private url: string;
   private user: string;
@@ -87,7 +137,7 @@ export class PayPalClient {
   private async doRequest(
     method: PAYPAL_METHODS,
     data: Record<string, any>
-  ): Promise<Record<string, any>> {
+  ): Promise<NVPResponse> {
     const payload = this.objectToNVP({
       ...data,
       USER: this.user,
@@ -96,14 +146,64 @@ export class PayPalClient {
       SIGNATURE: this.signature,
       VERSION: PAYPAL_VERSION,
     });
-    const result = await pRetry(
-      () =>
-        superagent
-          .post(this.url)
-          .set('content-type', 'application/x-www-form-urlencoded')
-          .send(payload),
-      this.retryOptions
-    );
-    return this.nvpToObject(result.text);
+    let result;
+    try {
+      result = await pRetry(
+        () =>
+          superagent
+            .post(this.url)
+            .set('content-type', 'application/x-www-form-urlencoded')
+            .send(payload),
+        this.retryOptions
+      );
+    } catch (err) {
+      throw new Error('Call to PayPal Failed');
+    }
+    const resultObj = this.nvpToObject(result.text) as NVPResponse;
+    if (
+      resultObj.ACK === PAYPAL_NVP_ACK_OPTIONS.Success ||
+      resultObj.ACK === PAYPAL_NVP_ACK_OPTIONS.SuccessWithWarning
+    ) {
+      return resultObj;
+    } else {
+      throw new PayPalClientError(result.text, resultObj);
+    }
+  }
+
+  /**
+   * Call the Paypal SetExpressCheckout NVP API
+   *
+   * Using the Paypal SetExpressCheckout API (https://developer.paypal.com/docs/nvp-soap-api/set-express-checkout-nvp/)
+   * we get back the response which contains a token that we can use in the next step of the transaction.
+   * The API is extensive. The options currently avialable only support the case of:
+   * - One digital good
+   * - In USD
+   * - As a recurring payment
+   * - Authorization is hardcoded as the Payment Action
+   *
+   * @param {<SetExpressCheckoutOptions>}
+   * @return {Promise<NVPSetCheckoutResponse>}
+   */
+  public async setExpressCheckout(
+    options: GetCheckoutTokenOptions
+  ): Promise<NVPSetCheckoutResponse> {
+    const decimalAmount = options.amountInCents / 100;
+    const stringifiedAmount = String(decimalAmount);
+    const method = PAYPAL_METHODS.SetExpressCheckout;
+    const data = {
+      PAYMENTREQUEST_0_AMT: stringifiedAmount,
+      RETURNURL: PLACEHOLDER_URL,
+      CANCELURL: PLACEHOLDER_URL,
+      NOSHIPPING: '1',
+      SOLUTIONTYPE: 'Sole',
+      PAYMENTREQUEST_0_ITEMAMT: stringifiedAmount,
+      L_PAYMENTREQUEST_0_ITEMCATEGORY0: 'Digital',
+      L_PAYMENTREQUEST_0_NAME0: options.itemName,
+      L_PAYMENTREQUEST_0_AMT0: stringifiedAmount,
+      L_PAYMENTREQUEST_0_QTY0: '1',
+      L_BILLINGTYPE0: 'RecurringPayments',
+      L_BILLINGAGREEMENTDESCRIPTION0: options.itemDescription,
+    };
+    return (await this.doRequest(method, data)) as NVPSetCheckoutResponse;
   }
 }
--- packages/fxa-auth-server/lib/payments/paypal.ts
+++ packages/fxa-auth-server/lib/payments/paypal.ts
@@ -1,11 +1,24 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+import Joi from '@hapi/joi';
 import { StatsD } from 'hot-shots';
 import { Logger } from 'mozlog';
 import { Container } from 'typedi';
 
 import { PayPalClient } from './paypal-client';
+
+export type GetCheckoutTokenOptions = {
+  amountInCents: number;
+  itemName: string;
+  itemDescription: string;
+};
+
+const getCheckoutTokenOptionsSchema = Joi.object({
+  amountInCents: Joi.number().required(),
+  itemName: Joi.string().required(),
+  itemDescription: Joi.string().required(),
+});
 
 type PaypalHelperOptions = {
   log: Logger;
@@ -20,5 +33,24 @@ export class PayPalHelper {
     this.log = options.log;
     this.client = Container.get(PayPalClient);
     this.metrics = Container.get(StatsD);
+  }
+
+  /**
+   * Get a token authorizing transaction to move to the next stage.
+   *
+   * If the call to PayPal fails, a PayPalClientError will be thrown.
+   *
+   * @param {<GetCheckoutTokenOptions>} - The options for the purchase item.
+   *      amountInCents (number) - The amount to charge in a whole number of cents e.g. $15.99 should be 1599
+   *      itemName (string) - Item name
+   *      itemDescription (string) - Item descriptiong
+   * @return {string} - The token from the paypal call.
+   */
+  public async getCheckoutToken(
+    options: GetCheckoutTokenOptions
+  ): Promise<string> {
+    Joi.assert(options, getCheckoutTokenOptionsSchema);
+    const response = await this.client.setExpressCheckout(options);
+    return response.TOKEN;
   }
 }
