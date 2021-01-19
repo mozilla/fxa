a84b0a27a7e2c27d99195cdf9649c2ec881ef062
Vijay Budhram <vbudhram@gmail.com>
2021-01-19 17:42:40 -0500

    feat(l10n): setup localization for auth errors

--- packages/fxa-settings/src/components/PageChangePassword/index.tsx
+++ packages/fxa-settings/src/components/PageChangePassword/index.tsx
@@ -19,8 +19,10 @@ import PasswordValidator from './PasswordValidator';
 import { ReactComponent as ValidIcon } from './valid.svg';
 import { ReactComponent as InvalidIcon } from './invalid.svg';
 import { ReactComponent as UnsetIcon } from './unset.svg';
-import { AuthUiErrors } from '../../lib/auth-errors';
+import { AuthUiErrors } from '../../lib/auth-errors/auth-errors';
 import { gql } from '@apollo/client';
+import { useLocalization } from '@fluent/react';
+import { Localized } from '@fluent/react';
 
 type FormData = {
   oldPassword: string;
@@ -72,6 +74,7 @@ export const PageChangePassword = ({}: RouteComponentProps) => {
   const [newPasswordErrorText, setNewPasswordErrorText] = useState<string>();
   const { primaryEmail } = useAccount();
   const navigate = useNavigate();
+  const { l10n } = useLocalization();
   const changePassword = usePasswordChanger({
     onSuccess: (response) => {
       logViewEvent(settingsViewName, 'change-password.success');
@@ -99,12 +102,13 @@ export const PageChangePassword = ({}: RouteComponentProps) => {
       navigate(HomePath);
     },
     onError: (e) => {
+      const localizedError = l10n.getString(`auth-error-${e.errno}`);
       if (e.errno === 103) {
         // incorrect password
-        setCurrentPasswordErrorText(e.message);
+        setCurrentPasswordErrorText(localizedError);
         setValue('oldPassword', '');
       } else {
-        setAlertText(e.message);
+        setAlertText(localizedError);
         revealAlertBar();
       }
     },
@@ -112,7 +116,10 @@ export const PageChangePassword = ({}: RouteComponentProps) => {
   const passwordValidator = new PasswordValidator(primaryEmail.email);
   const onFormSubmit = ({ oldPassword, newPassword }: FormData) => {
     if (oldPassword === newPassword) {
-      setNewPasswordErrorText(AuthUiErrors.PASSWORDS_MUST_BE_DIFFERENT.message);
+      const localizedError = l10n.getString(
+        `auth-error-${AuthUiErrors.PASSWORDS_MUST_BE_DIFFERENT.errno}`
+      );
+      setNewPasswordErrorText(localizedError);
       return;
     }
     changePassword.execute(
@@ -124,142 +131,167 @@ export const PageChangePassword = ({}: RouteComponentProps) => {
   };
 
   return (
-    <FlowContainer title="Change Password">
-      {alertBarRevealed && alertText && (
-        <AlertBar onDismiss={hideAlertBar} type="error">
-          <p data-testid="sign-out-error">Error text TBD. {alertText}</p>
-        </AlertBar>
-      )}
-      <form onSubmit={handleSubmit(onFormSubmit)}>
-        <h1>Stay safe — don't reuse passwords. Your password:</h1>
+    <Localized id="pw-change-header" attrs={{ title: true }}>
+      <FlowContainer title="Change Password">
+        {alertBarRevealed && alertText && (
+          <AlertBar onDismiss={hideAlertBar} type="error">
+            <p data-testid="sign-out-error">Error text TBD. {alertText}</p>
+          </AlertBar>
+        )}
+        <form onSubmit={handleSubmit(onFormSubmit)}>
+          <Localized id="pw-change-stay-safe">
+            <h1>Stay safe — don't reuse passwords. Your password:</h1>
+          </Localized>
+          <ul className="text-grey-400 text-xs m-3 list-inside">
+            <li data-testid="change-password-length">
+              <ValidationIcon
+                isSet={formState.dirtyFields.newPassword}
+                hasError={errors.newPassword?.types?.length}
+              />
+              <Localized id="pw-change-least-8-chars">
+                Must be at least 8 characters
+              </Localized>
+            </li>
+            <li data-testid="change-password-email">
+              <ValidationIcon
+                isSet={formState.dirtyFields.newPassword}
+                hasError={errors.newPassword?.types?.notEmail}
+              />
+              <Localized id="pw-change-not-contain-email">
+                Must not be your email address
+              </Localized>
+            </li>
+            <li data-testid="change-password-common">
+              <ValidationIcon
+                isSet={formState.dirtyFields.newPassword}
+                hasError={errors.newPassword?.types?.uncommon}
+              />
+              Must not match this{' '}
+              <LinkExternal
+                className="link-blue"
+                data-testid="nav-link-common-passwords"
+                href="https://support.mozilla.org/en-US/kb/password-strength"
+              >
+                list of common passwords
+              </LinkExternal>
+            </li>
+            <li data-testid="change-password-match">
+              <ValidationIcon
+                isSet={formState.dirtyFields.confirmPassword}
+                hasError={errors.confirmPassword?.types?.validate}
+              />
+              <Localized id="pw-change-must-match">
+                New password matches confirmation
+              </Localized>
+            </li>
+          </ul>
 
-        <ul className="text-grey-400 text-xs m-3 list-inside">
-          <li data-testid="change-password-length">
-            <ValidationIcon
-              isSet={formState.dirtyFields.newPassword}
-              hasError={errors.newPassword?.types?.length}
-            />
-            Must be at least 8 characters
-          </li>
-          <li data-testid="change-password-email">
-            <ValidationIcon
-              isSet={formState.dirtyFields.newPassword}
-              hasError={errors.newPassword?.types?.notEmail}
-            />
-            Must not be your email address
-          </li>
-          <li data-testid="change-password-common">
-            <ValidationIcon
-              isSet={formState.dirtyFields.newPassword}
-              hasError={errors.newPassword?.types?.uncommon}
-            />
-            Must not match this{' '}
-            <LinkExternal
-              className="link-blue"
-              data-testid="nav-link-common-passwords"
-              href="https://support.mozilla.org/en-US/kb/password-strength"
+          <div className="my-6">
+            <Localized id="pw-change-current-password" attrs={{ label: true }}>
+              <InputPassword
+                name="oldPassword"
+                label="Enter current password"
+                className="mb-2"
+                errorText={currentPasswordErrorText}
+                onChange={() => {
+                  if (currentPasswordErrorText) {
+                    setCurrentPasswordErrorText(undefined);
+                  }
+                  trigger('oldPassword');
+                }}
+                inputRef={register({
+                  required: true,
+                })}
+                prefixDataTestId="current-password"
+              />
+            </Localized>
+            <Localized id="pw-change-new-password" attrs={{ label: true }}>
+              <InputPassword
+                name="newPassword"
+                label="Enter new password"
+                className="mb-2"
+                errorText={newPasswordErrorText}
+                onChange={() => {
+                  if (newPasswordErrorText) {
+                    setNewPasswordErrorText(undefined);
+                  }
+                  trigger(['newPassword', 'confirmPassword']);
+                }}
+                inputRef={register({
+                  required: true,
+                  validate: {
+                    length: (value: string) => value.length > 7,
+                    notEmail: (value: string) => {
+                      return !passwordValidator.isSameAsEmail(
+                        value.toLowerCase()
+                      );
+                    },
+                    uncommon: async (value: string) => {
+                      // @ts-ignore
+                      const list = await import('fxa-common-password-list');
+                      const input = value.toLowerCase();
+                      return (
+                        !list.test(input) && !passwordValidator.isBanned(input)
+                      );
+                    },
+                  },
+                })}
+                prefixDataTestId="new-password"
+              />
+            </Localized>
+            <Localized id="pw-change-confirm-password" attrs={{ label: true }}>
+              <InputPassword
+                name="confirmPassword"
+                label="Confirm new password"
+                onChange={() => trigger(['newPassword', 'confirmPassword'])}
+                inputRef={register({
+                  required: true,
+                  validate: (value) => value === getValues().newPassword,
+                })}
+                prefixDataTestId="verify-password"
+              />
+            </Localized>
+          </div>
+
+          <div className="flex justify-center mb-4 mx-auto max-w-64">
+            <Localized id="pw-change-cancel-button">
+              <button
+                type="button"
+                className="cta-neutral mx-2 flex-1"
+                onClick={() => window.history.back()}
+                data-testid="cancel-password-button"
+              >
+                Cancel
+              </button>
+            </Localized>
+            <Localized id="pw-change-save-button">
+              <button
+                data-testid="save-password-button"
+                type="submit"
+                className="cta-primary mx-2 flex-1"
+                disabled={
+                  !formState.isDirty ||
+                  !formState.isValid ||
+                  changePassword.loading
+                }
+              >
+                Save
+              </button>
+            </Localized>
+          </div>
+
+          <Localized id="pw-change-forgot-password-link">
+            <a
+              className="link-blue text-sm justify-center flex"
+              data-testid="nav-link-reset-password"
+              href="/reset_password"
             >
-              list of common passwords
-            </LinkExternal>
-          </li>
-          <li data-testid="change-password-match">
-            <ValidationIcon
-              isSet={formState.dirtyFields.confirmPassword}
-              hasError={errors.confirmPassword?.types?.validate}
-            />
-            New password matches confirmation
-          </li>
-        </ul>
-
-        <div className="my-6">
-          <InputPassword
-            name="oldPassword"
-            label="Enter current password"
-            className="mb-2"
-            errorText={currentPasswordErrorText}
-            onChange={() => {
-              if (currentPasswordErrorText) {
-                setCurrentPasswordErrorText(undefined);
-              }
-              trigger('oldPassword');
-            }}
-            inputRef={register({
-              required: true,
-            })}
-            prefixDataTestId="current-password"
-          />
-          <InputPassword
-            name="newPassword"
-            label="Enter new password"
-            className="mb-2"
-            errorText={newPasswordErrorText}
-            onChange={() => {
-              if (newPasswordErrorText) {
-                setNewPasswordErrorText(undefined);
-              }
-              trigger(['newPassword', 'confirmPassword']);
-            }}
-            inputRef={register({
-              required: true,
-              validate: {
-                length: (value: string) => value.length > 7,
-                notEmail: (value: string) => {
-                  return !passwordValidator.isSameAsEmail(value.toLowerCase());
-                },
-                uncommon: async (value: string) => {
-                  // @ts-ignore
-                  const list = await import('fxa-common-password-list');
-                  const input = value.toLowerCase();
-                  return (
-                    !list.test(input) && !passwordValidator.isBanned(input)
-                  );
-                },
-              },
-            })}
-            prefixDataTestId="new-password"
-          />
-          <InputPassword
-            name="confirmPassword"
-            label="Confirm new password"
-            onChange={() => trigger(['newPassword', 'confirmPassword'])}
-            inputRef={register({
-              required: true,
-              validate: (value) => value === getValues().newPassword,
-            })}
-            prefixDataTestId="verify-password"
-          />
-        </div>
-
-        <div className="flex justify-center mb-4 mx-auto max-w-64">
-          <button
-            type="button"
-            className="cta-neutral mx-2 flex-1"
-            onClick={() => window.history.back()}
-            data-testid="cancel-password-button"
-          >
-            Cancel
-          </button>
-          <button
-            data-testid="save-password-button"
-            type="submit"
-            className="cta-primary mx-2 flex-1"
-            disabled={
-              !formState.isDirty || !formState.isValid || changePassword.loading
-            }
-          >
-            Save
-          </button>
-        </div>
-
-        <a
-          className="link-blue text-sm justify-center flex"
-          data-testid="nav-link-reset-password"
-          href="/reset_password"
-        >
-          Forgot password?
-        </a>
-      </form>
-    </FlowContainer>
+              Forgot password?
+            </a>
+          </Localized>
+        </form>
+      </FlowContainer>
+    </Localized>
   );
 };
 
