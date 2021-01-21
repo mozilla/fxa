842beb25435a95a7859b0660cde90c8172190f9e
Jared Hirsch <ohai@6a68.net>
2021-01-21 15:26:54 -0800

    feat(settings): localize header
    Because:
     - we want to localize the header
    
    This commit:
     - localizes the header
     - adds en-US strings
    
    Refs #5058.

--- packages/fxa-settings/public/locales/en-US/settings.ftl
+++ packages/fxa-settings/public/locales/en-US/settings.ftl
@@ -7,6 +7,17 @@
 app-default-title = Firefox Accounts
 app-page-title = { $title } | Firefox Accounts
 
+# HeaderLockup component
+
+header-menu-open = Close menu
+header-menu-closed = Site navigation menu
+header-back-to-top-link =
+  .title = Back to top
+header-title = Firefox account
+header-switch-title = Switch to classic design
+  .title = classic design link
+header-help = Help
+
 ## Settings Nav
 
 nav-settings = Settings
--- packages/fxa-settings/src/components/HeaderLockup/index.tsx
+++ packages/fxa-settings/src/components/HeaderLockup/index.tsx
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { Localized, useLocalization } from '@fluent/react';
 import React, { useState } from 'react';
 import LogoLockup from 'fxa-react/components/LogoLockup';
 import Header from 'fxa-react/components/Header';
@@ -15,12 +16,17 @@ import Nav from '../Nav';
 
 export const HeaderLockup = () => {
   const [navRevealedState, setNavState] = useState(false);
+  const { l10n } = useLocalization();
   const left = (
     <>
       <button
         className="desktop:hidden ltr:mr-6 rtl:ml-6 w-8 h-6 self-center"
         data-testid="header-menu"
-        aria-label={navRevealedState ? 'Close menu' : 'Site navigation menu'}
+        aria-label={
+          navRevealedState
+            ? l10n.getString('header-menu-open')
+            : l10n.getString('header-menu-closed')
+        }
         aria-haspopup={true}
         aria-expanded={navRevealedState}
         onClick={() => setNavState(!navRevealedState)}
@@ -28,41 +34,47 @@ export const HeaderLockup = () => {
         {navRevealedState ? <Close /> : <Menu />}
         {navRevealedState && <Nav />}
       </button>
-      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-      <a
-        href="#"
-        title="Back to top"
-        className="flex"
-        data-testid="back-to-top"
-      >
-        <LogoLockup>
-          <>
-            <span className="font-bold ltr:mr-2 rtl:ml-2">
-              Firefox accounts
-            </span>
-            <a
-              href="/settings/beta_optout"
-              title="classic design link"
-              className="cta-base cta-neutral transition-standard text-sm ltr:ml-4 rtl:mr-4 p-2"
-            >
-              Switch to classic design
-            </a>
-          </>
-        </LogoLockup>
-      </a>
+      <Localized id="header-back-to-top-link" attrs={{ title: true }}>
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a
+          href="#"
+          title="Back to top"
+          className="flex"
+          data-testid="back-to-top"
+        >
+          <LogoLockup>
+            <>
+              <Localized id="header-title">
+                <span className="font-bold ltr:mr-2 rtl:ml-2">
+                  Firefox accounts
+                </span>
+              </Localized>
+              <Localized id="header-switch-title" attrs={{ title: true }}>
+                <a
+                  href="/settings/beta_optout"
+                  title="classic design link"
+                  className="cta-base cta-neutral transition-standard text-sm ltr:ml-4 rtl:mr-4 p-2"
+                >
+                  Switch to classic design
+                </a>
+              </Localized>
+            </>
+          </LogoLockup>
+        </a>
+      </Localized>
     </>
   );
   const right = (
     <>
       <LinkExternal
         href="https://support.mozilla.org"
-        title="Help"
+        title={l10n.getString('header-help')}
         className="self-center"
         data-testid="header-sumo-link"
       >
         <Help
-          aria-label="Help"
-          title="Help"
+          aria-label={l10n.getString('header-help')}
+          title={l10n.getString('header-help')}
           role="img"
           className="w-6"
           data-testid="header-help"
