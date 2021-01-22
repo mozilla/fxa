344ce0169890a4e74f6c5dc3d3e41dc251e67e52
Jared Hirsch <ohai@6a68.net>
2021-01-22 14:23:27 -0800

    feat(settings): Localize the Add Secondary Email page
    Because:
     - we want to localize the Add Secondary Email page
    
    This commit:
     - localizes the Add Secondary Email page
     - adds en-US strings
    
    Refs #5063.

--- packages/fxa-settings/public/locales/en-US/settings.ftl
+++ packages/fxa-settings/public/locales/en-US/settings.ftl
@@ -74,6 +74,16 @@ display-name-input =
 submit-display-name = Save
 cancel-display-name = Cancel
 
+# Add secondary email page
+
+add-secondary-email-error = There was a problem creating this email.
+add-secondary-email-page-title
+  .title = Secondary email
+add-secondary-email-enter-address =
+  .label = Enter email address
+add-secondary-email-cancel-button = Cancel
+add-secondary-email-save-button = Save
+
 ## Two Step Authentication
 
 tfa-enter-totp = Now enter the security code from the authentication app.
--- packages/fxa-settings/src/components/PageSecondaryEmailAdd/index.tsx
+++ packages/fxa-settings/src/components/PageSecondaryEmailAdd/index.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent, useCallback, useRef, useState } from 'react';
 import { gql } from '@apollo/client';
+import { Localized, useLocalization } from '@fluent/react';
 import { RouteComponentProps, useNavigate } from '@reach/router';
 import { useAlertBar, useMutation } from '../../lib/hooks';
 import { logViewEvent, usePageViewEvent } from '../../lib/metrics';
@@ -23,6 +24,7 @@ export const PageSecondaryEmailAdd = (_: RouteComponentProps) => {
   const [errorText, setErrorText] = useState<string>();
   const [email, setEmail] = useState<string>();
   const inputRef = useRef<HTMLInputElement>(null);
+  const { l10n } = useLocalization();
   const navigate = useNavigate();
   const alertBar = useAlertBar();
   const goBack = useCallback(() => window.history.back(), []);
@@ -32,7 +34,7 @@ export const PageSecondaryEmailAdd = (_: RouteComponentProps) => {
       if (error.graphQLErrors?.length) {
         setErrorText(error.message);
       } else {
-        alertBar.error('There was a problem creating this email.');
+        alertBar.error(l10n.getString('add-secondary-email-error'));
         // TODO: old settings has no equivalent metrics event here
       }
     },
@@ -71,53 +73,64 @@ export const PageSecondaryEmailAdd = (_: RouteComponentProps) => {
   );
 
   return (
-    <FlowContainer title="Secondary email">
-      {alertBar.visible && (
-        <AlertBar onDismiss={alertBar.hide} type={alertBar.type}>
-          <p data-testid="add-email-error">{alertBar.content}</p>
-        </AlertBar>
-      )}
-      <VerifiedSessionGuard onDismiss={goBack} onError={goBack} />
-      <form
-        onSubmit={(ev) => {
-          ev.preventDefault();
-          if (inputRef.current) {
-            createSecondaryEmail({
-              variables: { input: { email } },
-            });
-            logViewEvent('settings.emails', 'submit');
-          }
-        }}
-      >
-        <div className="mt-4 mb-6" data-testid="secondary-email-input">
-          <InputText
-            label="Enter email address"
-            type="email"
-            onChange={checkEmail}
-            {...{ inputRef, errorText }}
-          />
-        </div>
+    <Localized id="add-secondary-email-page-title" attrs={{ title: true }}>
+      <FlowContainer title="Secondary email">
+        {alertBar.visible && (
+          <AlertBar onDismiss={alertBar.hide} type={alertBar.type}>
+            <p data-testid="add-email-error">{alertBar.content}</p>
+          </AlertBar>
+        )}
+        <VerifiedSessionGuard onDismiss={goBack} onError={goBack} />
+        <form
+          onSubmit={(ev) => {
+            ev.preventDefault();
+            if (inputRef.current) {
+              createSecondaryEmail({
+                variables: { input: { email } },
+              });
+              logViewEvent('settings.emails', 'submit');
+            }
+          }}
+        >
+          <div className="mt-4 mb-6" data-testid="secondary-email-input">
+            <Localized
+              id="add-secondary-email-enter-address"
+              attrs={{ label: true }}
+            >
+              <InputText
+                label="Enter email address"
+                type="email"
+                onChange={checkEmail}
+                {...{ inputRef, errorText }}
+              />
+            </Localized>
+          </div>
 
-        <div className="flex justify-center mx-auto max-w-64">
-          <button
-            type="button"
-            className="cta-neutral mx-2 flex-1"
-            data-testid="cancel-button"
-            onClick={goBack}
-          >
-            Cancel
-          </button>
-          <button
-            type="submit"
-            className="cta-primary mx-2 flex-1"
-            data-testid="save-button"
-            disabled={saveBtnDisabled}
-          >
-            Save
-          </button>
-        </div>
-      </form>
-    </FlowContainer>
+          <div className="flex justify-center mx-auto max-w-64">
+            <Localized id="add-secondary-email-cancel-button">
+              <button
+                type="button"
+                className="cta-neutral mx-2 flex-1"
+                data-testid="cancel-button"
+                onClick={goBack}
+              >
+                Cancel
+              </button>
+            </Localized>
+            <Localized id="add-secondary-email-save-button">
+              <button
+                type="submit"
+                className="cta-primary mx-2 flex-1"
+                data-testid="save-button"
+                disabled={saveBtnDisabled}
+              >
+                Save
+              </button>
+            </Localized>
+          </div>
+        </form>
+      </FlowContainer>
+    </Localized>
   );
 };
 
