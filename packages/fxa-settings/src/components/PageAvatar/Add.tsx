3940cb7fe1410cac7e6248548e2f21acdb63ded6
Dave Justice <djustice@mozilla.com>
2021-01-25 16:24:31 +0000

    feat(fxa-settings): upload and edit avatar
    - fixes #5021
    - fixes #5022

--- packages/fxa-settings/src/components/PageAvatar/Add.tsx
+++ packages/fxa-settings/src/components/PageAvatar/Add.tsx
@@ -2,34 +2,94 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { useAlertBar } from '../../lib/hooks';
-import React from 'react';
-import { Localized } from '@fluent/react';
+import React, { useRef, useState } from 'react';
 import { RouteComponentProps, useNavigate } from '@reach/router';
-
+import { Localized, useLocalization } from '@fluent/react';
+import { useAlertBar } from '../../lib/hooks';
+import { onFileChange } from '../../lib/file-utils';
+import { CapturePath, HomePath } from '../../constants';
+import { useAccount } from '../../models';
 import AlertBar from '../AlertBar';
 import Avatar from '../Avatar';
-// import AvatarCropper from '../AvatarCropper';
+import AvatarCropper from '../AvatarCropper';
 import ButtonIcon from '../ButtonIcon';
 import FlowContainer from '../FlowContainer';
 import { ReactComponent as AddIcon } from './add.svg';
 import { ReactComponent as CameraIcon } from './camera.svg';
-import { CapturePath, HomePath } from '../../constants';
+import { ReactComponent as RemoveIcon } from './remove.svg';
 
-function save() {
-  console.log('save avatar');
+const buttonClass = `mx-2 text-grey-500
+hover:text-grey-600 hover:text-grey-600
+focus:text-grey-400`;
+
+function remove() {
+  console.log('remove avatar');
 }
 
 export const PageAddAvatar = (_: RouteComponentProps) => {
   const navigate = useNavigate();
-  // const account = useAccount();
+  const { l10n } = useLocalization();
+  const { avatarUrl } = useAccount();
+  const fileInputRef = useRef(null);
   const alertBar = useAlertBar();
-  // const [errorText, setErrorText] = useState<string>();
   // const [saveEnabled, setSaveEnabled] = useState<boolean>();
+  const [imageSrc, setImageSrc] = useState(undefined);
   const handleSubmit = async (ev: React.FormEvent<HTMLFormElement>) => {
     ev.preventDefault();
   };
 
+  function onFileError() {
+    alertBar.error(l10n.getString('avatar-page-file-upload-error'));
+  }
+
+  function save() {
+    if (imageSrc) {
+      console.log('save avatar');
+    } else {
+      console.log('need to set avatar here.');
+    }
+  }
+
+  const onFileUpload = () => {
+    const fileInput: any = fileInputRef?.current;
+    if (fileInput) {
+      fileInput.click();
+    }
+  };
+
+  const hiddenFileInput = (
+    <input
+      type="file"
+      onChange={(event) => {
+        onFileChange(event, setImageSrc, onFileError);
+      }}
+      ref={fileInputRef}
+      className="sr-only"
+    />
+  );
+  const confirmBtns = (
+    <div className="mt-4 flex items-center justify-center">
+      <Localized id="avatar-page-close-button">
+        <button
+          className="cta-neutral mx-2 px-10"
+          onClick={() => navigate(HomePath, { replace: true })}
+          data-testid="close-button"
+        >
+          Close
+        </button>
+      </Localized>
+      <Localized id="avatar-page-save-button">
+        <button
+          className="cta-primary mx-2 px-10"
+          onClick={save}
+          disabled={true}
+          data-testid="save-button"
+        >
+          Save
+        </button>
+      </Localized>
+    </div>
+  );
   return (
     <Localized id="avatar-page-title" attrs={{ title: true }}>
       <FlowContainer title="Avatar">
@@ -40,64 +100,73 @@ export const PageAddAvatar = (_: RouteComponentProps) => {
           </AlertBar>
         )}
         <form onSubmit={handleSubmit}>
-          <Avatar className="mx-auto w-32" />
-
-          <div className="flex text-center justify-around max-w-xs my-4 mx-24">
-            <div className="cursor-pointer">
-              <Localized
-                id="avatar-page-add-photo-button"
-                attrs={{ title: true }}
-              >
-                <ButtonIcon
-                  title="Add photo"
-                  icon={[AddIcon, 22, 22]}
-                  classNames="mx-2 text-grey-500 hover:text-grey-600 hover:text-grey-600 focus:text-grey-400"
-                />
-              </Localized>
-              <Localized id="avatar-page-add-photo">
-                <p>Add Photo</p>
-              </Localized>
-            </div>
-            <div
-              onClick={() => navigate(CapturePath)}
-              className="cursor-pointer"
-            >
-              <Localized
-                id="avatar-page-take-photo-button"
-                attrs={{ title: true }}
-              >
-                <ButtonIcon
-                  title="Take photo"
-                  icon={[CameraIcon, 24, 22]}
-                  classNames="mx-2 text-grey-500 hover:text-grey-600 hover:text-grey-600 focus:text-grey-400"
-                />
-              </Localized>
-              <Localized id="avatar-page-take-photo">
-                <p>Take Photo</p>
-              </Localized>
-            </div>
-          </div>
-          <div className="mt-4 flex items-center justify-center">
-            <Localized id="avatar-page-close-button">
-              <button
-                className="cta-neutral mx-2 px-10"
-                onClick={() => navigate(HomePath, { replace: true })}
-                data-testid="close-button"
-              >
-                Close
-              </button>
-            </Localized>
-            <Localized id="avatar-page-save-button">
-              <button
-                className="cta-primary mx-2 px-10"
-                onClick={() => save()}
-                disabled={true}
-                data-testid="save-button"
-              >
-                Save
-              </button>
-            </Localized>
-          </div>
+          {imageSrc ? (
+            <>
+              <AvatarCropper src={imageSrc} />
+              {confirmBtns}
+            </>
+          ) : (
+            <>
+              <Avatar className="mx-auto w-32" />
+              <div className="flex text-center text-xs justify-around max-w-xs my-4 mx-12 mobileLandscape:mx-24">
+                <div onClick={onFileUpload} className="cursor-pointer flex-1">
+                  {hiddenFileInput}
+                  <Localized
+                    id="avatar-page-add-photo-button"
+                    attrs={{ title: true }}
+                  >
+                    <ButtonIcon
+                      testId="add-photo-btn"
+                      title="Add photo"
+                      icon={[AddIcon, 22, 22]}
+                      classNames={buttonClass}
+                    />
+                  </Localized>
+                  <Localized id="avatar-page-add-photo">
+                    <p>Add Photo</p>
+                  </Localized>
+                </div>
+                <div
+                  onClick={() => navigate(CapturePath)}
+                  className="cursor-pointer flex-1"
+                >
+                  <Localized
+                    id="avatar-page-take-photo-button"
+                    attrs={{ title: true }}
+                  >
+                    <ButtonIcon
+                      testId="take-photo-btn"
+                      title="Take photo"
+                      icon={[CameraIcon, 24, 22]}
+                      classNames={buttonClass}
+                    />
+                  </Localized>
+                  <Localized id="avatar-page-take-photo">
+                    <p>Take Photo</p>
+                  </Localized>
+                </div>
+                {avatarUrl && (
+                  <div onClick={remove} className="cursor-pointer flex-1">
+                    <Localized
+                      id="avatar-page-remove-photo-button"
+                      attrs={{ title: true }}
+                    >
+                      <ButtonIcon
+                        testId="remove-photo-btn"
+                        title="Remove photo"
+                        icon={[RemoveIcon, 24, 22]}
+                        classNames={buttonClass}
+                      />
+                    </Localized>
+                    <Localized id="avatar-page-remove-photo">
+                      <p>Remove photo</p>
+                    </Localized>
+                  </div>
+                )}
+              </div>
+              {confirmBtns}
+            </>
+          )}
         </form>
       </FlowContainer>
     </Localized>
