d66166178c2ab55d35b57caf11fd10587f4c1914
Bianca Danforth <biancadanforth@gmail.com>
2021-01-26 17:06:54 -0500

    feat(payments): Add createOrder handler to PayPal button

--- packages/fxa-payments-server/src/routes/Product/PayPalButton/index.stories.tsx
+++ packages/fxa-payments-server/src/routes/Product/PayPalButton/index.stories.tsx
@@ -1,7 +1,34 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { PaypalButton } from './index';
+import { PaypalButton, PaypalButtonProps } from './index';
+
+import { PickPartial } from '../../../lib/types';
+
+const Subject = ({
+  customer = null,
+  setPaymentError = () => {},
+  idempotencyKey = '',
+  ...props
+}: PickPartial<
+  PaypalButtonProps,
+  | 'apiClientOverrides'
+  | 'customer'
+  | 'setPaymentError'
+  | 'idempotencyKey'
+  | 'ButtonBase'
+>) => {
+  return (
+    <PaypalButton
+      {...{
+        customer,
+        setPaymentError,
+        idempotencyKey,
+        ...props,
+      }}
+    />
+  );
+};
 
 storiesOf('routes/Product/PaypalButton', module).add('default', () => (
-  <PaypalButton />
+  <Subject></Subject>
 ));
