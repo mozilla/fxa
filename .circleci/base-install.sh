17e06a38d2f40b999de6b43ec50ca073bb0f23be
Danny Coates <dannycoates@gmail.com>
2021-10-22 16:10:22 -0700

    chore(node): upgrade to node 16 (#10799)

--- .circleci/base-install.sh
+++ .circleci/base-install.sh
@@ -4,13 +4,10 @@ MODULE=$1
 DIR=$(dirname "$0")
 cd "$DIR/.."
 
-# npm install just enough to run these scripts
-npm i --ignore-scripts --no-optional --only=prod --no-package-lock
+yarn install --immutable
 node .circleci/modules-to-test.js | tee packages/test.list
 if ([[ "$MODULE" == "many" ]] && grep -e '.' packages/test.list > /dev/null) ||
     grep -e "$MODULE" -e 'all' packages/test.list > /dev/null; then
   ./.circleci/assert-branch.sh
   ./_scripts/create-version-json.sh
-
-  yarn install --immutable
 fi
