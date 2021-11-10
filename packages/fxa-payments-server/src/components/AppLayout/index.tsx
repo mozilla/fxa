73074a53eb2eb805d3c773fd9340b03fbc7a5933
Reino Muhl <reino.muhl@gmail.com>
2021-11-10 17:20:56 -0500

    remove language specific styling

--- packages/fxa-payments-server/src/components/AppLayout/index.tsx
+++ packages/fxa-payments-server/src/components/AppLayout/index.tsx
@@ -17,7 +17,6 @@ export const AppLayout = ({ children }: AppLayoutProps) => {
         data-testid="stage"
         className="fade-in-forward"
         style={{ opacity: 1 }}
-        lang={navigator.language}
       >
         {children}
       </div>
