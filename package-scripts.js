7af12ce1ee73832a9562490ae600c10c406037d6
Danny Coates <dannycoates@gmail.com>
2021-11-19 10:24:52 -0800

    feat(cli): add 'yarn delete services' command
    sometimes you may want to leave all your DBs running but completely start over with fresh services. The pm2 restart command doesn't fully reset the environment and sometimes that's what you want. Delete lets us remove the service from pm2 so we can start it fresh.

--- package-scripts.js
+++ package-scripts.js
@@ -1,21 +1,25 @@
 module.exports = {
   scripts: {
-    default: "nps help",
+    default: 'nps help',
     start: {
       default: `pm2 start _dev/pm2/infrastructure.config.js && _scripts/pm2-all.sh start && echo "Use 'yarn stop' to stop all the servers"`,
       infrastructure: `pm2 start _dev/pm2/infrastructure.config.js`,
       services: `_scripts/pm2-all.sh start`,
-      firefox: "./packages/fxa-dev-launcher/bin/fxa-dev-launcher &"
+      firefox: './packages/fxa-dev-launcher/bin/fxa-dev-launcher &',
     },
     stop: {
-      default: "pm2 kill",
+      default: 'pm2 kill',
       infrastructure: `pm2 stop _dev/pm2/infrastructure.config.js`,
-      services: `_scripts/pm2-all.sh stop`
+      services: `_scripts/pm2-all.sh stop`,
     },
     restart: {
-      default: "pm2 restart all",
+      default: 'pm2 restart all',
       infrastructure: `pm2 restart _dev/pm2/infrastructure.config.js`,
-      services: `_scripts/pm2-all.sh restart`
-    }
-  }
+      services: `_scripts/pm2-all.sh restart`,
+    },
+    delete: {
+      default: 'pm2 kill',
+      services: '_scripts/pm2-all.sh delete',
+    },
+  },
 };
