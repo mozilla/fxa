d149fb6da4a272b88724c6e7eeba2dff46e8112a
Danny Coates <dannycoates@gmail.com>
2021-11-22 15:21:26 -0800

    chore(dev): wait for containers on 'yarn start'
    On M1 we currently sometimes need to 'yarn restart services' after a 'yarn start' before things start working. Maybe if we wait for the containers to be responsive before starting services we can avoid this. The important one seems to be mysql.

--- _scripts/check-url.sh
+++ _scripts/check-url.sh
@@ -3,7 +3,7 @@
 RETRY=60
 for i in $(eval echo "{1..$RETRY}"); do
   if [ "$(curl -s -o /dev/null --silent -w "%{http_code}"  http://$1)" == "${2:-200}" ]; then
-    echo "took $SECONDS seconds"
+    echo "$1 took $SECONDS seconds"
     exit 0
   else
     if [ "$i" -lt $RETRY ]; then
