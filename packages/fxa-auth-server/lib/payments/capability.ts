mozilla/fxa

commit 878a991ce489e4b28d289734933e0b945a506eb7

feat(auth): remove redis customer caching
Because:

* We don't want to maintain multiple local caches now that
  we store copies in Firestore.
* It's more efficient to only manage one cache of Stripe data
  instead of maintaining multiple caches.
* Some methods were using customer functions that did not actually
  need the cached data, or all the data we fetched and cached.

This commit:

* Removes Redis caching of the customer object.
* Replaces customer calls with the appropriate underlying fetchCustomer
  call with the proper expansions in place as needed to reduce
  cache lookups.
* Replaces some customer calls with a db lookup to fetch the Stripe
  customer ID instead of a cache call.

Closes #10747

packages/fxa-auth-server/lib/payments/capability.ts
@@ -90,11 +90,9 @@ export class CapabilityService {
   public async stripeUpdate({
     sub,
     uid,
-    email,
   }: {
     sub: Stripe.Subscription;
     uid?: string | null;
-    email?: string | null;
   }) {
     if (typeof sub.customer !== 'string') {
       throw error.internalValidationError(
@@ -105,10 +103,10 @@ export class CapabilityService {
         'Subscription customer was not a string.'
       );
     }
-    if (!uid || !email) {
-      ({ uid, email } = await getUidAndEmailByStripeCustomerId(sub.customer));
+    if (!uid) {
+      ({ uid } = await getUidAndEmailByStripeCustomerId(sub.customer));
     }
-    if (!uid || !email) {
+    if (!uid) {
       // There's nothing to do if we can't find the user. We don't report it
       // as we expect this to occur in the case of a deleted user.
       return;
@@ -122,11 +120,7 @@ export class CapabilityService {
     if (affectedProductIds.length === 0) {
       return;
     }
-    const currentProductIds = await this.subscribedProductIds({
-      uid,
-      email,
-      forceRefresh: true,
-    });
+    const currentProductIds = await this.subscribedProductIds(uid);
     let priorProductIds = new Set([
       ...currentProductIds,
       ...affectedProductIds,
@@ -162,11 +156,7 @@ export class CapabilityService {
    * duplicate broadcasts telling RPs to turn on/off the user capability, but ensures
    * the user always has proper access to the purchased products.
    */
-  public async playUpdate(
-    uid: string,
-    email: string,
-    purchase: SubscriptionPurchase
-  ) {
+  public async playUpdate(uid: string, purchase: SubscriptionPurchase) {
     const affectedProductId = (
       await this.stripeHelper.purchasesToProductIds([purchase])
     ).shift();
@@ -174,11 +164,7 @@ export class CapabilityService {
       // Purchase is not mapped to a product id.
       return;
     }
-    const currentProductIds = await this.subscribedProductIds({
-      uid,
-      email,
-      forceRefresh: true,
-    });
+    const currentProductIds = await this.subscribedProductIds(uid);
     let priorProductIds;
     if (currentProductIds.includes(affectedProductId)) {
       // Remove the product id from the prior list for processing to assume that it
@@ -205,28 +191,19 @@ export class CapabilityService {
    * Return a map of capabilities to client ids for the user.
    */
   public async subscriptionCapabilities(
-    uid: string,
-    email: string
+    uid: string
   ): Promise<ClientIdCapabilityMap> {
-    const subscribedProducts = await this.subscribedProductIds({ uid, email });
+    const subscribedProducts = await this.subscribedProductIds(uid);
     return this.productIdsToClientCapabilities(subscribedProducts);
   }
 
   /**
    * Return a list of all product ids with an active subscription.
    */
-  private async subscribedProductIds({
-    uid,
-    email,
-    forceRefresh = false,
-  }: {
-    uid: string;
-    email: string;
-    forceRefresh?: boolean;
-  }) {
+  private async subscribedProductIds(uid: string) {
     const [subscribedStripeProducts, subscribedPlayProducts] =
       await Promise.all([
-        this.fetchSubscribedProductsFromStripe({ uid, email, forceRefresh }),
+        this.fetchSubscribedProductsFromStripe(uid),
         this.fetchSubscribedProductsFromPlay(uid),
       ]);
     return [
@@ -416,20 +393,12 @@ export class CapabilityService {
   /**
    * Fetch the list of ids of products purchased from Stripe.
    */
-  private async fetchSubscribedProductsFromStripe({
-    uid,
-    email,
-    forceRefresh = false,
-  }: {
-    uid: string;
-    email: string;
-    forceRefresh?: boolean;
-  }): Promise<string[]> {
-    const customer = await this.stripeHelper.customer({
-      uid,
-      email,
-      forceRefresh,
-    });
+  private async fetchSubscribedProductsFromStripe(
+    uid: string
+  ): Promise<string[]> {
+    const customer = await this.stripeHelper.fetchCustomer(uid, [
+      'subscriptions',
+    ]);
     if (!customer || !customer.subscriptions!.data) {
       return [];
     }

packages/fxa-auth-server/lib/routes/account.ts
@@ -1098,10 +1098,7 @@ export class AccountHandler {
       scope.contains('profile:subscriptions')
     ) {
       const capabilities =
-        await this.capabilityService.subscriptionCapabilities(
-          uid as string,
-          account.primaryEmail.email
-        );
+        await this.capabilityService.subscriptionCapabilities(uid as string);
       if (Object.keys(capabilities).length > 0) {
         res.subscriptionsByClientId = capabilities;
       }
@@ -1477,10 +1474,10 @@ export class AccountHandler {
     if (this.config.subscriptions.enabled) {
       try {
         if (this.stripeHelper) {
-          const customer = await this.stripeHelper.customer({
-            uid: uid as string,
-            email: email as string,
-          });
+          const customer = await this.stripeHelper.fetchCustomer(
+            uid as string,
+            ['subscriptions']
+          );
           if (!customer) {
             throw error.unknownCustomer(uid);
           }

packages/fxa-auth-server/lib/payments/stripe.ts
@@ -5,7 +5,6 @@ import { Firestore } from '@google-cloud/firestore';
 import * as Sentry from '@sentry/node';
 import cacheManager, {
   Cacheable,
-  CacheClear,
   CacheClearStrategy,
   CacheClearStrategyContext,
   CacheUpdate,
@@ -141,7 +140,6 @@ export class StripeHelper {
   // Note that this isn't quite accurate, as the auth-server logger has some extras
   // attached to it in Hapi.
   private log: Logger;
-  private customerCacheTtlSeconds: number;
   private plansAndProductsCacheTtlSeconds: number;
   private stripeTaxRatesCacheTtlSeconds: number;
   private webhookSecret: string;
@@ -158,7 +156,6 @@ export class StripeHelper {
    */
   constructor(log: Logger, config: ConfigType, statsd: StatsD) {
     this.log = log;
-    this.customerCacheTtlSeconds = config.subhub.customerCacheTtlSeconds;
     this.plansAndProductsCacheTtlSeconds = config.subhub.plansCacheTtlSeconds;
     this.stripeTaxRatesCacheTtlSeconds =
       config.subhub.stripeTaxRatesCacheTtlSeconds;
@@ -611,7 +608,7 @@ export class StripeHelper {
   }
 
   /**
-   * Updates the email types sent for this invoice. These types are concatentated
+   * Updates the email types sent for this invoice. These types are concatenated
    * on the value of a single invoice metadata key and are thus limited to 500
    * characters.
    */
@@ -889,11 +886,12 @@ export class StripeHelper {
           custom_fields: [{ name: MOZILLA_TAX_ID, value: taxId }],
         },
       });
-      await this.stripeFirestore.insertCustomerRecordWithBackfill(
+      return this.stripeFirestore.insertCustomerRecordWithBackfill(
         customer.metadata.userid,
         updatedCustomer
       );
     }
+    return;
   }
 
   /**
@@ -980,42 +978,6 @@ export class StripeHelper {
     return customer;
   }
 
-  static customerCacheKey = (args: any[]) => `customer-${args[0]}|${args[1]}`;
-
-  @Cacheable({
-    cacheKey: StripeHelper.customerCacheKey,
-    ttlSeconds: (args, context) => context.customerCacheTtlSeconds,
-  })
-  async cachedCustomer(
-    uid: string,
-    email: string
-  ): Promise<Stripe.Customer | void> {
-    return this.fetchCustomer(uid, [
-      'subscriptions',
-      'invoice_settings.default_payment_method',
-    ]);
-  }
-
-  /**
-   * Fetch a customer for the record from Stripe based on user ID & email.
-   *
-   * Uses Redis caching if configured.
-   */
-  async customer({
-    uid,
-    email,
-    forceRefresh = false,
-  }: {
-    uid: string;
-    email: string;
-    forceRefresh?: boolean;
-  }): Promise<Stripe.Customer | undefined> {
-    if (forceRefresh) {
-      await this.removeCustomerFromCache(uid, email);
-    }
-    return (await this.cachedCustomer(uid, email)) || undefined;
-  }
-
   /**
    * On FxA deletion, if the user is a Stripe Customer:
    * - delete the stripe customer to delete
@@ -1032,7 +994,6 @@ export class StripeHelper {
           {}
         );
       }
-      await this.removeCustomerFromCache(uid, email);
     }
   }
 
@@ -1049,7 +1010,7 @@ export class StripeHelper {
     email: string,
     subscriptionId: string
   ): Promise<Stripe.Subscription | void> {
-    const customer = await this.customer({ uid, email });
+    const customer = await this.fetchCustomer(uid, ['subscriptions']);
     if (!customer) {
       return;
     }
@@ -1161,28 +1122,6 @@ export class StripeHelper {
       (sub) => sub.items.data.find((item) => item.plan.id === planId) != null
     );
   }
-
-  /**
-   * Delete a cached customer record based on user ID & email.
-   */
-  async refreshCachedCustomer(
-    uid: string,
-    email: string
-  ): Promise<Stripe.Customer | undefined> {
-    try {
-      return await this.customer({ uid, email, forceRefresh: true });
-    } catch (err) {
-      this.log.error(`subhub.refreshCachedCustomer.failed`, { err });
-      return;
-    }
-  }
-
-  /**
-   * Remove the cache entry for a customer account
-   * This is to be used on account deletion
-   */
-  @CacheClear({ cacheKey: StripeHelper.customerCacheKey })
-  async removeCustomerFromCache(uid: string, email: string) {}
 
   /**
    * Fetches all plans that are attached to a product from the cached products
@@ -1354,13 +1293,9 @@ export class StripeHelper {
    * validate this is an appropriate change for tier use.
    */
   async changeSubscriptionPlan(
-    subscriptionId: string,
+    subscription: Stripe.Subscription,
     newPlanId: string
   ): Promise<Stripe.Subscription> {
-    const subscription = await this.expandResource<Stripe.Subscription>(
-      subscriptionId,
-      SUBSCRIPTIONS_RESOURCE
-    );
     const currentPlanId = subscription.items.data[0].plan.id;
     if (currentPlanId === newPlanId) {
       throw error.subscriptionAlreadyChanged();
@@ -1373,7 +1308,7 @@ export class StripeHelper {
     };
 
     const newSubscription = await this.stripe.subscriptions.update(
-      subscriptionId,
+      subscription.id,
       {
         cancel_at_period_end: false,
         items: [
@@ -1789,7 +1724,7 @@ export class StripeHelper {
         throw error.internalValidationError(
           'formatSubscriptionsForSupport',
           sub,
-          new Error(`Product invalid for subcription: ${sub.id}`)
+          new Error(`Product invalid for subscription: ${sub.id}`)
         );
       }
       const product_name = product.name;
@@ -2252,10 +2187,7 @@ export class StripeHelper {
     } = baseDetails;
 
     const { lastFour, cardType } =
-      await this.extractCustomerDefaultPaymentDetails({
-        uid,
-        email,
-      });
+      await this.extractCustomerDefaultPaymentDetails(uid);
 
     const upcomingInvoice = await this.stripe.invoices.retrieveUpcoming({
       subscription: subscription.id,
@@ -2284,17 +2216,13 @@ export class StripeHelper {
     };
   }
 
-  async extractCustomerDefaultPaymentDetails({
-    uid,
-    email,
-  }: {
-    uid: string;
-    email: string;
-  }) {
+  async extractCustomerDefaultPaymentDetails(uid: string) {
     let lastFour = null;
     let cardType = null;
 
-    const customer = await this.customer({ uid, email });
+    const customer = await this.fetchCustomer(uid, [
+      'invoice_settings.default_payment_method',
+    ]);
     if (!customer) {
       throw error.unknownCustomer(uid);
     }
@@ -2302,10 +2230,8 @@ export class StripeHelper {
     if (customer.invoice_settings.default_payment_method) {
       // Post-SCA customer with a default PaymentMethod
       // default_payment_method *should* be expanded, but just in case...
-      const paymentMethod = await this.expandResource(
-        customer.invoice_settings.default_payment_method,
-        PAYMENT_METHOD_RESOURCE
-      );
+      const paymentMethod = customer.invoice_settings
+        .default_payment_method as Stripe.PaymentMethod;
       if (paymentMethod.card) {
         ({ last4: lastFour, brand: cardType } = paymentMethod.card);
       }
@@ -2492,9 +2418,8 @@ export class StripeHelper {
    * Process a customer event that needs to be saved to Firestore.
    */
   async processCustomerEventToFirestore(event: Stripe.Event) {
-    const { data } = event;
     const customer = await this.stripe.customers.retrieve(
-      (data.object as Stripe.Customer).id
+      (event.data.object as Stripe.Customer).id
     );
     const { uid } = await getUidAndEmailByStripeCustomerId(customer.id);
     if (!uid) {
@@ -2511,9 +2436,8 @@ export class StripeHelper {
    * Process a subscription event that needs to be saved to Firestore.
    */
   async processSubscriptionEventToFirestore(event: Stripe.Event) {
-    const { data } = event;
     const subscription = await this.stripe.subscriptions.retrieve(
-      (data.object as Stripe.Subscription).id
+      (event.data.object as Stripe.Subscription).id
     );
     return this.stripeFirestore.insertSubscriptionRecordWithBackfill(
       subscription
@@ -2524,9 +2448,8 @@ export class StripeHelper {
    * Process a invoice event that needs to be saved to Firestore.
    */
   async processInvoiceEventToFirestore(event: Stripe.Event) {
-    const { data } = event;
     const invoice = await this.stripe.invoices.retrieve(
-      (data.object as Stripe.Invoice).id
+      (event.data.object as Stripe.Invoice).id
     );
 
     try {
@@ -2550,17 +2473,12 @@ export class StripeHelper {
    * only change in their entirety.
    */
   async processPaymentMethodEventToFirestore(event: Stripe.Event) {
-    const { data } = event;
-
-    const paymentMethod = data.object as Stripe.PaymentMethod;
-
-    if (event.type === 'payment_method.detached') {
-      await this.stripeFirestore.removePaymentMethodRecord(paymentMethod.id!);
-    } else {
-      await this.stripeFirestore.insertPaymentMethodRecordWithBackfill(
-        paymentMethod
-      );
-    }
+    const paymentMethod = await this.stripe.paymentMethods.retrieve(
+      (event.data.object as Stripe.PaymentMethod).id
+    );
+    return this.stripeFirestore.insertPaymentMethodRecordWithBackfill(
+      paymentMethod
+    );
   }
 
   /**
@@ -2598,9 +2516,7 @@ export class StripeHelper {
         case 'payment_method.automatically_updated':
         case 'payment_method.detached':
         case 'payment_method.updated':
-          if (!event.request?.id) {
-            await this.processPaymentMethodEventToFirestore(event);
-          }
+          await this.processPaymentMethodEventToFirestore(event);
           break;
         default: {
           handled = false;

packages/fxa-auth-server/lib/routes/subscriptions/google.ts
@@ -51,9 +51,6 @@ export class GoogleIapHandler {
       throw error.unknownAppName(appName);
     }
 
-    // Lookup the email for the user as we need it for capability checks
-    const { email } = (await this.db.account(uid)).primaryEmail;
-
     let purchase;
     try {
       purchase = await this.playBilling.purchaseManager.registerToUserAccount(
@@ -79,7 +76,7 @@ export class GoogleIapHandler {
           );
       }
     }
-    await this.capabilityService.playUpdate(uid, email, purchase);
+    await this.capabilityService.playUpdate(uid, purchase);
     return { tokenValid: true };
   }
 }

packages/fxa-auth-server/lib/routes/subscriptions/paypal-notifications.ts
@@ -110,10 +110,9 @@ export class PayPalNotificationHandler extends PayPalHandler {
       });
       return;
     }
-    const customer = await this.stripeHelper.customer({
-      uid: account.uid,
-      email: account.email,
-    });
+    const customer = await this.stripeHelper.fetchCustomer(account.uid, [
+      'subscriptions',
+    ]);
     if (!customer) {
       this.log.error('handleMpCancel', {
         message: 'Stripe customer not found',
@@ -137,9 +136,8 @@ export class PayPalNotificationHandler extends PayPalHandler {
       nextPeriodValidSubscription
     ) {
       const { uid, email } = account;
-      const subscriptions = await this.stripeHelper.formatSubscriptionsForEmails(
-        customer
-      );
+      const subscriptions =
+        await this.stripeHelper.formatSubscriptionsForEmails(customer);
       await this.mailer.sendSubscriptionPaymentProviderCancelledEmail(
         account.emails,
         account,

packages/fxa-auth-server/lib/routes/subscriptions/paypal.ts
@@ -53,7 +53,7 @@ export class PayPalHandler extends StripeWebhookHandler {
    */
   async getCheckoutToken(request: AuthRequest) {
     this.log.begin('subscriptions.getCheckoutToken', request);
-    const { uid, email } = await handleAuth(this.db, request.auth, true);
+    const { email } = await handleAuth(this.db, request.auth, true);
     await this.customs.check(request, email, 'getCheckoutToken');
 
     const { currencyCode } = request.payload as Record<string, string>;
@@ -81,10 +81,9 @@ export class PayPalHandler extends StripeWebhookHandler {
     );
     await this.customs.check(request, email, 'createSubscriptionWithPaypal');
 
-    let customer = await this.stripeHelper.customer({
-      uid,
-      email,
-    });
+    let customer = await this.stripeHelper.fetchCustomer(uid, [
+      'subscriptions',
+    ]);
 
     if (!customer) {
       throw error.unknownCustomer(uid);
@@ -271,10 +270,9 @@ export class PayPalHandler extends StripeWebhookHandler {
     const { uid, email } = await handleAuth(this.db, request.auth, true);
     await this.customs.check(request, email, 'updatePaypalBillingAgreement');
 
-    let customer = await this.stripeHelper.customer({
-      uid,
-      email,
-    });
+    let customer = await this.stripeHelper.fetchCustomer(uid, [
+      'subscriptions',
+    ]);
 
     if (!customer) {
       throw error.unknownCustomer(uid);

packages/fxa-auth-server/lib/routes/subscriptions/play-pubsub.ts
@@ -63,16 +63,6 @@ export class PlayPubsubHandler {
       return {};
     }
     const uid = purchase.userId;
-
-    // Lookup the email for the user as we need it for capability checks
-    let email;
-    try {
-      email = (await this.db.account(uid)).primaryEmail.email;
-    } catch (err) {
-      reportSentryError(err, request);
-      return {};
-    }
-
     const updatedPurchase =
       await this.playBilling.purchaseManager.processDeveloperNotification(
         developerNotification.packageName,
@@ -83,7 +73,7 @@ export class PlayPubsubHandler {
       return {};
     }
 
-    await this.capabilityService.playUpdate(uid, email, updatedPurchase);
+    await this.capabilityService.playUpdate(uid, updatedPurchase);
 
     return {};
   }

packages/fxa-auth-server/lib/routes/subscriptions/stripe-webhook.ts
@@ -271,7 +271,7 @@ export class StripeWebhookHandler extends StripeHandler {
       ['active', 'trialing'].includes(sub.status) &&
       (event.data?.previous_attributes as any)?.status === 'incomplete'
     ) {
-      return this.capabilityService.stripeUpdate({ sub, uid, email });
+      return this.capabilityService.stripeUpdate({ sub, uid });
     }
     return;
   }
@@ -284,9 +284,9 @@ export class StripeWebhookHandler extends StripeHandler {
     event: Stripe.Event
   ) {
     const sub = event.data.object as Stripe.Subscription;
-    let uid, email;
+    let uid;
     try {
-      ({ uid, email } = await this.sendSubscriptionDeletedEmail(sub));
+      ({ uid } = await this.sendSubscriptionDeletedEmail(sub));
     } catch (err) {
       // FIXME: If the customer was deleted, we don't send an email that their subscription
       //        was cancelled. This is because the email requires a bunch of details that
@@ -297,9 +297,11 @@ export class StripeWebhookHandler extends StripeHandler {
       reportSentryError(err, request);
       return;
     }
-    await this.capabilityService.stripeUpdate({ sub, uid, email });
+    await this.capabilityService.stripeUpdate({ sub, uid });
     if (this.paypalHelper) {
-      const customer = await this.stripeHelper.customer({ uid, email });
+      const customer = await this.stripeHelper.fetchCustomer(uid, [
+        'subscriptions',
+      ]);
       if (!customer || customer.deleted) {
         return;
       }
@@ -323,7 +325,6 @@ export class StripeWebhookHandler extends StripeHandler {
       );
       return;
     }
-    this.stripeHelper.removeCustomerFromCache(uid, account.email);
   }
 
   /**
@@ -446,7 +447,6 @@ export class StripeWebhookHandler extends StripeHandler {
     const { uid, email } = await this.sendSubscriptionPaymentExpiredEmail(
       source
     );
-    await this.stripeHelper.removeCustomerFromCache(uid, email);
   }
 
   /**
@@ -674,13 +674,13 @@ export class StripeWebhookHandler extends StripeHandler {
    * whether the user still has an account.
    *
    * We receive a subscription deleted event for the following:
-   *   1. A user canceled subscripiton.
+   *   1. A user canceled subscription.
    *   2. Subscription canceled after multiple Stripe attempts to pay an invoice.
    *   3. PayPal processor canceled the subscription after failed attempts.
    *   4. A user deleted their account.
    *   5. An admin or support agent cancelled the subscription.
    *
-   * (1) and (5) are handled at the time of cancelation, so we do not send an additional
+   * (1) and (5) are handled at the time of cancellation, so we do not send an additional
    * email here.
    */
   async sendSubscriptionDeletedEmail(subscription: Stripe.Subscription) {

commit aded68b92fedaff0ffe6a225f192b1bdf9b9cf25

fix(configs): fix CapabilityService's dep on StripeHelper again
Because:
 - CapabilityService uses StripeHelper without checking relevant feature
   flags, leading to https://github.com/mozilla/fxa/issues/10123, whose
   fix was broken by
   https://github.com/mozilla/fxa/commit/878a991ce489e4b28d289734933e0b945a506eb7

This commit:
 - update the temporary fix in
   https://github.com/mozilla/fxa/commit/3f5880ee168889c5d243c40846c533576144675c
   due to a change how CapabilityService uses StripeHelper

packages/fxa-auth-server/lib/payments/capability.ts
@@ -55,7 +55,7 @@ export class CapabilityService {
       ? Container.get(StripeHelper)
       : ({
           purchasesToProductIds: () => Promise.resolve([]),
-          customer: () => Promise.resolve(null),
+          fetchCustomer: () => Promise.resolve(null),
           allAbbrevPlans: () => Promise.resolve([]),
         } as unknown as StripeHelper);
     this.profileClient = Container.get(ProfileClient);

packages/fxa-auth-server/lib/routes/account.ts
@@ -1476,7 +1476,7 @@ export class AccountHandler {
     let webSubscriptions: Awaited<WebSubscription[]> = [];
     let iapGooglePlaySubscriptions: Awaited<GooglePlaySubscription[]> = [];
 
-    if (this.config.subscriptions.enabled) {
+    if (this.config.subscriptions?.enabled && this.stripeHelper) {
       try {
         const customer = await this.stripeHelper.fetchCustomer(uid as string, [
           'subscriptions',