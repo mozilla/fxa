1ea7fdd3120451a1fae3d72f578f02a29953be95
Danny Coates <dannycoates@gmail.com>
2021-11-22 16:58:30 -0800

    chore(dev): wait for containers on 'yarn start' (#11119)
    On M1 we currently sometimes need to 'yarn restart services' after a 'yarn start' before things start working. Maybe if we wait for the containers to be responsive before starting services we can avoid this. The important one seems to be mysql.

--- package-scripts.js
+++ package-scripts.js
@@ -2,8 +2,8 @@ module.exports = {
   scripts: {
     default: 'nps help',
     start: {
-      default: `pm2 start _dev/pm2/infrastructure.config.js && _scripts/pm2-all.sh start && echo "Use 'yarn stop' to stop all the servers"`,
-      infrastructure: `pm2 start _dev/pm2/infrastructure.config.js`,
+      default: `_dev/pm2/start.sh && _scripts/pm2-all.sh start && pm2 restart sync && echo "Use 'yarn stop' to stop all the servers"`,
+      infrastructure: `_dev/pm2/start.sh`,
       services: `_scripts/pm2-all.sh start`,
       firefox: './packages/fxa-dev-launcher/bin/fxa-dev-launcher &',
     },
