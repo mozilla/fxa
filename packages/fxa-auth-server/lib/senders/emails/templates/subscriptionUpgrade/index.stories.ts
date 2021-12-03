44d80714de6b8e56db617a7538be8d9a883db757
Lisa Chan <lchan@mozilla.com>
2021-12-03 12:03:26 -0500

    feat(auth-server): convert subscriptionUpgrade email template to stack (#11207)
    * feat(auth-server): convert subscriptionUpgrade email template to stack
    
    Because:
    
    * We are actively converting old FxA emails to our new modernized stack.
    
    This commit:
    
    * Converts the `subscriptionUpgrade` subscription platform email to the new stack.
    
    Closes #9293

---
+++ packages/fxa-auth-server/lib/senders/emails/templates/subscriptionUpgrade/index.stories.ts
@@ -0,0 +1,30 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { Meta } from '@storybook/html';
+import { subplatStoryWithProps } from '../../storybook-email';
+
+export default {
+  title: 'SubPlat Emails/Templates/subscriptionUpgrade',
+} as Meta;
+
+const createStory = subplatStoryWithProps(
+  'subscriptionUpgrade',
+  'Sent when a user upgrades their subscription.',
+  {
+    paymentAmountNew: '£123,121.00',
+    paymentAmountOld: '¥99,991',
+    productIconURLNew:
+      'https://accounts-static.cdn.mozilla.net/product-icons/mozilla-vpn-email.png',
+    productIconURLOld:
+      'https://accounts-static.cdn.mozilla.net/product-icons/mozilla-vpn-email.png',
+    productNameNew: 'Product Name B',
+    productNameOld: 'Product Name A',
+    productPaymentCycle: 'month',
+    paymentProrated: '$5,231.00',
+    subscriptionSupportUrl: 'http://localhost:3030/support',
+  }
+);
+
+export const SubscriptionUpgrade = createStory();
