733697fca1febedeb7dcf53b5b2ce86f87fde90c
Ivo Plamenac <iplamenac@mozilla.com>
2021-12-06 09:30:44 -0800

    wip remove view

--- packages/fxa-payments-server/src/components/Coupon/index.tsx
+++ packages/fxa-payments-server/src/components/Coupon/index.tsx
@@ -1,4 +1,9 @@
-import React, { useState, useContext } from 'react';
+import React, {
+  useState,
+  useContext,
+  MouseEventHandler,
+  FormEventHandler,
+} from 'react';
 import { Localized } from '@fluent/react';
 import { AppContext } from '../../lib/AppContext';
 import './index.scss';
@@ -12,10 +17,24 @@ type CouponProps = {
 
 export const Coupon = ({ isMobile, className = 'default' }: CouponProps) => {
   const role = isMobile ? undefined : 'complementary';
+  const [hasCoupon, setHasCoupon] = useState(false);
 
   const validator = useValidatorState({
     initialState: undefined,
   });
+
+  const onSubmit: FormEventHandler = (event: any) => {
+    event.preventDefault();
+    debugger;
+    event.stopPropagation();
+    setHasCoupon(true);
+  };
+
+  const removeCoupon: MouseEventHandler<HTMLButtonElement> = (event: any) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setHasCoupon(false);
+  };
 
   return (
     <section
@@ -25,34 +44,40 @@ export const Coupon = ({ isMobile, className = 'default' }: CouponProps) => {
     >
       <div className="coupon-component-inner">
         <Localized id="coupon-header">
-          <div class="coupon-header">
-            <h4>Discount</h4>
+          <div className="coupon-header">
+            <h4>{hasCoupon ? 'Discount Reward Applied' : 'Discount'}</h4>
           </div>
         </Localized>
-        <Form validator={validator} onSubmit={onSubmit}>
-          <Localized attrs={{ placeholder: false }} id={undefined}>
-            <Input
-              type="text"
-              name="coupon"
-              data-testid="coupon"
-              placeholder="Enter code"
-            />
-          </Localized>
-          <SubmitButton name="apply">
-            <Localized id="sub-apply">
-              <span>Apply</span>
+        {hasCoupon ? (
+          <div className="flex">
+            <div className="coupon-details">
+              <div>Coupon Name</div>
+              <div>Expires 1/1/2020</div>
+            </div>
+            <button onClick={removeCoupon}>
+              <span>Remove</span>
+            </button>
+          </div>
+        ) : (
+          <Form validator={validator} onSubmit={onSubmit}>
+            <Localized attrs={{ placeholder: false }} id={undefined}>
+              <Input
+                type="text"
+                name="coupon"
+                data-testid="coupon"
+                placeholder="Enter code"
+              />
             </Localized>
-          </SubmitButton>
-        </Form>
+            <SubmitButton name="apply">
+              <Localized id="sub-apply">
+                <span>Apply</span>
+              </Localized>
+            </SubmitButton>
+          </Form>
+        )}
       </div>
     </section>
   );
-};
-
-const onSubmit = (event: Event) => {
-  event.preventDefault();
-  event.stopPropagation();
-  alert('hello');
 };
 
 export default Coupon;
