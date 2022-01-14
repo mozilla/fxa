051a1c042ed89ca00d3fb1261427734ed4eb7d34
Vijay Budhram <vbudhram@mozilla.com>
2022-01-13 20:47:23 -0500

    fix(mysql): Use utf8mb4 on the `emailBounces` table (#11618)

---
+++ packages/db-migrations/databases/fxa/patches/patch-125-124.sql
@@ -0,0 +1,6 @@
+-- -- Drop new column and new constraint
+
+-- -- Drop new stored procedures
+
+-- -- Decrement the schema version
+-- UPDATE dbMetadata SET value = '124' WHERE name = 'schema-patch-level';
