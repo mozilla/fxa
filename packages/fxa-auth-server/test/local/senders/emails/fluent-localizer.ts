4cb9d575c15054794ca8a946398a3d3341dc145c
Lauren Zugai <lauren@zugai.com>
2022-01-27 11:29:38 -0600

    fix(auth): Copy 'public' to dist, don't load non-existent Fluent bundles, Fluent strategy tweak (#11753)
    Because:
    * For Fluent translations, we pull in ftl files from public/locales, which wasn't present in dist
    * Fluent was attempting to load bundles from 'OTHER_EN_LOCALES' which was causing "missing translation" errors
    * We were attempting to load English dialects that may not have been correct, e.g. showing 'en-CA' dialect to US users, due to the Fluent strategy
    
    This commit:
    * Adds 'public' dir to fxa-auth-server copy list on build
    * Tells the bundler to load 'en-GB' if the user locale matches one in 'OTHER_EN_LOCALES', moved into 'fxa-shared' to share with Payments and renamed to EN_GB_LOCALES
    * Changes the Fluent strategy from 'filtering' to 'matching'
    * Adds tests
    
    fixes #11745
    
    Co-authored-by: Dan Schomburg <94418270+dschom@users.noreply.github.com>

--- packages/fxa-auth-server/test/local/senders/emails/fluent-localizer.ts
+++ packages/fxa-auth-server/test/local/senders/emails/fluent-localizer.ts
@@ -2,13 +2,154 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { FluentBundle, FluentResource } from '@fluent/bundle';
 import chai, { assert } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import { splitPlainTextLine } from '../../../../lib/senders/emails/fluent-localizer';
+import FluentLocalizer, {
+  splitPlainTextLine,
+  parseAcceptLanguage,
+} from '../../../../lib/senders/emails/fluent-localizer';
+
+import { NodeLocalizerBindings } from '../../../../lib/senders/emails/localizer-bindings-node';
 
 chai.use(chaiAsPromised);
 
 describe('fluent localizer', () => {
+  describe('fetches bundles', () => {
+    let LocalizerBindings = new NodeLocalizerBindings();
+    let localizer = new FluentLocalizer(LocalizerBindings);
+
+    it('fails with a bad localizer 1l0n basePath', () => {
+      assert.throws(() => {
+        let LocalizerBindings = new NodeLocalizerBindings({
+          l10n: {
+            basePath: '/not/a/apth',
+          },
+        });
+        new FluentLocalizer(LocalizerBindings);
+      }, 'Invalid l10n basePath');
+    });
+
+    it('produces the current locales', async () => {
+      const { currentLocales } = await localizer.setupLocalizer(
+        'de-CH,it;q=0.8,en-US;q=0.5,en;q=0.3'
+      );
+
+      assert.deepEqual(currentLocales, ['de', 'it', 'en-US', 'en']);
+    });
+
+    it('selects the proper locale', async () => {
+      const { selectedLocale } = await localizer.setupLocalizer(
+        'de-DE,en-US;q=0.7,en;q=0.3'
+      );
+
+      assert.equal(selectedLocale, 'de');
+    });
+
+    it('generates a proper bundle', async () => {
+      const { generateBundles } = await localizer.setupLocalizer('de,en');
+
+      const bundles: Array<FluentBundle> = [];
+      for await (const bundle of generateBundles(['de'])) {
+        bundles.push(bundle);
+      }
+
+      assert.lengthOf(bundles, 1);
+      assert.include(bundles[0].locales, 'de');
+    });
+
+    describe('localizes properly', () => {
+      it('localizes properly with preferred language', async () => {
+        const { l10n } = await localizer.setupLocalizer(
+          'de-DE,en-US;q=0.7,en;q=0.3'
+        );
+
+        const result = await l10n.formatValue(
+          'subscriptionAccountFinishSetup-action',
+          {}
+        );
+
+        assert.equal(result, 'Passwort erstellen');
+      });
+
+      it('localizes properly with preferred Dialect', async () => {
+        const { l10n } = await localizer.setupLocalizer(
+          'en-GB,en-CA;q=0.7,en;q=0.3'
+        );
+
+        const result = await l10n.formatValue('unblockCode-prompt', {});
+
+        assert.equal(
+          result,
+          'If yes, here is the authorisation code you need:'
+        );
+      });
+    });
+  });
+
+  describe('lanugage negotiation', () => {
+    it('handles empty case', () => {
+      const result = parseAcceptLanguage('');
+
+      assert.deepEqual(result, ['en']);
+    });
+
+    it('ignores unknown language', () => {
+      const result = parseAcceptLanguage('zy');
+      assert.deepEqual(result, ['en']);
+    });
+
+    it('handles en case', () => {
+      const result = parseAcceptLanguage('en');
+
+      // Note: We are using the 'filtering' mode for negotiate languages so all are going to be provided
+      assert.deepEqual(result, ['en']);
+    });
+
+    it('handles en-* case', () => {
+      const result = parseAcceptLanguage('en-US');
+
+      // Note: We are using the 'filtering' mode for negotiate languages so all are english dialects are going to be provided
+      assert.deepEqual(result, ['en-US', 'en']);
+    });
+
+    it('handles alias to en-GB', () => {
+      const result = parseAcceptLanguage('en-NZ');
+
+      assert.deepEqual(result, ['en-GB', 'en']);
+    });
+
+    it('always has default language, en, present', () => {
+      const result = parseAcceptLanguage('de');
+
+      assert.deepEqual(result, ['de', 'en']);
+    });
+
+    it('is falls back to root language if dialect is missing', () => {
+      const result = parseAcceptLanguage('fr-FR');
+
+      assert.deepEqual(result, ['fr', 'en']);
+    });
+
+    it('resolves dialects', () => {
+      const result = parseAcceptLanguage('es-MX');
+
+      assert.deepEqual(result, ['es-MX', 'en']);
+    });
+
+    it('handles multiple languages', () => {
+      const result = parseAcceptLanguage('ja, de-CH, en-US, en');
+
+      assert.deepEqual(result, ['ja', 'de', 'en-US', 'en']);
+    });
+
+    it('handles multiple languages with en-GB alias', () => {
+      const result = parseAcceptLanguage('en-NZ, en-GB, en-MY');
+
+      assert.deepEqual(result, ['en-GB', 'en']);
+    });
+  });
+
   describe('key value extraction', () => {
     const pair = {
       key: 'foo_2-Bar',
