7b1c62eaab1904589932765abb783d8891f3a444
Dan Schomburg <94418270+dschom@users.noreply.github.com>
2022-02-09 12:32:23 -0800

    patch(auth): Fixes build error in CI. (#11903)
    This commit:
    - Addresses build error in CI
    
    Because:
    - The command `yarn workspace fxa-auth-server build` was failing
    - Switching to require statements in redis.js file clears typescript error saying the 'default export was not defined'.

--- packages/fxa-auth-server/lib/redis.js
+++ packages/fxa-auth-server/lib/redis.js
@@ -1,8 +1,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-import { RedisShared } from 'fxa-shared/db/redis';
-import { resolve } from 'path';
+const { RedisShared } = require('fxa-shared/db/redis');
+const { resolve } = require('path');
 
 ('use strict');
 
