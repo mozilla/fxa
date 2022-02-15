823985e493304562eaa01540c9b67fab839fc512
Ivo Plamenac <iplamenac@mozilla.com>
2022-02-14 16:14:10 -0800

    reverting linter changes

--- packages/fxa-admin-server/src/graphql.ts
+++ packages/fxa-admin-server/src/graphql.ts
@@ -1,3 +1,4 @@
+
 /*
  * -------------------------------------------------------
  * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
@@ -7,133 +8,133 @@
 /* tslint:disable */
 /* eslint-disable */
 export enum BounceType {
-  unmapped = 'unmapped',
-  Permanent = 'Permanent',
-  Transient = 'Transient',
-  Complaint = 'Complaint',
+    unmapped = "unmapped",
+    Permanent = "Permanent",
+    Transient = "Transient",
+    Complaint = "Complaint"
 }
 
 export enum BounceSubType {
-  unmapped = 'unmapped',
-  Undetermined = 'Undetermined',
-  General = 'General',
-  NoEmail = 'NoEmail',
-  Suppressed = 'Suppressed',
-  MailboxFull = 'MailboxFull',
-  MessageTooLarge = 'MessageTooLarge',
-  ContentRejected = 'ContentRejected',
-  AttachmentRejected = 'AttachmentRejected',
-  Abuse = 'Abuse',
-  AuthFailure = 'AuthFailure',
-  Fraud = 'Fraud',
-  NotSpam = 'NotSpam',
-  Other = 'Other',
-  Virus = 'Virus',
+    unmapped = "unmapped",
+    Undetermined = "Undetermined",
+    General = "General",
+    NoEmail = "NoEmail",
+    Suppressed = "Suppressed",
+    MailboxFull = "MailboxFull",
+    MessageTooLarge = "MessageTooLarge",
+    ContentRejected = "ContentRejected",
+    AttachmentRejected = "AttachmentRejected",
+    Abuse = "Abuse",
+    AuthFailure = "AuthFailure",
+    Fraud = "Fraud",
+    NotSpam = "NotSpam",
+    Other = "Other",
+    Virus = "Virus"
 }
 
 export interface Location {
-  city?: Nullable<string>;
-  country?: Nullable<string>;
-  countryCode?: Nullable<string>;
-  state?: Nullable<string>;
-  stateCode?: Nullable<string>;
+    city?: Nullable<string>;
+    country?: Nullable<string>;
+    countryCode?: Nullable<string>;
+    state?: Nullable<string>;
+    stateCode?: Nullable<string>;
 }
 
 export interface AttachedClient {
-  clientId?: Nullable<string>;
-  deviceId?: Nullable<string>;
-  sessionTokenId?: Nullable<string>;
-  refreshTokenId?: Nullable<string>;
-  isCurrentSession?: Nullable<boolean>;
-  deviceType?: Nullable<string>;
-  name?: Nullable<string>;
-  scope?: Nullable<string[]>;
-  location?: Nullable<Location>;
-  userAgent?: Nullable<string>;
-  os?: Nullable<string>;
-  createdTime?: Nullable<number>;
-  createdTimeFormatted?: Nullable<string>;
-  lastAccessTime?: Nullable<number>;
-  lastAccessTimeFormatted?: Nullable<string>;
-  approximateLastAccessTime?: Nullable<number>;
-  approximateLastAccessTimeFormatted?: Nullable<string>;
+    clientId?: Nullable<string>;
+    deviceId?: Nullable<string>;
+    sessionTokenId?: Nullable<string>;
+    refreshTokenId?: Nullable<string>;
+    isCurrentSession?: Nullable<boolean>;
+    deviceType?: Nullable<string>;
+    name?: Nullable<string>;
+    scope?: Nullable<string[]>;
+    location?: Nullable<Location>;
+    userAgent?: Nullable<string>;
+    os?: Nullable<string>;
+    createdTime?: Nullable<number>;
+    createdTimeFormatted?: Nullable<string>;
+    lastAccessTime?: Nullable<number>;
+    lastAccessTimeFormatted?: Nullable<string>;
+    approximateLastAccessTime?: Nullable<number>;
+    approximateLastAccessTimeFormatted?: Nullable<string>;
 }
 
 export interface EmailBounce {
-  email: string;
-  templateName: string;
-  bounceType: BounceType;
-  bounceSubType: BounceSubType;
-  createdAt: number;
+    email: string;
+    templateName: string;
+    bounceType: BounceType;
+    bounceSubType: BounceSubType;
+    createdAt: number;
 }
 
 export interface Email {
-  email: string;
-  isVerified: boolean;
-  isPrimary: boolean;
-  createdAt: number;
+    email: string;
+    isVerified: boolean;
+    isPrimary: boolean;
+    createdAt: number;
 }
 
 export interface RecoveryKeys {
-  createdAt?: Nullable<number>;
-  verifiedAt?: Nullable<number>;
-  enabled?: Nullable<boolean>;
+    createdAt?: Nullable<number>;
+    verifiedAt?: Nullable<number>;
+    enabled?: Nullable<boolean>;
 }
 
 export interface SecurityEvents {
-  uid?: Nullable<string>;
-  nameId?: Nullable<number>;
-  verified?: Nullable<boolean>;
-  ipAddrHmac?: Nullable<string>;
-  createdAt?: Nullable<number>;
-  tokenVerificationId?: Nullable<string>;
-  name?: Nullable<string>;
+    uid?: Nullable<string>;
+    nameId?: Nullable<number>;
+    verified?: Nullable<boolean>;
+    ipAddrHmac?: Nullable<string>;
+    createdAt?: Nullable<number>;
+    tokenVerificationId?: Nullable<string>;
+    name?: Nullable<string>;
 }
 
 export interface SessionTokens {
-  tokenId?: Nullable<string>;
-  tokenData?: Nullable<string>;
-  uid?: Nullable<string>;
-  createdAt?: Nullable<number>;
-  uaBrowser?: Nullable<string>;
-  uaBrowserVersion?: Nullable<string>;
-  uaOS?: Nullable<string>;
-  uaOSVersion?: Nullable<string>;
-  uaDeviceType?: Nullable<string>;
-  lastAccessTime?: Nullable<number>;
+    tokenId?: Nullable<string>;
+    tokenData?: Nullable<string>;
+    uid?: Nullable<string>;
+    createdAt?: Nullable<number>;
+    uaBrowser?: Nullable<string>;
+    uaBrowserVersion?: Nullable<string>;
+    uaOS?: Nullable<string>;
+    uaOSVersion?: Nullable<string>;
+    uaDeviceType?: Nullable<string>;
+    lastAccessTime?: Nullable<number>;
 }
 
 export interface Totp {
-  verified: boolean;
-  createdAt: number;
-  enabled: boolean;
+    verified: boolean;
+    createdAt: number;
+    enabled: boolean;
 }
 
 export interface Account {
-  uid: string;
-  email: string;
-  emailVerified: boolean;
-  createdAt: number;
-  disabledAt?: Nullable<number>;
-  emails?: Nullable<Email[]>;
-  emailBounces?: Nullable<EmailBounce[]>;
-  totp?: Nullable<Totp[]>;
-  recoveryKeys?: Nullable<RecoveryKeys[]>;
-  sessionTokens?: Nullable<SessionTokens[]>;
-  securityEvents?: Nullable<SecurityEvents[]>;
-  attachedClients?: Nullable<AttachedClient[]>;
+    uid: string;
+    email: string;
+    emailVerified: boolean;
+    createdAt: number;
+    disabledAt?: Nullable<number>;
+    emails?: Nullable<Email[]>;
+    emailBounces?: Nullable<EmailBounce[]>;
+    totp?: Nullable<Totp[]>;
+    recoveryKeys?: Nullable<RecoveryKeys[]>;
+    sessionTokens?: Nullable<SessionTokens[]>;
+    securityEvents?: Nullable<SecurityEvents[]>;
+    attachedClients?: Nullable<AttachedClient[]>;
 }
 
 export interface IQuery {
-  accountByUid(uid: string): Nullable<Account> | Promise<Nullable<Account>>;
-  accountByEmail(email: string): Nullable<Account> | Promise<Nullable<Account>>;
-  getEmailsLike(search: string): Nullable<Email[]> | Promise<Nullable<Email[]>>;
+    accountByUid(uid: string): Nullable<Account> | Promise<Nullable<Account>>;
+    accountByEmail(email: string): Nullable<Account> | Promise<Nullable<Account>>;
+    getEmailsLike(search: string): Nullable<Email[]> | Promise<Nullable<Email[]>>;
 }
 
 export interface IMutation {
-  unverifyEmail(email: string): boolean | Promise<boolean>;
-  disableAccount(uid: string): boolean | Promise<boolean>;
-  clearEmailBounce(email: string): boolean | Promise<boolean>;
+    unverifyEmail(email: string): boolean | Promise<boolean>;
+    disableAccount(uid: string): boolean | Promise<boolean>;
+    clearEmailBounce(email: string): boolean | Promise<boolean>;
 }
 
 type Nullable<T> = T | null;
