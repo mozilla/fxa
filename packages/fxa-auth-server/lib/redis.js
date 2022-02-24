bf2de1b6a1deda571a9404d96557079e75829231
dschom <dschomburg@mozilla.com>
2022-02-24 09:16:51 -0800

    Revert "patch(auth): Fixes build error in CI. (#11903)"
    This reverts commit 7b1c62eaab1904589932765abb783d8891f3a444.

--- packages/fxa-auth-server/lib/redis.js
+++ packages/fxa-auth-server/lib/redis.js
@@ -1,8 +1,8 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
-const { RedisShared } = require('fxa-shared/db/redis');
-const { resolve } = require('path');
+import { RedisShared } from 'fxa-shared/db/redis';
+import { resolve } from 'path';
 
 ('use strict');
 
