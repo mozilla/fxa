48f0be4ff523355ac4bff43df00b48008fa88178
Reino Muhl <reino.muhl@gmail.com>
2022-03-03 14:23:34 -0500

    revert react-dom on payments

--- _scripts/firestore.sh
+++ _scripts/firestore.sh
@@ -5,5 +5,4 @@ cd "$DIR/../_dev/firebase"
 
 # the "demo-" prefix for --project is special
 # see https://firebase.google.com/docs/emulator-suite/connect_firestore#choose_a_firebase_project
-# docker run --rm -p 4400:4400 -p 4500:4500 -p 8085:8085 -p 9090:9090 -p 9299:9299 -v "$(pwd)":/home/node --name firebase-tools andreysenov/firebase-tools:latest firebase emulators:start --project demo-fxa
-docker run --rm -p 4400:4400 -p 4500:4500 -p 8085:8085 -p 9090:9090 -p 9299:9299 -v "$(pwd)":/home/node --name firebase-tools andreysenov/firebase-tools:10.2.0-node-lts-alpine firebase emulators:start --project demo-fxa
+docker run --rm -p 4400:4400 -p 4500:4500 -p 8085:8085 -p 9090:9090 -p 9299:9299 -v "$(pwd)":/home/node --name firebase-tools andreysenov/firebase-tools:latest firebase emulators:start --project demo-fxa
