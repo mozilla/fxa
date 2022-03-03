21b19a98fba6b23c5a85c41826f612c8496febe9
Reino Muhl <reino.muhl@gmail.com>
2022-03-03 12:30:45 -0500

    chore(shared): apollo plugin to return promise Because:
    * Apollo plugin functions need return a promise.
    
    This commit:
    
    * Change the apollo plugin functions to async.
    
    Closes #

--- packages/fxa-shared/nestjs/sentry/sentry.plugin.ts
+++ packages/fxa-shared/nestjs/sentry/sentry.plugin.ts
@@ -16,7 +16,7 @@ import { ApolloError } from 'apollo-server';
 import { ExtraContext, reportRequestException } from './reporting';
 
 export const SentryPlugin = {
-  requestDidStart(requestContext: any) {
+  async requestDidStart(requestContext: any) {
     return {
       didEncounterErrors(ctx: any) {
         // If we couldn't parse the operation, don't
