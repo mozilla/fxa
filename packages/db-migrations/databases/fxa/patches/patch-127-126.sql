fc381edf852d5e09b065d5ca007fee70a7886cdd
Mill <msoper@mozilla.com>
2022-04-15 15:03:59 -0700

    feat(fxa-admin-panel): fix patch to include procedures and use them

--- packages/db-migrations/databases/fxa/patches/patch-127-126.sql
+++ packages/db-migrations/databases/fxa/patches/patch-127-126.sql
@@ -1,5 +1,10 @@
--- -- Drop new column
--- ALTER TABLE emailBounces DROP COLUMN diagnosticCode;
+-- -- Drop new column and new constraint
+-- ALTER TABLE emailBounces
+-- DROP COLUMN diagnosticCode;
+
+-- -- Drop new stored procedures
+-- DROP PROCEDURE `createEmailBounce_3`;
+-- DROP PROCEDURE `fetchEmailBounces_3`;
 
 -- -- Decrement the schema version
 -- UPDATE dbMetadata SET value = '126' WHERE name = 'schema-patch-level';
