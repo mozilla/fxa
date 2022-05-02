a3125498d3c6e69d4a29054339707fe9c9013267
Mill <msoper@mozilla.com>
2022-05-02 09:43:44 -0700

    fix(fxa-settings) remove static list of feature from account deletion warning
    fix(fxa-settings) Add change to l10n files, remove unused constants
    
    fix(fxa-settings) remove outdated Pocket reference from checklist
    
    fix(fxa-settings): update string id on updated string

--- packages/fxa-settings/src/constants/index.tsx
+++ packages/fxa-settings/src/constants/index.tsx
@@ -5,5 +5,3 @@
 export const ROOTPATH = '/';
 export const HomePath = '/settings';
 export const DeleteAccountPath = '/settings/delete_account';
-export const VPNLink = 'https://vpn.mozilla.org/';
-export const MonitorLink = 'https://monitor.firefox.com/';
