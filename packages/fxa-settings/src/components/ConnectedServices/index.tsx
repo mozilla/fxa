03732b2a637df261cb1c850199b6d9bab14a1ff3
Meghan Sardesai <msardesai@mozilla.com>
2022-05-11 12:08:22 -0400

    style(settings): fix alignment of headers on Settings page Because:
    * We want to maintain consistent design and UX throughout the site
    
    This commit:
    
    * Updates h2 classes to align all headers
    
    Closes #9324

--- packages/fxa-settings/src/components/ConnectedServices/index.tsx
+++ packages/fxa-settings/src/components/ConnectedServices/index.tsx
@@ -186,7 +186,7 @@ export const ConnectedServices = () => {
 
   return (
     <section className="mt-11" data-testid="settings-connected-services">
-      <h2 className="font-header font-bold ltr:ml-4 rtl:mr-4 mb-4 relative">
+      <h2 className="font-header font-bold mobileLandscape:ltr:ml-6 mobileLandscape:rtl:ml-6 ltr:ml-4 rtl:mr-4 mb-4 relative">
         <span id="connected-services" className="nav-anchor"></span>
         <Localized id="cs-heading">Connected Services</Localized>
       </h2>
