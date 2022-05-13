b85e51f8903d14d1c0d64277b6ed094fb62c42ff
Anna Sobiepanek <asobiepanek@mozilla.com>
2022-05-13 14:39:13 -0400

    chore(payments): Missing markup in payment-confirm string

--- packages/fxa-payments-server/src/components/PaymentConsentCheckbox/index.tsx
+++ packages/fxa-payments-server/src/components/PaymentConsentCheckbox/index.tsx
@@ -1,11 +1,8 @@
 import React, { useContext } from 'react';
 import { Checkbox } from '../fields';
 import { Plan } from '../../store/types';
-import {
-  getDefaultPaymentConfirmText,
-  getLocalizedCurrency,
-} from '../../lib/formats';
 import { productDetailsFromPlan } from 'fxa-shared/subscriptions/metadata';
+import { formatPlanPricing, getLocalizedCurrency } from '../../lib/formats';
 import AppContext from '../../lib/AppContext';
 import { Localized } from '@fluent/react';
 
@@ -25,6 +22,13 @@ export const PaymentConsentCheckbox = ({
     navigatorLanguages
   );
 
+  const planPricing = formatPlanPricing(
+    plan.amount,
+    plan.currency,
+    plan.interval,
+    plan.interval_count
+  );
+
   return (
     <Localized
       id={`payment-confirm-with-legal-links-${plan.interval}`}
@@ -39,12 +43,11 @@ export const PaymentConsentCheckbox = ({
       }}
     >
       <Checkbox name="confirm" data-testid="confirm" onClick={onClick} required>
-        {getDefaultPaymentConfirmText(
-          plan.amount,
-          plan.currency,
-          plan.interval,
-          plan.interval_count
-        )}
+        I authorize Mozilla, maker of Firefox products, to charge my payment
+        method <strong>{planPricing}</strong>, according to{' '}
+        <a href={termsOfService}>Terms of Service</a> and{' '}
+        <a href={privacyNotice}>Privacy Notice</a>, until I cancel my
+        subscription.
       </Checkbox>
     </Localized>
   );
