974455b28cd52b7f42505f1fb49bfde2b25d6a76
Bianca Danforth <biancadanforth@gmail.com>
2022-06-07 18:15:01 -0400

    only compile backend TS packages for speed

--- _scripts/git-precommit-hook.sh
+++ _scripts/git-precommit-hook.sh
@@ -12,20 +12,25 @@ fi
 
 echo "modified packages found"
 
-# 3. Build all packages in the list
+# 3. Build all back-end TS packages in the list
 ORIGINAL_IFS=$IFS
 IFS=$'\n'
+# We don't need to worry about front-end packages that use TS, since they must build successfully for CI to pass.
+BACKEND_PACKAGES=( "fxa-auth-server" "fxa-event-broker" "fxa-graphql-api" "fxa-shared" "fxa-support-panel" "fxa-admin-server" )
 INCLUDE_ARGS=''
 for package_modified in $PACKAGES_MODIFIED; do
-    INCLUDE_ARGS="$INCLUDE_ARGS --include ${package_modified}"
+  if [[ " ${BACKEND_PACKAGES[@]} " =~ " ${package_modified} " ]]; then
+      INCLUDE_ARGS="$INCLUDE_ARGS --include ${package_modified}"
+  fi
 done
 IFS=$ORIGINAL_IFS
 INCLUDE_ARGS=`echo "$INCLUDE_ARGS" | xargs` # trim whitespace at beginning of string
-echo "building all modified and dependent packages..."
-if ! `yarn workspaces foreach --verbose ${INCLUDE_ARGS} run build > artifacts/build-modified-and-dependent.log`;
+echo "compiling all modified and dependent backend TS packages..."
+# We don't need to write files to disk, so use --noEmit here instead of --build for speed.
+if ! `yarn workspaces foreach --verbose --topological-dev ${INCLUDE_ARGS} run compile > artifacts/compiling-affected-backend-packages.log`;
 then
   echo -e "\n###########################################################\n"
-  echo "# fxa couldn't build modified and dependent packages. see ./artifacts/build-modified-and-dependent.log for details."
+  echo "# fxa couldn't build one or more packages. see artifacts/compiling-affected-backend-packages.log for details."
   echo -e "\n# If you see any unrelated TS errors, you could have a stale build of another package that changed recently. Try restarting fxa via 'yarn start'. See #12912."
   echo -e "\n###########################################################\n"
   exit 1
