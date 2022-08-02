da64dbbb83eec79841e7bccd205cc26d11a9b70f
Barry Chen <bchen@mozilla.com>
2022-08-02 17:03:15 -0500

    chore(pm2): watch styles in fxa-react
    Because:
     - we should rebuild the styles in content, payments, and settings when
       the styles in fxa-react change
    
    This commit:
     - watch the styles dir in fxa-react for the css "app" PM2 configs in
       content, payments, and settings

--- packages/fxa-payments-server/pm2.config.js
+++ packages/fxa-payments-server/pm2.config.js
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+const path = require('path');
 const PATH = process.env.PATH.split(':')
   .filter((p) => !p.includes(process.env.TMPDIR))
   .join(':');
@@ -61,6 +62,11 @@ module.exports = {
         'src/components/**/*.css',
         'src/**/*.tsx',
         require.resolve('fxa-react/configs/tailwind.js'),
+        path.normalize(
+          `${path.dirname(
+            require.resolve('fxa-react/configs/tailwind.js')
+          )}/../styles`
+        ),
       ],
       ignore_watch: ['src/styles/tailwind.out.*'],
       time: true,
