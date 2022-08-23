2cadde224e1c3b624f121d3044aab5b4a4c515cc
Ankita Shrivastava <ashrivastava@mozilla.com>
2022-08-23 15:47:59 -0400

    test(functional): rewrite display name and delete account tests in playwright

---
+++ packages/functional-tests/tests/settings/displayName.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect, newPages } from '../../lib/fixtures/standard';
+
+test.describe('severity-2 #smoke', () => {
+  // https://testrail.stage.mozaws.net/index.php?/cases/view/1293371
+  // https://testrail.stage.mozaws.net/index.php?/cases/view/1293373
+  test('add/change/cancel/remove the display name', async ({
+    pages: { settings, displayName },
+  }) => {
+    await settings.goto();
+    expect(await settings.displayName.statusText()).toEqual('None');
+    await settings.displayName.clickAdd();
+    await displayName.setDisplayName('TestUser1');
+
+    // Click cancel to cancel adding a display name
+    await displayName.clickCancelDisplayName();
+    await settings.displayName.clickAdd();
+    await displayName.setDisplayName('TestUser1');
+    await displayName.submit();
+
+    // Verify the added display name
+    expect(await settings.displayName.statusText()).toEqual('TestUser1');
+    await settings.displayName.clickAdd();
+    await displayName.setDisplayName('TestUser2');
+
+    // Click cancel to cancel changing the display name
+    await displayName.clickCancelDisplayName();
+    await settings.displayName.clickAdd();
+    await displayName.setDisplayName('TestUser2');
+    await displayName.submit();
+
+    //Verify the changed display name
+    expect(await settings.displayName.statusText()).toEqual('TestUser2');
+
+    // Remove display name
+    await settings.displayName.clickAdd();
+    await displayName.setDisplayName('');
+    await displayName.submit();
+    expect(await settings.displayName.statusText()).toEqual('None');
+  });
+});
