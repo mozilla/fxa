e79ccc3b3859658bdaad5bb5474f8b321bb333fa
Ankita Shrivastava <ashrivastava@mozilla.com>
2022-11-08 13:40:11 -0500

    split tests to run parallely

--- packages/functional-tests/scripts/test-ci.sh
+++ packages/functional-tests/scripts/test-ci.sh
@@ -28,4 +28,5 @@ yarn workspaces foreach \
 
 npx pm2 ls
 
-yarn workspace functional-tests test
+circleci tests glob "packages/functional-tests/tests/**/*.spec.ts" | circleci tests split > tests-to-run.txt
+yarn workspace functional-tests test --grep=$(cat tests-to-run.txt)
