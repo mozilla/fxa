5bb01fb7db5ac0867660724be466dee0b81f4724
Julian Poyourow <julian@poyourow.com>
2022-11-09 08:55:10 -0800

    fix(auth-server): rename automatic_tax to automaticTax
    Because:
    
    * There was a typo in the original implementation that caused automatic
      tax not be applied to any subscription created with automatic tax
      enabled.
    
    This commit:
    
    * Fixes the typo.
    
    Closes FXA-6277

--- packages/fxa-auth-server/lib/routes/subscriptions/stripe.ts
+++ packages/fxa-auth-server/lib/routes/subscriptions/stripe.ts
@@ -587,7 +587,7 @@ export class StripeHandler {
       }
 
       const taxOptions = this.automaticTax
-        ? { automatic_tax: taxSubscription }
+        ? { automaticTax: taxSubscription }
         : { taxRateId };
 
       const subscription: any =
