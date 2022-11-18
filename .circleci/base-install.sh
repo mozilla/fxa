9958ba19c1b4c2becd8334a3ccf1595c39608150
dschom <dschomburg@mozilla.com>
2022-11-18 15:17:58 -0800

    wip

--- .circleci/base-install.sh
+++ .circleci/base-install.sh
@@ -5,12 +5,12 @@ cd "$DIR/.."
 
 
 # If there is no difference in the yarn lock file, skip the install
-if cmp -s "yarn.lock" "base.yarn.lock"; then
-    print "Yarn lock up to date. Skipping base install"
-else
-    print "Updated yarn lock detected. Running yarn install!"
-    yarn install --immutable --immutable-cache
-if
+# if cmp -s "yarn.lock" "base.yarn.lock"; then
+#     print "Yarn lock up to date. Skipping base install"
+# else
+#     print "Updated yarn lock detected. Running yarn install!"
+#     yarn install --immutable --immutable-cache
+# if
 
 node .circleci/modules-to-test.js | tee packages/test.list
 ./_scripts/create-version-json.sh
