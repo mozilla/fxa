34a08465260512cabc8a446ee0b77c173cff3a04
Mill <msoper@mozilla.com>
2022-12-29 13:53:33 -0800

    storybook(fxa-settings): add signin_reported to storybook Because:
    * We're going through the remaining fxa-content-server Backbone views
      and adding React versions of them into Storybook as a first step to
    moving them into the React app.
    
    This commit:
    
    * Adds basic l10n, metrics, tests, and a React version for the
      `signin_reported` view
    
    Closes #https://mozilla-hub.atlassian.net/browse/FXA-6418

---
+++ packages/fxa-settings/src/pages/SigninReported/en.ftl
@@ -0,0 +1,2 @@
+signin-reported-header = Thank you for your vigilance
+signin-reported-message = Our team has been notified. Reports like this help us fend off intruders.
---
+++ packages/fxa-settings/src/pages/SigninReported/index.tsx
@@ -0,0 +1,37 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import React from 'react';
+import { usePageViewEvent } from '../../lib/metrics';
+import { FtlMsg } from 'fxa-react/lib/utils';
+import { RouteComponentProps } from '@reach/router';
+
+const SigninReported = (props: RouteComponentProps) => {
+  usePageViewEvent('signin-reported', {
+    entrypoint_variation: 'react',
+  });
+
+  return (
+    <>
+      <div className="mb-4">
+        <h1 className="card-header">
+          <FtlMsg id="signin-reported-header">
+            Thank you for your vigilance
+          </FtlMsg>
+        </h1>
+      </div>
+      <section>
+        <div className="error"></div>
+        <FtlMsg id="signin-reported-message">
+          <p className="my-4 text-sm">
+            Our team has been notified. Reports like this help us fend off
+            intruders.
+          </p>
+        </FtlMsg>
+      </section>
+    </>
+  );
+};
+
+export default SigninReported;
