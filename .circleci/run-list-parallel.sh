ace968b555b1b1e6ec9191f8d88fcfee8d4a1d37
dschom <dschomburg@mozilla.com>
2023-01-04 10:51:02 -0800

    wip

--- .circleci/run-list-parallel.sh
+++ .circleci/run-list-parallel.sh
@@ -10,20 +10,12 @@ if [[ ! -f .lists/$1 ]]; then
   exit 1
 fi
 
-# Check for memory limit argument
-if [[ $2 == "" ]]; then
-  echo "Missing max memory argument! Supply an second argument that puts in upper bound on memory. e.g. run-list-parallel.sh unit-test.list 2GB"
-  exit 1
-fi
-
 LIST=$1
 JOBLOG="--joblog artifacts/tests/$LIST.log"
-MEM_MAX=$2
 
 mkdir -p artifacts/tests
 
 # Executes the command in the LIST file in parallel. Some notes on options
 # Setting --load let's us wait for a heavy test suite to finish before starting another one
-# Setting --ungroup makes the output stream better, although it is then interlaced
 # Setting --joblog preserves the output in a log file.
-parallel --load 75% --halt 0 --joblog artifacts/tests/$LIST.log ----memfree $MEM_MAX --retires 1 < .lists/$LIST
+parallel --load 50% --halt 0 --joblog artifacts/tests/$LIST.log < .lists/$LIST
