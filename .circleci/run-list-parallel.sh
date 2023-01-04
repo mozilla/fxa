76162bf30da819a69e022e8165f22bc3449c4686
dschom <dschomburg@mozilla.com>
2023-01-04 09:06:55 -0800

    wip

--- .circleci/run-list-parallel.sh
+++ .circleci/run-list-parallel.sh
@@ -1,18 +1,24 @@
-#/bin/bash -ex
+#!/bin/bash -e
 
-LIST=$1
-MEM_SUSPEND=$2
-
-
-if [[ $LIST == "" ]]; then
-  echo "Missing list argument! Supply an argument. e.g. report-converage.sh packages/unit-test-includes.list"
+# Check for required list argument
+if [[ $1 == "" ]]; then
+  echo "Missing list argument! Supply an argument. e.g. run-list-parallel.sh unit-test.list 2GB"
   exit 1
 fi
-
-if [ ! -f $LIST ]; then
+if [[ ! -f .lists/$1 ]]; then
   echo "List isn't a valid file: $LIST"
   exit 1
 fi
+
+# Check for memory limit argument
+if [[ $2 == "" ]]; then
+  echo "Missing max memory argument! Supply an second argument that puts in upper bound on memory. e.g. run-list-parallel.sh unit-test.list 2GB"
+  exit 1
+fi
+
+LIST=$1
+JOBLOG="--joblog artifacts/tests/$LIST.log"
+MEM_SUSPEND=$2
 
 mkdir -p artifacts/tests
 
@@ -20,4 +26,4 @@ mkdir -p artifacts/tests
 # Setting --load let's us wait for a heavy test suite to finish before starting another one
 # Setting --ungroup makes the output stream better, although it is then interlaced
 # Setting --joblog preserves the output in a log file.
-parallel --load 75% --halt 0 --joblog artifacts/tests/$LIST.log --cf --memsuspend $2 < .lists/$LIST
+parallel --load 75% --halt 0 --cf --joblog artifacts/tests/$LIST.log --memsuspend  $MEM_SUSPEND < .lists/$LIST
