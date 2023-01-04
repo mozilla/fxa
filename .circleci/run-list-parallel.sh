578ac2514aac0fca9eb26d85fcc78a51ea70daa8
dschom <dschomburg@mozilla.com>
2023-01-04 11:50:36 -0800

    wip

--- .circleci/run-list-parallel.sh
+++ .circleci/run-list-parallel.sh
@@ -2,7 +2,7 @@
 
 # Check for required list argument
 if [[ $1 == "" ]]; then
-  echo "Missing list argument! Supply an argument. e.g. run-list-parallel.sh unit-test.list 2GB"
+  echo "Missing list argument! Supply an argument. e.g. run-list-parallel.sh unit-test.list"
   exit 1
 fi
 if [[ ! -f .lists/$1 ]]; then
@@ -13,9 +13,14 @@ fi
 LIST=$1
 JOBLOG="--joblog artifacts/tests/$LIST.log"
 
+if [[ $2 == "" ]]; then
+else
+  MAX_JOBS="-j $2"
+fi
+
 mkdir -p artifacts/tests
 
 # Executes the command in the LIST file in parallel. Some notes on options
 # Setting --load let's us wait for a heavy test suite to finish before starting another one
 # Setting --joblog preserves the output in a log file.
-parallel --load 50% --halt 0 --joblog artifacts/tests/$LIST.log < .lists/$LIST
+parallel $MAX_JOBS --load 50% --halt 0 --joblog artifacts/tests/$LIST.log < .lists/$LIST
