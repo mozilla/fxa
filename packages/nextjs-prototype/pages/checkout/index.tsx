103c49df8d707f927f9e1219f7bcaa92fcb6bf3e
Reino Muhl <reino.muhl@gmail.com>
2023-01-10 17:27:12 -0500

    Add tailwind and config like other packages

--- packages/nextjs-prototype/pages/checkout/index.tsx
+++ packages/nextjs-prototype/pages/checkout/index.tsx
@@ -4,8 +4,10 @@ export default function CheckoutPage() {
   return (
     <>
       <h1>Checkout Homepage</h1>
-      <p>This would be a fallback page if no plan is found or provided</p>
-      <p>
+      <p className="bg-green-200">
+        This would be a fallback page if no plan is found or provided
+      </p>
+      <p className="bg-red-500">
         I thought maybe we could use this page to display all SubPlat available
         products.
       </p>
---
+++ packages/nextjs-prototype/tailwind.config.js
@@ -0,0 +1,29 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Please refer to fxa-react/configs/tailwind for the main config file
+
+const { resolve } = require('path');
+const extractImportedComponents = require('fxa-react/extract-imported-components');
+const config = require('fxa-react/configs/tailwind');
+
+if (process.env.NODE_ENV === 'production') {
+  const matches = extractImportedComponents(
+    resolve(__dirname, 'src', 'components')
+  );
+
+  config.content.push(...matches);
+} else {
+  config.content = [];
+  config.content.push('../fxa-react/components/**/*.tsx');
+  config.content.push('./pages/**/*.{js,ts,jsx,tsx}'); // Add nextjs react content
+  config.content.push('./components/**/*.{js,ts,jsx,tsx}'); // Add nextjs react content
+  config.safelist = undefined; // Remove for now - Causes 10s-30s compile times
+}
+
+// remove this once Payments is using Tailwind and we can enable '@tailwind base'
+config.corePlugins = {};
+config.corePlugins.preflight = false;
+
+module.exports = config;
