247685015e902679422ea9fedfc70056c1ab8618
Ivo Plamenac <iplamenac@mozilla.com>
2023-01-11 11:36:17 -0800

    adding the abilty to configure webpack + adding babel-loader to webpack

---
+++ packages/gatsby-prototype/gatsby-node.js
@@ -0,0 +1,24 @@
+exports.onCreateWebpackConfig = ({
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.[jt]sx?$/,
+          include: [/node_modules(.*[/\\])/],
+          use: 'babel-loader',
+        },
+      ],
+    },
+    plugins: [
+      plugins.define({
+        __DEVELOPMENT__: stage === `develop` || stage === `develop-html`,
+      }),
+    ],
+  });
+};
