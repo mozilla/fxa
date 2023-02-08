832acc2ea91059c8835e6178e4a54daceb5e2023
Reino Muhl <reino.muhl@gmail.com>
2023-02-08 13:33:14 -0500

    remove yarn.lock changes

--- packages/fxa-payments-server/src/components/PaymentConsentCheckbox/index.tsx
+++ packages/fxa-payments-server/src/components/PaymentConsentCheckbox/index.tsx
@@ -1,4 +1,5 @@
 import { Localized } from '@fluent/react';
+import LinkExternal from 'fxa-react/components/LinkExternal';
 import { urlsFromProductConfig } from 'fxa-shared/subscriptions/configuration/utils';
 import React, { useContext } from 'react';
 
@@ -27,16 +28,20 @@ export const PaymentConsentCheckbox = ({
     <Localized
       id="payment-confirm-with-legal-links-static"
       elems={{
-        termsOfServiceLink: <a href={termsOfService}>Terms of Service</a>,
-        privacyNoticeLink: <a href={privacyNotice}>Privacy Notice</a>,
+        termsOfServiceLink: (
+          <LinkExternal href={termsOfService}>Terms of Service</LinkExternal>
+        ),
+        privacyNoticeLink: (
+          <LinkExternal href={privacyNotice}>Privacy Notice</LinkExternal>
+        ),
       }}
     >
       <Checkbox name="confirm" data-testid="confirm" onClick={onClick} required>
         I authorize Mozilla, maker of Firefox products, to charge my payment
         method for the amount shown, according to{' '}
-        <a href={termsOfService}>Terms of Service</a> and{' '}
-        <a href={privacyNotice}>Privacy Notice</a>, until I cancel my
-        subscription.
+        <LinkExternal href={termsOfService}>Terms of Service</LinkExternal> and{' '}
+        <LinkExternal href={privacyNotice}>Privacy Notice</LinkExternal>, until
+        I cancel my subscription.
       </Checkbox>
     </Localized>
   );
