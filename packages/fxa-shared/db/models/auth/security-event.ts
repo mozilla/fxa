1cca487a301d4cc6f9f05fb0653827f078f51ec7
Vijay Budhram <vbudhram@gmail.com>
2023-03-02 08:29:23 -0500

    fix(auth): convert record security event to ts

--- packages/fxa-shared/db/models/auth/security-event.ts
+++ packages/fxa-shared/db/models/auth/security-event.ts
@@ -16,6 +16,22 @@ const EVENT_NAMES = {
   'account.enable': 5,
   'account.disable': 6,
   'account.login.failure': 7, // User attempted to login but failed
+  'account.two_factor_added': 8,
+  'account.two_factor_requested': 9,
+  'account.two_factor_challenge_failure': 10,
+  'account.two_factor_challenge_success': 11,
+  'account.two_factor_removed': 12,
+  'account.password_reset_requested': 13,
+  'account.password_reset_success': 14,
+  'account.recovery_key_added': 15,
+  'account.recovery_key_challenge_failure': 16,
+  'account.recovery_key_challenge_success': 17,
+  'account.recovery_key_removed': 18,
+  'account.password_added': 19,
+  'account.password_changed': 20,
+  'account.secondary_email_added': 21,
+  'account.secondary_email_removed': 22,
+  'account.primary_secondary_swapped': 23,
 } as const;
 
 export type SecurityEventNames = keyof typeof EVENT_NAMES;
