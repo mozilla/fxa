c1af3c61ac0a0b63d3b6e08f4aeb83ffcc897a2d
Mill <msoper@mozilla.com>
2023-04-06 11:20:15 -0700

    bug(fxa-settings): unlink password input visibility toggle Because:
    * We want the password inputs to toggle visibility independent of each
      other. In other words, if you click on the "eye" icon next to either
    input, we want to change the visibility of only that input, and not the
    neighboring input.
    
    This commit:
    
    * Removes the logic which linked the visibility status for the two
      inputs.
    
    Closes #FXA-7150

--- packages/fxa-settings/src/components/InputPassword/mocks.tsx
+++ packages/fxa-settings/src/components/InputPassword/mocks.tsx
@@ -2,23 +2,14 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import React, { useState } from 'react';
+import React from 'react';
 import InputPassword from '.';
 
 export const SubjectWithPairedInputs = () => {
-  const [areBothPasswordsVisible, setAreBothPasswordsVisible] =
-    useState<boolean>(false);
-
   return (
     <>
-      <InputPassword
-        {...{ areBothPasswordsVisible, setAreBothPasswordsVisible }}
-        label="Make me visible..."
-      />
-      <InputPassword
-        {...{ areBothPasswordsVisible, setAreBothPasswordsVisible }}
-        label="...and I'll be visible too."
-      />
+      <InputPassword label="Make me visible..." />
+      <InputPassword label="...and I'll be visible too." />
       <p className="text-sm italic mt-10">
         <strong>Tip:</strong> Toggling visibility in one field controls
         visibility in both.
