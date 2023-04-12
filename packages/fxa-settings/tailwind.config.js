185a000128aead2e6b463634cb14ee404188a8ee
Mill <msoper@mozilla.com>
2023-04-12 12:35:45 -0700

    bug: fix missing animations in fxa-settings Because:
    * Some animations which worked on the content-server are not working in
      React. This is bc of CSP headers blocking inline style tags within the
    svgs. on the content-server the images are generally loaded in as
    backgrounds and therefore do not hit the CSP header.
    
    This commit:
    
    * Removes inline styles and adds images to storybook
    
    Closes #FXA-7166

--- packages/fxa-settings/tailwind.config.js
+++ packages/fxa-settings/tailwind.config.js
@@ -23,10 +23,50 @@ if (process.env.STORYBOOK_BUILD === '1') {
   config.content.push('./.storybook/design-guide/**/*.tsx');
 }
 
-config.theme.extend.backgroundImage = {
-  ...config.theme.extend.backgroundImage,
-  /* TODO: move this to `fxa-react`, FXA-5745 */
-  'ff-logo': "url('../../../fxa-react/images/ff-logo.svg')",
+config.content.push('./src/components/images/**/*.svg');
+
+config.theme.extend = {
+  ...config.theme.extend,
+  backgroundImage: {
+    ...config.theme.extend.backgroundImage,
+    /* TODO: move this to `fxa-react`, FXA-5745 */
+    'ff-logo': "url('../../../fxa-react/images/ff-logo.svg')",
+  },
+  keyframes: {
+    ...config.theme.extend.keyframes,
+    sparkle: { '0%': { opacity: 0 }, '50%': { opacity: 1 } },
+    'sparkle-lag-beginning': {
+      '0%': { opacity: 1 },
+      '30%': { opacity: 0.5 },
+      '70%': { opacity: 0 },
+    },
+    'sparkle-lag-end': {
+      '0%': { opacity: 1 },
+      '40%': { opacity: 0.5 },
+      '70%': { opacity: 1 },
+      '100%': { opacity: 0 },
+    },
+    'pulse-up': {
+      '0%': { transform: 'translateY(0)' },
+      '80%': { transform: 'translateY(0)' },
+      '90%': { transform: 'translateY(-3%)' },
+    },
+    beat: {
+      '0%': { transform: 'scale(1)' },
+      '60%': { transform: 'scale(.8)' },
+      '90%': { transform: 'scale(.9)' },
+    },
+  },
+  animation: {
+    ...config.theme.extend.animation,
+    sparkle: 'sparkle 2s ease-in-out infinite',
+    'sparkle-delay': 'sparkle 2s ease-in-out .5s infinite',
+    'sparkle-long-loop': 'sparkle 3s ease-in-out .3s infinite',
+    'sparkle-lag-beginning': 'sparkle-lag-beginning 2.5s ease-in-out infinite',
+    'sparkle-lag-end': 'sparkle-lag-end 1.5s ease-in-out infinite',
+    'pulse-up': 'pulse-up 5s ease-in-out infinite',
+    heart: 'beat 1.5s infinite',
+  },
 };
 
 module.exports = config;
