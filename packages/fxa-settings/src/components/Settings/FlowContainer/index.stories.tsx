ef66987bbb71de4cb07942046fcb7349d46c3051
Mill <msoper@mozilla.com>
2023-04-18 08:47:11 -0700

    feat(fxa-settings): create new flow container for account recovery Because:
    * We want to manage the longer flow of views using a wizard component
    
    This commit:
    
    * While this ticket originally asked for us to create a new flow
      container, it seems like that may not be necessary, and can instead
    just wrap the existing flow container in a wizard component that manages
    moving the user through a series of steps
    
    Closes #FXA-7228

--- packages/fxa-settings/src/components/Settings/FlowContainer/index.stories.tsx
+++ packages/fxa-settings/src/components/Settings/FlowContainer/index.stories.tsx
@@ -2,10 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Meta } from '@storybook/react';
 import { withLocalization } from '../../../../.storybook/decorators';
 import { FlowContainer } from '.';
+import { ProgressBar } from '../ProgressBar';
 import { LocationProvider } from '@reach/router';
 import { MOCK_CONTENT, MOCK_SUBTITLE, MOCK_TITLE } from './mocks';
 
@@ -27,3 +28,89 @@ export const Default = () => (
     {MOCK_CONTENT}
   </FlowContainer>
 );
+
+export const WizardExample = () => {
+  const [currentStep, setCurrentStep] = useState(1);
+  return (
+    <>
+      {currentStep === 1 && (
+        <FlowContainer
+          title="Example wizard flow"
+          {...{ currentStep, setCurrentStep }}
+          customBackButtonTitle="Previous page title"
+          customBackButtonLocalizationId="wizard-flow-back-button-title-first-step"
+          onBackButtonClick={() => {
+            window.history.back();
+          }}
+        >
+          <ProgressBar currentStep={1} numberOfSteps={3} />
+          <h2 className="font-bold text-xl mb-2">This is your first step.</h2>
+          <p>You should click the button below to proceed to the next step.</p>
+          <button
+            className="cta-primary cta-base-p m-2 flex-1"
+            type="button"
+            onClick={() => {
+              setCurrentStep(currentStep + 1);
+            }}
+          >
+            Next view
+          </button>
+        </FlowContainer>
+      )}
+      {currentStep === 2 && (
+        <FlowContainer
+          title="Example wizard flow"
+          {...{ currentStep, setCurrentStep }}
+          customBackButtonTitle="Example wizard flow"
+          customBackButtonLocalizationId="wizard-flow-back-button-title-wizard-step"
+          onBackButtonClick={() => {
+            setCurrentStep(currentStep - 1);
+          }}
+        >
+          <ProgressBar currentStep={2} numberOfSteps={3} />
+          <h2 className="font-bold text-xl mb-2">This is the second step.</h2>
+          <p>Click the button to proceed to the final step.</p>
+          <button
+            className="cta-primary cta-base-p m-2 flex-1"
+            type="button"
+            onClick={() => {
+              setCurrentStep(currentStep + 1);
+            }}
+          >
+            Next view
+          </button>
+        </FlowContainer>
+      )}
+      {
+        // Example wizard flow generated
+        currentStep === 3 && (
+          <FlowContainer
+            title="Example wizard flow"
+            {...{ currentStep, setCurrentStep }}
+            onBackButtonClick={() => {
+              setCurrentStep(currentStep - 1);
+            }}
+            customBackButtonTitle="Example wizard flow"
+            customBackButtonLocalizationId="wizard-flow-back-button-title-wizard-step"
+          >
+            <ProgressBar currentStep={3} numberOfSteps={3} />
+            <h2 className="font-bold text-xl mb-2">This is the end! </h2>
+            <p>
+              Take an arbitrary action by clicking the button, or navigate back
+              to a prior step.
+            </p>
+            <button
+              className="cta-primary cta-base-p m-2 flex-1"
+              type="button"
+              onClick={() => {
+                alert('You did it!');
+              }}
+            >
+              Finish flow
+            </button>
+          </FlowContainer>
+        )
+      }
+    </>
+  );
+};
--- packages/fxa-settings/src/components/Settings/PageRecoveryKeyCreate/index.tsx
+++ packages/fxa-settings/src/components/Settings/PageRecoveryKeyCreate/index.tsx
@@ -2,10 +2,42 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import React, { useState } from 'react';
 import { RouteComponentProps } from '@reach/router';
+import { usePageViewEvent } from '../../../lib/metrics';
 
-export const PageRecoveryKeyCreate = (_: RouteComponentProps) => {
-  return <>Hello I'm the new Recovery Key Create Page</>;
+export const PageRecoveryKeyCreate = (props: RouteComponentProps) => {
+  const [currentStep, setCurrentStep] = useState(1);
+  usePageViewEvent('settings.account-recovery');
+
+  /*
+    The content here will obviously be replaced as we complete the separate views for this flow. This page will use the same pattern as the example wizard in storybook for `FlowContainer`. All steps will be separate components which use the `FlowContainer` and accept `currentStep` and `setCurrentStep` as the props necessary to move the user through the flow.
+  */
+  return (
+    <>
+      {
+        // Create an account recovery key
+        currentStep === 1 && (
+          <>
+            <p>first step</p>
+            <button
+              className="cta-primary cta-base-p mx-2 flex-1"
+              type="button"
+              onClick={() => {
+                setCurrentStep(currentStep + 1);
+              }}
+            >
+              click to move to next view
+            </button>
+          </>
+        )
+      }
+      {
+        // Enter your password again to get started
+        currentStep === 2 && <p>second step</p>
+      }
+    </>
+  );
 };
 
 export default PageRecoveryKeyCreate;
