8caac15b1dc93453cdf240a02da75b747c839a10
Peter deHaan <peter@deseloper.com>
2023-05-02 15:38:01 -0700

    chore(copyright): Add missing copyright headers to fxa-admin-server

--- packages/fxa-admin-server/src/auth/auth-header.guard.ts
+++ packages/fxa-admin-server/src/auth/auth-header.guard.ts
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { GqlExecutionContext } from '@nestjs/graphql';
--- packages/fxa-admin-server/src/database/database.module.ts
+++ packages/fxa-admin-server/src/database/database.module.ts
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 import { Module } from '@nestjs/common';
 import { MetricsFactory } from 'fxa-shared/nestjs/metrics.service';
 import { DatabaseService } from './database.service';
