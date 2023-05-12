168e46efcef7baff228e33bcf60268d449fcfa4a
Reino Muhl <reino.muhl@gmail.com>
2023-05-12 15:01:52 -0400

    task(auth): include metricsUid if artifacts not set
    Because:
    - If events.emit data includes uid it should be included on
      request.auth.artifacts, as metricsUid, even if the artifacts property
      was initial null.
    
    This Commit:
    - Sets metricsUid on request.auth.artifacts, even if the artifacts
      property was initially null.
    
    Closes FXA-6967

--- packages/fxa-auth-server/lib/metrics/events.js
+++ packages/fxa-auth-server/lib/metrics/events.js
@@ -104,8 +104,11 @@ module.exports = (log, config) => {
       // eslint-disable-next-line @typescript-eslint/no-this-alias
       const request = this;
 
-      if (data && data.uid && request.auth && request.auth.artifacts) {
-        request.auth.artifacts.metricsUid = data.uid;
+      if (data && data.uid && request.auth) {
+        request.auth.artifacts = {
+          ...request.auth.artifacts,
+          metricsUid: data.uid,
+        };
       }
 
       const isMetricsEnabled = await request.app.isMetricsEnabled;
