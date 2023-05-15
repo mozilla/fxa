a02c0f606567fac94b577235383477d12e9be753
Ankita Shrivastava <ashrivastava@mozilla.com>
2023-05-15 10:57:13 -0400

    test(cleanup): remove all the migrated tests from old suites

--- packages/fxa-content-server/tests/functional.js
+++ packages/fxa-content-server/tests/functional.js
@@ -4,41 +4,21 @@
 
 const testsSettings = require('./functional_settings');
 module.exports = testsSettings.concat([
-  'tests/functional/fx_browser_relier.js',
   'tests/functional/oauth_webchannel.js',
-  'tests/functional/oauth_require_totp.js',
+
   // new and flaky tests above here',
-  'tests/functional/500.js',
-  'tests/functional/back_button_after_start.js',
-  // 'tests/functional/cookies_disabled.js',
-  'tests/functional/email_domain_mx_validation.js',
   'tests/functional/email_opt_in.js',
-  'tests/functional/force_auth.js',
   'tests/functional/fx_desktop_handshake.js',
-  'tests/functional/fx_ios_v1_sign_in.js',
-  'tests/functional/fx_ios_v1_sign_up.js',
   'tests/functional/oauth_permissions.js',
   'tests/functional/oauth_prompt_none.js',
-  'tests/functional/oauth_query_param_validation.js',
-  'tests/functional/oauth_reset_password.js',
-  'tests/functional/oauth_settings_clients.js',
   'tests/functional/oauth_sign_up.js',
-  'tests/functional/oauth_sync_sign_in.js',
   'tests/functional/pages.js',
-  'tests/functional/post_verify/newsletters.js',
   'tests/functional/post_verify/force_password_change.js',
   'tests/functional/post_verify/account_recovery.js',
   'tests/functional/post_verify/secondary_email.js',
-  'tests/functional/push.js',
-  'tests/functional/refreshes_metrics.js',
-  'tests/functional/robots_txt.js',
   'tests/functional/sign_in.js',
   'tests/functional/sign_in_blocked.js',
-  'tests/functional/sign_in_cached.js',
-  'tests/functional/sign_up.js',
-  'tests/functional/sync_v3_sign_in.js',
   'tests/functional/sync_v3_sign_up.js',
-  'tests/functional/sync_v3_reset_password.js',
   'tests/functional/sync_v3_settings.js',
 
   // Disabled because of https://github.com/mozilla/fxa/issues/9863
@@ -54,6 +34,17 @@ module.exports = testsSettings.concat([
   // 'tests/functional/fx_firstrun_v2.js',
   // 'tests/functional/tos.js',
   // 'tests/functional/confirm.js',
+  //'tests/functional/fx_ios_v1_sign_in.js',
+  //'tests/functional/fx_ios_v1_sign_up.js',
+  //'tests/functional/oauth_query_param_validation.js',
+  //'tests/functional/refreshes_metrics.js',
+  //'tests/functional/fx_browser_relier.js',
+  //'tests/functional/500.js',
+  //'tests/functional/email_domain_mx_validation.js',
+  //'tests/functional/post_verify/newsletters.js',
+  //''tests/functional/robots_txt.js',
+  //'tests/functional/push.js',
+  //''tests/functional/back_button_after_start.js',
 
   // Disabled because this test migrated to Playwright
   // See `/functional-test`
@@ -75,7 +66,17 @@ module.exports = testsSettings.concat([
   //'tests/functional/sign_up_with_code.js',
   //'tests/functional/sync_v3_email_first.js',
   // 'tests/functional/reset_password.js',
+  //'tests/functional/oauth_reset_password.js',
   // 'tests/functional/legal.js',
+  //'tests/functional/sign_in_cached.js',
+  // 'tests/functional/cookies_disabled.js',
+  //'tests/functional/sign_up.js',
+  //'tests/functional/oauth_settings_clients.js',
+  //'tests/functional/oauth_require_totp.js',
+  //'tests/functional/force_auth.js',
+  //'tests/functional/sync_v3_sign_in.js',
+  //'tests/functional/sync_v3_reset_password.js',
+  //'tests/functional/oauth_sync_sign_in.js',
 ]);
 
 // Mocha tests are only exposed during local dev, not on prod-like
