46496b5b27b32838e5ce34377fa715842b431d26
dschom <dschomburg@mozilla.com>
2023-05-23 17:14:30 -0700

    task(CI): Make smoke test URLs configurable
    Because:
    - We want to try running smoke tests against a new environment
    
    This Commit:
    - Adds pipeline parameters for configuring standard domains
    - Transfers pipeline parameters to a set of standard envs
    - Updates playwright tests to use these envs
    - Updates functional content server tests to use these envs

--- packages/functional-tests/lib/targets/production.ts
+++ packages/functional-tests/lib/targets/production.ts
@@ -1,14 +1,23 @@
 import { TargetName } from '.';
 import { RemoteTarget } from './remote';
 
+const ACCOUNTS_DOMAIN = process.env.ACCOUNTS_DOMAIN || 'accounts.firefox.com';
+const ACCOUNTS_API_DOMAIN =
+  process.env.ACCOUNTS_API_DOMAIN || 'api.accounts.firefox.com';
+const PAYMENTS_DOMAIN =
+  process.env.PAYMENTS_DOMAIN || 'subscriptions.firefox.com';
+const RELIER_DOMAIN =
+  process.env.RELIER_DOMAIN || 'production-123done.herokuapp.com';
+
 export class ProductionTarget extends RemoteTarget {
   static readonly target = 'production';
   readonly name: TargetName = ProductionTarget.target;
-  readonly contentServerUrl = 'https://accounts.firefox.com';
-  readonly paymentsServerUrl = 'https://subscriptions.firefox.com';
-  readonly relierUrl = 'https://production-123done.herokuapp.com/';
+
+  readonly contentServerUrl = `https://${ACCOUNTS_DOMAIN}`;
+  readonly paymentsServerUrl = `https://${PAYMENTS_DOMAIN}`;
+  readonly relierUrl = `https://${RELIER_DOMAIN}`;
 
   constructor() {
-    super('https://api.accounts.firefox.com');
+    super(`https://${ACCOUNTS_API_DOMAIN}`);
   }
 }
--- packages/functional-tests/lib/targets/stage.ts
+++ packages/functional-tests/lib/targets/stage.ts
@@ -1,15 +1,23 @@
 import { TargetName } from '.';
 import { RemoteTarget } from './remote';
 
+const ACCOUNTS_DOMAIN =
+  process.env.ACCOUNTS_DOMAIN || 'accounts.stage.mozaws.net';
+const ACCOUNTS_API_DOMAIN =
+  process.env.ACCOUNTS_API_DOMAIN || 'api-accounts.stage.mozaws.net';
+const PAYMENTS_DOMAIN =
+  process.env.PAYMENTS_DOMAIN || 'payments.stage.mozaws.net';
+const RELIER_DOMAIN =
+  process.env.RELIER_DOMAIN || 'stage-123done.herokuapp.com';
+
 export class StageTarget extends RemoteTarget {
   static readonly target = 'stage';
   readonly name: TargetName = StageTarget.target;
-  readonly contentServerUrl = 'https://accounts.stage.mozaws.net';
-  readonly paymentsServerUrl =
-    'https://payments-stage.fxa.nonprod.cloudops.mozgcp.net';
-  readonly relierUrl = 'https://stage-123done.herokuapp.com';
+  readonly contentServerUrl = `https://${ACCOUNTS_DOMAIN}`;
+  readonly paymentsServerUrl = `https://${PAYMENTS_DOMAIN}`;
+  readonly relierUrl = `https://${RELIER_DOMAIN}`;
 
   constructor() {
-    super('https://api-accounts.stage.mozaws.net');
+    super(`https://${ACCOUNTS_API_DOMAIN}`);
   }
 }
