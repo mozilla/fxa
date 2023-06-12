1532daa5e69e31dfd970c3738f76b44a1d974727
Valerie Pomerleau <vpomerleau@mozilla.com>
2023-06-12 15:59:46 -0700

    task(fxa-settings): Revert localized content in key download file
    Because:
    
    * Localized plain text file is not rendering as expected on Android and Windows.
    * Hidden directionality characters get copied with key, resulting in the key being marked as invalid during password reset.
    * Reversal is temporary to prevent this issue from blocking deployment while next steps are determined.
    
    This commit:
    
    * Serve key only in the file download.
    * Add an optional query param to test downloading a detailed file 1) with BOM (byte-order-mark), 2) with charset defined, or 3) with TextEncoder
    * Add stories to download the 3 test options
    
    Closes #FXA-7688

--- packages/fxa-settings/src/components/ButtonDownloadRecoveryKey/index.stories.tsx
+++ packages/fxa-settings/src/components/ButtonDownloadRecoveryKey/index.stories.tsx
@@ -5,7 +5,7 @@
 import React from 'react';
 import { Meta } from '@storybook/react';
 import AppLayout from '../AppLayout';
-import ButtonDownloadRecoveryKey from '.';
+import ButtonDownloadRecoveryKey, { fileContentVariation } from '.';
 import { withLocalization } from '../../../.storybook/decorators';
 import { Account, AppContext } from '../../models';
 import { MOCK_ACCOUNT, mockAppContext } from '../../models/mocks';
@@ -28,12 +28,16 @@ const accountWithLongEmail = {
   },
 } as unknown as Account;
 
-const storyWithContext = (account: Account) => {
+const storyWithContext = (
+  account: Account,
+  fileType?: fileContentVariation
+) => {
   const story = () => (
     <AppContext.Provider value={mockAppContext({ account })}>
       <AppLayout>
-        {' '}
-        <ButtonDownloadRecoveryKey {...{ recoveryKeyValue, viewName }} />
+        <ButtonDownloadRecoveryKey
+          {...{ recoveryKeyValue, viewName, fileType }}
+        />
       </AppLayout>
     </AppContext.Provider>
   );
@@ -43,3 +47,19 @@ const storyWithContext = (account: Account) => {
 export const Default = storyWithContext(account);
 
 export const WithVeryLongEmail = storyWithContext(accountWithLongEmail);
+
+// The following stories are added for testing purposes
+export const DetailedDownloadWithBOM = storyWithContext(
+  account,
+  fileContentVariation['withBOM']
+);
+
+export const DetailedDownloadWithCharSet = storyWithContext(
+  account,
+  fileContentVariation['withCharSet']
+);
+
+export const DetailedDownloadWithTextEncoder = storyWithContext(
+  account,
+  fileContentVariation['withTextEncoder']
+);
