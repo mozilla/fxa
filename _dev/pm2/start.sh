8a4ed3350a5fe2a162b76171882c5faf512eea6d
Reino Muhl <reino.muhl@gmail.com>
2023-07-20 10:07:46 -0400

    feat(infra): wait for db patches on startup
    Because:
    
    * On stack startup, services start before mysql patches have completed,
      causing profile-server to fail on startup.
    
    This commit:
    
    * Adds script to check if patcher script has started and is still
      running.
    
    Closes #

--- _dev/pm2/start.sh
+++ _dev/pm2/start.sh
@@ -16,3 +16,6 @@ echo "waiting for containers to start"
 _scripts/check-url.sh localhost:4100/health
 _scripts/check-url.sh localhost:9299/api/config
 _scripts/check-mysql.sh
+
+echo "waiting for DB patches"
+_scripts/check-db-patcher.sh
