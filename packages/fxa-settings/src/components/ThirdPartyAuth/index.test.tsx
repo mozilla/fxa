e6ece61ec9a64303e443567ea5c1464ca68a079b
dschom <dschomburg@mozilla.com>
2023-08-11 11:29:42 -0700

    task(settings): Use React Form component to submit google / apple auth form
    Because:
    - We wanted the code in the third party auth to use react conventions
    
    This Commit:
    - Creates a third party sign in form that is rendered.
    - Submits this form when click 'continueWith' buttons.

--- packages/fxa-settings/src/components/ThirdPartyAuth/index.test.tsx
+++ packages/fxa-settings/src/components/ThirdPartyAuth/index.test.tsx
@@ -12,10 +12,105 @@ function renderWith(props?: any) {
   return renderWithLocalizationProvider(<Subject {...props} />);
 }
 
-describe('ThirdPartyAuth component', () => {
-  it('renders as expected', async () => {
-    renderWith({ enabled: true });
-    expect(screen.getByText('Continue with Google')).toBeInTheDocument();
-    expect(screen.getByText('Continue with Apple')).toBeInTheDocument();
+const mockFormSubmit = jest.fn();
+
+describe('ThirdPartyAuthComponent', () => {
+  // Form submission not supported in jest test. Instead, prevent the submission
+  // and illustrate a 'short circuit' operation.
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    return false;
+  };
+  const onContinueWithApple = jest.fn().mockImplementation(onSubmit);
+  const onContinueWithGoogle = jest.fn().mockImplementation(onSubmit);
+
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it('renders', async () => {
+    renderWith({
+      enabled: true,
+    });
+
+    await screen.findByText('Continue with Google');
+    await screen.findByText('Continue with Apple');
+    expect(mockFormSubmit).not.toBeCalled();
+
+    expect(
+      (await screen.findByTestId('google-signin-form-state')).getAttribute(
+        'value'
+      )
+    ).toEqual('');
+
+    expect(
+      (await screen.findByTestId('apple-signin-form-state')).getAttribute(
+        'value'
+      )
+    ).toEqual('');
+  });
+
+  it('submits apple form', async () => {
+    renderWith({
+      enabled: true,
+      showSeparator: true,
+      onContinueWithApple,
+      onContinueWithGoogle,
+    });
+
+    const button = await screen.findByText('Continue with Apple');
+    button.click();
+
+    // Ensure the hidden input for state was updated.
+    expect(
+      (await screen.findByTestId('apple-signin-form-state')).getAttribute(
+        'value'
+      )
+    ).not.toEqual('');
+    expect(onContinueWithApple).toBeCalled();
+    expect(onContinueWithGoogle).not.toBeCalled();
+  });
+
+  it('submits google form', async () => {
+    renderWith({
+      enabled: true,
+      showSeparator: true,
+      onContinueWithApple,
+      onContinueWithGoogle,
+    });
+
+    const button = await screen.findByText('Continue with Google');
+    button.click();
+
+    // Ensure the hidden input for state was updated.
+    expect(
+      (await screen.findByTestId('google-signin-form-state')).getAttribute(
+        'value'
+      )
+    ).not.toEqual('');
+    expect(onContinueWithGoogle).toBeCalled();
+    expect(onContinueWithApple).not.toBeCalled();
+  });
+
+  it('hides separator', async () => {
+    renderWith({
+      enabled: true,
+      showSeparator: false,
+      onContinueWithApple,
+      onContinueWithGoogle,
+    });
+
+    expect(screen.queryByText('Or')).toBeNull();
+  });
+
+  it('shows separator', async () => {
+    renderWith({
+      enabled: true,
+      showSeparator: true,
+      onContinueWithApple,
+      onContinueWithGoogle,
+    });
+
+    expect(screen.queryByText('Or')).toBeDefined();
   });
 });
