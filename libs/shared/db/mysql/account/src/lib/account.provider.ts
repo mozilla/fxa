278c47ea2982352fe29fb09fbf776578533f50d8
Ben Bangert <ben@groovie.org>
2023-09-14 08:54:13 -0700

    feat: add nestJS service loading to payments-next
    Because:
    
    * We want to be able to call server-side logic with our business classes
      directly from the nextJS app before rendering on the server.
    
    This pull request:
    
    * Adds a NestJS application context to the NextJS app that can be used
      to load services and call methods on them.

---
+++ libs/shared/db/mysql/account/src/lib/account.provider.ts
@@ -0,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import { Provider } from '@nestjs/common';
+
+import { MySQLConfig } from '../../../core/src';
+import {
+  AccountDatabase,
+  AccountDbProvider,
+  setupAccountDatabase,
+} from './setup';
+
+export const AccountDatabaseFactory: Provider<AccountDatabase> = {
+  provide: AccountDbProvider,
+  useFactory: (mysqlConfig: MySQLConfig) => {
+    return setupAccountDatabase(mysqlConfig);
+  },
+  inject: [MySQLConfig],
+};
