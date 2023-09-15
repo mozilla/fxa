d712394f15e1dfc7ca6f727ced98993d105e9f49
Valerie Pomerleau <vpomerleau@mozilla.com>
2023-09-14 17:29:19 -0700

    fix(l10n): Revert file merge in fxa-settings grunt file
    Because:
    
    * Merging the branding.ftl and react.ftl files into the settings.ftl file (to allow new brand terms to be visible in local development) was resulting in string duplication during l10n extract
    
    This commit:
    
    * Reverts the changes to fxa-settings gruntfile

--- packages/fxa-settings/Gruntfile.js
+++ packages/fxa-settings/Gruntfile.js
@@ -3,12 +3,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 module.exports = function (grunt) {
-  const srcPaths = [
-    '../fxa-shared/l10n/branding.ftl',
-    '.license.header',
-    '../fxa-react/components/**/*.ftl',
-    'src/**/*.ftl',
-  ];
+  const srcPaths = ['.license.header', 'src/**/*.ftl'];
   const testPaths = [
     '../fxa-shared/l10n/branding.ftl',
     '../fxa-react/components/**/*.ftl',
