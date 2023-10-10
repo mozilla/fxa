c01a146632a84e2cd1637863c464a311cbd8fbe8
Ankita Shrivastava <ashrivastava@mozilla.com>
2023-10-10 17:00:50 -0400

    testing

--- packages/functional-tests/playwright.config.ts
+++ packages/functional-tests/playwright.config.ts
@@ -19,7 +19,8 @@ if (CI) {
   // Overall maxFailures is now dependent on the number of retries, workers
   retries = 3;
   workers = 2;
-  maxFailures = retries * workers * 2;
+  //maxFailures = retries * workers * 2;
+  maxFailures = 0;
 }
 
 const config: PlaywrightTestConfig<TestOptions, WorkerOptions> = {
