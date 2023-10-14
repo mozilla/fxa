91a6add875c7eabb296c4c1f31a6e8a0c5d73259
dschom <dschomburg@mozilla.com>
2023-10-13 18:17:39 -0700

    Squash: Update icons in storybooks

--- packages/fxa-settings/.storybook/design-guide/Header.tsx
+++ packages/fxa-settings/.storybook/design-guide/Header.tsx
@@ -5,7 +5,11 @@ const Header = () => {
   return (
     <div className="bg-grey-50 flex items-center justify-between sticky z-20 top-0 py-4 pb-3 border-b border-grey-400/25 px-2 -mt-4 -ml-12 -mr-12">
       <div className="flex items-center gap-2">
-        <img src="assets/logo.svg" alt="firefox account logo" />
+        <img
+          className="h-12"
+          src="assets/moz-m-logo.svg"
+          alt="firefox account logo"
+        />
         <h1 className="text-xl font-bold text-grey-900">Design Guide</h1>
       </div>
       <div>
