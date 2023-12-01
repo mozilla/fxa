33a802c1bd9712e399886daf75f2ae5349f738f0
Lauren Zugai <lauren@zugai.com>
2023-12-01 16:49:07 -0600

    chore(script): Rework config-fxios script for iOS testing, remove replace-in-file
    Because:
    * The script has been broken since firefox-ios moved to Rust components and we want to do local iOS testing
    
    This commit:
    * Reworks the script that replaced several strings containing our endpoints to change a parameter used to call FxAConfig in the firefox-ios directory instead. On the Rust side we're now checking against the well-known file for these endpoints instead of using hard-coded values
    * Removes replace-in-file in favor of fs since while this approach is still not robust, the new regex checks for a FxAConfig function call and replaces a specific parameter and replace-in-file does not seem to support regex/partial replacements like this
    
    Closes FXA-2195

--- _scripts/config-fxios.js
+++ _scripts/config-fxios.js
@@ -1,53 +1,47 @@
 #!/usr/bin/env node
 
-var path = require("path");
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-var r = require("replace-in-file");
+const path = require('path');
+const fs = require('fs');
 
-var ios_path = process.env.FIREFOX_IOS_HOME;
-
-if (!ios_path) {
-  throw new Error("FIREFOX_IOS_HOME is not set");
+const iosPath = process.env.FIREFOX_IOS_HOME;
+if (!iosPath) {
+  throw new Error('FIREFOX_IOS_HOME is not set');
 }
 
-var config_path = path.join(
-  ios_path,
-  "Account",
-  "FirefoxAccountConfiguration.swift"
-);
+// This matches lines containing `FxAConfig(` with a `server: server` param.
+// We want to change this so `FxAConfig` is called with our `contentUrl`.
+const regex = /FxAConfig\([^)]*server: server[^)]*\)/g;
+const replaceFrom = /\bserver: server\b/;
+const replaceTo = 'contentUrl: "http://localhost:3030"';
 
-function replace(options) {
-  return new Promise(function(resolve, reject) {
-    r(options, function(err) {
-      if (err) reject(err);
-      resolve();
-    });
-  });
+function replace() {
+  const filePath = path.join(iosPath, 'RustFxA', 'RustFirefoxAccounts.swift');
+  let fileContent = fs.readFileSync(filePath, 'utf8');
+  const match = fileContent.match(regex);
+
+  if (match) {
+    const replaced = match[0].replace(replaceFrom, replaceTo);
+    fileContent = fileContent.replace(match[0], replaced);
+
+    fs.writeFileSync(filePath, fileContent, 'utf8');
+    console.log(
+      '✅ Successfully updated the firefox-ios FxAConfig to use localhost.'
+    );
+  } else {
+    if (fileContent.includes(replaceTo)) {
+      console.log(
+        'The firefox-ios FxAConfig was already updated to use localhost.'
+      );
+    } else {
+      throw new Error(
+        `Did not find the \`server\` parameter in any \`FxAConfig\` call to replace. Check ${filePath} - the code on the firefox-ios side may have changed.`
+      );
+    }
+  }
 }
 
-return replace({
-  files: config_path,
-  replace: /https:\/\/accounts.firefox.com/g,
-  with: "http://localhost:3030"
-})
-  .then(function() {
-    return replace({
-      files: config_path,
-      replace: "https://api.accounts.firefox.com/v1",
-      with: "http://localhost:9000/v1"
-    });
-  })
-  .then(function() {
-    return replace({
-      files: config_path,
-      replace: "https://oauth.accounts.firefox.com/v1",
-      with: "http://localhost:9010/v1"
-    });
-  })
-  .then(function() {
-    return replace({
-      files: config_path,
-      replace: "https://profile.accounts.firefox.com/v1",
-      with: "http://localhost:1111/v1"
-    });
-  });
+replace();
