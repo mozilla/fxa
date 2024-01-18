5c3b317b2eb22daa63ba9747dbf5646dd95db973
Julian Poyourow <julian@poyourow.com>
2024-01-18 10:39:13 -0800

    now server side component

--- libs/payments/ui/src/lib/client/ServerActionTest.tsx
+++ libs/payments/ui/src/lib/client/ServerActionTest.tsx
@@ -1,57 +1,19 @@
-'use client';
+'use server';
 
-import { useState } from 'react';
 import { getRandomNumberAction } from '../server/actions/getRandomNumberAction';
 
-export function ServerActionTest() {
-  const [loading, setLoading] = useState(false);
-  const [min, setMin] = useState(0);
-  const [max, setMax] = useState(10);
-  const [serverRandomValue, setServerRandomValue] = useState<
-    number | undefined
-  >();
-
-  const callServer = () => {
-    setLoading(true);
-    getRandomNumberAction(min, max)
-      .then((result) => {
-        setServerRandomValue(result.randomNumber);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
-  };
-
+export async function ServerActionTest() {
   return (
-    <div>
-      Random value from server is: {serverRandomValue}
-      <br />
-      <br />
-      <b>Min:</b>
-      <br />
-      <input
-        value={min}
-        onChange={(event) => setMin(parseInt(event.target.value))}
-        disabled={loading}
-        type="number"
-      ></input>
+    <form action={getRandomNumberAction}>
+      <input defaultValue="0" name="min" type="number"></input>
       <br />
       <br />
       <b>Max:</b>
       <br />
-      <input
-        value={max}
-        onChange={(event) => setMax(parseInt(event.target.value))}
-        disabled={loading}
-        type="number"
-      ></input>
+      <input defaultValue="10" name="max" type="number"></input>
       <br />
       <br />
-      <button onClick={callServer} disabled={loading}>
-        Submit Test
-      </button>
-      <br />
-      {loading && <>Loading...</>}
-    </div>
+      <button type="submit">Submit Test</button>
+    </form>
   );
 }
--- libs/payments/ui/src/lib/server/actions/getRandomNumberAction.ts
+++ libs/payments/ui/src/lib/server/actions/getRandomNumberAction.ts
@@ -6,11 +6,18 @@ const waitFor = (ms: number) => {
   });
 };
 
-export async function getRandomNumberAction(min: number, max: number) {
-  console.log('Received data from client', min, max);
+export async function getRandomNumberAction(formData: FormData) {
+  console.log(
+    'Received data from client',
+    formData.get('min'),
+    formData.get('max')
+  );
 
   // It takes a really long time to generate this super secure random number...
   await waitFor(2000);
+
+  const min = formData.get('min') as unknown as number;
+  const max = formData.get('max') as unknown as number;
 
   const random = Math.floor(Math.random() * (max - min + 1) + min);
 
