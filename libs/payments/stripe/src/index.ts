ff7413d833c6fde6769fd6b2f8f3ba034382e714
Lisa Chan <lchan@mozilla.com>
2024-02-12 13:44:40 -0500

    feat(payments-stripe): Create StripeManager

--- libs/payments/stripe/src/index.ts
+++ libs/payments/stripe/src/index.ts
@@ -8,3 +8,4 @@ export { ProductFactory } from './lib/factories/product.factory';
 export { SubscriptionItemFactory } from './lib/factories/subscription-item.factory';
 export { SubscriptionFactory } from './lib/factories/subscription.factory';
 export * from './lib/stripe.client';
+export * from './lib/stripe.manager';
