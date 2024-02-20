3ab897e629455218329c22f5bb97713396a8395d
Reino Muhl <reino.muhl@gmail.com>
2024-02-20 07:48:50 -0500

    feat(next): add NextAuth Because:
    * Need to be able to authenticate with FxA
    
    This commit:
    
    * Add NextAuth to facilitate promp and no prompt signin with FxA.
    
    Closes #FXA-7522

--- apps/payments/next/app/[offeringId]/checkout/page.tsx
+++ apps/payments/next/app/[offeringId]/checkout/page.tsx
@@ -2,6 +2,7 @@ import { PurchaseDetails, TermsAndPrivacy } from '@fxa/payments/ui/server';
 
 import { getCartData, getContentfulContent } from '../../_lib/apiClient';
 import { app } from '../../_nestapp/app';
+import { auth, signIn, signOut } from 'apps/payments/next/auth';
 
 interface CheckoutParams {
   offeringId: string;
@@ -23,6 +24,7 @@ export default async function Index({ params }: { params: CheckoutParams }) {
   const [contentful, cart] = await Promise.all([contentfulData, cartData]);
   /* eslint-disable @typescript-eslint/no-unused-vars */
   const cartService = await app.getCartService();
+  const session = await auth();
 
   return (
     <>
@@ -43,7 +45,48 @@ export default async function Index({ params }: { params: CheckoutParams }) {
           className="h-[640px] flex items-center justify-center"
           aria-label="Section under construction"
         >
-          Section Under Construction
+          {/*
+            Temporary section to test NextAuth prompt/no prompt signin
+            To be deleted as part of FXA-7521/FXA-7523 if not sooner where necessary
+          */}
+          {!session ? (
+            <div className="flex flex-col gap-4">
+              <form
+                action={async () => {
+                  'use server';
+                  await signIn('fxa');
+                }}
+              >
+                <button className="flex items-center justify-center bg-blue-500 font-semibold h-12 rounded-md text-white w-full p-4">
+                  <div className="block">Sign In - Login</div>
+                </button>
+              </form>
+              <form
+                action={async () => {
+                  'use server';
+                  await signIn('fxa', undefined, { prompt: 'none' });
+                }}
+              >
+                <button className="flex items-center justify-center bg-blue-500 font-semibold h-12 rounded-md text-white w-full p-4">
+                  <div className="block">Sign In - No Prompt</div>
+                </button>
+              </form>
+            </div>
+          ) : (
+            <div className="flex flex-col items-center gap-4">
+              <p>Hello {session?.user?.id}</p>
+              <form
+                action={async () => {
+                  'use server';
+                  await signOut();
+                }}
+              >
+                <button className="flex items-center justify-center bg-blue-500 font-semibold h-12 rounded-md text-white w-full p-4">
+                  <div className="block">Sign Out</div>
+                </button>
+              </form>
+            </div>
+          )}
         </section>
 
         <TermsAndPrivacy
