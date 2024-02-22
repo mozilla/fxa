d007ec9354e2a797a16da05f31ed61c663fded30
Lisa Chan <lchan@mozilla.com>
2024-02-22 09:02:25 -0500

    feat(payments-stripe): Create StripeManager isCustomerStripeTaxEligible

--- libs/payments/stripe/src/lib/stripe.manager.ts
+++ libs/payments/stripe/src/lib/stripe.manager.ts
@@ -3,9 +3,22 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import { Injectable } from '@nestjs/common';
+import { Stripe } from 'stripe';
 import { StripeClient } from './stripe.client';
 
 @Injectable()
 export class StripeManager {
   constructor(private client: StripeClient) {}
+
+  async isCustomerStripeTaxEligible(customer: Stripe.Customer) {
+    if (!customer.tax) {
+      // TODO: FXA-8891
+      throw new Error('customer.tax is not present');
+    }
+
+    return (
+      customer.tax?.automatic_tax === 'supported' ||
+      customer.tax?.automatic_tax === 'not_collecting'
+    );
+  }
 }
