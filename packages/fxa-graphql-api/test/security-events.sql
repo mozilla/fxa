f038bde0d4d4028b9ce6f14d2a2addbf6150f92a
dschom <dschomburg@mozilla.com>
2024-03-12 12:01:16 -0700

    bug(graphql): Fix mismatch in test state
    Because:
    - We could get in state where tests would fail
    
    This Commit
    - Makes sure the test database is inline with the latest state

--- packages/fxa-graphql-api/test/security-events.sql
+++ packages/fxa-graphql-api/test/security-events.sql
@@ -5,6 +5,7 @@ CREATE TABLE `securityEvents` (
   `ipAddrHmac` binary(32) NOT NULL,
   `createdAt` bigint NOT NULL,
   `tokenVerificationId` binary(16) DEFAULT NULL,
+  `ipAddr` VARCHAR(39) DEFAULT NULL,
   KEY `nameId` (`nameId`),
   KEY `securityEvents_uid_tokenVerificationId` (`uid`,`tokenVerificationId`),
   KEY `securityEvents_uid_ipAddrHmac_createdAt` (`uid`,`ipAddrHmac`,`createdAt`),
