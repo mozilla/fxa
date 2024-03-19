5526cb96900b97c151ea8f986a3ecfdcc84add8f
Meghan Sardesai <msardesai@mozilla.com>
2024-03-19 17:35:01 -0400

    feat(payments-next): add `LocalizationProvider` wrapper client Because:
    * We want to localize all copy in `payments-next`.
    
    This commit:
    
    * Wraps `children` in Fluent `<LocalizationProvider>`.
    * Adds to `Checkout` page as a means to test the client component, and that localization works as expected.
    * Adds `getBundleGenerator` to l10n.ts.
    
    Closes FXA-7827

--- apps/payments/next/app/layout.tsx
+++ apps/payments/next/app/layout.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 // TODO - Move shared SVGs to a shared library in fxa/libs/shared/*
 import mozillaLogo from '../images/moz-logo-bw-rgb.svg';
 import './styles/global.css';
+import { Providers } from './providers';
 
 // TODO - Replace these placeholders as part of FXA-8227
 export const metadata = {
@@ -36,7 +37,7 @@ export default function RootLayout({
           />
         </header>
         <main className="mt-16 mx-7 min-h-[calc(100vh_-_4rem)] tablet:grid tablet:grid-cols-[minmax(min-content,500px)_minmax(20rem,1fr)] tablet:grid-rows-[min-content] tablet:gap-x-8 tablet:mt-20 tablet:mb-auto desktop:grid-cols-[600px_1fr]">
-          {children}
+          <Providers>{children}</Providers>
         </main>
       </body>
     </html>
---
+++ apps/payments/next/app/providers/index.tsx
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { ReactLocalization, LocalizationProvider } from '@fluent/react';
+import {
+  LocalizerClient,
+  LocalizerBindingsClient,
+} from '@fxa/shared/l10n/client';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [l10n, setL10n] = useState<ReactLocalization>();
+
+  useEffect(() => {
+    const setLocalization = async () => {
+      const locale = navigator.language;
+      const bindings = new LocalizerBindingsClient();
+      const localizerClient = new LocalizerClient(bindings);
+      const { l10n } = await localizerClient.setupReactLocalization(locale);
+
+      setL10n(l10n);
+    };
+
+    setLocalization();
+  }, []);
+
+  return (
+    <>
+      {l10n ? (
+        <LocalizationProvider l10n={l10n}>{children}</LocalizationProvider>
+      ) : (
+        <>{children}</>
+      )}
+    </>
+  );
+}
