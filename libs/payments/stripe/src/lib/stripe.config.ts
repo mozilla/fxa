cbd8ddd86183dc7e66df3994f4e28ecc2e70a076
Reino Muhl <reino.muhl@gmail.com>
2024-04-24 14:58:09 -0400

    fix(next): add missing deps to nestapp Because:
    * Payments Next errors on start up since NestApp had missing modules.
    
    This commit:
    
    * Adds missing modules and config to NestApp.
    
    Closes #

--- libs/payments/stripe/src/lib/stripe.config.ts
+++ libs/payments/stripe/src/lib/stripe.config.ts
@@ -2,12 +2,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+import { Transform } from 'class-transformer';
 import { IsObject, IsString } from 'class-validator';
 
 export class StripeConfig {
   @IsString()
   public readonly apiKey!: string;
 
+  @Transform(
+    ({ value }) => (value instanceof Object ? value : JSON.parse(value)),
+    { toClassOnly: true }
+  )
   @IsObject()
   public readonly taxIds!: { [key: string]: string };
 }
