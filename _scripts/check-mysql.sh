43cb4a0e5a3022b082b440865da81f4e3559ec77
Tarik Eshaq <teshaq@mozilla.com>
2024-04-25 17:12:37 -0400

    bug: Use docker command to check if mysql db is alive

--- _scripts/check-mysql.sh
+++ _scripts/check-mysql.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -e
-
-PORT=${1:-3306}
-HOST=${2:-localhost}
 RETRY=60
+CONTAINER_NAME="mydb"
+
 for i in $(eval echo "{1..$RETRY}"); do
-  if echo PING | nc "$HOST" "$PORT" | grep -q 'mysql'; then
+  if DOCKER_CLI_HINTS=false docker exec -it $CONTAINER_NAME /bin/bash -c 'mysqladmin ping' > /dev/null; then
+    echo "MySQL DB is ready"
     exit 0
   else
     if [ "$i" -lt $RETRY ]; then
+      echo "MySQL DB could not be reached, trying again"
       sleep 1
     fi
   fi
 done
-
+echo "MySQL DB Could not be reached, please double check your docker setup"
 exit 1
