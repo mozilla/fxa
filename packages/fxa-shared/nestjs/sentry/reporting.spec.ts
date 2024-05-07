74c32be9e34468e38e904a8d60bbff4450d4890f
dschom <dschomburg@mozilla.com>
2024-05-07 10:26:54 -0700

    bug(graphql): Prevent reporting of known auth-server errors
    Because:
    - We were reporting known error response states that come from auth-server and were intended for flow control to Sentry
    
    This Commit:
    - Adds an extra check so we do not report these states needlessly.
    - Fixes some tests so they adhere to latest sentry types.

--- packages/fxa-shared/nestjs/sentry/reporting.spec.ts
+++ packages/fxa-shared/nestjs/sentry/reporting.spec.ts
@@ -3,13 +3,61 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 import * as uuid from 'uuid';
 
-import { filterObject } from './reporting';
+import { filterObject, isAuthServerError } from './reporting';
 
 const FILTERED = '[Filtered]';
 
 function getUid() {
   return uuid.v4().replace(/-/g, '');
 }
+
+describe('detects auth server error', () => {
+  it('flags when code and errno are present', () => {
+    const result = isAuthServerError({
+      name: 'foo',
+      message: 'bar',
+      extensions: {
+        code: 401,
+        errno: 101,
+      },
+    });
+
+    expect(result).toBeTruthy();
+  });
+
+  it('does not flag when code is missing', () => {
+    const result = isAuthServerError({
+      name: 'foo',
+      message: 'bar',
+      extensions: {
+        errno: 101,
+      },
+    });
+
+    expect(result).toBeFalsy();
+  });
+
+  it('does not flag when errno is missing', () => {
+    const result = isAuthServerError({
+      name: 'foo',
+      message: 'bar',
+      extensions: {
+        code: 500,
+      },
+    });
+
+    expect(result).toBeFalsy();
+  });
+
+  it('does not flag general errors', () => {
+    const result = isAuthServerError({
+      name: 'foo',
+      message: 'bar',
+    });
+
+    expect(result).toBeFalsy();
+  });
+});
 
 describe('filterObject', () => {
   it('should be defined', () => {
@@ -19,25 +67,7 @@ describe('filterObject', () => {
   // Test Sentry QueryParams filtering types
   it('should filter array of key/value arrays', () => {
     const input = {
-      extra: [
-        ['foo', getUid()],
-        ['baz', getUid()],
-        ['bar', 'fred'],
-      ],
-    };
-    const expected = {
-      extra: [
-        ['foo', FILTERED],
-        ['baz', FILTERED],
-        ['bar', 'fred'],
-      ],
-    };
-    const output = filterObject(input);
-    expect(output).toEqual(expected);
-  });
-
-  it('should filter an object of key/value pairs', () => {
-    const input = {
+      type: undefined,
       extra: {
         foo: getUid(),
         baz: getUid(),
@@ -45,6 +75,28 @@ describe('filterObject', () => {
       },
     };
     const expected = {
+      type: undefined,
+      extra: {
+        foo: FILTERED,
+        baz: FILTERED,
+        bar: 'fred',
+      },
+    };
+    const output = filterObject(input);
+    expect(output).toEqual(expected);
+  });
+
+  it('should filter an object of key/value pairs', () => {
+    const input = {
+      type: undefined,
+      extra: {
+        foo: getUid(),
+        baz: getUid(),
+        bar: 'fred',
+      },
+    };
+    const expected = {
+      type: undefined,
       extra: {
         foo: FILTERED,
         baz: FILTERED,
@@ -57,18 +109,20 @@ describe('filterObject', () => {
 
   it('should skip nested arrays that are not valid key/value arrays', () => {
     const input = {
-      extra: [
-        ['foo', getUid()],
-        ['bar', 'fred'],
-        ['fizz', 'buzz', 'parrot'],
-      ],
+      type: undefined,
+      extra: {
+        foo: getUid(),
+        bar: 'fred',
+        fizz: ['buzz', 'parrot'],
+      },
     };
     const expected = {
-      extra: [
-        ['foo', FILTERED],
-        ['bar', 'fred'],
-        ['fizz', 'buzz', 'parrot'],
-      ],
+      type: undefined,
+      extra: {
+        foo: FILTERED,
+        bar: 'fred',
+        fizz: ['buzz', 'parrot'],
+      },
     };
     const output = filterObject(input);
     expect(output).toEqual(expected);
