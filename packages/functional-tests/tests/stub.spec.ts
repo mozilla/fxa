fd99340dfc6ac807a552a8c8496beaed546cf6e9
Ankita Shrivastava <ashrivastava@mozilla.com>
2024-05-07 12:05:00 -0400

    remove stub.spec.ts for testing

--- packages/functional-tests/tests/stub.spec.ts
+++ packages/functional-tests/tests/stub.spec.ts
@@ -1,7 +1,6 @@
-import { test } from '../lib/fixtures/standard';
+// import { test } from '../lib/fixtures/standard';
 
-test('stub', async ({ page, testAccountTracker }) => {
-  const credentials = await testAccountTracker.signUp();
-  console.log(credentials);
-  await page.pause();
-});
+// test('stub', async ({ credentials, page }) => {
+//   console.log(credentials);
+//   await page.pause();
+// });
