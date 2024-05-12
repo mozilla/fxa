6aff7187ff05e666dd8d847c744208689d496601
Meghan Sardesai <msardesai@mozilla.com>
2024-05-12 00:12:20 -0700

    feat(cart): add util function for cart handleEligibilityStatus Because:
    * We want to create a util function to handle the EligibilityStatus and ensure that the Cart is created with the correct CartEligibilityStatus, CartState and ErrorReasonId (if necessary).
    
    This commit:
    
    * Creates a util function in libs/payments/cart to handle the EligibilityStatus to ensure that the Cart is created with the correct CartEligibilityStatus, CartState and ErrorReasonId (if necessary).
    
    Closes FXA-9537

--- libs/payments/cart/src/lib/cart.utils.spec.ts
+++ libs/payments/cart/src/lib/cart.utils.spec.ts
@@ -1,3 +1,6 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 import { StripeError } from '@stripe/stripe-js';
 import { stripeErrorToErrorReasonId } from './cart.utils';
 import { CartErrorReasonId } from '@fxa/shared/db/mysql/account';
