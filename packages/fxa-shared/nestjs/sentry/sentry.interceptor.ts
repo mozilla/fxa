dacb04ca1961fc58fb5c575835098a0838420f54
dschom <dschomburg@mozilla.com>
2024-05-20 11:19:17 -0700

    bug(gql): Follow fix for ignoring known auth server errors in graphql-api
    Because:
    - We are still seeing the 'The authentication token could not be found' sentry issue.
    - GQL is using fxa-shared not libs!
    
    This Commit:
    - Fixes a bug where the isAuthServerError wasn't added to the sentry.interceptor in fxa-shared as well.

--- packages/fxa-shared/nestjs/sentry/sentry.interceptor.ts
+++ packages/fxa-shared/nestjs/sentry/sentry.interceptor.ts
@@ -15,7 +15,11 @@ import {
 import * as Sentry from '@sentry/node';
 import { Transaction } from '@sentry/types';
 
-import { isApolloError, processException } from './reporting';
+import {
+  isApolloError,
+  processException,
+  isAuthServerError,
+} from './reporting';
 
 @Injectable()
 export class SentryInterceptor implements NestInterceptor {
@@ -45,6 +49,10 @@ export class SentryInterceptor implements NestInterceptor {
               return;
             }
           }
+
+          // Skip known auth-server errors
+          if (isAuthServerError(exception)) return;
+
           // Skip ApolloErrors
           if (isApolloError(exception)) return;
 
