39efc8ddf4499af825c450214f10e4f1ef8be501
Ben Bangert <100193+bbangert@users.noreply.github.com>
2024-06-17 23:04:42 +0000

    chore: update nx to 19.3 Because:
    * We want to use the latest version of Nx.
    
    This commit:
    
    * Updates Nx to 19.3.

--- libs/shared/account/account/src/index.ts
+++ libs/shared/account/account/src/index.ts
@@ -2,3 +2,4 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 export * from './lib/account.manager';
+export * from './lib/account.error';
