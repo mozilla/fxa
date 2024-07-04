54036eaaf75ace0a367b170f81a1a4b4dfa46f5d
Katrina Anderson <3422688+Trinaa@users.noreply.github.com>
2024-07-04 09:32:10 -0400

    fix(functional-tests): failures due to rate limiting in coupon tests Because:
    * coupon tests are failing in stage due to IP rate limiting
    
    This commit:
    
    * adds signin as a pre-requisite to coupon tests (since all subscription tests now require signin, the steps have been encapsulated in an automatic fixture to reduce code duplication)
    
    Closes # FXA-10045

--- packages/functional-tests/tests/subscription-tests/coupons-ui-tests/couponExpired.spec.ts
+++ packages/functional-tests/tests/subscription-tests/coupons-ui-tests/couponExpired.spec.ts
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import { expect, test } from '../../../lib/fixtures/standard';
 import { Coupon } from '../../../pages/products';
+import { expect, test } from '../subscriptionFixtures';
 
 test.describe('severity-2 #smoke', () => {
   test.describe('coupon test expired', () => {
