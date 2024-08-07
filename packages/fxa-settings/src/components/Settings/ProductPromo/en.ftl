7bdf024bbd4ddff1e902c426672ea303b0b20a3f
Vijay Budhram <vbudhram@gmail.com>
2024-08-07 11:51:37 -0400

    feat(promo): Add conditional for ProductPromo for Mozilla Monitor Plus

--- packages/fxa-settings/src/components/Settings/ProductPromo/en.ftl
+++ packages/fxa-settings/src/components/Settings/ProductPromo/en.ftl
@@ -3,5 +3,8 @@
 product-promo-monitor =
  .alt = { -product-mozilla-monitor }
 product-promo-monitor-description = Find where your private info is exposed — and take it back
+product-promo-monitor-plus-description = Privacy Matters: Find where your private info is exposed and take it back
 # Links out to the Monitor site
 product-promo-monitor-cta = Get free scan
+# Links out to the Monitor pricing site
+product-promo-monitor-plus-cta = Get started
--- packages/fxa-settings/src/components/Settings/ProductPromo/index.stories.tsx
+++ packages/fxa-settings/src/components/Settings/ProductPromo/index.stories.tsx
@@ -6,6 +6,9 @@ import React from 'react';
 import ProductPromo, { ProductPromoProps, ProductPromoType } from '.';
 import { Meta } from '@storybook/react';
 import { withLocalization } from 'fxa-react/lib/storybooks';
+import { AppContext } from '../../../models';
+import { mockAppContext } from '../../../models/mocks';
+import { MozServices } from '../../../lib/types';
 
 export default {
   title: 'Components/Settings/ProductPromo',
@@ -13,17 +16,62 @@ export default {
   decorators: [withLocalization],
 } as Meta;
 
-const storyWithProps = (props: ProductPromoProps, storyName?: string) => {
-  const story = () => <ProductPromo {...props} />;
+const storyWithProps = (
+  props: ProductPromoProps,
+  account: any,
+  storyName?: string
+) => {
+  const story = () => (
+    <AppContext.Provider
+      value={mockAppContext({
+        account,
+      })}
+    >
+      <ProductPromo {...props} />
+    </AppContext.Provider>
+  );
+
   if (storyName) story.storyName = storyName;
   return story;
 };
 
 export const SettingsWithMonitor = storyWithProps(
   { type: ProductPromoType.Settings },
+  {
+    attachedClients: [{ name: MozServices.Monitor }],
+    subscriptions: [],
+  },
   'Settings with Monitor (resize window)'
 );
 export const SidebarWithMonitor = storyWithProps(
   { type: ProductPromoType.Sidebar },
+  {
+    attachedClients: [{ name: MozServices.Monitor }],
+    subscriptions: [],
+  },
   'Sidebar with Monitor (resize window)'
+);
+export const SidebarWithNoMonitor = storyWithProps(
+  { type: ProductPromoType.Sidebar },
+  {
+    attachedClients: [],
+    subscriptions: [],
+  },
+  'Sidebar with no Monitor'
+);
+export const SidebarWithMonitorNoPlus = storyWithProps(
+  { type: ProductPromoType.Sidebar },
+  {
+    attachedClients: [{ name: MozServices.Monitor }],
+    subscriptions: [],
+  },
+  'Sidebar with Monitor no Plus'
+);
+export const SidebarWithMonitorPlus = storyWithProps(
+  { type: ProductPromoType.Sidebar },
+  {
+    attachedClients: [{ name: MozServices.Monitor }],
+    subscriptions: [{ productName: MozServices.MonitorPlus }],
+  },
+  'Sidebar with Monitor with Plus'
 );
