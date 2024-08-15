a34dc09ecb833ec5e87a58bc589af1341f969df5
Lauren Zugai <lauren@zugai.com>
2024-08-15 16:30:55 +0100

    feat(react): Build out Index / email-first component
    Because:
    * We are converting our flows from Backbone to React
    
    This commit:
    * Creates the initial component buildout and displays states in Storybook

---
+++ packages/fxa-settings/src/pages/Index/en.ftl
@@ -0,0 +1,13 @@
+## Index / home page
+
+index-header = Enter your email
+index-sync-header = Continue to your { -product-mozilla-account }
+index-sync-subheader = Sync your passwords, tabs, and bookmarks everywhere you use { -brand-firefox }.
+# $serviceName - the service (e.g., Pontoon) that the user is signing into with a Mozilla account
+index-subheader-with-servicename = Continue to { $serviceName }
+index-subheader-with-logo = Continue to <span>{ $serviceLogo }</span>
+index-subheader-default = Continue to account settings
+index-cta = Sign up or sign in
+index-account-info = A { -product-mozilla-account } also unlocks access to more privacy-protecting products from { -brand-mozilla }.
+index-email-input =
+  .label = Enter your email
---
+++ packages/fxa-settings/src/pages/Index/mocks.tsx
@@ -0,0 +1,56 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import React from 'react';
+import { LocationProvider } from '@reach/router';
+import { MozServices } from '../../lib/types';
+import { IntegrationType } from '../../models';
+import { IndexIntegration } from './interfaces';
+import Index from '.';
+import { MOCK_CLIENT_ID } from '../mocks';
+
+export function createMockIndexWebIntegration(): IndexIntegration {
+  return {
+    type: IntegrationType.Web,
+    isSync: () => false,
+    getService: () => undefined,
+  };
+}
+
+export function createMockIndexSyncIntegration(): IndexIntegration {
+  return {
+    type: IntegrationType.OAuth,
+    isSync: () => true,
+    getService: () => MOCK_CLIENT_ID,
+  };
+}
+
+export function createMockIndexOAuthIntegration({
+  clientId = MOCK_CLIENT_ID,
+}): IndexIntegration {
+  return {
+    type: IntegrationType.OAuth,
+    isSync: () => false,
+    getService: () => clientId,
+  };
+}
+
+export const Subject = ({
+  integration = createMockIndexWebIntegration(),
+  serviceName = MozServices.Default,
+}: {
+  integration?: IndexIntegration;
+  serviceName?: MozServices;
+}) => {
+  return (
+    <LocationProvider>
+      <Index
+        {...{
+          integration,
+          serviceName,
+        }}
+      />
+    </LocationProvider>
+  );
+};
