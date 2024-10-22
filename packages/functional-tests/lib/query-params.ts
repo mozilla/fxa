4f71490f9551aff632aa0ba919ebdf9ff5b40a81
Vijay Budhram <vbudhram@gmail.com>
2024-10-22 11:21:37 -0400

    fix(tests): Add Sync OAuth to playwright and update tests

--- packages/functional-tests/lib/query-params.ts
+++ packages/functional-tests/lib/query-params.ts
@@ -4,6 +4,7 @@
 
 // This file contains query params that don't reflect states that can be reached from 123done.
 import uaStrings from './ua-strings';
+import { FF_OAUTH_CLIENT_ID } from './channels';
 
 export const oauthWebchannelV1 = new URLSearchParams({
   context: 'oauth_webchannel_v1',
@@ -22,6 +23,21 @@ export const syncMobileOAuthQueryParams = new URLSearchParams({
     'https://identity.mozilla.com/apps/oldsync https://identity.mozilla.com/tokens/session',
   state: 'fakestate',
   automatedBrowser: 'true',
+});
+
+export const syncDesktopOAuthQueryParams = new URLSearchParams({
+  ...Object.fromEntries(oauthWebchannelV1.entries()),
+  client_id: FF_OAUTH_CLIENT_ID, // Firefox Desktop
+  code_challenge_method: 'S256',
+  code_challenge: '2oc_C4v1qHeefWAGu5LI5oDG1oX4FV_Itc148D8_oQI',
+  // eslint-disable-next-line camelcase
+  keys_jwk:
+    'eyJrdHkiOiJFQyIsImNydiI6IlAtMjU2IiwieCI6ImdUejVIWFJfa2pxSFRtMG43ZjhxcDMybVZFaHZ1cGo1dXNUV1h5TWZsb1kiLCJ5IjoiVER5TlhkalhibHZld1pWLVc5MXNDZU9fRWd0NU9WYXhpblBzOEFTQ3owZyJ9',
+  scope:
+    'https://identity.mozilla.com/apps/oldsync https://identity.mozilla.com/tokens/session',
+  state: 'fakestate',
+  automatedBrowser: 'true',
+  service: 'sync',
 });
 
 export const syncDesktopV3QueryParams = new URLSearchParams({
