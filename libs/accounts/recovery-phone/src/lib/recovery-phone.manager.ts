d758f4939e8d2e7c2ea1e006d6cd19218c52c13c
Vijay Budhram <vbudhram@gmail.com>
2024-11-13 11:56:41 -0500

    feat(phone): Add `unconfirmed` to RecoveryPhoneManager

--- libs/accounts/recovery-phone/src/lib/recovery-phone.manager.ts
+++ libs/accounts/recovery-phone/src/lib/recovery-phone.manager.ts
@@ -12,11 +12,17 @@ import {
   RecoveryNumberAlreadyExistsError,
   RecoveryNumberInvalidFormatError,
 } from './recovery-phone.errors';
+import { Redis } from 'ioredis';
+
+const RECORD_EXPIRATION_SECONDS = 10 * 60;
 
 @Injectable()
 export class RecoveryPhoneManager {
+  private readonly redisPrefix = 'sms-attempt';
+
   constructor(
-    @Inject(AccountDbProvider) private readonly db: AccountDatabase
+    @Inject(AccountDbProvider) private readonly db: AccountDatabase,
+    @Inject('Redis') private readonly redisClient: Redis
   ) {}
 
   private isE164Format(phoneNumber: string) {
@@ -56,5 +62,61 @@ export class RecoveryPhoneManager {
       }
       throw err;
     }
+  }
+
+  /**
+   * Store phone number data and SMS code for a user.
+   *
+   * @param uid The user's unique identifier
+   * @param code The SMS code to associate with this UID
+   * @param phoneNumber The phone number to store
+   * @param isSetup Flag indicating if this SMS is to set up a number or verify an existing one
+   * @param lookupData Optional lookup data for the phone number
+   */
+  async storeUnconfirmed(
+    uid: string,
+    code: string,
+    phoneNumber: string,
+    isSetup: boolean,
+    lookupData?: Record<string, any>
+  ): Promise<void> {
+    const redisKey = `${this.redisPrefix}:${uid}:${code}`;
+    const data = {
+      phoneNumber,
+      isSetup,
+      lookupData: lookupData ? JSON.stringify(lookupData) : null,
+    };
+
+    await this.redisClient.set(
+      redisKey,
+      JSON.stringify(data),
+      'EX',
+      RECORD_EXPIRATION_SECONDS
+    );
+  }
+
+  /**
+   * Retrieve phone number data for a user using uid and sms code.
+   *
+   * @param uid The user's unique identifier
+   * @param code The SMS code associated with this user
+   * @returns The stored phone number data if found, or null if not found
+   */
+  async getUnconfirmed(
+    uid: string,
+    code: string
+  ): Promise<{
+    phoneNumber: string;
+    isSetup: boolean;
+    lookupData: Record<string, any> | null;
+  } | null> {
+    const redisKey = `${this.redisPrefix}:${uid}:${code}`;
+    const data = await this.redisClient.get(redisKey);
+
+    if (!data) {
+      return null;
+    }
+
+    return JSON.parse(data);
   }
 }
