d3e30c3620a0f05fd354fc01f9c29b4f135eec64
dschom <dschomburg@mozilla.com>
2025-03-10 09:27:12 -0700

    task(recovery-phone): Also block on twilio country country
    Because:
    - It's not enough to block by IP
    - Not all +1 numbers reside in our initial target regions of US and Canada
    
    This Commit:
    - Adds config for validCountryCodes.
    - Adds call to get lookup data on phone number during setup.
    - If look up data cannot be retrieved, phone number is deemed invalid.
    - If look up data indicates country code isn't a target country, phone number is deemed invalid.
    - Updates auth server config to include validCountryCodes
    - Modifies phoneNumberLook up so that extra fields can be passed in conditionally.
    - Updates tests to reflect how phone number prefixes and country code actually interact with configs.
    - Adds some comments to config classes.

--- libs/accounts/recovery-phone/src/lib/recovery-phone.service.config.ts
+++ libs/accounts/recovery-phone/src/lib/recovery-phone.service.config.ts
@@ -5,23 +5,47 @@
 import { IsArray, IsBoolean, IsNumber, IsObject } from 'class-validator';
 
 export class RecoveryPhoneServiceConfig {
+  /**
+   * A set of valid phone number prefix that sms can be sent to. e.g. +1
+   */
   @IsArray()
   public validNumberPrefixes?: Array<string>;
 
+  /**
+   * A set of valid country codes that SMS can be sent to.
+   */
+  @IsArray()
+  public validCountryCodes!: Array<string>;
+
+  /**
+   * The max sms pumping risk score accepted.
+   */
   @IsNumber()
   public smsPumpingRiskThreshold?: number;
 }
 
 export class RecoveryPhoneConfig {
+  /**
+   * Whether or not the recovery phone feature is supported.
+   */
   @IsBoolean()
   public enabled?: boolean;
 
+  /**
+   * These are a set of allowed regions based on IP that are accepted for recovery phone setup.
+   */
   @IsArray()
   public allowedRegions?: Array<string>;
 
+  /**
+   * Total number of registrations a single number can have
+   */
   @IsNumber()
   public maxRegistrationsPerNumber?: number;
 
+  /**
+   * SMS specific config
+   */
   @IsObject()
   public sms?: RecoveryPhoneServiceConfig;
 }
