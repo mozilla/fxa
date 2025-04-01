20c9aca44ebf1eae82437e27d622854969aa5f74
Reino Muhl <10620585+StaberindeZA@users.noreply.github.com>
2025-04-01 17:18:06 -0400

    feat(iap): add google iap service Because:
    - Move and restructure existing Google IAP methods into the new libs
      Google IAP Service
    
    This commit:
    
    - Moves the logic and tests for the methods listed below, with minor
      restructuring, into GoogleIapService
      - getActiveSubscriptions
      - processNotification
      - registerToUserAccount
    
    Closes #FXA-10731

--- libs/payments/iap/src/lib/types.ts
+++ libs/payments/iap/src/lib/types.ts
@@ -1,6 +1,7 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+import { Stripe } from 'stripe';
 
 export interface FirestoreAppleIapPurchaseRecord {
   userId: string;
@@ -55,3 +56,9 @@ export interface FirestoreGoogleIapPurchaseRecord {
   verifiedAt: number;
   latestNotificationType?: number;
 }
+
+export type IapExtraStripeInfo = {
+  product_id: Stripe.Product['id'];
+  product_name: Stripe.Product['name'];
+  price_id: Stripe.Plan['id'];
+};
