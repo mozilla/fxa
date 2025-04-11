d906e8692cd27d1b2e7bd6eccd9e5c46ccc20f0b
julianpoyourow <julian@poyourow.com>
2025-04-11 16:09:30 +0000

    fix(google): support romanian postal codes
    Because:
    
    - Google geocode does not place romanian postal codes
    
    This commit:
    
    - Adds a bypass for romanian postal codes
    
    Closes FXA-11482

--- libs/google/src/lib/google.manager.ts
+++ libs/google/src/lib/google.manager.ts
@@ -5,12 +5,27 @@
 import { Injectable } from '@nestjs/common';
 import { AddressType } from '@googlemaps/google-maps-services-js';
 import { GoogleClient } from './google.client';
+import { ROMANIAN_POSTAL_CODES } from './google.constants';
 
 @Injectable()
 export class GoogleManager {
   constructor(private googleClient: GoogleClient) {}
 
   async validateAndFormatPostalCode(postalCode: string, countryCode: string) {
+    // Google maps does not geocode Romanian postal codes correctly.
+    if (countryCode === 'RO' || countryCode === 'ROU') {
+      const isRomanianPostalCode = ROMANIAN_POSTAL_CODES.some((regex) =>
+        regex.test(postalCode)
+      );
+
+      if (isRomanianPostalCode) {
+        return {
+          isValid: true,
+          formattedPostalCode: postalCode,
+        };
+      }
+    }
+
     const { results } = await this.googleClient.geocode({
       address: postalCode,
       components: `country:${countryCode}|postal_code:${postalCode}`,
