3b8e32fce7ce8ddec4135c9c0169252aaeffdb50
Valerie Pomerleau <vpomerleau@mozilla.com>
2025-05-09 12:27:38 -0700

    feat(settings): Split and update design ConfirmTotpResetPassword
    Because:
    
    * We are setting up this flow to expand and include recovery phone option
    * We want to simplify components and remove internal branches between 2FA TOTP and backup authentication codes
    * We want to assign individual routes to each page
    * We want to update to the new designs for these pages
    
    This commit:
    
    * Split ConfirmTotpResetPassword into to components (adding ConfirmBackupCodeResetPassword)
    * Update design of both pages
    * Add missing localization
    * Swap FormVerifyCode to FormVerifyTotp that disables input unless code of correct length is input
    * Add container tests
    * Fix act warning in CompleteResetPassword tests
    
    Closes #FXA-11511

--- packages/fxa-settings/src/pages/ResetPassword/CompleteResetPassword/index.test.tsx
+++ packages/fxa-settings/src/pages/ResetPassword/CompleteResetPassword/index.test.tsx
@@ -73,7 +73,7 @@ describe('CompleteResetPassword page', () => {
       expect(screen.queryByText(serviceRelayText)).not.toBeInTheDocument();
     });
 
-    it('renders expected text when service=relay', () => {
+    it('renders expected text when service=relay', async () => {
       renderWithLocalizationProvider(
         <Subject
           isDesktopServiceRelay={true}
@@ -81,7 +81,9 @@ describe('CompleteResetPassword page', () => {
           recoveryKeyExists={false}
         />
       );
-      screen.getByText(serviceRelayText);
+      await waitFor(() =>
+        expect(screen.getByText(serviceRelayText)).toBeVisible()
+      );
     });
 
     it('renders as expected for account without sync', async () => {
