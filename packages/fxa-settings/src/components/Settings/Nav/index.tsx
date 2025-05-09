d8a745e796efda69c87266f29699aef69b319166
MagentaManifold <17zhaomingyuan@gmail.com>
2025-05-09 17:20:07 -0400

    fix(settings): make elements use proper focus colors Because:
    * Front-page Settings elements focus use system default outlines.
    
    This commit:
    
    * makes elements use proper focus outline colors and adds rounded corners to focus outlines.
    
    Closes #FXA-5787

--- packages/fxa-settings/src/components/Settings/Nav/index.tsx
+++ packages/fxa-settings/src/components/Settings/Nav/index.tsx
@@ -18,6 +18,12 @@ export interface NavRefProps {
 }
 
 const navActiveClass = 'nav-active';
+const focusClasses =
+  'focus-visible:outline focus-visible:outline-blue-500 focus-visible:outline-2';
+const navItemClasses = classNames(
+  'inline-block py-1 px-2 hover:bg-grey-100 rounded-sm',
+  focusClasses
+);
 
 // Update the active nav class when this percentage of a section is shown on screen
 const STANDARD_SECTION_THRESHOLD = 0.8;
@@ -151,10 +157,7 @@ export const Nav = ({
                   data-testid="nav-link-profile"
                   href="#profile"
                   ref={profileLinkRef}
-                  className={classNames(
-                    navActiveClass,
-                    'inline-block py-1 px-2 hover:bg-grey-100'
-                  )}
+                  className={classNames(navActiveClass, navItemClasses)}
                 >
                   Profile
                 </a>
@@ -165,7 +168,7 @@ export const Nav = ({
                 <a
                   href="#security"
                   data-testid="nav-link-security"
-                  className="inline-block py-1 px-2 hover:bg-grey-100"
+                  className={navItemClasses}
                   ref={securityLinkRef}
                 >
                   Security
@@ -177,7 +180,7 @@ export const Nav = ({
                 <a
                   href="#connected-services"
                   data-testid="nav-link-connected-services"
-                  className="inline-block py-1 px-2 hover:bg-grey-100"
+                  className={navItemClasses}
                   ref={connectedServicesLinkRef}
                 >
                   Connected Services
@@ -190,7 +193,7 @@ export const Nav = ({
                   <a
                     href="#linked-accounts"
                     data-testid="nav-link-linked-accounts"
-                    className="inline-block py-1 px-2 hover:bg-grey-100"
+                    className={navItemClasses}
                     ref={linkedAccountsLinkRef}
                   >
                     Linked Accounts
@@ -203,7 +206,7 @@ export const Nav = ({
                 <a
                   href="#data-collection"
                   data-testid="nav-link-data-collection"
-                  className="inline-block py-1 px-2 hover:bg-grey-100"
+                  className={navItemClasses}
                   ref={dataCollectionLinkRef}
                 >
                   Data Collection and Use
@@ -216,7 +219,11 @@ export const Nav = ({
         {hasSubscription && (
           <li className="mb-5">
             <LinkExternal
-              className="font-bold"
+              className={classNames(
+                'font-bold',
+                focusClasses,
+                'rounded-sm outline-offset-2'
+              )}
               data-testid="nav-link-subscriptions"
               href="/subscriptions"
             >
@@ -232,7 +239,11 @@ export const Nav = ({
         {marketingCommPrefLink && (
           <li>
             <LinkExternal
-              className="font-bold"
+              className={classNames(
+                'font-bold',
+                focusClasses,
+                'rounded-sm outline-offset-2'
+              )}
               data-testid="nav-link-newsletters"
               href={marketingCommPrefLink}
             >
