68b6e0f00cec6e0883bae03689fb4cfac483206c
Barry Chen <bchen@mozilla.com>
2025-05-21 22:12:00 -0500

    chore(perf): load LegalWithMarkdown lazily

--- packages/fxa-settings/src/pages/Legal/Terms/index.tsx
+++ packages/fxa-settings/src/pages/Legal/Terms/index.tsx
@@ -2,12 +2,15 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { RouteComponentProps } from '@reach/router';
-import LegalWithMarkdown, {
-  FetchLegalDoc,
-} from '../../../components/LegalWithMarkdown';
+import { FetchLegalDoc } from '../../../components/LegalWithMarkdown';
 import { LegalDocFile } from '../../../lib/file-utils-legal';
+import LoadingSpinner from 'fxa-react/components/LoadingSpinner';
+
+const LegalWithMarkdown = lazy(
+  () => import('../../../components/LegalWithMarkdown')
+);
 
 export const viewName = 'legal-terms';
 
@@ -20,12 +23,14 @@ const LegalTerms = ({
   locale,
   fetchLegalDoc,
 }: LegalTermsProps & RouteComponentProps) => (
-  <LegalWithMarkdown
-    {...{ locale, fetchLegalDoc, viewName }}
-    headingTextFtlId="legal-terms-heading"
-    headingText="Terms of Service"
-    legalDocFile={LegalDocFile.terms}
-  />
+  <Suspense fallback={<LoadingSpinner fullScreen />}>
+    <LegalWithMarkdown
+      {...{ locale, fetchLegalDoc, viewName }}
+      headingTextFtlId="legal-terms-heading"
+      headingText="Terms of Service"
+      legalDocFile={LegalDocFile.terms}
+    />
+  </Suspense>
 );
 
 export default LegalTerms;
