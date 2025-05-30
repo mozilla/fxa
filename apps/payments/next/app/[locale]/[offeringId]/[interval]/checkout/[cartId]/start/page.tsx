4db5c78a4367a4d3f073360e00cd57faec6cce5b
elizabeth-ilina <eilina@mozilla.com>
2025-05-30 17:04:10 -0400

    fix(payments-next):Applied coupon code before sign-in does not remain on checkout page after signing in Because:
    * Before signing in, a valid coupon was applied to the subscription. However, after signing in, the coupon is no longer on the subscription.
    
    This commit:
    
    *
    
    Closes #FXA-11344

--- apps/payments/next/app/[locale]/[offeringId]/[interval]/checkout/[cartId]/start/page.tsx
+++ apps/payments/next/app/[locale]/[offeringId]/[interval]/checkout/[cartId]/start/page.tsx
@@ -86,6 +86,8 @@ export default async function Checkout({
     redirectSearchParams.postalCode = cart.taxAddress.postalCode;
   }
 
+  redirectSearchParams.oldCartId = params.cartId;
+
   const redirectTo = buildRedirectUrl(
     params.offeringId,
     params.interval,
