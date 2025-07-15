28120c623ccea121b67127b3be42ec5768448454
Valerie Pomerleau <vpomerleau@mozilla.com>
2025-07-15 09:46:11 -0700

    fix storybook rendering for Page2faSetup

--- packages/fxa-settings/src/components/Settings/Page2faSetup/mocks.tsx
+++ packages/fxa-settings/src/components/Settings/Page2faSetup/mocks.tsx
@@ -3,27 +3,66 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import React from 'react';
-import { MOCK_ACCOUNT, mockAppContext } from '../../../models/mocks';
+import {
+  MOCK_ACCOUNT,
+  mockAppContext,
+  mockSession,
+} from '../../../models/mocks';
 import { Account, AppContext } from '../../../models';
 import Page2faSetup from '.';
-import { LocationProvider } from '@reach/router';
-import { MOCK_2FA_SECRET_KEY_RAW, MOCK_BACKUP_CODES } from '../../../pages/mocks';
+import {
+  LocationProvider,
+  createHistory,
+  createMemorySource,
+  NavigateFn,
+} from '@reach/router';
+import { action } from '@storybook/addon-actions';
+import {
+  MOCK_2FA_SECRET_KEY_RAW,
+  MOCK_BACKUP_CODES,
+  PLACEHOLDER_QR_CODE,
+} from '../../../pages/mocks';
 
 export const MOCK_TOTP_INFO = {
-  qrCodeUrl: 'https://example.com/fake-qr.png',
+  qrCodeUrl: PLACEHOLDER_QR_CODE,
   secret: MOCK_2FA_SECRET_KEY_RAW,
   recoveryCodes: MOCK_BACKUP_CODES,
+};
+
+// Create a safe navigate function for Storybook that matches NavigateFn type
+const createSafeNavigate = (): NavigateFn => {
+  return (to: string | number, options?: any) => {
+    action('Navigate')(`to: ${to}`, options);
+    return Promise.resolve();
+  };
 };
 
 export const Subject = ({ account: accountOverrides = {} }) => {
   const account = {
     ...MOCK_ACCOUNT,
+    createTotp: async () => {
+      action('createTotp called')();
+      return MOCK_TOTP_INFO;
+    },
     ...accountOverrides,
   } as Account;
+
+  const source = createMemorySource('/settings/two_step_authentication');
+  const history = createHistory(source);
+  const historyWithSafeNavigate = {
+    ...history,
+    navigate: createSafeNavigate(),
+  };
+
   return (
-    <LocationProvider>
-      <AppContext.Provider value={{ ...mockAppContext({ account }) }}>
-          <Page2faSetup />
+    <LocationProvider history={historyWithSafeNavigate}>
+      <AppContext.Provider
+        value={{
+          ...mockAppContext({ account }),
+          session: mockSession(true),
+        }}
+      >
+        <Page2faSetup />
       </AppContext.Provider>
     </LocationProvider>
   );
