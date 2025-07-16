dcda8040038d0d875d627c0f03b14e11636734ec
julianpoyourow <julian@poyourow.com>
2025-07-16 17:00:53 +0000

    chore(functional-tests): disable flaky functional tests
    Because:
    
    - These tests have been intermittently failing
    
    This commit:
    
    - Disables the tests

--- packages/functional-tests/tests/subscription-tests/subscription.spec.ts
+++ packages/functional-tests/tests/subscription-tests/subscription.spec.ts
@@ -44,6 +44,7 @@ test.describe('severity-2 #smoke', () => {
       pages: { relier, signin, subscribe },
       credentials,
     }, { project }) => {
+      test.fixme(true, "Blocking tag while works in dev, has been flaky");
       test.skip(
         project.name === 'production',
         'no real payment method available in prod'
