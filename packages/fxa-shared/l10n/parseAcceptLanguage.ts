795015038fa81c0ad224f90ad81c115237986897
Davey Alvarez <daveyalvarez03@gmail.com>
2025-07-29 21:18:15 +0100

    chore(l10n): remove unused l10n code Because:
    * All references to l10n code has been migrated to the /libs l10n shared directory. No references to /packages l10n shared code remain
    
    This commit:
    
    * Removes the /packages l10n code and tests
    
    Closes #PAY-2065

--- packages/fxa-shared/l10n/parseAcceptLanguage.ts
+++
@@ -1,6 +0,0 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-// TODO - FXA-8413 - Delete file. Replaced by @fxa/shared/l10n
-
-export { parseAcceptLanguage } from '@fxa/shared/l10n';
