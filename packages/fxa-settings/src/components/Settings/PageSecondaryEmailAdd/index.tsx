d675845c635b632eb673923cacbc4ddd73a6d3a3
Vijay Budhram <vbudhram@gmail.com>
2025-09-17 11:41:48 -0400

    Revert "bug(mfa): Use errorBoundary for MfaGuard if invalid JWT is present"
    This reverts commit 71456977643615cf70a24842d677af23b2b9d1d3.

--- packages/fxa-settings/src/components/Settings/PageSecondaryEmailAdd/index.tsx
+++ packages/fxa-settings/src/components/Settings/PageSecondaryEmailAdd/index.tsx
@@ -10,7 +10,6 @@ import { logViewEvent, usePageViewEvent } from '../../../lib/metrics';
 import { SETTINGS_PATH } from '../../../constants';
 import InputText from '../../InputText';
 import FlowContainer from '../FlowContainer';
-import VerifiedSessionGuard from '../VerifiedSessionGuard';
 import { isEmailMask, isEmailValid } from 'fxa-shared/email/helpers';
 import { useAccount, useAlertBar } from 'fxa-settings/src/models';
 import { AuthUiErrorNos } from 'fxa-settings/src/lib/auth-errors/auth-errors';
@@ -87,7 +86,6 @@ export const PageSecondaryEmailAdd = (_: RouteComponentProps) => {
   return (
     <Localized id="add-secondary-email-page-title" attrs={{ title: true }}>
       <FlowContainer title="Secondary email" subtitle={subtitleText}>
-        <VerifiedSessionGuard onDismiss={goHome} onError={goHome} />
         <form
           onSubmit={(ev) => {
             ev.preventDefault();
