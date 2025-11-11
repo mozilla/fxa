5cae7f0897a4011d15497d56ac1ea07f77d7ea25
Valerie Pomerleau <v.pomerleau@gmail.com>
2025-11-10 16:51:07 -0800

    Revert "fix(api): Return 'verified' from 'account/login'"

--- packages/fxa-auth-server/lib/routes/account.ts
+++ packages/fxa-auth-server/lib/routes/account.ts
@@ -1427,11 +1427,6 @@ export class AccountHandler {
         );
       }
 
-      // This has stuck around for backwards compatibility and is not currently
-      // used by our front-end. RPs should be encouraged to use 'sessionVerified'
-      // or 'emailVerified' instead.
-      response.verified = response.emailVerified && response.sessionVerified;
-
       await this.signinUtils.cleanupReminders(response, accountRecord);
 
       if (response.emailVerified && response.sessionVerified) {
@@ -2420,7 +2415,6 @@ export const accountRoutes = (
               .description(DESCRIPTION.verificationReason),
             emailVerified: isA.boolean().required(),
             sessionVerified: isA.boolean().required(),
-            verified: isA.boolean().required(),
             authAt: isA.number().integer().description(DESCRIPTION.authAt),
             metricsEnabled: isA.boolean().required(),
           }),
