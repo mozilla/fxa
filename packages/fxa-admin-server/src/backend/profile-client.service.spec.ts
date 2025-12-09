2a03b49779afa24b7eed9bc11fae77ded66c011a
MagentaManifold <17zhaomingyuan@gmail.com>
2025-12-09 17:45:43 -0500

    feat(admin-panel): fix disable account throws error Because:
    * we used the wrong endpoint URL
    
    This commit:
    
    * use profileClient in libs instead of the ad hoc one
    
    Closes #FXA-12700

--- packages/fxa-admin-server/src/backend/profile-client.service.spec.ts
+++ packages/fxa-admin-server/src/backend/profile-client.service.spec.ts
@@ -1,60 +1,29 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
-import { Provider } from '@nestjs/common';
+
+import { ProfileClient } from '@fxa/profile/client';
 import { Test, TestingModule } from '@nestjs/testing';
-import superagent from 'superagent';
+import { MockConfig, MockLogService, MockMetricsFactory } from '../mocks';
+import { ProfileClientFactory } from './profile-client.service';
 
-import { AuthClientService } from './auth-client.service';
-import { ProfileClientService } from './profile-client.service';
-import { ConfigService } from '@nestjs/config';
+describe('ProfileClient Service', () => {
+  let service: ProfileClient;
 
-describe('ProfileClientService', () => {
-  let service: ProfileClientService;
-  let authClient: any;
-  const profileUrl = 'https://test.com';
-  const secretToken = 'secret123';
-
-  beforeEach(async () => {
-    authClient = {};
-    const MockConfig: Provider = {
-      provide: ConfigService,
-      useValue: {
-        get: jest.fn().mockImplementation((key: string) => {
-          if (key === 'profileServer') {
-            return {
-              url: profileUrl,
-              secretBearerToken: secretToken,
-            };
-          }
-          return null;
-        }),
-      },
-    };
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        ProfileClientService,
         MockConfig,
-        { provide: AuthClientService, useValue: authClient },
+        MockLogService,
+        MockMetricsFactory,
+        ProfileClientFactory,
       ],
     }).compile();
 
-    service = module.get<ProfileClientService>(ProfileClientService);
+    service = module.get<ProfileClient>(ProfileClient);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
-  });
-
-  it('deletes cache', async () => {
-    const mockDelete = jest.spyOn(superagent, 'delete') as jest.Mock;
-    const mockSet = jest.fn().mockResolvedValue({ text: '{}' });
-    mockDelete.mockReturnValueOnce({
-      set: mockSet,
-    });
-    const result = await service.deleteCache('1234abcd');
-    expect(result).toStrictEqual({});
-    expect(mockSet).toBeCalledWith('Authorization', 'Bearer ' + secretToken);
-    expect(mockDelete).toBeCalledWith(profileUrl + '/cache/1234abcd');
   });
 });
