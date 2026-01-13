c2f29ca6a7ad0af235ea12f0d490493515aa32ee
Wil Clouser <wclouser@mozilla.com>
2026-01-13 08:56:33 -0800

    chore(fxa-settings): Adjust loading modal window size.  Fix FXA-12851

--- packages/fxa-settings/src/components/CardLoadingSpinner/index.tsx
+++ packages/fxa-settings/src/components/CardLoadingSpinner/index.tsx
@@ -22,7 +22,7 @@ export const CardLoadingSpinner = ({
   return (
     <div
       className={classNames(
-        'card flex items-center justify-center h-full max-w-48 mobileLandscape:h-32',
+        'card flex items-center justify-center min-h-48 mobileLandscape:min-h-64',
         className
       )}
     >
