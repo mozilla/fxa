0975a47f68762fcd2f0af48c42f2f8447d7d475a
Davey Alvarez <daveyalvarez03@gmail.com>
2026-01-27 13:22:58 -0800

    feat(payments-api): Add version and heartbeat endpoints to payments-api Because:
    * These endpoints are required for argo pod health checks
    
    This commit:
    
    * Adds in the endpoints required to pass health checks
    
    Closes #

--- apps/payments/api/src/app/app.controller.spec.ts
+++ apps/payments/api/src/app/app.controller.spec.ts
@@ -13,9 +13,26 @@ describe('AppController', () => {
   });
 
   describe('getData', () => {
-    it('should return "Hello API"', () => {
+    it('should return "Hello world"', () => {
       const appController = app.get<AppController>(AppController);
-      expect(appController.getData()).toEqual({ message: 'Hello API' });
+      expect(appController.getData()).toEqual({ message: 'Hello world' });
+    });
+  });
+
+  describe('service status endpoints', () => {
+    it('__heartbeat__ should return empty object', () => {
+      const appController = app.get<AppController>(AppController);
+      expect(appController.__heartbeat__()).toEqual({});
+    });
+
+    it('__lbheartbeat__ should return empty object', () => {
+      const appController = app.get<AppController>(AppController);
+      expect(appController.__lbheartbeat__()).toEqual({});
+    });
+
+    it('__version__ should return version object', () => {
+      const appController = app.get<AppController>(AppController);
+      expect(appController.__version__()).toEqual({ version: '0.0.0' });
     });
   });
 });
--- apps/payments/api/src/app/app.service.spec.ts
+++ apps/payments/api/src/app/app.service.spec.ts
@@ -17,4 +17,18 @@ describe('AppService', () => {
       expect(service.getData()).toEqual({ message: 'Hello API' });
     });
   });
+
+  describe('service status endpoints', () => {
+    it('__heartbeat__ should return empty object', () => {
+      expect(service.__heartbeat__()).toEqual({});
+    });
+
+    it('__lbheartbeat__ should return empty object', () => {
+      expect(service.__lbheartbeat__()).toEqual({});
+    });
+
+    it('__version__ should return version object', () => {
+      expect(service.__version__()).toEqual({ version: '0.0.0' });
+    });
+  });
 });
