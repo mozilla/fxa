24ec930060c80029527b8188e411226211902c55
Davey Alvarez <daveyalvarez03@gmail.com>
2026-02-03 10:26:51 -0800

    polish(payments-api): Remove root accessor and default api path prefix Because:
    * Payments API shares code and services with other applications in the fxa ecosystem, but came with a default /api/ base path prefix for all routes
    * This prefix causes some confusion for the service health checks, which expect all heartbeat and version endpoints to be off of the root path for all services
    
    This commit:
    
    * Removes the /api/ base prefix, as it is not required by internal or external stakeholders, and appears to be an inherited default value
    * removes the root ('/') endpoint, as its purpose is not yet defined
    
    Closes #N/A (relates to SVCSE-4181)

--- apps/payments/api/src/main.ts
+++ apps/payments/api/src/main.ts
@@ -11,12 +11,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     rawBody: true,
   });
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3037;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `🚀 Application is running on: http://localhost:${port}`
   );
 }
 
