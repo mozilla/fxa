fc46080bb3be4bd2af6274e4cd745c1d2a85e18b
dschom <dschomburg@mozilla.com>
2026-02-13 16:31:35 -0800

    chore(event-broker): Update refs to use libs instead of fxa-shared
    Because:
    - Cleaning up code duplication
    
    This Commit:
    - References @fxa/shared/sentry
    - References @fxa/shared/otel
    - References @fxa/shared/monitoring

--- packages/fxa-event-broker/jest.config.js
+++ packages/fxa-event-broker/jest.config.js
@@ -1,22 +1,17 @@
 const { pathsToModuleNameMapper } = require('ts-jest');
-const { compilerOptions } = require('./tsconfig.build.json');
+const { compilerOptions } = require('../../tsconfig.base.json');
 module.exports = {
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
-  modulePaths: ['<rootDir>/../dist/'],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/../../../',
+  }),
+  modulePaths: ['<rootDir>/../../../'],
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: 'src',
   testRegex: '.spec.ts$',
   transform: {
-    '^.+\\.(t|j)s$': [
-      'ts-jest',
-      {
-        isolatedModules: true,
-      },
-    ],
+    '^.+\\.(t|j)s$': 'ts-jest',
   },
-  transformIgnorePatterns: [
-    "/node_modules/(?!axios/)",
-  ],
+  transformIgnorePatterns: ['/node_modules/(?!axios/)'],
   coverageDirectory: './coverage',
   testEnvironment: 'node',
 };
