a97b3195e17c9cf347f2676174844bdf54e406d9
Nicholas Shirley <nshirley@mozilla.com>
2026-03-27 10:09:44 -0600

    fix(auth): Add missing security event name to db
    Because:
     - The `account.recovery_codes_set` event name is not in the db
     - And that causes the recording of the event to fail
    
    This Commit:
     - Adds the missing event name to the db
     - Fixes type issue so these are caught sooner
    
    Closes: FXA-13351

---
+++ packages/db-migrations/databases/fxa/patches/patch-187-186.sql
@@ -0,0 +1,5 @@
+-- SET NAMES utf8mb4 COLLATE utf8mb4_bin;
+
+-- DELETE FROM securityEventNames WHERE name = 'account.recovery_codes_set';
+
+-- UPDATE dbMetadata SET value = '186' WHERE name = 'schema-patch-level';
