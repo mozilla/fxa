065b12c5a28b54b31223723d2493743bddc653af
Valerie Pomerleau <vpomerleau@mozilla.com>
2026-04-21 14:37:28 -0700

    fix page delete account storybook

--- packages/fxa-settings/src/components/Settings/PageDeleteAccount/index.stories.tsx
+++ packages/fxa-settings/src/components/Settings/PageDeleteAccount/index.stories.tsx
@@ -8,6 +8,8 @@ import { LocationProvider } from '@reach/router';
 import { Meta } from '@storybook/react';
 import { withLocalization } from 'fxa-react/lib/storybooks';
 import SettingsLayout from '../SettingsLayout';
+import { Account, AppContext } from '../../../models';
+import { mockAppContext, MOCK_ACCOUNT } from '../../../models/mocks';
 
 export default {
   title: 'Pages/Settings/DeleteAccount',
@@ -15,10 +17,31 @@ export default {
   decorators: [withLocalization],
 } as Meta;
 
-export const Default = () => (
-  <LocationProvider>
-    <SettingsLayout>
-      <PageDeleteAccount />
-    </SettingsLayout>
-  </LocationProvider>
-);
+const accountWithPassword = {
+  ...MOCK_ACCOUNT,
+  hasPassword: true,
+  destroy: () => Promise.resolve(),
+} as unknown as Account;
+
+const accountWithoutPassword = {
+  ...MOCK_ACCOUNT,
+  hasPassword: false,
+  destroy: () => Promise.resolve(),
+} as unknown as Account;
+
+const storyWithContext = (account: Account) => {
+  const story = () => (
+    <LocationProvider>
+      <AppContext.Provider value={mockAppContext({ account })}>
+        <SettingsLayout>
+          <PageDeleteAccount />
+        </SettingsLayout>
+      </AppContext.Provider>
+    </LocationProvider>
+  );
+  return story;
+};
+
+export const Default = storyWithContext(accountWithPassword);
+
+export const WithoutPassword = storyWithContext(accountWithoutPassword);
