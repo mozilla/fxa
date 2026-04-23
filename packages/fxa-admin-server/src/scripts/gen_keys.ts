9b6e74299866ceef41423fdb1c73865a8ce419fd
dschom <dschomburg@mozilla.com>
2026-04-22 18:57:35 -0700

    chore(admin-server): replace esbuild with tsc/ts-node in packages/fxa-admin-server
    Because:
    - We want to test & develop with what we deploy.
    
    This commit:
    - Part of the esbuild -> tsc/ts-node migration (split of a6d1a55ae4, PR mozilla/fxa#20390).
    - Scope: files under packages/fxa-admin-server.

--- packages/fxa-admin-server/src/scripts/gen_keys.ts
+++ packages/fxa-admin-server/src/scripts/gen_keys.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env node -r esbuild-register
+#!/usr/bin/env node -r ts-node/register/transpile-only -r tsconfig-paths/register
 
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
