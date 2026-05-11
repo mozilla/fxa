10443d2bef5856ff89c75bf46bcb4402e03fc721
Lauren Zugai <lauren@zugai.com>
2026-05-11 12:49:38 -0500

    Revert "feat(auth-server): account-level authorizations for browser services"
    This reverts commit 8ada43684ce83c5c8e9a2fc841ffc94f8b7aaee0.

--- packages/fxa-settings/src/lib/oauth/hooks.tsx
+++ packages/fxa-settings/src/lib/oauth/hooks.tsx
@@ -126,9 +126,6 @@ async function getOAuthData(
   if (integration.data.accessType === 'offline') {
     opts.access_type = integration.data.accessType;
   }
-  if (integration.data.service) {
-    opts.service = integration.data.service;
-  }
 
   const result: OAuthData | null = await authClient.createOAuthCode(
     sessionToken,
