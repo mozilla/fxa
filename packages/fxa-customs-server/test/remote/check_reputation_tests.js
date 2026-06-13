16d546e69d7af46ca762e19c3234ac3cec5104a5
Nicholas Shirley <nshirley@mozilla.com>
2026-06-12 20:10:47 -0600

    test(customs): remove flaky check_reputation timing subtest
    Fixes FXA-13959

--- packages/fxa-customs-server/test/remote/check_reputation_tests.js
+++ packages/fxa-customs-server/test/remote/check_reputation_tests.js
@@ -33,18 +33,6 @@ config.reputationService = {
   timeout: 25,
 };
 
-// We use a restify based test reputation client to query the reputation server stub
-// in here for testing purposes, but also instantiate an instance of the actual reputation
-// client to verify some of the behavior in the module.
-var repJSClientConfig = {
-  serviceUrl: config.reputationService.baseUrl,
-  id: 'root',
-  key: 'toor',
-  timeout: 25,
-};
-var ipr = require('../../lib/ipReputationClient');
-var repJSClient = new ipr(repJSClientConfig);
-
 var testServer = new TestServer(config);
 
 var client = restifyClients.createJsonClient({
@@ -76,37 +64,6 @@ ENDPOINTS.forEach((endpoint) => {
       t.notOk(err, 'no errors were returned');
       t.end();
     });
-  });
-
-  test('query reputation stub directly using ip-reputation-js-client', (t) => {
-    return reputationClient
-      .getAsync('/heartbeat')
-      .spread(function (req, res, obj) {
-        t.equal(res.statusCode, 200, 'clears reputation for TEST_IP');
-        return repJSClient.remove(TEST_IP);
-      })
-      .then(() => {
-        return repJSClient.get(TEST_IP);
-      })
-      .then(function (response) {
-        t.equal(
-          response.statusCode,
-          404,
-          'reputation value for TEST_IP not found'
-        );
-        var f =
-          response &&
-          response.timingPhases &&
-          response.timingPhases.total &&
-          typeof response.timingPhases.total === 'number' &&
-          response.timingPhases.total > 0.0;
-        t.equal(f, true, 'response contains timing data');
-        t.end();
-      })
-      .catch(function (err) {
-        t.fail(err);
-        t.end();
-      });
   });
 
   test(`does not block ${endpoint} for IP with nonexistent reputation`, (t) => {
