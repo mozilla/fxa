dd900197b515e0a525a702c820322a49f8e1c1db
Dan Schomburg <dschomburg@mozilla.com>
2026-08-06 13:13:33 -0700

    feat(settings): add mobile Connect this device card
    Because:
     - We are updating pairing flows and want to land UI components / pages first.
     - Adds the mobile screen asking the user to confirm connecting this device.
    
    This commit:
     - Adds `packages/fxa-settings/src/pages/Pair2/Supplicant/ConnectThisDevice/index.tsx`.
     - Adds storybook stories for the page.
     - Adds l10n files and tests.
    
    Closes #FXA-14233

---
+++ packages/fxa-settings/src/pages/Pair2/Supplicant/ConnectThisDevice/index.stories.tsx
@@ -0,0 +1,41 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import React from 'react';
+import { Meta } from '@storybook/react';
+import { withLocalization } from 'fxa-react/lib/storybooks';
+import ConnectThisDevice from '.';
+import {
+  MOCK_METADATA_UNKNOWN_LOCATION,
+  MOCK_METADATA_WITH_LOCATION,
+} from '../../../../components/DeviceInfoBlock/mocks';
+import {
+  MOCK_LONG_EMAIL,
+  MOCK_METADATA_LONG_DEVICE_NAME,
+  Subject,
+} from './mocks';
+
+export default {
+  title: 'Pages/Pair2/Supplicant/ConnectThisDevice',
+  component: ConnectThisDevice,
+  decorators: [withLocalization],
+} as Meta;
+
+export const WithDeviceName = () => <Subject />;
+
+// No device name on the pairing payload, so the device line falls back to the OS.
+export const WithoutDeviceName = () => (
+  <Subject remoteMetadata={MOCK_METADATA_WITH_LOCATION} />
+);
+
+// Geo lookup resolved nothing, so the location line reads "Location unknown".
+export const WithUnknownLocation = () => (
+  <Subject remoteMetadata={MOCK_METADATA_UNKNOWN_LOCATION} />
+);
+
+export const WithLongDeviceName = () => (
+  <Subject remoteMetadata={MOCK_METADATA_LONG_DEVICE_NAME} />
+);
+
+export const WithLongEmail = () => <Subject email={MOCK_LONG_EMAIL} />;
---
+++ packages/fxa-settings/src/pages/Pair2/Supplicant/ConnectThisDevice/mocks.tsx
@@ -0,0 +1,28 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import ConnectThisDevice, { ConnectThisDeviceProps } from '.';
+import { MOCK_METADATA_WITH_DEVICE_NAME } from '../../../../components/DeviceInfoBlock/mocks';
+import { RemoteMetadata } from '../../../../lib/types';
+import { MOCK_EMAIL } from '../../../mocks';
+
+// No dashes or spaces, so the only way this fits is if the block wraps it.
+export const MOCK_METADATA_LONG_DEVICE_NAME: RemoteMetadata = {
+  ...MOCK_METADATA_WITH_DEVICE_NAME,
+  deviceName: 'LaurelsExtremelyLongMacBookPro16inch2023',
+};
+
+// Likewise: neither `@` nor `.` is a wrap opportunity, so this only fits if
+// the paragraph breaks mid-word.
+export const MOCK_LONG_EMAIL =
+  'laurelsextremelylongaccountname@anevenlongerexampledomainname.com';
+
+export const Subject = ({
+  email = MOCK_EMAIL,
+  remoteMetadata = MOCK_METADATA_WITH_DEVICE_NAME,
+  onConnect = () => {},
+  onCancel = () => {},
+}: Partial<ConnectThisDeviceProps> = {}) => (
+  <ConnectThisDevice {...{ email, remoteMetadata, onConnect, onCancel }} />
+);
