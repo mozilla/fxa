76efbbfefb92615a3ed98b572ea2af60b29352e7
Vijay Budhram <vbudhram@gmail.com>
2026-08-18 14:31:17 -0400

    test(jest-transforms): cover the SVG component name helper
    ## Because
    
    - The `toPascalCase` helper that replaced `camelcase` has no direct test in either copy.
    - Coverage today is indirect. `LoadingSpinner` and `ButtonIcon` render an SVG and never assert the generated name, so the separator, camelCase and digit rules can break without a test failing.
    
    ## This pull request
    
    - Adds `packages/fxa-react/lib/svg-transform.test.ts`. It asserts the generated component name for six basename shapes.
    - Adds `packages/fxa-settings/src/lib/jest-file-transform.test.ts` with the same six shapes, against the second copy of the helper.
    - Touches no helper logic. This is a test-only follow-up to review comments on the original change.
    
    ## Issue that this pull request solves
    
    Closes: https://mozilla-hub.atlassian.net/browse/FXA-13929

--- packages/fxa-settings/config/jest/fileTransform.js
+++ packages/fxa-settings/config/jest/fileTransform.js
@@ -3,7 +3,18 @@
 'use strict';
 
 const path = require('path');
-const camelcase = require('camelcase');
+
+// Turns an SVG basename into PascalCase. Twin of the helper in
+// packages/fxa-react/svg-transform.js.
+function toPascalCase(name) {
+  return name
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .split(/[_.\- ]+/)
+    .filter(Boolean)
+    .map((part) => part.charAt(0).toUpperCase() + part.slice(1).toLowerCase())
+    .join('')
+    .replace(/\d[a-z]/g, (match) => match.toUpperCase());
+}
 
 // This is a custom Jest transformer turning file imports into filenames.
 // http://facebook.github.io/jest/docs/en/webpack.html
@@ -15,9 +26,7 @@ module.exports = {
     if (filename.match(/\.svg$/)) {
       // Based on how SVGR generates a component name:
       // https://github.com/smooth-code/svgr/blob/01b194cf967347d43d4cbe6b434404731b87cf27/packages/core/src/state.js#L6
-      const pascalCaseFilename = camelcase(path.parse(filename).name, {
-        pascalCase: true,
-      });
+      const pascalCaseFilename = toPascalCase(path.parse(filename).name);
       const componentName = `Svg${pascalCaseFilename}`;
       return {
         code: `const React = require('react');
