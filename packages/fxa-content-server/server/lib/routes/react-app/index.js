1a2e341c88c007346983d3f46947e9f9eb023156
Dan Schomburg <dschomburg@mozilla.com>
2026-09-24 18:02:38 -0700

    fix(content-server): serve the pair connect_hint route on reload
    Because:
    
    * Reloading /pair/supplicant/connect_hint returned the content-server 404
      because only the settings router knew about the path.
    
    This commit:
    
    * Adds pair/supplicant/connect_hint to the content-server frontend route
      list and to the pair2 React route group.
    
    Closes #FXA-14603

--- packages/fxa-content-server/server/lib/routes/react-app/index.js
+++ packages/fxa-content-server/server/lib/routes/react-app/index.js
@@ -185,6 +185,7 @@ const getReactRouteGroups = (showReactApp, reactRoute) => {
         'pair/authority/sync_success',
         'pair/authority/timeout_and_cancel',
         'pair/supplicant/approve_signin',
+        'pair/supplicant/connect_hint',
         'pair/supplicant/connect_this_device',
         'pair/supplicant/download_firefox',
         'pair/supplicant/ready_to_scan',
